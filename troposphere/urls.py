a7cbc1e6082fafec0fdd0f47f5dcba309743a920
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-03-22 21:27:21 -0700

    Application route catch-all

--- troposphere/urls.py
+++ troposphere/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'troposphere.views.root'),
-    url(r'^application$', 'troposphere.views.application', name='application'),
+    url(r'^application', 'troposphere.views.application', name='application'),
     url(r'^maintenance$', 'troposphere.views.maintenance', name='maintenance'),
     url(r'^forbidden$', 'troposphere.views.forbidden', name='forbidden'),
     url(r'^login$', 'troposphere.views.login'),
