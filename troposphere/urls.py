a92e864ce3a737ad922572880fa2dd73717d2585
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-03-22 18:08:16 -0700

    Add maintenance view

--- troposphere/urls.py
+++ troposphere/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'troposphere.views.root'),
     url(r'^application$', 'troposphere.views.application', name='application'),
+    url(r'^maintenance$', 'troposphere.views.maintenance', name='maintenance'),
 )
--- troposphere/views.py
+++ troposphere/views.py
@@ -5,5 +5,19 @@ def root(request):
     return redirect('application')
 
 def application(request):
+    records, disabled_login = get_maintenance()
+    if disabled_login:
+        return redirect('maintenance')
+
     context = {}
     return render(request, 'application.html', context)
+
+def get_maintenance():
+    """
+    Returns a list of maintenance records along with a boolean to indicate
+    whether or not login should be disabled
+    """
+    return ([], False)
+
+def maintenance(request):
+    return HttpResponse("We're undergoing maintenance", status=503)
