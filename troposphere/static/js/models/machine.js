2cbc4a15eedea7761691d367009c568e68e05f81
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-03-25 13:43:36 -0700

    Add version formatter on machine, convert start_date to javascript Date on parse

--- troposphere/static/js/models/machine.js
+++ troposphere/static/js/models/machine.js
@@ -4,7 +4,14 @@ define(['underscore', 'models/base'], function(_, Base) {
         defaults: { 'model_name': 'machine' },
         parse: function(response) {
             response.id = response.alias;
+            response.start_date = new Date(response.start_date);
             return response;
+        },
+        pretty_version: function() {
+            var parts = this.get('version').split('.');
+            for (var i = 0; i <= 3 - parts.length; i++)
+                parts.push("0");
+            return parts.join(".");
         }
     });
 
