7b5e78d2f844d2de3734df97069786d6efaf2737
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-03-24 20:18:42 -0700

    Import dashboard icons

--- troposphere/static/js/components/dashboard.js
+++ troposphere/static/js/components/dashboard.js
@@ -13,19 +13,19 @@ define(['react', 'underscore', 'components/page_header'], function(React, _, Pag
 
     var items = [
         {
-            image: '/resources/images/icon_launchnewinstance.png',
+            image: static_root + 'images/icon_launchnewinstance.png',
             title: 'Launch New Instance',
             description: 'Browse Atmosphere\'s list of available images and select one to launch a new instance.',
             href: 'app_store'
         },
         {
-            image: '/resources/images/icon_gethelp.png',
+            image: static_root + 'images/icon_gethelp.png',
             title: 'Browse Help Resources',
             description: 'View a video tutorial, read the how-to guides, or email the Atmosphere support team.',
             href: 'help'
         },
         {
-            image: '/resources/images/icon_settings.png',
+            image: static_root + 'images/icon_settings.png',
             title: 'Change Your Settings',
             description: 'Modify your account settings, view your resource quota, or request more resources.',
             href: 'settings'
