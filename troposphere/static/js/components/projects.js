7a317a4043f09e763a14b12d66371ff500eda8ef
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-03-27 15:28:28 -0700

    Project view uses fluid responsive grid

--- troposphere/static/js/components/projects.js
+++ troposphere/static/js/components/projects.js
@@ -2,15 +2,36 @@ define(['react', 'underscore', 'components/page_header',
 'components/intro', 'collections/projects'], function(React, _,
 PageHeader, Intro, ProjectCollection) {
 
+    var ProjectItemMixin = {
+        render: function() {
+            return React.DOM.li({className: 'project-item row ' + this.getClassName()},
+                React.DOM.div({className: 'project-item-name col-md-8'}, this.renderName()),
+                React.DOM.div({className: 'project-item-details col-md-4'}, this.renderDetails()));
+        }
+    };
+
+    var InstanceProjectItem = React.createClass({
+        mixins: [ProjectItemMixin],
+        getClassName: function() {
+            return 'instance';
+        },
+        renderName: function() {
+            return React.DOM.a({}, this.props.model.get('name_or_id'));
+        },
+        renderDetails: function() {
+            return React.DOM.span({}, this.props.model.get('status'));
+        }
+    });
+
     var ProjectItems = React.createClass({
         render: function() {
             var project = this.props.project;
             var items = [];
             items = items.concat(project.get('instances').map(function(instance) {
-                return React.DOM.li({}, instance.id);
+                return InstanceProjectItem({model: instance});
             }));
 
-            return React.DOM.ul({className: 'project-items'}, items);
+            return React.DOM.ul({className: 'project-items container-fluid'}, items);
         }
     });
 
