983f3b1c8ee8c3932866df730b025998d292fab4
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-03-28 16:03:36 -0700

    Add links to volumes from projects list

--- troposphere/static/js/components/projects.js
+++ troposphere/static/js/components/projects.js
@@ -18,6 +18,7 @@ PageHeader, Intro, ProjectCollection, router, Time) {
         },
         renderName: function() {
             var instance = this.props.model;
+            /* TODO: Move this to the model */
             var instance_url = function(instance) {
                 var provider_id = instance.get('identity').provider;
                 var identity_id = instance.get('identity').id;
@@ -44,7 +45,20 @@ PageHeader, Intro, ProjectCollection, router, Time) {
             return 'volume';
         },
         renderName: function() {
-            return React.DOM.a({}, this.props.model.get('name_or_id'));
+            /* TODO: Move this to the model */
+            var volume_url = function(volume) {
+                var provider_id = volume.get('identity').provider;
+                var identity_id = volume.get('identity').id;
+                return 'provider/' + provider_id + '/identity/' + identity_id + '/volumes/' + volume.id;
+            };
+            var volume = this.props.model;
+            var url = volume_url(volume);
+            return React.DOM.a({
+                href: url_root + '/' + url,
+                onClick: function(e) {
+                    e.preventDefault();
+                    router.navigate(url, {trigger: true});
+                }}, volume.get('name_or_id'));
         },
         renderDetails: function() {
             return [this.props.model.get('size') + ' GB, created ',
@@ -57,10 +71,10 @@ PageHeader, Intro, ProjectCollection, router, Time) {
             var project = this.props.project;
             var items = [];
             items = items.concat(project.get('instances').map(function(instance) {
-                return InstanceProjectItem({model: instance});
+                return InstanceProjectItem({key: instance.id, model: instance});
             }));
             items = items.concat(project.get('volumes').map(function(volume) {
-                return VolumeProjectItem({model: volume});
+                return VolumeProjectItem({key: volume.id, model: volume});
             }));
 
             return React.DOM.ul({className: 'project-items container-fluid'}, items);
