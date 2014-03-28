ce0fbe297b95a5d99904604f5d166dbc1e2f332c
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-03-28 14:40:34 -0700

    Notifications close correctly

--- troposphere/static/js/components/notifications.js
+++ troposphere/static/js/components/notifications.js
@@ -1,4 +1,4 @@
-define(['react', 'notifications', 'jquery'], function(React, notifications, $) {
+define(['react', 'notifications'], function(React, notifications) {
 
     // Number of milliseconds to display a notification
     var timeout = 10 * 1000;
@@ -7,22 +7,26 @@ define(['react', 'notifications', 'jquery'], function(React, notifications, $) {
         getInitialState: function() {
             return {model: null};
         },
-        componentWillMount: function() {
+        componentDidMount: function() {
             notifications.bind('add', function(model) {
                 this.setState({model: model});
             }, this);
         },
+        closeNotification: function() {
+            this.setState({model: null});
+        },
         render: function() {
             var content = [];
-            if (this.state.model)
-                content = React.DOM.div({className: 'alert alert-'+this.state.model.get('type')+' fade in'},
-                    React.DOM.button({className: 'close', 'data-dismiss': 'alert'},
+            if (this.state.model) {
+                content = React.DOM.div({className: 'alert alert-'+this.state.model.get('type')},
+                    React.DOM.button({className: 'close', onClick: this.closeNotification},
                         '\u00d7'
                     ),
                     React.DOM.strong({}, this.state.model.get('header')),
                     " ",
-                    React.DOM.span({dangerouslySetInnerHTML: {'__html': this.state.model.get('body') }})
+                    React.DOM.span({dangerouslySetInnerHTML: {'__html': this.state.model.get('body')}})
                 )
+            }
             return React.DOM.div({id: 'notifications'}, content);
         },
         componentDidUpdate: function(prevProps, prevState, root) {
@@ -30,8 +34,8 @@ define(['react', 'notifications', 'jquery'], function(React, notifications, $) {
                 return;
             if (!this.state.model.get('sticky'))
                 setTimeout(function() {
-                    $('.close').trigger('click');
-                }, timeout);
+                    this.setState({model: null});
+                }.bind(this), timeout);
         }
     });
 
