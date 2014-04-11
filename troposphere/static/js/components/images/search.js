f34c6c275e49e480f9f5fa1eaeb60571ac4a68c2
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-04-11 12:52:36 -0700

    Handle query change to state in image search box

--- troposphere/static/js/components/images/search.js
+++ troposphere/static/js/components/images/search.js
@@ -9,19 +9,25 @@ define(['react'], function(React) {
     var SearchContainer = React.createClass({
         getInitialState: function() {
             return {
-                showAdvancedOptions: false
+                showAdvancedOptions: false,
+                query: ""
             }
         },
         toggleAdvancedOptions: function(e) {
             e.preventDefault();
             this.setState({showAdvancedOptions: !this.state.showAdvancedOptions});
         },
+        handleChange: function(e) {
+            this.setState({query: e.target.value});
+        },
         render: function() {
             return React.DOM.div({},
                 React.DOM.input({
                     type: 'text',
                     className: 'form-control',
-                    placeholder: 'Search by Image Name, Tag, OS, and more'
+                    placeholder: 'Search by Image Name, Tag, OS, and more',
+                    onChange: this.handleChange,
+                    value: this.state.query
                 }),
                 React.DOM.a({
                     onClick: this.toggleAdvancedOptions,
