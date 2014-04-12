0ed54d51529f68fda56ad2da8f169e6dea8e5584
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-04-11 18:22:12 -0700

    Add search results to, well, search results page

--- troposphere/static/js/components/images/search_results.js
+++ troposphere/static/js/components/images/search_results.js
@@ -1,8 +1,30 @@
-define(['react'], function(React) {
-    var SearchResults = React.createClass({
-        render: function() {
-            return React.DOM.div({}, this.props.query);
+define(['react', 'components/images/search',
+'components/page_header', 'components/mixins/loading', 'rsvp',
+'controllers/applications'], function(React, SearchBox, PageHeader,
+LoadingMixin, RSVP, Images) {
+
+    var Results = React.createClass({
+        mixins: [LoadingMixin],
+        model: function() {
+            return Images.searchApplications(this.props.query);
+        },
+        renderContent: function() {
+            return React.DOM.ul({},
+                this.state.model.map(function(app) {
+                    return React.DOM.li({}, app.get('name')); 
+                }));
         }
     });
+
+    var SearchResults = React.createClass({
+        render: function() {
+            return React.DOM.div({}, 
+                PageHeader({title: "Image Search"}),
+                SearchBox({query: this.props.query}),
+                Results({query: this.props.query}))
+        }
+    });
+
     return SearchResults;
+
 });
