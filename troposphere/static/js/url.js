4bef6fbc57eb90204b2740b9b0046ea396194a54
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-04-24 14:39:04 -0700

    Change variable names in url module

--- troposphere/static/js/url.js
+++ troposphere/static/js/url.js
@@ -3,14 +3,14 @@ define(['underscore'], function(_) {
 
     var generators = {
         instance: function(model) {
-            var provider_id = model.get('identity').provider;
-            var identity_id = model.get('identity').id;
-            return 'provider/' + provider_id + '/identity/' + identity_id + '/instances/' + model.id;
+            var providerId = model.get('identity').provider;
+            var identityId = model.get('identity').id;
+            return 'provider/' + providerId + '/identity/' + identityId + '/instances/' + model.id;
         },
         volume: function(model) {
-            var provider_id = model.get('identity').provider;
-            var identity_id = model.get('identity').id;
-            return 'provider/' + provider_id + '/identity/' + identity_id + '/volumes/' + model.id;
+            var providerId = model.get('identity').provider;
+            var identityId = model.get('identity').id;
+            return 'provider/' + providerId + '/identity/' + identityId + '/volumes/' + model.id;
         }
     };
 
