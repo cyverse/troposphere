8110d5ddc810886ca3bb7c72ac2908d05ec6176b
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-05-01 11:23:11 -0700

    Add success notifcation for project creation

--- troposphere/static/js/controllers/projects.js
+++ troposphere/static/js/controllers/projects.js
@@ -1,4 +1,4 @@
-define(['react', 'collections/projects', 'models/project', 'rsvp'], function(React, Collection, Model, RSVP) {
+define(['react', 'collections/projects', 'models/project', 'rsvp', 'controllers/notifications'], function(React, Collection, Model, RSVP, Notifications) {
 
     var projects = new Collection();
 
@@ -17,6 +17,7 @@ define(['react', 'collections/projects', 'models/project', 'rsvp'], function(Rea
             var model = new Model();
             model.save({name: name, description: description}, {
                 success: function(model) {
+                    Notifications.success('Success', 'Created new project "' + model.get('name') + '"');
                     resolve(model);
                 },
                 error: function(model, response) {
