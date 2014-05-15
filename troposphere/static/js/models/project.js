7110ef6f9c9f125deeb79651da8bcb6129d39a5f
jchansen <jason_hansen@outlook.com>
2014-05-15 15:55:37 -0700

    remove trailing slash from project url

--- troposphere/static/js/models/project.js
+++ troposphere/static/js/models/project.js
@@ -16,7 +16,7 @@ InstanceCollection, Instance, VolumeCollection, Volume) {
         },
         url: function() {
             if (this.id)
-                return this.urlRoot + '/project/' + this.id + '/';
+                return this.urlRoot + '/project/' + this.id;
             else
                 return this.urlRoot + '/project/';
         },
