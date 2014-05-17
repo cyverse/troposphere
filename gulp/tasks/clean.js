afc5c0792b9af13eb6c432ae1412a9867107168e
jchansen <jason_hansen@outlook.com>
2014-05-17 13:45:55 -0700

    break out images gulp task

---
+++ gulp/tasks/clean.js
@@ -0,0 +1,17 @@
+/**
+ * Delete specified directories and all files in them.
+ *
+ * ---------------------------------------------------------------
+ *
+ */
+
+var clean = require('gulp-clean');
+
+module.exports = function (gulp) {
+
+  gulp.task('clean', function () {
+    return gulp.src(['troposphere/assets'], {read: false})
+      .pipe(clean({force: true}));
+  });
+
+};
--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,7 @@ var gulp = require('gulp'),
 //minifycss = require('gulp-minify-css'),
 //jshint = require('gulp-jshint'),
 //uglify = require('gulp-uglify'),
-//imagemin = require('gulp-imagemin'),
 //rename = require('gulp-rename'),
-  clean = require('gulp-clean'),
   concat = require('gulp-concat'),
   notify = require('gulp-notify');
 //cache = require('gulp-cache'),
@@ -18,28 +16,11 @@ var paths = {
   images: ['troposphere/static/images/**']
 };
 
-// Styles
 require('gulp/tasks/styles')(gulp);
-
-// Scripts
 require('gulp/tasks/copyScriptsToAssets')(gulp);
-
-//gulp.task('scripts', ['copyScriptsToAssets']);
 require('gulp/tasks/rjs')(gulp);
-
-// Images
-gulp.task('images', function () {
-  return gulp.src(paths.images)
-    //.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('troposphere/assets/images'))
-    .pipe(notify({ message: 'Images build task complete', onLast: true }));
-});
-
-// Clean
-gulp.task('clean', function () {
-  return gulp.src(['troposphere/assets'], {read: false})
-    .pipe(clean({force: true}));
-});
+require('gulp/tasks/images')(gulp);
+require('gulp/tasks/clean')(gulp);
 
 // Watch
 gulp.task('watch', function () {
@@ -48,15 +29,6 @@ gulp.task('watch', function () {
   gulp.watch('troposphere/static/images/**', ['images']);
 });
 
-
-
 gulp.task('default', ['clean'], function () {
-  //gulp.start('styles', 'scripts', 'images');
-  gulp.start('requirejsBuild');
+  gulp.start('styles', 'copyScriptsToAssets', 'images');
 });
-
-gulp.task('dev', ['clean'], function () {
-  gulp.start('styles', 'scripts', 'images');
-});
-
-//gulp.task('default', ['clean', 'requirejsBuild']);
