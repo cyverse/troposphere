56004898476b57db4c91af380a8aa7fdeb4b3e44
jchansen <jason_hansen@outlook.com>
2014-05-18 11:19:23 -0700

    break out action list

--- troposphere/static/js/components/instances/detail.js
+++ troposphere/static/js/components/instances/detail.js
@@ -11,87 +11,10 @@ define(
     'components/common/ButtonDropdown.react',
     'components/common/Glyphicon.react',
     './InstanceAttributes.react',
-    './InstanceLinks.react'
+    './InstanceLinks.react',
+    './ActionList.react'
   ],
-  function (React, PageHeader, LoadingMixin, Instance, RSVP, Time, InstanceController, URL, ButtonDropdown, Glyphicon, InstanceAttributes, InstanceLinks) {
-
-    var ActionList = React.createClass({
-      renderButton: function (text, onClick, disabled) {
-        return React.DOM.button({className: 'btn btn-default',
-            onClick: onClick,
-            disabled: disabled},
-          text);
-      },
-      renderStartStopButton: function () {
-        if (!this.props.isOpenstack)
-          return null;
-
-        if (this.props.instance.get('status') == 'shutoff')
-          return this.renderButton([Glyphicon({name: 'share-alt'}), " Start"],
-            InstanceController.start.bind(null, this.props.instance));
-        else
-          return this.renderButton([Glyphicon({name: 'stop'}), " Stop"],
-            InstanceController.stop.bind(null, this.props.instance));
-      },
-      renderSuspendButton: function () {
-        if (!this.props.isOpenstack)
-          return null;
-
-        var disabled = !this.props.instance.get('is_active');
-
-        if (this.props.instance.get('status') == 'suspended')
-          return this.renderButton([Glyphicon({name: 'play'}), ' Resume'],
-            InstanceController.resume.bind(null, this.props.instance));
-        else
-          return this.renderButton([Glyphicon({name: 'pause'}), ' Suspend'],
-            InstanceController.suspend.bind(null, this.props.instance),
-            disabled);
-      },
-      renderRebootButton: function () {
-        var disabled = !this.props.instance.get('is_active');
-        var items = [React.DOM.li({}, React.DOM.a({}, "Soft reboot"))];
-        if (this.props.isOpenstack)
-          items.push(React.DOM.li({}, React.DOM.a({}, "Hard reboot")));
-
-        return ButtonDropdown({buttonContent: [Glyphicon({name: 'repeat'}), " Reboot"],
-          disabled: disabled}, items);
-      },
-      renderTerminateButton: function () {
-        var handleClick = InstanceController.terminate.bind(null, this.props.instance);
-        return this.renderButton([Glyphicon({name: 'remove'}), " Terminate"], handleClick);
-      },
-      renderResizeButton: function () {
-        if (!this.props.isOpenstack)
-          return null;
-
-        var disabled = !this.props.instance.get('is_active') || this.props.instance.get('is_resize');
-        return this.renderButton([Glyphicon({name: 'resize-full'}), " Resize"], null, disabled);
-      },
-      renderImageRequestButton: function () {
-        var disabled = !this.props.instance.get('is_active');
-        return this.renderButton([Glyphicon({name: 'camera'}), " Image"], null, disabled);
-      },
-      renderReportButton: function () {
-        var disabled = !this.props.instance.get('is_active');
-        var onClick = function () {
-          var url = URL.reportInstance(this.props.instance);
-          Backbone.history.navigate(url, {trigger: true});
-        }.bind(this);
-        return this.renderButton([Glyphicon({name: 'inbox'}), " Report"], onClick, disabled);
-      },
-      render: function () {
-        return React.DOM.div({},
-          React.DOM.h2({}, "Actions"),
-          React.DOM.div({},
-            this.renderStartStopButton(), // OS only
-            this.renderSuspendButton(), // OS only
-            this.renderRebootButton(),
-            this.renderTerminateButton(),
-            this.renderResizeButton(), // OS only
-            this.renderImageRequestButton(),
-            this.renderReportButton()));
-      }
-    });
+  function (React, PageHeader, LoadingMixin, Instance, RSVP, Time, InstanceController, URL, ButtonDropdown, Glyphicon, InstanceAttributes, InstanceLinks, ActionList) {
 
     var InstancePage = React.createClass({
       render: function () {
