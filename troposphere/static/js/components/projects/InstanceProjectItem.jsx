4402aee8c04f5c87152bdee3cf16e2ed56229276
jchansen <jason_hansen@outlook.com>
2014-05-17 23:19:30 -0700

    break out InstanceProjectItem

---
+++ troposphere/static/js/components/projects/InstanceProjectItem.jsx
@@ -0,0 +1,39 @@
+define(
+  [
+    'react',
+    'url',
+    './ProjectItemMixin'
+  ],
+  function (React, URL, ProjectItemMixin) {
+
+    return React.createClass({
+      mixins: [ProjectItemMixin],
+
+      getClassName: function () {
+        return 'instance';
+      },
+
+      renderName: function () {
+        var instance = this.props.model;
+        var href = URL.instance(instance, {absolute: true});
+        var navigateOnClick = function (e) {
+          e.preventDefault();
+          Backbone.history.navigate(URL.instance(instance), {trigger: true});
+        };
+
+        return (
+          <a href={href} onClick={navigateOnClick}>
+            {this.props.model.get('name_or_id')}
+          </a>
+        );
+      },
+
+      renderDetails: function () {
+        var machine_name = this.props.model.get('machine_name') || this.props.model.get('machine_alias');
+        var ip = this.props.model.get('public_ip_address');
+        return [ip ? ip + ', ' : '', 'from ', <a>machine_name</a>];
+      }
+
+    });
+
+  });
