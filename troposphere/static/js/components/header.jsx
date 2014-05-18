fe12c1684d1a15823bd68bf1f227ec94d6858a3f
jchansen <jason_hansen@outlook.com>
2014-05-17 21:49:35 -0700

    convert header to jsx syntax

---
+++ troposphere/static/js/components/header.jsx
@@ -0,0 +1,42 @@
+define(
+  [
+    'react'
+  ],
+  function (React) {
+
+    var LoginLink = React.createClass({
+      render: function () {
+        return <a href="/login">Login</a>;
+      }
+    });
+
+    var LogoutLink = React.createClass({
+      render: function () {
+        return <a href="/logout">{"Logout " + this.props.username}</a>;
+      }
+    });
+
+    var Header = React.createClass({
+      render: function () {
+
+        var profile = this.props.profile;
+        var rightChild = profile ? LogoutLink({username: profile.get('username')}) : LoginLink();
+
+        return (
+          <header class="clearfix">
+            <a href="/" id="logo">
+              <img src="/assets/images/mini_logo.png" alt="iPlant Cloud Services" height="30" width="30"/>
+              <span>Atmosphere </span>
+              <span id="tagline">iPlant Cloud Services</span>
+            </a>
+            <div id="header-nav">
+                {rightChild}
+            </div>
+          </header>
+        );
+      }
+    });
+
+    return Header;
+
+  });
