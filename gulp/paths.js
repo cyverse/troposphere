834ffedbc86a14ef535316a6a50a99c583be33e6
jchansen <jason_hansen@outlook.com>
2014-05-17 21:34:45 -0700

    add gulp task to convert react templates to js files

--- gulp/paths.js
+++ gulp/paths.js
@@ -1,6 +1,7 @@
 module.exports = {
   css: ['troposphere/static/css/**'],
-  scripts: ['troposphere/static/js/**'],
+  scripts: ['troposphere/static/js/**/*.js'],
   images: ['troposphere/static/images/**'],
-  rootSassFileForApp: 'troposphere/static/css/app/app.scss'
+  rootSassFileForApp: 'troposphere/static/css/app/app.scss',
+  jsxTemplates: 'troposphere/static/js/**/*.jsx'
 };
