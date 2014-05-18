077d56e1e1011e075b06800dafce618ad098318c
jchansen <jason_hansen@outlook.com>
2014-05-18 01:04:21 -0700

    convert gravatar to jsx syntax

---
+++ troposphere/static/js/components/common/Gravatar.react.js
@@ -0,0 +1,55 @@
+/** @jsx React.DOM */
+
+define(
+  [
+    'react'
+  ],
+  function (React) {
+
+    return React.createClass({
+
+      getDefaultProps: function () {
+        return {
+          //type: profile ? profile.get('settings')['icon_set'] : 'default',
+          type: 'default',
+          size: 50
+        };
+      },
+
+      propTypes: {
+        hash: React.PropTypes.string,
+        type: React.PropTypes.string,
+        size: React.PropTypes.number
+      },
+
+      getSrc: function (hash, icon_set, size) {
+        switch (icon_set) {
+          case 'unicorn':
+            return "//unicornify.appspot.com/avatar/" + hash + "?s=" + size;
+          case 'wavatar':
+            return "//www.gravatar.com/avatar/" + hash + "?d=wavatar&s=" + size;
+          case 'monster':
+            return "//www.gravatar.com/avatar/" + hash + "?d=monsterid&s=" + size;
+          case 'retro':
+            return "//www.gravatar.com/avatar/" + hash + "?d=retro&s=" + size;
+          case 'robot':
+            return "//robohash.org/" + hash + "?size=" + size + "x" + size;
+          default:
+            return "//www.gravatar.com/avatar/" + hash + "?d=identicon&s=" + size;
+        }
+      },
+
+      render: function () {
+        /* If a type is specified in props, use it. Otherwise, use the
+         * profile setting
+         */
+
+        var imgSrc = this.getSrc(this.props.hash, this.props.type, this.props.size);
+        return (
+          <img src={imgSrc} width={this.props.size} height={this.props.size}/>
+        );
+      }
+
+    });
+
+  });
