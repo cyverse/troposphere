4d43c523ce63ed3d88ab9a1c3c53f84e4874c464
jchansen <jason_hansen@outlook.com>
2014-05-18 13:58:41 -0700

    break out ApplicationCardList

--- troposphere/static/js/components/applications/cards.js
+++ troposphere/static/js/components/applications/cards.js
@@ -1,26 +1,11 @@
 define(
   [
-    'react',
-    'components/common/Gravatar.react',
-    'backbone',
-    'url',
     './Rating.react',
     './Tags.react',
-    './Bookmark.react',
-    './ApplicationCard.react'
+    './ApplicationCard.react',
+    './ApplicationCardList.react'
   ],
-  function (React, Gravatar, Backbone, URL, Rating, Tags, Bookmark, ApplicationCard) {
-
-    var ApplicationCardList = React.createClass({
-      render: function () {
-        var apps = this.props.applications;
-        return React.DOM.div({},
-          React.DOM.h3({}, this.props.title),
-          React.DOM.ul({className: 'app-card-list'}, apps.map(function (app) {
-            return React.DOM.li({}, ApplicationCard({application: app}));
-          })));
-      }
-    });
+  function (Rating, Tags, ApplicationCard, ApplicationCardList) {
 
     return {
       "ApplicationCardList": ApplicationCardList,
