5a6bda9d00ed3d3225a81955f361412963ee57d4
jchansen <jason_hansen@outlook.com>
2014-05-18 01:16:07 -0700

    convert button_group to jsx template

---
+++ troposphere/static/js/components/common/ButtonGroup.react.js
@@ -0,0 +1,36 @@
+/** @jsx React.DOM */
+
+define(
+  [
+    'react',
+    'underscore',
+    'bootstrap'
+  ],
+  function (React, _, Bootstrap) {
+
+    return React.createClass({
+      render: function () {
+        var actions = _.map(this.props.actions, function (callback, text) {
+          return (
+            <li>
+              <a href='#' onClick={callback}>{text}</a>
+            </li>
+          );
+        });
+
+        return (
+          <div className='btn-group'>
+            <button type='button' className='btn btn-default dropdown-toggle' data-toggle='dropdown'>
+              {this.props.text}
+              <span>{" "}</span>
+              <span className='caret'></span>
+            </button>
+            <ul className='dropdown-menu' role='menu'>
+              {actions}
+            </ul>
+          </div>
+        );
+      }
+    });
+
+  });
