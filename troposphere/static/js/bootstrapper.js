59ca247e1ad073a882fff05464087a4683925e93
jchansen <jason_hansen@outlook.com>
2014-05-18 23:17:49 -0700

    break startup script out of main for finer grained control

---
+++ troposphere/static/js/bootstrapper.js
@@ -0,0 +1,41 @@
+define(
+  [
+    'jquery',
+    'react',
+    'components/Root.react',
+    'rsvp',
+    'models/session'
+  ],
+  function ($, React, Application, RSVP, Session) {
+
+    return {
+      run: function () {
+        // Catch-all for errors within promises
+        RSVP.on('error', function (reason) {
+          console.assert(false, reason);
+        });
+
+        var session = new Session();
+
+        // todo: remove in production - development mode only
+        window.access_token = "fake-token";
+        window.expires = "it's a mystery!";
+
+        if (window.access_token) {
+          $.ajaxSetup({
+            headers: {'Authorization': 'Bearer ' + window.access_token}
+          });
+          session.set({
+            access_token: window.access_token,
+            expires: window.expires
+          });
+        }
+
+        $(document).ready(function () {
+          var app = Application({session: session});
+          React.renderComponent(app, document.getElementById('application'));
+        });
+      }
+    }
+
+  });
