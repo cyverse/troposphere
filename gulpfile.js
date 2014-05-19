76ada8a600c183b313abec07700ce3438b0afb6c
jchansen <jason_hansen@outlook.com>
2014-05-19 16:50:27 -0700

    make paths relative in gulpfile to resolve server deploy error
    Gulp can’t find the tasks on the server without using relative paths.
    I think the reason it works on the client side might (?) be that
    virtual env is in my project, and in opt/env/ on the server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-var gulp = require('gulp'),
-  sass = require('gulp-ruby-sass');
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 
 // Styles
-require('gulp/tasks/styles')(gulp);
-require('gulp/tasks/sass')(gulp);
+require('./gulp/tasks/styles')(gulp);
+require('./gulp/tasks/sass')(gulp);
 
 // Scripts
-require('gulp/tasks/copyScriptsToAssets')(gulp);
-require('gulp/tasks/copyBowerComponentsToAssets')(gulp);
-require('gulp/tasks/rjs')(gulp);
-require('gulp/tasks/react')(gulp);
+require('./gulp/tasks/copyScriptsToAssets')(gulp);
+require('./gulp/tasks/copyBowerComponentsToAssets')(gulp);
+require('./gulp/tasks/rjs')(gulp);
+require('./gulp/tasks/react')(gulp);
 
 // Images
-require('gulp/tasks/images')(gulp);
+require('./gulp/tasks/images')(gulp);
 
 // Misc
-require('gulp/tasks/clean')(gulp);
-require('gulp/tasks/watch')(gulp);
+require('./gulp/tasks/clean')(gulp);
+require('./gulp/tasks/watch')(gulp);
 
 gulp.task('default', ['clean'], function () {
   gulp.start('sass', 'copyScriptsToAssets', 'copyBowerComponentsToAssets', 'react', 'images');
