2d5216c570c1842800bda8130f8fa1a0772ff815
jchansen <jason_hansen@outlook.com>
2014-05-18 23:17:49 -0700

    break startup script out of main for finer grained control

--- troposphere/static/js/main.js
+++ troposphere/static/js/main.js
@@ -27,37 +27,10 @@ require.config({
 
 require(
   [
-    'jquery',
-    'react',
-    'components/Root.react',
-    'rsvp',
-    'models/session'
-  ], function($, React, Application, RSVP, Session) {
+    'bootstrapper'
+  ],
+  function (bootstrapper) {
+    'use strict';
 
-    // Catch-all for errors within promises
-    RSVP.on('error', function(reason) {
-        console.assert(false, reason);
-    });
-
-    var session = new Session();
-
-    // todo: remove in production - development mode only
-    window.access_token = "fake-token";
-    window.expires = "it's a mystery!";
-
-    if (window.access_token) {
-        $.ajaxSetup({
-            headers: {'Authorization' :'Bearer ' + window.access_token}
-        });
-        session.set({
-            access_token: window.access_token,
-            expires: window.expires
-        });
-    }
-
-    $(document).ready(function() {
-        var app = Application({session: session});
-        React.renderComponent(app, document.getElementById('application'));
-    });
-
-});
+    bootstrapper.run();
+  });
