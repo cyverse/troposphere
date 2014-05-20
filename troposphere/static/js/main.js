617e8bada9e07a6c8a2111148065682bc252ba6c
jchansen <jason_hansen@outlook.com>
2014-05-19 22:24:01 -0700

    use bower version of backbone, underscore and jquery
    We will combine and minimize them into the app js when compiled for
    production

--- troposphere/static/js/main.js
+++ troposphere/static/js/main.js
@@ -2,12 +2,12 @@ require.config({
   baseUrl: '/assets/js',
   paths: {
     /* TODO: use minified versions in production */
-    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery',
-    backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
+    jquery: '../bower_components/jquery/dist/jquery',
+    backbone: '../bower_components/backbone/backbone',
     marionette: '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
-    underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
+    underscore: '../bower_components/underscore/underscore',
     google: 'https://www.google.com/jsapi',
     bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap',
     moment: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min',
