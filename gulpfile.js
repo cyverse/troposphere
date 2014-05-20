2a1483dd0bfca1fa5a614d5304ffb28b661fb5b0
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-05-19 17:12:05 -0700

    Relative urls in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,20 @@ var gulp = require('gulp'),
   sass = require('gulp-ruby-sass');
 
 // Styles
-require('gulp/tasks/styles')(gulp);
-require('gulp/tasks/sass')(gulp);
+require('./gulp/tasks/styles')(gulp);
+require('./gulp/tasks/sass')(gulp);
 
 // Scripts
-require('gulp/tasks/copyScriptsToAssets')(gulp);
-require('gulp/tasks/rjs')(gulp);
-require('gulp/tasks/react')(gulp);
+require('./gulp/tasks/copyScriptsToAssets')(gulp);
+require('./gulp/tasks/rjs')(gulp);
+require('./gulp/tasks/react')(gulp);
 
 // Images
-require('gulp/tasks/images')(gulp);
+require('./gulp/tasks/images')(gulp);
 
 // Misc
-require('gulp/tasks/clean')(gulp);
-require('gulp/tasks/watch')(gulp);
+require('./gulp/tasks/clean')(gulp);
+require('./gulp/tasks/watch')(gulp);
 
 gulp.task('default', ['clean'], function () {
   gulp.start('sass', 'copyScriptsToAssets', 'react', 'images');
