fdeab3b523b7b6f18970d70449efdc686a848758
jchansen <jason_hansen@outlook.com>
2014-05-19 22:51:50 -0700

    remove unused onClick callback
    Since we’re allow the app to navigate, we don’t need the callbacks

--- troposphere/static/js/components/SidebarListItem.react.js
+++ troposphere/static/js/components/SidebarListItem.react.js
@@ -10,18 +10,13 @@ define(
 
     return React.createClass({
 
-      handleClick: function (e) {
-        e.preventDefault();
-        this.props.onNavigate(this.props.id.join('/'), {trigger: true});
-      },
-
       render: function () {
         var icon = this.props.icon ? <Glyphicon name={this.props.icon}/> : null;
         var href = '/application/' + this.props.id.join('/');
 
         return (
           <li className={this.props.active ? 'active' : ''}>
-            <a href={href} onClick={this.handleClick}>
+            <a href={href}>
               {icon}
               {this.props.text}
             </a>
