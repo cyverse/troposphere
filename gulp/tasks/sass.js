628d6971bd99d3242026026dc21611d9ca3f0bbb
jchansen <jason_hansen@outlook.com>
2014-05-22 15:17:35 -0700

    sass gulp task transfers files to .tmp for production env

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,14 +1,16 @@
 var notify = require('gulp-notify');
 var sass = require('gulp-ruby-sass');
 var paths = require('../paths');
+var gutil = require('gulp-util');
 
 module.exports = function (gulp) {
 
   gulp.task('sass', function () {
+    var dest = (gutil.env.type === 'production' ? '.tmp/css/app' : 'troposphere/assets/css/app');
     return gulp.src(paths.rootSassFileForApp)
       .pipe(sass({sourcemap: true}))
-      .pipe(gulp.dest('troposphere/assets/css/app'))
-      .pipe(notify({ message: 'Sass task complete', onLast: true }));
+      .pipe(gulp.dest(dest))
+      .pipe(notify({ message: 'Sass task complete, copied files to: ' + dest, onLast: true }));
   });
 
 };
