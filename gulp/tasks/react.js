a2190ba5a8e85471e463f132cde89aa5028e8d47
jchansen <jason_hansen@outlook.com>
2014-05-22 15:48:17 -0700

    add production switch to modify react deploy directory

--- gulp/tasks/react.js
+++ gulp/tasks/react.js
@@ -12,14 +12,17 @@
 var react = require('gulp-react');
 var notify = require('gulp-notify');
 var paths = require('../paths');
+var gutil = require('gulp-util');
 
 module.exports = function (gulp) {
 
   gulp.task('react', function (cb) {
+    var dest = (gutil.env.type === 'production' ? '.tmp/js' : 'troposphere/assets/js');
+
     return gulp.src(paths.jsxTemplates)
       .pipe(react())
-      .pipe(gulp.dest('troposphere/assets/js'))
-      .pipe(notify({ message: 'Converted React templates to JS syntax', onLast: true }));
+      .pipe(gulp.dest(dest))
+      .pipe(notify({ message: 'Transformed React JSX templates and copied to: ' + dest, onLast: true }));
   });
 
 };
