25faae63772aeea43830050f4caa077c13bcfe7a
jchansen <jason_hansen@outlook.com>
2014-05-22 18:26:05 -0700

    Improve header comments

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,13 +1,13 @@
 /**
- * Move all images to specified directory
+ * Copy images to the destination directory
  *
  * ---------------------------------------------------------------
  *
- * # dev task config
- * Copy the images to destination directory
+ * # default task config
+ * Copy files directly to the assets directory for easier development.
  *
  * # prod task config
- * Minify the images, then copy to destination directory.
+ * Minify the images, then copy them to the .tmp directory where r.js will take over.
  *
  */
 
@@ -18,7 +18,7 @@ var gutil = require('gulp-util');
 
 module.exports = function (gulp) {
 
-  gulp.task('images', function () {
+  gulp.task('images', function (done) {
     var dest = (gutil.env.type === 'production' ? '.tmp/images' : 'troposphere/assets/images');
 
     return gulp.src(paths.images)
