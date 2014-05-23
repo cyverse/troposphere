dd121223dc105b99ae72e8e0dd91ea1cc64d7953
jchansen <jason_hansen@outlook.com>
2014-05-22 18:45:49 -0700

    use non-cdn version of rsvp

--- troposphere/static/js/main.js
+++ troposphere/static/js/main.js
@@ -12,7 +12,7 @@ require.config({
     bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap',
     moment: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min',
     react: '//cdnjs.cloudflare.com/ajax/libs/react/0.10.0/react',
-    rsvp: '//cdn.jsdelivr.net/rsvp/3.0/rsvp.amd.min'
+    rsvp: '../bower_components/rsvp/rsvp.amd'
   },
   shim: {
     underscore: {
