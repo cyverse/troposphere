d64c934e412c8a2f8228b0fd27541d30f7619538
Chris LaRose <cjlarose@gmail.com>
2014-05-23 12:07:55 -0700

    Add ApplicationStore.isSynced()

--- troposphere/static/js/stores/applications.js
+++ troposphere/static/js/stores/applications.js
@@ -1,38 +1,43 @@
-define(['underscore', 'backbone', 'stores/collections/applications'], function(_, Backbone, Applications) {
+define(['underscore', 'backbone', 'collections/applications'], function(_, Backbone, Applications) {
 
-    var CHANGE_EVENT = 'change';
+  var CHANGE_EVENT = 'change';
 
-    var _applications;
+  var _applications = new Applications();
+  var _synced = false;
 
-    var ApplicationStore = {
-        getAll: function() {
-            return _applications;
-        },
-        getFeatured: function() {
-            return new Applications(_applications.filter(function(app) {
-                return app.get('featured');
-            }));
-        },
-        fetch: function() {
-            apps = new Applications(); 
-            apps.on('sync', function(coll) {
-                _applications = coll;
-                this.emitChange();
-            }.bind(this));
-            apps.fetch();
-        },
-        addChangeListener: function(callback) {
-            this.on(CHANGE_EVENT, callback);
-        },
-        removeChangeListener: function(callback) {
-            this.off(CHANGE_EVENT, callback);
-        },
-        emitChange: function() {
-            this.trigger(CHANGE_EVENT);
-        }
-    };
+  var ApplicationStore = {
+    isSynced: function() {
+      return _synced;
+    },
+    getAll: function() {
+      return _applications;
+    },
+    getFeatured: function() {
+      return new Applications(_applications.filter(function(app) {
+        return app.get('featured');
+      }));
+    },
+    fetch: function() {
+      apps = new Applications();
+      apps.on('sync', function(coll) {
+        _applications = coll;
+        _synced = true;
+        this.emitChange();
+      }.bind(this));
+      apps.fetch();
+    },
+    addChangeListener: function(callback) {
+      this.on(CHANGE_EVENT, callback);
+    },
+    removeChangeListener: function(callback) {
+      this.off(CHANGE_EVENT, callback);
+    },
+    emitChange: function() {
+      this.trigger(CHANGE_EVENT);
+    }
+  };
 
-    _.extend(ApplicationStore, Backbone.Events);
+  _.extend(ApplicationStore, Backbone.Events);
 
-    return ApplicationStore;
+  return ApplicationStore;
 });
