0657f481780af59aaee69147cf6677323df4061e
Chris LaRose <cjlarose@gmail.com>
2014-05-23 11:49:14 -0700

    Remove unnecessary require and initialize from watch gulp task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -9,11 +9,8 @@ var clean = require('gulp-clean');
 var livereload = require('gulp-livereload');
 var notify = require('gulp-notify');
 var paths = require('../paths');
-var scripts = require('./scripts');
 
 module.exports = function (gulp) {
-
-  scripts(gulp);
 
   gulp.task('watch', function () {
     gulp.watch(paths.css, ['sass']);
