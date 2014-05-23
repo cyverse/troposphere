d09a7f15f635b28e56fbb0bf895097b492aeb3be
jchansen <jason_hansen@outlook.com>
2014-05-22 18:22:55 -0700

    Add header comments

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,3 +1,16 @@
+/**
+ * Transform SASS files to CSS and copy result to destination directory.
+ *
+ * ---------------------------------------------------------------
+ *
+ * # default task config
+ * Copy files directly to the assets directory for easier development.
+ *
+ * # production task config
+ * Copy files to .tmp directory where r.js will take over.
+ *
+ */
+
 var notify = require('gulp-notify');
 var sass = require('gulp-ruby-sass');
 var paths = require('../paths');
@@ -5,13 +18,13 @@ var gutil = require('gulp-util');
 
 module.exports = function (gulp) {
 
-  gulp.task('sass', function () {
+  gulp.task('sass', function (done) {
     var dest = (gutil.env.type === 'production' ? '.tmp/css/app' : 'troposphere/assets/css/app');
 
     return gulp.src(paths.rootSassFileForApp)
       .pipe(sass({sourcemap: true}))
       .pipe(gulp.dest(dest))
-      .pipe(notify({ message: 'Transformed SCSS to CSS and copied files to: ' + dest, onLast: true }));
+      .pipe(notify({ message: 'Transformed SCSS to CSS and copied files to: ' + dest, onLast: true }))
   });
 
 };
