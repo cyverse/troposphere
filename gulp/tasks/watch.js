9f82d0b6ea6bc977fe6de302a363db5bd444fa77
Chris LaRose <cjlarose@gmail.com>
2014-05-23 11:45:55 -0700

    Fix watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -9,12 +9,15 @@ var clean = require('gulp-clean');
 var livereload = require('gulp-livereload');
 var notify = require('gulp-notify');
 var paths = require('../paths');
+var scripts = require('./scripts');
 
 module.exports = function (gulp) {
 
+  scripts(gulp);
+
   gulp.task('watch', function () {
     gulp.watch(paths.css, ['sass']);
-    gulp.watch(paths.scripts, ['copyScriptsToAssets']);
+    gulp.watch(paths.scripts, ['scripts']);
     gulp.watch(paths.images, ['images']);
     gulp.watch(paths.jsxTemplates, ['react']);
   });
