c873a2c229fc974e8aa6c05388b768a1b21955b3
jchansen <jason_hansen@outlook.com>
2014-05-24 14:05:56 -0700

    reformat applications

--- troposphere/static/js/collections/applications.js
+++ troposphere/static/js/collections/applications.js
@@ -1,11 +1,18 @@
-define(['collections/base', 'models/application'], function(Base, Application) {
+define(
+  [
+    'collections/base',
+    'models/application'
+  ],
+  function (Base, Application) {
 
     return Base.extend({
-        model: Application,
-        url: function(){
-            return url = this.urlRoot
-                + '/' + this.model.prototype.defaults.model_name;
-        }
+      model: Application,
+
+      url: function () {
+        return url = this.urlRoot
+          + '/' + this.model.prototype.defaults.model_name;
+      }
+      
     });
 
-});
+  });
