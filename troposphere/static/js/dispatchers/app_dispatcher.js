e7214213ff79a820b7470edbd01a0d6cd6b6ba67
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-05-27 13:19:06 -0700

    Using _.extend correctly in app dispatcher

--- troposphere/static/js/dispatchers/app_dispatcher.js
+++ troposphere/static/js/dispatchers/app_dispatcher.js
@@ -1,13 +1,15 @@
 define(['dispatchers/dispatcher', 'underscore'], function(Dispatcher, _) {
 
-  var AppDispatcher = _.extend(Dispatcher, {
+  var AppDispatcher = {
     handleRouteAction: function(action) {
       Dispatcher.dispatch({
         source: 'ROUTE_ACTION',
         action: action
       });
     }
-  });
+  };
+
+  _.extend(AppDispatcher, Dispatcher);
 
   return AppDispatcher;
 });
