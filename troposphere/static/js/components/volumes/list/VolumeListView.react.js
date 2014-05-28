a0b14ace61fd9d301b46eff18adb366410a5ece3
jchansen <jason_hansen@outlook.com>
2014-05-28 14:35:33 -0700

    display static list when navigating to /volumes

---
+++ troposphere/static/js/components/volumes/list/VolumeListView.react.js
@@ -0,0 +1,52 @@
+/** @jsx React.DOM */
+
+define(
+  [
+    'react'
+  ],
+  function (React) {
+
+    return React.createClass({
+
+      propTypes: {
+        volumes: React.PropTypes.instanceOf(Backbone.Collection).isRequired
+      },
+
+      render: function () {
+        return (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>{"#"}</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        );
+      }
+
+    });
+
+  });
--- troposphere/static/js/routers/volumes.js
+++ troposphere/static/js/routers/volumes.js
@@ -6,13 +6,15 @@ define(
     'components/Root.react',
     'react',
     'context',
-    'components/volumes/VolumeDetailsPage.react'
+    'components/volumes/VolumeDetailsPage.react',
+    'components/volumes/VolumeListPage.react'
   ],
-  function (Marionette, Root, React, context, VolumeDetailsPage) {
+  function (Marionette, Root, React, context, VolumeDetailsPage, VolumeListPage) {
     'use strict';
 
     var Router = Marionette.AppRouter.extend({
       appRoutes: {
+        'volumes': 'showVolumes',
         'provider/:provider_id/identity/:identity_id/volumes/:volume_id': 'showVolumeDetail'
       }
     });
@@ -27,6 +29,10 @@ define(
           route: route || Backbone.history.getFragment()
         });
         React.renderComponent(app, document.getElementById('application'));
+      },
+
+      showVolumes: function () {
+        this.render(VolumeListPage(), "volumes");
       },
 
       showVolumeDetail: function (providerId, identityId, volumeId) {
