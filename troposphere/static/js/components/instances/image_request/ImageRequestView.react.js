c77d1660c1055d0721bae63b02588368ed99b7d9
jchansen <jason_hansen@outlook.com>
2014-05-29 12:35:35 -0700

    add html for request image form

--- troposphere/static/js/components/instances/image_request/ImageRequestView.react.js
+++ troposphere/static/js/components/instances/image_request/ImageRequestView.react.js
@@ -18,21 +18,187 @@ define(
         var instance = this.props.instance,
             provider = this.props.provider;
 
+        renderInputControlGroup = function(){
+          return (
+            <div className="control-group">
+              <label htmlFor="name" className="control-label">Image Name</label>
+              <div className="controls">
+                <input type="text" name="name" className="span5" maxlength={30} size={15} />
+                <div className="help-block">
+                  <strong>Suggested Format:</strong>
+                  [Descriptive Name] [Your Username] [Version]
+                  <br />
+                Please limit name to 30 characters.
+                </div>
+              </div>
+            </div>
+          );
+        }
+
+        var renderControlGroup = function(label, id){
+          return (
+            <div className="control-group">
+              <label htmlFor="instance" className="control-label">{label}</label>
+              <div className="controls">{id}</div>
+              <input type="hidden" name="instance" value={id} />
+            </div>
+          );
+        }
+
+        var renderDropdownControlGroup = function(){
+          return (
+            <div className="control-group">
+              <label htmlFor="provider" className="control-label">Cloud for Deployment</label>
+              <div className="controls">
+                <select name="provider">
+                  <option value={4}>iPlant Cloud - Tucson</option>
+                </select>
+                <div className="help-block">Select the cloud provider on which you plan to use this image.</div>
+              </div>
+            </div>
+          );
+        }
+
+        var renderTextAreaControlGroup = function(label, description, name){
+          return (
+            <div className="control-group">
+              <label htmlFor={name} className="control-label">{label}</label>
+              <div className="controls">
+                <textarea name={name} rows="4" />
+                <div className="help-block">{description}</div>
+              </div>
+            </div>
+          );
+        }
+
+        var displayNoneStyle = {display: 'none'};
+        var renderImageTagsControlGroup = function(){
+          return (
+            <div className="control-group">
+              <label htmlFor="tags" className="control-label">Image Tags</label>
+              <div className="controls tagger_container">
+                <div className="tagger clearfix">
+                  <ul className="tag_list">
+                    <li className="edit_tags">
+                      <a className="tag_adder" title="Add a tag">Edit Tags</a>
+                    </li>
+                    <li className="tag_controls" style={displayNoneStyle}>
+                      <form>
+                        <input placeholder="New Tag" type="text" className="new_tag" />
+                      </form>
+                      <div className="add_tag_button">Add</div>
+                      <div className="done_tagging_button">✔ Done</div>
+                      <ul className="suggested_tags_holder" style={displayNoneStyle} />
+                    </li>
+                  </ul>
+                </div>
+                <input type="hidden" name="tags" className="tag_input" />
+                <div className="help-block">Please include tags that will help users decide whether this image will suit their
+                needs. You can include the operating system, installed software, or configuration information. E.g. Ubuntu,
+                NGS Viewers, MAKER, QIIME, etc.
+                </div>
+              </div>
+            </div>
+          );
+        }
+
+        var renderImageVisibilityControlGroup = function(){
+          return (
+            <div className="control-group">
+              <label htmlFor="vis" className="control-label">Image Visibility</label>
+              <div className="controls">
+                <select name="vis">
+                  <option value="public">Public</option>
+                  <option value="private">Private</option>
+                  <option value="select">Specific Users</option>
+                </select>
+                <div className="help-block" id="vis_help">A VM image can be made visible to you, a select group of users or to
+                everyone. If you want visibility restricted to a select group of users, provide us a list of iPlant
+                usernames. Public visibility means that any user will be able to launch the instance.
+                </div>
+              </div>
+            </div>
+          );
+        }
+
+        var renderCheckboxControlGroup = function(){
+          return (
+            <div className="control-group">
+              <div className="controls">
+                <label className="checkbox">
+                  <input type="checkbox" id="licensed_software" />
+                I certify that this image does not contain license-restricted software that is prohibited from being
+                distributed within a virtual or cloud environment.
+                </label>
+                <br />
+              </div>
+            </div>
+          );
+        }
+
+        var renderFormActions = function(){
+          return (
+            <div class="form-actions">
+              <input type="submit" class="btn btn-primary request_image_submit" value="Request Imaging"/>
+              <div class="help-block">Note: All volumes must be detached from an instance before it can be imaged.</div>
+            </div>
+          );
+        }
+
         var helpText = function(){
           return (
             <p>
               Please read the
-              <a href="https://pods.iplantcollaborative.org/wiki/x/oIZy" target="_blank">wiki page about requesting an image of your instance</a>
+              <a href="https://pods.iplantcollaborative.org/wiki/x/oIZy" target="_blank"> wiki page about requesting an image of your instance </a>
               before completing the form below.
             </p>
           );
         }
+
+        var systemFilesHelpText = (
+          <div>
+            If your VM image requires a change in an iPlant-managed system file (see
+              <a href="https://pods.iplantcollaborative.org/wiki/x/oIZy" target="_blank">{"Important Notes #4"}</a>
+            ), let us know the change and why the change is necessary.
+          </div>
+        );
 
         return (
           <div className="imaging_form module">
             <PageHeader title="Request Imaging" helpText={helpText}/>
 
             <form className="request_imaging_form form-horizontal" method="post">
+
+            {renderInputControlGroup()}
+            {renderControlGroup("Instance ID", "0bbbc916-0c76-4006-a86c-e570c96ff819")}
+            {renderControlGroup("IP Address", "128.196.64.25")}
+            {renderDropdownControlGroup()}
+            {renderTextAreaControlGroup(
+              "Description of the Image",
+              "Concisely describe the tools installed and their purpose. Please include key words that will " +
+                "help users search for this image and decide whether it will suit their needs.",
+              "description"
+            )}
+            {renderTextAreaControlGroup(
+              "List Installed Software",
+              "List any software that you have installed. If the paths to the executables are different " +
+                "from /usr/bin or /usr/local/bin, list those also.",
+              "software"
+            )}
+            {renderTextAreaControlGroup(
+              "iPlant-managed System Files",
+              systemFilesHelpText,
+              "sys"
+            )}
+            {renderTextAreaControlGroup(
+              "Files to exclude",
+              "If your instance has files you'd like to exclude from the image, list them here. Write one path per line.",
+              "exclude"
+            )}
+            {renderImageTagsControlGroup()}
+            {renderImageVisibilityControlGroup()}
+            {renderCheckboxControlGroup()}
+            {renderFormActions()}
 
             </form>
 
