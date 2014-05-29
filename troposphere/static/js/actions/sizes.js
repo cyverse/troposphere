8558c313d84c041bc39035a4e33fd30555f2b322
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-05-29 14:47:50 -0700

    Add Size actions and store

---
+++ troposphere/static/js/actions/sizes.js
@@ -0,0 +1,17 @@
+define(['dispatchers/app_dispatcher'], function(AppDispatcher) {
+
+  var SizeActions = {
+    constants: {
+      fetch: 'size_fetch'
+    },
+    fetch: function(providerId, identityId) {
+      AppDispatcher.handleRouteAction({
+        actionType: this.constants.fetch,
+        providerId: providerId,
+        identityId: identityId
+      });
+    }
+  };
+
+  return SizeActions;
+});
