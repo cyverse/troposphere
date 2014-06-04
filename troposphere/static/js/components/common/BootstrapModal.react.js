e02dae4c8e1b08697df4f15dfbfcb3bdf0000084
jchansen <jason_hansen@outlook.com>
2014-06-03 23:24:08 -0700

    add alternate mixin for Bootstrap modals

---
+++ troposphere/static/js/components/common/BootstrapModal.react.js
@@ -0,0 +1,67 @@
+/** @jsx React.DOM */
+
+define(
+  [
+    'react',
+    'components/mixins/BootstrapModalMixin.react'
+  ],
+  function (React, BootstrapModalMixin) {
+
+    // Example Usage from http://bl.ocks.org/insin/raw/8449696/
+    // render: function(){
+    // <div>
+    //   ...custom components...
+    //   <ExampleModal
+    //      ref="modal"
+    //      show={false}
+    //      header="Example Modal"
+    //      buttons={buttons}
+    //      handleShow={this.handleLog.bind(this, 'Modal about to show', 'info')}
+    //      handleShown={this.handleLog.bind(this, 'Modal showing', 'success')}
+    //      handleHide={this.handleLog.bind(this, 'Modal about to hide', 'warning')}
+    //      handleHidden={this.handleLog.bind(this, 'Modal hidden', 'danger')}
+    //    >
+    //      <p>I'm the content.</p>
+    //      <p>That's about it, really.</p>
+    //    </ExampleModal>
+    // </div>
+    //
+
+    // To show the modal, call this.refs.modal.show() from the parent component:
+    // handleShowModal: function() {
+    //   this.refs.modal.show();
+    // }
+
+    return React.createClass({
+      mixins: [BootstrapModalMixin],
+
+      render: function () {
+        var buttons = this.props.buttons.map(function (button) {
+          return <button type="button" className={'btn btn-' + button.type} onClick={button.handler}>
+            {button.text}
+          </button>
+        });
+
+        return (
+          <div className="modal fade">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  {this.renderCloseButton()}
+                  <strong>{this.props.header}</strong>
+                </div>
+                <div className="modal-body">
+                  {this.props.children}
+                </div>
+                <div className="modal-footer">
+                  {buttons}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+
+    });
+
+  });
---
+++ troposphere/static/js/components/mixins/BootstrapModalMixin.react.js
@@ -0,0 +1,109 @@
+/** @jsx React.DOM */
+
+define(
+  [
+    'react',
+    'jquery',
+    'bootstrap'
+  ],
+  function (React, $) {
+
+    //
+    // Copied from http://bl.ocks.org/insin/raw/8449696/
+    //
+    
+    var BootstrapModalMixin = function () {
+      var handlerProps = [
+        'handleShow',
+        'handleShown',
+        'handleHide',
+        'handleHidden'
+      ];
+
+      var bsModalEvents = {
+        handleShow: 'show.bs.modal',
+        handleShown: 'shown.bs.modal',
+        handleHide: 'hide.bs.modal',
+        handleHidden: 'hidden.bs.modal'
+      };
+
+      return {
+        propTypes: {
+          handleShow: React.PropTypes.func,
+          handleShown: React.PropTypes.func,
+          handleHide: React.PropTypes.func,
+          handleHidden: React.PropTypes.func,
+          backdrop: React.PropTypes.bool,
+          keyboard: React.PropTypes.bool,
+          show: React.PropTypes.bool,
+          remote: React.PropTypes.string
+        },
+
+        getDefaultProps: function () {
+          return {
+            backdrop: true,
+            keyboard: true,
+            show: true,
+            remote: ''
+          }
+        },
+
+        componentDidMount: function () {
+          var $modal = $(this.getDOMNode()).modal({
+            backdrop: this.props.backdrop,
+            keyboard: this.props.keyboard,
+            show: this.props.show,
+            remote: this.props.remote
+          });
+
+          handlerProps.forEach(function (prop) {
+            if (this[prop]) {
+              $modal.on(bsModalEvents[prop], this[prop])
+            }
+            if (this.props[prop]) {
+              $modal.on(bsModalEvents[prop], this.props[prop])
+            }
+          }.bind(this))
+        },
+
+        componentWillUnmount: function () {
+          var $modal = $(this.getDOMNode())
+          handlerProps.forEach(function (prop) {
+            if (this[prop]) {
+              $modal.off(bsModalEvents[prop], this[prop])
+            }
+            if (this.props[prop]) {
+              $modal.off(bsModalEvents[prop], this.props[prop])
+            }
+          }.bind(this))
+        },
+
+        hide: function () {
+          $(this.getDOMNode()).modal('hide')
+        },
+
+        show: function () {
+          $(this.getDOMNode()).modal('show')
+        },
+
+        toggle: function () {
+          $(this.getDOMNode()).modal('toggle')
+        },
+
+        renderCloseButton: function () {
+          return (
+            <button type="button"
+                    className="close"
+                    onClick={this.hide}
+            >
+              {'\u00d7'}
+            </button>
+          );
+        }
+
+      }
+    }();
+
+    return BootstrapModalMixin;
+
+  });
