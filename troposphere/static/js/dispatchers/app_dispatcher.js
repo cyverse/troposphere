427c236e542bdb4729e67ffa5f710609bdfb081f
jchansen <jason_hansen@outlook.com>
2014-06-07 15:41:41 -0700

    change formatting

--- troposphere/static/js/dispatchers/app_dispatcher.js
+++ troposphere/static/js/dispatchers/app_dispatcher.js
@@ -1,15 +1,20 @@
-define(['dispatchers/dispatcher', 'underscore'], function(Dispatcher, _) {
+define(
+  [
+    'dispatchers/dispatcher',
+    'underscore'
+  ],
+  function (Dispatcher, _) {
 
-  var AppDispatcher = {
-    handleRouteAction: function(action) {
-      Dispatcher.dispatch({
-        source: 'ROUTE_ACTION',
-        action: action
-      });
-    }
-  };
+    var AppDispatcher = {
+      handleRouteAction: function (action) {
+        Dispatcher.dispatch({
+          source: 'ROUTE_ACTION',
+          action: action
+        });
+      }
+    };
 
-  _.extend(AppDispatcher, Dispatcher);
+    _.extend(AppDispatcher, Dispatcher);
 
-  return AppDispatcher;
-});
+    return AppDispatcher;
+  });
--- troposphere/static/js/dispatchers/dispatcher.js
+++ troposphere/static/js/dispatchers/dispatcher.js
@@ -1,40 +1,45 @@
-define(['rsvp', 'underscore'], function(RSVP, _) {
+define(
+  [
+    'rsvp',
+    'underscore'
+  ],
+  function (RSVP, _) {
 
-  var _callbacks = [];
-  var _promises = [];
+    var _callbacks = [];
+    var _promises = [];
 
-  var _addPromise = function(cb, payload) {
-    _promises.push(new RSVP.Promise(function(resolve, reject) {
-      if (cb(payload))
-        resolve(payload);
-      else
-        reject(new Error("Dispatcher callback unsuccessful"));
-    }));
-  };
+    var _addPromise = function (cb, payload) {
+      _promises.push(new RSVP.Promise(function (resolve, reject) {
+        if (cb(payload))
+          resolve(payload);
+        else
+          reject(new Error("Dispatcher callback unsuccessful"));
+      }));
+    };
 
-  var _clearPromises = function() {
-    _promises = [];
-  };
+    var _clearPromises = function () {
+      _promises = [];
+    };
 
-  var Dispatcher = {
-    register: function(cb) {
-      _callbacks.push(cb);
-      return _callbacks.length - 1;
-    },
-    dispatch: function(payload) {
-      _.each(_callbacks, function(cb) {
-        _addPromise(cb, payload);
-      });
-      RSVP.all(_promises).then(_clearPromises);
-    },
-    waitFor: function(/*array*/ promiseIndexes, /*function*/ callback) {
-      var selectedPromises = _.map(promiseIndexes, function(index) {
-        return _promises[index];
-      });
-      return RSVP.all(selectedPromises).then(callback);
-    }
-  };
+    var Dispatcher = {
+      register: function (cb) {
+        _callbacks.push(cb);
+        return _callbacks.length - 1;
+      },
+      dispatch: function (payload) {
+        _.each(_callbacks, function (cb) {
+          _addPromise(cb, payload);
+        });
+        RSVP.all(_promises).then(_clearPromises);
+      },
+      waitFor: function (/*array*/ promiseIndexes, /*function*/ callback) {
+        var selectedPromises = _.map(promiseIndexes, function (index) {
+          return _promises[index];
+        });
+        return RSVP.all(selectedPromises).then(callback);
+      }
+    };
 
-  return Dispatcher;
+    return Dispatcher;
 
-});
+  });
