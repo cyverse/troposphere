8b0638bca6ddf15d8f9e10904aa9fbfdaf6409d6
jchansen <jason_hansen@outlook.com>
2014-06-07 00:39:17 -0700

    lazy load instances when requested

--- troposphere/static/js/stores/InstanceStore.js
+++ troposphere/static/js/stores/InstanceStore.js
@@ -6,9 +6,10 @@ define(
     'rsvp',
     'collections/instances',
     'constants/InstanceConstants',
-    'controllers/notifications'
+    'controllers/notifications',
+    'stores/IdentityStore'
   ],
-  function (_, Dispatcher, Store, RSVP, InstanceCollection, InstanceConstants, NotificationController) {
+  function (_, Dispatcher, Store, RSVP, InstanceCollection, InstanceConstants, NotificationController, IdentityStore) {
 
     var _instances = null;
     var _isFetching = false;
@@ -17,11 +18,41 @@ define(
     // CRUD Operations
     //
 
-    var fetchInstances = function () {
+    var fetchInstancesFor = function (providerId, identityId) {
+      var promise = new RSVP.Promise(function (resolve, reject) {
+        var instances = new InstanceCollection(null, {
+          provider_id: providerId,
+          identity_id: identityId
+        });
+        // make sure promise returns the right instances collection
+        // for when this function is called multiple times
+        (function(instances, resolve){
+          instances.fetch().done(function(){
+            resolve(instances);
+          });
+        })(instances, resolve)
+      });
+      return promise;
+    };
+
+    var fetchInstances = function (identities) {
       _isFetching = true;
       var promise = new RSVP.Promise(function (resolve, reject) {
-        var instances = new InstanceCollection();
-        instances.fetch().done(function () {
+        // return an array of promises (one for each volume collection being fetched)
+        var promises = identities.map(function (identity) {
+          var providerId = identity.get('provider_id');
+          var identityId = identity.get('id');
+          return fetchInstancesFor(providerId, identityId);
+        });
+
+        // When all instance collections are fetched...
+        RSVP.all(promises).then(function (instanceCollections) {
+          // Combine results into a single volume collection
+          var instances = new InstanceCollection();
+          for (var i = 0; i < instanceCollections.length; i++) {
+            instances.add(instanceCollections[i].toJSON());
+          }
+          // Save the results to local cache
           _isFetching = false;
           _instances = instances;
           resolve();
@@ -38,9 +69,12 @@ define(
 
       getAll: function () {
         if(!_instances && !_isFetching) {
-          fetchInstances().then(function(){
-            InstanceStore.emitChange();
-          }.bind(this));
+          var identities = IdentityStore.getAll();
+          if(identities) {
+            fetchInstances(identities).then(function () {
+              InstanceStore.emitChange();
+            }.bind(this));
+          }
         }
         return _instances;
       }
