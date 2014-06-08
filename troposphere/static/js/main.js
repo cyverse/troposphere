d4c445776968a219d1eb62868dee90749913ed6b
jchansen <jason_hansen@outlook.com>
2014-06-08 16:14:44 -0700

    set jquery and react to minified versions

--- troposphere/static/js/main.js
+++ troposphere/static/js/main.js
@@ -9,7 +9,7 @@ require.config({
     //    //If the CDN location fails, load from this location
     //    'lib/jquery'
     //]
-    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery',
+    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
     backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
     marionette: '//cdnjs.cloudflare.com/ajax/libs/backbone.marionette/1.8.6/core/amd/backbone.marionette.min',
     'backbone.wreqr': '//cdnjs.cloudflare.com/ajax/libs/backbone.wreqr/0.1.0/backbone.wreqr.min',
@@ -18,7 +18,7 @@ require.config({
     google: 'https://www.google.com/jsapi',
     bootstrap: '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min',
     moment: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min',
-    react: '//cdnjs.cloudflare.com/ajax/libs/react/0.10.0/react-with-addons',
+    react: '//cdnjs.cloudflare.com/ajax/libs/react/0.10.0/react-with-addons.min',
     rsvp: '//cdn.jsdelivr.net/rsvp/3.0/rsvp.amd.min',
     chosen: '//cdnjs.cloudflare.com/ajax/libs/chosen/1.1.0/chosen.jquery.min',
     toastr: '//cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.2/js/toastr.min'
