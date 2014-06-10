50dbcd20398d4f7f9bd6a03fe5128e2400d7d58a
jchansen <jason_hansen@outlook.com>
2014-06-10 12:52:23 -0700

    add margin to bottom of tag for proper overflow display

--- troposphere/static/js/components/instances/detail/InstanceInfo.react.js
+++ troposphere/static/js/components/instances/detail/InstanceInfo.react.js
@@ -11,25 +11,25 @@ define(
       render: function () {
 
         return (
-          <div className="instance-info clearfix">
+          <div className="instance-info-section">
 
-            <div className="instance-image icon-container">
+            <div className="instance-image">
               <a href="/application/images/9ab516c9-c39f-595e-a990-977642da4c0e">
-                <img src="//www.gravatar.com/avatar/918bf82f238c6c264fc7701e1ff61363?d=identicon&amp;s=150" width="150" height="150"/>
+                <img src="//www.gravatar.com/avatar/918bf82f238c6c264fc7701e1ff61363?d=identicon&amp;s=100" width="100" height="100"/>
               </a>
             </div>
 
             <div className="instance-info">
-              <div>Instance Name</div>
-              <div>Launched on March 3, 2014</div>
-              <div>Instance Tags:</div>
+              <h4 className="instance-name">Instance Name</h4>
+              <div className="instance-launch-date">Launched on March 3, 2014</div>
+              <div className="instance-tags">Instance Tags:</div>
               <ul className="tags">
-                <li className="tag">
-                  <a href="#">Existing Tag</a>
-                </li>
-                <li className="tag">
-                  <a href="#">Existing Tag</a>
-                </li>
+                <li className="tag"><a href="#">Batman</a></li>
+                <li className="tag"><a href="#">Bruce Wayne</a></li>
+                <li className="tag"><a href="#">Superman</a></li>
+                <li className="tag"><a href="#">Ironman</a></li>
+                <li className="tag"><a href="#">Black Widow</a></li>
+                <li className="tag"><a href="#">HULK SMASH!!</a></li>
               </ul>
             </div>
 
