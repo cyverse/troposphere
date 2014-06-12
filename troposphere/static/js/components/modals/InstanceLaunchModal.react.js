291da229a402f81715bfe940e3a138130961f39f
jchansen <jason_hansen@outlook.com>
2014-06-11 22:42:17 -0700

    create dedicated Instance Launch Modal.

---
+++ troposphere/static/js/components/modals/InstanceLaunchModal.react.js
@@ -0,0 +1,169 @@
+/** @jsx React.DOM */
+
+define(
+  [
+    'react',
+    'components/mixins/BootstrapModalMixin.react',
+    'stores/ProviderStore',
+    'stores/IdentityStore',
+    'stores/SizeStore',
+    './instance_launch/MachineSelect.react',
+    './instance_launch/IdentitySelect.react',
+    './instance_launch/InstanceSizeSelect.react'
+  ],
+  function (React, BootstrapModalMixin, ProviderStore, IdentityStore, SizeStore, MachineSelect, IdentitySelect, InstanceSizeSelect) {
+
+    // Example Usage from http://bl.ocks.org/insin/raw/8449696/
+    // render: function(){
+    // <div>
+    //   ...custom components...
+    //   <ExampleModal
+    //      ref="modal"
+    //      show={false}
+    //      header="Example Modal"
+    //      buttons={buttons}
+    //      handleShow={this.handleLog.bind(this, 'Modal about to show', 'info')}
+    //      handleShown={this.handleLog.bind(this, 'Modal showing', 'success')}
+    //      handleHide={this.handleLog.bind(this, 'Modal about to hide', 'warning')}
+    //      handleHidden={this.handleLog.bind(this, 'Modal hidden', 'danger')}
+    //    >
+    //      <p>I'm the content.</p>
+    //      <p>That's about it, really.</p>
+    //    </ExampleModal>
+    // </div>
+    //
+
+    // To show the modal, call this.refs.modal.show() from the parent component:
+    // handleShowModal: function() {
+    //   this.refs.modal.show();
+    // }
+
+    function getState() {
+      return {
+        providers: ProviderStore.getAll(),
+        identities: IdentityStore.getAll()
+      };
+    }
+
+    return React.createClass({
+      mixins: [BootstrapModalMixin],
+
+      //
+      // Mounting & State
+      // ----------------
+      //
+      getInitialState: function(){
+        return getState();
+      },
+
+      updateState: function () {
+        if (this.isMounted()) this.setState(getState());
+      },
+
+      componentDidMount: function () {
+        ProviderStore.addChangeListener(this.updateState);
+        IdentityStore.addChangeListener(this.updateState);
+        SizeStore.addChangeListener(this.updateState);
+      },
+
+      componentDidUnmount: function () {
+        ProviderStore.removeChangeListener(this.updateState);
+        IdentityStore.removeChangeListener(this.updateState);
+        SizeStore.removeChangeListener(this.updateState);
+      },
+
+      //
+      // Render
+      // ------
+      //
+      render: function () {
+        var buttons = null;
+        //var buttons = this.props.buttons.map(function (button) {
+        //  return (
+        //    <button key={button.text} type="button" className={'btn btn-' + button.type} onClick={button.handler}>
+        //      {button.text}
+        //    </button>
+        //  );
+        //});
+
+        var emptyFunction = function(){};
+
+        var content;
+        if(this.state.identities && this.state.providers){
+
+          // Use selected identity or default to the first one
+          this.state.identityId = this.state.identityId || this.state.identities.first().id;
+
+          // Use selected machine (image version) or default to the first one
+          // todo: we should be sorting these by date or version number before selecting the first one
+          var machines = this.props.application.get('machines');
+          this.state.machineId = this.state.machineId || machines.first().id;
+
+          var selectedIdentity = this.state.identities.get(this.state.identityId);
+          var selectedProvider = this.state.providers.get(selectedIdentity.get('provider_id'));
+
+          content = (
+            <form role='form'>
+
+              <div className='form-group'>
+                <label htmlFor='instance-name'>Instance Name</label>
+                <input type='text' className='form-control' id='instance-name' onChange={emptyFunction}/>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='machine'>Version</label>
+                <MachineSelect machineId={this.state.machineId}
+                               machines={machines}
+                               onChange={emptyFunction}
+                />
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='identity'>Identity</label>
+                <IdentitySelect
+                    onChange={emptyFunction}
+                    identityId={this.state.identityId}
+                    identities={this.state.identities}
+                    providers={this.state.providers}
+                />
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='size'>Instance Size</label>
+                <InstanceSizeSelect
+                    provider={selectedProvider}
+                    identity={selectedIdentity}
+                    onChange={emptyFunction}
+                />
+              </div>
+            </form>
+          );
+        }else{
+          content = (
+            <div className="loading"></div>
+          );
+        }
+
+        return (
+          <div className="modal fade">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  {this.renderCloseButton()}
+                  <strong>{this.props.header}</strong>
+                </div>
+                <div className="modal-body">
+                  {content}
+                </div>
+                <div className="modal-footer">
+                  {buttons}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+
+    });
+
+  });
