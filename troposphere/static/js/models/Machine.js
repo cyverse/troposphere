9f9149d7c00a475a32711a6cb5f758010c4bc7f9
jchansen <jason_hansen@outlook.com>
2014-06-11 22:42:33 -0700

    add missing function to Machine model so it pretty prints the version again

--- troposphere/static/js/models/Machine.js
+++ troposphere/static/js/models/Machine.js
@@ -16,10 +16,22 @@ define(
       computed: {
         pretty_version: function () {
           var parts = this.get('version').split('.');
-          for (var i = 0; i <= 3 - parts.length; i++)
+          for (var i = 0; i <= 3 - parts.length; i++) {
             parts.push("0");
+          }
           return parts.join(".");
         }
+      },
+
+      /*
+       * Here, were override the get method to allow lazy-loading of computed
+       * attributes
+       */
+      get: function (attr) {
+        if (typeof this.computed !== "undefined" && typeof this.computed[attr] === 'function') {
+          return this.computed[attr].call(this);
+        }
+        return Backbone.Model.prototype.get.call(this, attr);
       }
 
     });
