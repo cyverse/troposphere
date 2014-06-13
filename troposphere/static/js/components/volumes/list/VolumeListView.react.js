01a92d6f9d8730ed6f9eb02c88edded80ec5a4c9
jchansen <jason_hansen@outlook.com>
2014-06-12 23:21:10 -0700

    add action drop downs to volume list page

--- troposphere/static/js/components/volumes/list/VolumeListView.react.js
+++ troposphere/static/js/components/volumes/list/VolumeListView.react.js
@@ -16,6 +16,40 @@ define(
         volumes: React.PropTypes.instanceOf(Backbone.Collection).isRequired
       },
 
+      onChange: function(volume, e){
+        var command = e.target.value;
+        var volumeName = volume.get('name');
+        if(command === "attach"){
+          console.log("attach" + volumeName);
+        }else if(command === "detach"){
+          console.log("detach" + volumeName);
+        }else if(command === "destroy"){
+          console.log("destroy" + volumeName);
+        }
+        e.target.value = e.target.options[0].value;
+      },
+
+      getVolumeAttachOptions: function(volume){
+        var placeholderMessage = "Unattached";
+        return (
+          <select name="tags" data-placeholder={placeholderMessage} className="form-control" onChange={this.onChange.bind(this, volume)}>
+            <option>{placeholderMessage}</option>
+            <option value="attach">Attach to...</option>
+            <option value="destroy">Destroy</option>
+          </select>
+        );
+      },
+
+      getVolumeDetachOptions: function(volume){
+        var placeholderMessage = "Attached to iPlant Base Instance";
+        return (
+          <select name="tags" data-placeholder={placeholderMessage} className="form-control" onChange={this.onChange.bind(this, volume)}>
+            <option>{placeholderMessage}</option>
+            <option value="detach">Detach</option>
+          </select>
+        );
+      },
+
       render: function () {
         var volumes = this.props.volumes.map(function (volume) {
           var volumeName = volume.get('name');
@@ -30,9 +64,15 @@ define(
                 </a>
               </td>
               <td>{description}</td>
+              <td>
+                { volume.get('status') === "available" ? this.getVolumeAttachOptions(volume) : this.getVolumeDetachOptions(volume) }
+              </td>
+              <td>
+                200 GB on <a href="#">iPlant Cloud - Tucson</a>
+              </td>
             </tr>
           );
-        });
+        }.bind(this));
 
         var helpText = function(){
           return (
@@ -40,7 +80,8 @@ define(
               <p>This page shows volumes you've created across all providers</p>
             </div>
           );
-        }
+        };
+
         return (
           <div>
             <PageHeader title="All Volumes" helpText={helpText}/>
@@ -49,6 +90,8 @@ define(
                 <tr>
                   <th>Name</th>
                   <th>Description</th>
+                  <th>Status</th>
+                  <th>Size, Provider</th>
                 </tr>
               </thead>
               <tbody>
