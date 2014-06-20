688835b1c46597128d2af061c33f1c8c879d26e8
jchansen <jason_hansen@outlook.com>
2014-06-20 16:21:46 -0700

    remove non-existent action

--- troposphere/static/js/actions/SizeActions.js
+++ troposphere/static/js/actions/SizeActions.js
@@ -5,16 +5,7 @@ define(
   function (AppDispatcher) {
 
     var SizeActions = {
-      constants: {
-        fetch: 'size_fetch'
-      },
-      fetch: function (providerId, identityId) {
-        AppDispatcher.handleRouteAction({
-          actionType: this.constants.fetch,
-          providerId: providerId,
-          identityId: identityId
-        });
-      }
+
     };
 
     return SizeActions;
