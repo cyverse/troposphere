5a629fabf7f54278c595b2dded674c962d7b4f76
jchansen <jason_hansen@outlook.com>
2014-06-20 15:38:03 -0700

    swap header to bootstrap, add version and logout links

--- troposphere/static/js/components/Header.react.js
+++ troposphere/static/js/components/Header.react.js
@@ -24,18 +24,62 @@ define(
         var profile = this.props.profile;
         var rightChild = profile ? LogoutLink({username: profile.get('username')}) : LoginLink();
 
+//        return (
+//          <header className="clearfix">
+//            <a href="/" id="logo">
+//              <img src="/assets/images/mini_logo.png" alt="iPlant Cloud Services" height="30" width="30"/>
+//              <span>Atmosphere </span>
+//              <span id="tagline">iPlant Cloud Services</span>
+//            </a>
+//            <div id="header-nav">
+//                {rightChild}
+//            </div>
+//          </header>
+//        );
+
         return (
-          <header className="clearfix">
-            <a href="/" id="logo">
-              <img src="/assets/images/mini_logo.png" alt="iPlant Cloud Services" height="30" width="30"/>
-              <span>Atmosphere </span>
-              <span id="tagline">iPlant Cloud Services</span>
-            </a>
-            <div id="header-nav">
-                {rightChild}
+          <div className="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div className="container-fluid">
+
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="/">
+                  Atmosphere: iPlant Cloud Services
+                </a>
+              </div>
+
+              <div className="navbar-collapse collapse">
+                <ul className="nav navbar-nav">
+                  <li className="">
+                    <a href="#">Dashboard</a>
+                  </li>
+                </ul>
+
+                <ul className="nav navbar-nav navbar-right">
+                  <li className="dropdown">
+                    <a className="dropdown-toggle" href="#" data-toggle="dropdown">jchansen
+                      <b className="caret"></b>
+                    </a>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <a href="/version">Version</a>
+                      </li>
+                      <li>
+                        <a href="/logout">Sign out</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </header>
+          </div>
         );
+
       }
     });
 
