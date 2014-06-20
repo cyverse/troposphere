ad4becc0673cb2781a7294ced027dba8320a238c
jchansen <jason_hansen@outlook.com>
2014-06-20 16:03:49 -0700

    fix login/logout display

--- troposphere/static/js/components/Header.react.js
+++ troposphere/static/js/components/Header.react.js
@@ -8,13 +8,31 @@ define(
 
     var LoginLink = React.createClass({
       render: function () {
-        return <a href="/login">Login</a>;
+        return (
+          <li className="dropdown">
+            <a href="/login">Login</a>
+          </li>
+        );
       }
     });
 
     var LogoutLink = React.createClass({
       render: function () {
-        return <a href="/logout?cas=True">{"Logout " + this.props.username}</a>;
+        return (
+          <li className="dropdown">
+            <a className="dropdown-toggle" href="#" data-toggle="dropdown">jchansen
+              <b className="caret"></b>
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a href="/version">Version</a>
+              </li>
+              <li>
+                <a href="/logout?cas=True">Sign out</a>
+              </li>
+            </ul>
+          </li>
+        );
       }
     });
 
@@ -22,20 +40,7 @@ define(
       render: function () {
 
         var profile = this.props.profile;
-        var rightChild = profile ? LogoutLink({username: profile.get('username')}) : LoginLink();
-
-//        return (
-//          <header className="clearfix">
-//            <a href="/" id="logo">
-//              <img src="/assets/images/mini_logo.png" alt="iPlant Cloud Services" height="30" width="30"/>
-//              <span>Atmosphere </span>
-//              <span id="tagline">iPlant Cloud Services</span>
-//            </a>
-//            <div id="header-nav">
-//                {rightChild}
-//            </div>
-//          </header>
-//        );
+        var loginLogoutLink = profile ? LogoutLink({username: profile.get('username')}) : LoginLink();
 
         return (
           <div className="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -61,19 +66,7 @@ define(
                 </ul>
 
                 <ul className="nav navbar-nav navbar-right">
-                  <li className="dropdown">
-                    <a className="dropdown-toggle" href="#" data-toggle="dropdown">jchansen
-                      <b className="caret"></b>
-                    </a>
-                    <ul className="dropdown-menu">
-                      <li>
-                        <a href="/version">Version</a>
-                      </li>
-                      <li>
-                        <a href="/logout">Sign out</a>
-                      </li>
-                    </ul>
-                  </li>
+                  {loginLogoutLink}
                 </ul>
               </div>
             </div>
