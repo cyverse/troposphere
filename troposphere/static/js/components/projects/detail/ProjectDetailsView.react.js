c0db76707b31aae21644f3f81bdb53efb5ff646d
jchansen <jason_hansen@outlook.com>
2014-07-10 17:09:47 -0700

    resource page layout roughly correct

--- troposphere/static/js/components/projects/detail/ProjectDetailsView.react.js
+++ troposphere/static/js/components/projects/detail/ProjectDetailsView.react.js
@@ -15,7 +15,7 @@ define(
 
       render: function () {
         return (
-          <div>
+          <div className="project-details">
 
             <div className="secondary-nav">
               <div className="container">
@@ -31,86 +31,84 @@ define(
             </div>
 
             <div className="container">
-
-              <div className="sub-menu col-md-2">
-                <button className="btn btn-primary">Create</button>
-                <ul>
-                  <li className="active"><a href="#">Instances</a></li>
-                  <li><a href="#">Volumes</a></li>
-                </ul>
-              </div>
-
-              <div className="col-md-10">
-
-                <div className="button-bar">
-                  <button className="btn btn-default">
-                    <i className="glyphicon glyphicon-folder-open"/>
-                  </button>
-                </div>
-
-                <div className="row">
-
-                  <div className="scrollable-content col-md-9">
-                    <div>
-                      <div className="header">
-                        <i className="glyphicon glyphicon-tasks"></i>
-                        <h2>Instances</h2>
+              <table>
+                <tbody>
+                  <tr>
+                    <td className="td-sub-menu">
+                      <div className="sub-menu">
+                        <button className="btn btn-primary">Create</button>
+                        <ul>
+                          <li className="active"><a href="#">Instances</a></li>
+                          <li><a href="#">Volumes</a></li>
+                        </ul>
                       </div>
-                      <table className="table">
-                        <thead>
-                          <tr>
-                            <th><input type="checkbox"/></th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                          </tr>
-                          <tr>
-                            <td><input type="checkbox"/></td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                          </tr>
-                          <tr>
-                            <td>3</td>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
+                    </td>
 
-                  </div>
+                    <td className="td-project-content">
+                      <div className="project-content">
 
-                  <div className="side-panel col-md-3">
-                    <div className="header">
-                      <span className="title">Details</span>
-                    </div>
-                    <ul>
-                      <li>
-                        <span class="instance-detail-label">Launched</span>
-                        <span class="instance-detail-value">
-                          <time title="2014-07-01T12:12:19-07:00" datetime="2014-07-01T19:12:19+00:00">Jul 1, 2014 (9 days ago)</time>
-                        </span>
-                      </li>
-                      <li>
-                        <span class="instance-detail-label">ID</span>
-                        <span class="instance-detail-value">9a1f18c7-fe6d-4d2f-85ee-c3bfc572c6a2</span>
-                      </li>
-                    </ul>
-                  </div>
+                        <div className="button-bar">
+                          <button className="btn btn-default">
+                            <i className="glyphicon glyphicon-folder-open"/>
+                          </button>
+                        </div>
 
-                </div>
+                        <div className="resource-list">
 
-              </div>
+                          <div className="scrollable-content">
+                            <div>
+                              <div className="header">
+                                <i className="glyphicon glyphicon-tasks"></i>
+                                <h2>Instances</h2>
+                              </div>
+                              <table className="table">
+                                <thead>
+                                  <tr>
+                                    <th><div className="resource-checkbox"></div></th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Username</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td><div className="resource-checkbox"></div></td>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+
+                          </div>
+
+                          <div className="side-panel">
+                            <div className="header">
+                              <span className="title">Details</span>
+                            </div>
+                            <ul>
+                              <li>
+                                <span class="instance-detail-label">Launched</span>
+                                <span class="instance-detail-value">
+                                  <time title="2014-07-01T12:12:19-07:00" datetime="2014-07-01T19:12:19+00:00">Jul 1, 2014 (9 days ago)</time>
+                                </span>
+                              </li>
+                              <li>
+                                <span class="instance-detail-label">ID</span>
+                                <span class="instance-detail-value">9a1f18c7-fe6d-4d2f-85ee-c3bfc572c6a2</span>
+                              </li>
+                            </ul>
+                          </div>
+
+                        </div>
+
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
             </div>
           </div>
         );
