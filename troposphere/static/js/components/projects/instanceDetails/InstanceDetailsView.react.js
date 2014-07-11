0bb0fc5d92bb67e5c89e42508f0d347721ad1a99
jchansen <jason_hansen@outlook.com>
2014-07-10 23:58:57 -0700

    can see project instance details page

--- troposphere/static/js/components/projects/instanceDetails/InstanceDetailsView.react.js
+++ troposphere/static/js/components/projects/instanceDetails/InstanceDetailsView.react.js
@@ -4,18 +4,18 @@ define(
   [
     'react',
     'backbone',
-    '../../instances/detail/InstanceDetails.react',
-    'stores/ProjectInstanceStore',
+    '../../instances/detail/InstanceDetailsView.react',
+    'stores/InstanceStore',
     'stores/ProviderStore',
     'stores/SizeStore',
     'stores/IdentityStore',
     'controllers/NotificationController'
   ],
-  function (React, Backbone, InstanceDetails, ProjectInstanceStore, ProviderStore, SizeStore, IdentityStore, NotificationController) {
+  function (React, Backbone, InstanceDetails, InstanceStore, ProviderStore, SizeStore, IdentityStore, NotificationController) {
 
-    function getState(project) {
+    function getState(project, instanceId) {
       return {
-        instances: ProjectInstanceStore.getInstancesInProject(project),
+        instance: InstanceStore.get(instanceId),
         providers: ProviderStore.getAll()
       };
     }
@@ -28,11 +28,11 @@ define(
       },
 
       getInitialState: function(){
-        return getState(this.props.project);
+        return getState(this.props.project, this.props.instanceId);
       },
 
       componentDidMount: function () {
-        ProjectInstanceStore.addChangeListener(this.updateState);
+        InstanceStore.addChangeListener(this.updateState);
         ProviderStore.addChangeListener(this.updateState);
 
         // todo: IdentityStore is only included here because InstanceStore.get(instanceId) is
@@ -44,32 +44,32 @@ define(
       },
 
       componentWillUnmount: function () {
-        ProjectInstanceStore.removeChangeListener(this.updateState);
+        InstanceStore.removeChangeListener(this.updateState);
         ProviderStore.removeChangeListener(this.updateState);
         IdentityStore.removeChangeListener(this.updateState);
         SizeStore.removeChangeListener(this.updateState);
       },
 
       updateState: function(){
-        if (this.isMounted()) this.setState(getState(this.props.project));
+        if (this.isMounted()) this.setState(getState(this.props.project, this.props.instanceId));
       },
 
       render: function () {
-        if(this.state.instances && this.state.providers) {
-          var instance = this.state.instances.get(this.props.instanceId);
-          if(!instance) NotificationController.error(null, "No volume with id: " + this.props.volumeId);
+        if(this.state.instance && this.state.providers) {
+          //var instance = this.state.instances.get(this.props.instanceId);
+          //if(!instance) NotificationController.error(null, "No instance with id: " + this.props.volumeId);
 
-          var providerId = instance.get('identity').provider;
+          var providerId = this.state.instance.get('identity').provider;
           var provider = this.state.providers.get(providerId);
 
-          var identityId = instance.get('identity').id;
-          var sizeId = instance.get('size_alias');
+          var identityId = this.state.instance.get('identity').id;
+          var sizeId = this.state.instance.get('size_alias');
           var sizes = SizeStore.getAllFor(providerId, identityId);
           if(sizes) {
             var size = sizes.get(sizeId);
 
             return (
-              <InstanceDetails instance={instance} provider={provider} size={size}/>
+              <InstanceDetails instance={this.state.instance} provider={provider} size={size}/>
             );
           }
         }
