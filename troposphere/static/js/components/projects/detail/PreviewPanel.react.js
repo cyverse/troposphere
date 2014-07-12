bd501f98843122bae80f42b480c4c3705661bb9d
jchansen <jason_hansen@outlook.com>
2014-07-12 11:15:35 -0700

    add instance preview placeholder

--- troposphere/static/js/components/projects/detail/PreviewPanel.react.js
+++ troposphere/static/js/components/projects/detail/PreviewPanel.react.js
@@ -3,9 +3,10 @@
 define(
   [
     'react',
-    'backbone'
+    'backbone',
+    '../resources/instance/preview/InstancePreviewView.react'
   ],
-  function (React, Backbone) {
+  function (React, Backbone, InstancePreviewView) {
 
     return React.createClass({
 
@@ -14,23 +15,19 @@ define(
       },
 
       render: function () {
+        var resourcePreview;
+        if(true){
+          resourcePreview = (
+            <InstancePreviewView/>
+          );
+        }
+
         return (
           <div className="side-panel">
             <div className="header">
               <span className="title">Details</span>
             </div>
-            <ul>
-              <li>
-                <span>Launched</span>
-                <span>
-                  <time>Jul 1, 2014 (9 days ago)</time>
-                </span>
-              </li>
-              <li>
-                <span>ID</span>
-                <span>9a1f18c7-fe6d-4d2f-85ee-c3bfc572c6a2</span>
-              </li>
-            </ul>
+            {resourcePreview}
           </div>
         );
       }
