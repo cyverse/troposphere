e47cc9caa3b659f2c7e070bb81c8f3aadc25eed8
jchansen <jason_hansen@outlook.com>
2014-07-14 11:31:27 -0700

    beginning of convert all project resources to real data
    Can’t use cached data on project/:id/volume because it doesn’t cache
    all the fields (status and attach_data are not updated)

--- troposphere/static/js/components/projects/detail/ProjectDetails.react.js
+++ troposphere/static/js/components/projects/detail/ProjectDetails.react.js
@@ -10,14 +10,18 @@ define(
     './SubMenu.react',
     './ButtonBar.react',
     'stores/ProjectInstanceStore',
-    'stores/ProjectVolumeStore'
+    'stores/ProjectVolumeStore',
+    'stores/InstanceStore',
+    'stores/VolumeStore'
   ],
-  function (React, Backbone, InstanceTable, VolumeTable, PreviewPanel, SubMenu, ButtonBar, ProjectInstanceStore, ProjectVolumeStore) {
+  function (React, Backbone, InstanceTable, VolumeTable, PreviewPanel, SubMenu, ButtonBar, ProjectInstanceStore, ProjectVolumeStore, InstanceStore, VolumeStore) {
 
     function getState(project) {
       return {
-        instances: ProjectInstanceStore.getInstancesInProject(project),
-        volumes: ProjectVolumeStore.getVolumesInProject(project),
+        instances: InstanceStore.getAll(),
+        volumes: VolumeStore.getAll(),
+        projectInstances: ProjectInstanceStore.getInstancesInProject(project),
+        projectVolumes: ProjectVolumeStore.getVolumesInProject(project),
         selectedResource: null
       };
     }
@@ -33,16 +37,20 @@ define(
       },
 
       componentDidMount: function () {
-        ProjectInstanceStore.addChangeListener(this.onUpdate);
-        ProjectVolumeStore.addChangeListener(this.onUpdate);
+        InstanceStore.addChangeListener(this.updateState);
+        VolumeStore.addChangeListener(this.updateState);
+        ProjectInstanceStore.addChangeListener(this.updateState);
+        ProjectVolumeStore.addChangeListener(this.updateState);
       },
 
       componentWillUnmount: function () {
-        ProjectInstanceStore.removeChangeListener(this.onUpdate);
-        ProjectVolumeStore.removeChangeListener(this.onUpdate);
+        InstanceStore.removeChangeListener(this.updateState);
+        VolumeStore.removeChangeListener(this.updateState);
+        ProjectInstanceStore.removeChangeListener(this.updateState);
+        ProjectVolumeStore.removeChangeListener(this.updateState);
       },
 
-      onUpdate: function(){
+      updateState: function(){
         if (this.isMounted()) this.setState(getState(this.props.project));
       },
 
@@ -51,14 +59,29 @@ define(
       },
 
       render: function () {
-        if(this.state.instances && this.state.volumes) {
+        if(this.state.projectInstances && this.state.projectVolumes && this.state.instances && this.state.volumes) {
+          
+          // Figure out which volumes are real
+          var volumes = new this.state.volumes.constructor();
+
+          this.state.projectVolumes.map(function(projectVolume){
+            var realVolume = this.state.volumes.get(projectVolume.id);
+            if(realVolume){
+              volumes.push(realVolume);
+            }else {
+              console.log(projectVolume.get('name') + " is not real.");
+              projectVolume.isRealVolume = false;
+              volumes.push(projectVolume);
+            }
+          }.bind(this));
+
           return (
             <div className="project-content">
               <ButtonBar/>
               <div className="resource-list">
                 <div className="scrollable-content">
                   <InstanceTable instances={this.state.instances} project={this.props.project} onResourceSelected={this.onResourceSelected}/>
-                  <VolumeTable volumes={this.state.volumes} project={this.props.project} onResourceSelected={this.onResourceSelected}/>
+                  <VolumeTable volumes={volumes} project={this.props.project} onResourceSelected={this.onResourceSelected}/>
                 </div>
                 <PreviewPanel resource={this.state.selectedResource}/>
               </div>
