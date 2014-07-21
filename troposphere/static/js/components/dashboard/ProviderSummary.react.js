9cc2851817045c9edaa4e7b181b5d7917c3a08c7
jchansen <jason_hansen@outlook.com>
2014-07-21 16:55:05 -0700

    add more details to provider summaries

--- troposphere/static/js/components/dashboard/ProviderSummary.react.js
+++ troposphere/static/js/components/dashboard/ProviderSummary.react.js
@@ -17,14 +17,22 @@ define(
         volumes: React.PropTypes.instanceOf(Backbone.Collection).isRequired
       },
 
-      calculateCpuUsage: function(instances, provider, quota, sizes){
-        var maxCpuCount = quota.cpu;
-
-        var providerInstances = instances.filter(function(instance){
+      getProviderInstances: function(instances, provider){
+        return instances.filter(function(instance){
           return instance.get('identity').provider === provider.id;
         });
+      },
 
-        var currentCpuCount = providerInstances.reduce(function(memo, instance){
+      getProviderVolumes: function(volumes, provider){
+         return volumes.filter(function(volume){
+          return volume.get('identity').provider === provider.id;
+        });
+      },
+
+      calculateCpuUsage: function(instances, quota, sizes){
+        var maxCpuCount = quota.cpu;
+
+        var currentCpuCount = instances.reduce(function(memo, instance){
           var size = sizes.findWhere({alias: instance.get('size_alias')});
           return memo + size.get('cpu');
         }.bind(this), 0);
@@ -32,36 +40,64 @@ define(
         return currentCpuCount + "/" + maxCpuCount;
       },
 
-      calculateStorageUsage: function(volumes, provider, quota){
-        var maxStorage = quota.disk;
+      calculateMemoryUsage: function(instances, quota, sizes){
+        var maxMemory = quota.mem;
 
-        var providerVolumes = volumes.filter(function(volume){
-          return volume.get('identity').provider === provider.id;
-        });
+        var currentMemory = instances.reduce(function(memo, instance){
+          var size = sizes.findWhere({alias: instance.get('size_alias')});
+          return memo + size.get('mem');
+        }.bind(this), 0);
 
-        var currentStorageCount = providerVolumes.reduce(function(memo, volume){
+        return currentMemory + "/" + maxMemory;
+      },
+
+      calculateStorageUsage: function(volumes, quota){
+        var maxStorage = quota.storage;
+
+        var currentStorage = volumes.reduce(function(memo, volume){
           return memo + volume.get('size')
         }.bind(this), 0);
 
-        return currentStorageCount + "/" + maxStorage;
+        return currentStorage + "/" + maxStorage;
+      },
+
+      calculateStorageCountUsage: function(volumes, quota){
+        var maxStorageCount = quota.storage_count;
+
+        var currentStorageCount = volumes.reduce(function(memo, volume){
+          return memo + 1;
+        }.bind(this), 0);
+
+        return currentStorageCount + "/" + maxStorageCount;
       },
 
       render: function () {
         var identity = this.props.identity;
         var provider = this.props.providers.get(identity.get('provider_id'));
         var quota = identity.get('quota');
-
         var sizes = SizeStore.getAllFor(provider.id, identity.id);
+
         if(sizes){
-          var cpuUsage = this.calculateCpuUsage(this.props.instances, provider, quota, sizes);
-          var storageUsage = this.calculateStorageUsage(this.props.volumes, provider, quota);
+
+          var providerInstances = this.getProviderInstances(this.props.instances, provider);
+          var providerVolumes = this.getProviderInstances(this.props.volumes, provider);
+
+          var cpuUsage = this.calculateCpuUsage(providerInstances, quota, sizes);
+          var memoryUsage = this.calculateMemoryUsage(providerInstances, quota, sizes);
+          var storageUsage = this.calculateStorageUsage(providerVolumes, quota);
+          var storageCountUsage = this.calculateStorageCountUsage(providerVolumes, quota);
 
           return (
             <div>
               <h4>{provider.get('name')}</h4>
               <ul>
+                <li>{providerInstances.length + " Instances"}</li>
                 <li>{cpuUsage + " CPUs"}</li>
+                <li>{memoryUsage + " GBs Memory"}</li>
+
+                <li>{providerVolumes.length + " Volumes"}</li>
                 <li>{storageUsage + " GBs Storage"}</li>
+                <li>{storageCountUsage + " Storage Volumes"}</li>
               </ul>
             </div>
           );
