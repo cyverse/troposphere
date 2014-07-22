b0114ea449c0b47ef52ce94c833a4cd2ce2e8e0b
jchansen <jason_hansen@outlook.com>
2014-07-21 18:06:26 -0700

    style instance history to match image version style

--- troposphere/static/js/components/dashboard/InstanceHistoryList.react.js
+++ troposphere/static/js/components/dashboard/InstanceHistoryList.react.js
@@ -20,13 +20,35 @@ define(
 
         if(instanceHistories){
           content = instanceHistories.map(function (instance) {
+            var startDate = instance.get('start_date');
+            var endDate = instance.get('end_date');
+
+            var formattedStartDate = startDate.format("MMM DD, YYYY");
+            var formattedEndDate = endDate.format("MMM DD, YYYY");
+
+            var now = moment();
+            var timeSpan = now.diff(startDate, "days");
+
             return (
               <div key={instance.id}>
-                <strong>{instance.get('name')}</strong>
-                <ul>
-                  <li>{"start_date: " + instance.get('start_date').format("MMM DD, YYYY")}</li>
-                  <li>{"end_date: " + instance.get('end_date').format("MMM DD, YYYY")}</li>
-                </ul>
+                <div className="image-versions">
+                  <ul>
+                    <li>
+                      <div>
+                        <img className="image-version-image" src="http://placehold.it/63x63"/>
+                        <div className="image-version-details">
+                          <div className="version">
+                            <span>
+                              <strong>{instance.get('name')}</strong>
+                            </span>
+                          </div>
+                          <div>{formattedStartDate + " - " + formattedEndDate}</div>
+                          <div>{timeSpan + " days ago"}</div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
               </div>
             );
           }.bind(this));
