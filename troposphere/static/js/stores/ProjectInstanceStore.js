9e619dc2df2c5237b652cef92dd2330b67d36ab5
jchansen <jason_hansen@outlook.com>
2014-07-28 14:44:50 -0700

    remove unused functions

--- troposphere/static/js/stores/ProjectInstanceStore.js
+++ troposphere/static/js/stores/ProjectInstanceStore.js
@@ -16,58 +16,8 @@ define(
     var _isBeingFetched = {};
 
     //
-    // Project Instance Model
-    // Mainly a helper for generating add/remove urls
-    //
-
-    var ProjectInstance = Backbone.Model.extend({
-      urlRoot: function() {
-        return globals.API_ROOT + "/project/" + this.project.id + "/instance";
-      },
-
-      url: function () {
-        var url = Backbone.Model.prototype.url.apply(this) + globals.slash();
-        return url;
-      },
-
-      initialize: function(options){
-        this.instance = options.instance;
-        this.project = options.project;
-        this.set("id", this.instance.id);
-      }
-    });
-
-    var ProjectInstanceCollection = Backbone.Collection.extend({
-      model: Instance,
-
-      url: function () {
-        var url = globals.API_ROOT + "/project/" + this.project.id + "/instance" + globals.slash();
-        return url;
-      },
-
-      initialize: function(models, options){
-        this.project = options.project;
-      }
-    });
-
-    //
     // CRUD Operations
     //
-
-    var fetchProjectInstances = function (project) {
-      _isBeingFetched[project.id] = true;
-      var promise = new RSVP.Promise(function (resolve, reject) {
-        var projectInstances = new ProjectInstanceCollection(null, {
-          project: project
-        });
-        projectInstances.fetch().done(function () {
-          _isBeingFetched[project.id] = false;
-          _projectInstances[project.id] = projectInstances;
-          resolve();
-        });
-      });
-      return promise;
-    };
 
     function addInstanceToProject(instance, project){
       var projectInstance = new ProjectInstance({
@@ -108,20 +58,6 @@ define(
     //
 
     var ProjectInstanceStore = {
-
-//      getInstancesInProject: function (project) {
-//        var projectInstances = _projectInstances[project.id];
-//        var instancesAreBeingFetched = _isBeingFetched[project.id];
-//
-//        // If there are no instances for the project, and the instances aren't being fetched
-//        // already, then go fetch them, otherwise return the instances we already have
-//        if(!projectInstances && !instancesAreBeingFetched) {
-//          fetchProjectInstances(project).then(function(){
-//            ProjectInstanceStore.emitChange();
-//          }.bind(this));
-//        }
-//        return projectInstances;
-//      }
 
     };
 
