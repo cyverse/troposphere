789f06a6d19065a91b51886147387a3b264f2505
jchansen <jason_hansen@outlook.com>
2014-08-12 11:37:17 -0700

    refactor remaining instance actions

--- troposphere/static/js/actions/InstanceActions.js
+++ troposphere/static/js/actions/InstanceActions.js
@@ -2,16 +2,15 @@ define(
   [
     'dispatchers/AppDispatcher',
     'constants/InstanceConstants',
-    'react',
+    'constants/ProjectInstanceConstants',
+    'models/Instance',
     'globals',
     'context',
-    'controllers/NotificationController',
     'url',
     './modalHelpers/InstanceModalHelpers',
-    'constants/ProjectInstanceConstants',
-    'models/Instance'
+    'controllers/NotificationController'
   ],
-  function (AppDispatcher, InstanceConstants, React, globals, context, NotificationController, URL, InstanceModalHelpers, ProjectInstanceConstants, Instance) {
+  function (AppDispatcher, InstanceConstants, ProjectInstanceConstants, Instance, globals, context, URL, InstanceModalHelpers, NotificationController) {
 
     return {
 
@@ -29,55 +28,105 @@ define(
       // ------------------------
 
       updateInstanceAttributes: function (instance, newAttributes) {
+        var oldName = instance.get('name');
+        var oldTags = instance.get('tags');
+        var that = this;
 
-        // var update = function(instance){
-        //   instance.save({
-        //     name: instance.get('name'),
-        //     tags: instance.get('tags')
-        //   }, {
-        //     patch: true
-        //   }).done(function(){
-        //     var successMessage = "Instance " + instance.get('name') + " updated.";
-        //     //NotificationController.success(successMessage);
-        //     InstanceStore.emitChange();
-        //   }).fail(function(){
-        //     var failureMessage = "Error updating Instance " + instance.get('name') + ".";
-        //     NotificationController.error(failureMessage);
-        //     InstanceStore.emitChange();
-        //   });
-        // };
+        instance.set({
+          name: newAttributes.name,
+          tags: newAttributes.tags
+        });
 
-        instance.set(newAttributes);
-        AppDispatcher.handleRouteAction({
-          actionType: InstanceConstants.INSTANCE_UPDATE,
-          instance: instance
+        instance.save({
+          name: instance.get('name'),
+          tags: instance.get('tags')
+        }, {
+          patch: true
+        }).done(function () {
+          NotificationController.success(null, "Instance name and tags updated");
+          that.dispatch(InstanceConstants.UPDATE_INSTANCE, {instance: instance});
+        }).fail(function () {
+          var message = "Error updating Instance " + instance.get('name') + ".";
+          NotificationController.error(message);
+
+          instance.set({
+            name: oldName,
+            tags: oldTags
+          });
+          that.dispatch(InstanceConstants.UPDATE_INSTANCE, {instance: instance});
         });
       },
 
       addTagToInstance: function(tag, instance){
+        var instanceTags = instance.get('tags');
+        var that = this;
 
-        // var addTagToInstance = function(tag, instance){
-        //   var instanceTags = instance.get('tags');
-        //   instanceTags.push(tag.get('name'));
-        //   instance.save({
-        //     tags: instanceTags
-        //   }, {
-        //     patch: true
-        //   }).done(function(){
-        //     InstanceStore.emitChange();
-        //   }).fail(function(){
-        //     var failureMessage = "Error adding tag to Instance";
-        //     NotificationController.error(failureMessage);
-        //     InstanceStore.emitChange();
-        //   });
-        // };
+        instanceTags.push(tag.get('name'));
 
-        AppDispatcher.handleRouteAction({
-          actionType: InstanceConstants.INSTANCE_ADD_TAG,
-          tag: tag,
-          instance: instance
+        // instance.set({tags: instanceTags});
+        // that.dispatch(InstanceConstants.UPDATE_INSTANCE, {instance: instance});
+
+        instance.save({
+          tags: instanceTags
+        }, {
+          patch: true
+        }).done(function () {
+          that.dispatch(InstanceConstants.UPDATE_INSTANCE, {instance: instance});
+        }).fail(function () {
+          NotificationController.error(null, "Error adding tag to Instance");
+          that.dispatch(InstanceConstants.UPDATE_INSTANCE, {instance: instance});
         });
       },
+
+      _terminate: function(payload, options){
+        var instance = payload.instance;
+        var project = payload.project;
+        var that = this;
+
+        // todo: change instance state to show that it's being terminated
+
+        instance.destroy().done(function () {
+          NotificationController.success(null, 'Instance terminated');
+
+          that.dispatch(InstanceConstants.REMOVE_INSTANCE, {instance: instance});
+
+          // todo: the proper thing to do is to poll until the instance is actually terminated
+          // and THEN remove it from the project. Need to add a callback to support that.
+          // InstanceStore.pollUntilFinalState(instance);
+          that.dispatch(ProjectInstanceConstants.REMOVE_INSTANCE_FROM_PROJECT, {
+            instance: instance,
+            project: project
+          });
+
+        }).fail(function (response) {
+          NotificationController.error(null, 'Instance could not be terminated');
+          //that.dispatch(InstanceConstants.ADD_INSTANCE, {instance: instance});
+        });
+      },
+
+      terminate: function(payload, options){
+        var instance = payload.instance;
+        var redirectUrl = payload.redirectUrl;
+        var project = payload.project;
+        var that = this;
+
+        InstanceModalHelpers.terminate({
+          instance: instance
+        },{
+          onConfirm: function () {
+            that._terminate(payload, options);
+            if(redirectUrl) Backbone.history.navigate(redirectUrl, {trigger: true});
+          }
+        });
+      },
+
+      terminate_noModal: function(payload, options){
+        this._terminate(payload, options);
+      },
+
+      // ----------------
+      // Instance Actions
+      // ----------------
 
       suspend: function (instance) {
         var that = this;
@@ -163,72 +212,7 @@ define(
         });
       },
 
-      _terminate: function(payload, options){
-        var instance = payload.instance;
-        var project = payload.project;
-        var that = this;
-
-        // todo: change instance state to show that it's being terminated
-
-        instance.destroy().done(function () {
-          NotificationController.success(null, 'Instance terminated');
-
-          that.dispatch(InstanceConstants.REMOVE_INSTANCE, {instance: instance});
-
-          // todo: the proper thing to do is to poll until the instance is actually terminated
-          // and THEN remove it from the project. Need to add a callback to support that.
-          // InstanceStore.pollUntilFinalState(instance);
-          that.dispatch(ProjectInstanceConstants.REMOVE_INSTANCE_FROM_PROJECT, {
-            instance: instance,
-            project: project
-          });
-
-        }).fail(function (response) {
-          NotificationController.error(null, 'Instance could not be terminated');
-          //that.dispatch(InstanceConstants.ADD_INSTANCE, {instance: instance});
-        });
-      },
-
-      terminate: function(payload, options){
-        var instance = payload.instance;
-        var redirectUrl = payload.redirectUrl;
-        var project = payload.project;
-        var that = this;
-
-        InstanceModalHelpers.terminate({
-          instance: instance
-        },{
-          onConfirm: function () {
-            that._terminate(payload, options);
-            if(redirectUrl) Backbone.history.navigate(redirectUrl, {trigger: true});
-          }
-        });
-      },
-
-      terminate_noModal: function(payload, options){
-        this._terminate(payload, options);
-      },
-
       launch: function(application){
-
-
-
-        // var launch_AddToProject = function(identity, machineId, sizeId, instanceName, project){
-        //   launch(identity, machineId, sizeId, instanceName, {
-        //     afterCreate: function(instance){
-        //       _pendingProjectInstances[project.id] = _pendingProjectInstances[project.id] || new InstanceCollection();
-        //       _pendingProjectInstances[project.id].add(instance);
-        //     },
-        //     afterSave: function(instance){
-        //       _pendingProjectInstances[project.id].remove(instance);
-        //       ProjectActions.addItemToProject(project, instance);
-        //     },
-        //     afterSaveError: function(instance){
-        //       _pendingProjectInstances[project.id].remove(instance);
-        //     }
-        //   })
-        // };
-
         var that = this;
 
         InstanceModalHelpers.launch({
@@ -295,7 +279,8 @@ define(
             NotificationController.info(null, "An image of your instance has been requested");
           },
           error: function (response, status, error) {
-            NotificationController.error(null, response.responseText);
+            console.log(response.responseText);
+            NotificationController.error(null, "An image of your instance could not be requested");
           }
         });
       },
@@ -332,12 +317,13 @@ define(
           dataType: 'json',
           contentType: 'application/json',
           success: function (model) {
-            NotificationController.info(null, "Your instance problems have been sent to support.");
+            NotificationController.info(null, "Your instance report has been sent to support.");
             var instanceUrl = URL.instance(instance);
             Backbone.history.navigate(instanceUrl, {trigger: true});
           },
           error: function (response, status, error) {
-            NotificationController.error(null, response.responseText);
+            console.log(response.responseText);
+            NotificationController.error(null, "Your instance report could not be sent to support");
           }
         });
       }
