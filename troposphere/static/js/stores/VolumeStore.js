d194c0c051d2654c1e931694b467f44425aeb615
jchansen <jason_hansen@outlook.com>
2014-08-11 21:36:22 -0700

    fix formatting

--- troposphere/static/js/stores/VolumeStore.js
+++ troposphere/static/js/stores/VolumeStore.js
@@ -1,184 +1,179 @@
 define(function (require) {
-    'use strict';
+  'use strict';
 
-    //
-    // Dependencies
-    // ------------
-    //
+  //
+  // Dependencies
+  // ------------
+  //
 
   var _ = require('underscore'),
       Dispatcher = require('dispatchers/Dispatcher'),
       Store = require('stores/Store'),
-      Q = require('q'),
       VolumeCollection = require('collections/VolumeCollection'),
       Volume = require('models/Volume'),
       VolumeConstants = require('constants/VolumeConstants'),
-      NotificationController = require('controllers/NotificationController'),
-      IdentityStore = require('stores/IdentityStore'),
-      VolumeAttachNotifications = require('components/notifications/VolumeAttachNotifications.react'),
-      ProjectActions = require('actions/ProjectActions');
+      NotificationController = require('controllers/NotificationController');
 
+  //
+  // Private variables
+  //
 
-    //
-    // Private variables
-    //
+  var _volumes = new VolumeCollection();
+  var _isFetching = false;
+  var validStates = ["available", "in-use", "error_deleting"];
+  var pollingFrequency = 5 * 1000;
 
-    var _volumes = new VolumeCollection();
-    var _isFetching = false;
-    var validStates = ["available", "in-use", "error_deleting"];
-    var pollingFrequency = 5*1000;
+  //
+  // CRUD Operations
+  //
 
-    //
-    // CRUD Operations
-    //
+  function add(volume) {
+    _volumes.add(volume);
+  }
 
-    function add(volume){
-      _volumes.add(volume);
+  function update(volume) {
+    var existingModel = _volumes.get(volume);
+    if (!existingModel) throw new Error("Volume doesn't exist.");
+    _volumes.add(volume, {merge: true});
+    pollUntilBuildIsFinished(volume);
+  }
+
+  function remove(volume) {
+    _volumes.remove(volume);
+  }
+
+  //
+  // Polling functions
+  //
+
+  var _volumesBuilding = [];
+  var pollUntilBuildIsFinished = function (volume) {
+    return;
+    if (volume.id) {
+      _volumesBuilding.push(volume);
+      fetchAndRemoveIfFinished(volume);
     }
+  };
 
-    function update(volume){
-      var existingModel = _volumes.get(volume);
-      if(!existingModel) throw new Error("Volume doesn't exist.");
-      _volumes.add(volume, {merge: true});
-      pollUntilBuildIsFinished(volume);
-    }
-
-    function remove(volume){
-      _volumes.remove(volume);
-    }
-
-    //
-    // Polling functions
-    //
-
-    var _volumesBuilding = [];
-    var pollUntilBuildIsFinished = function(volume){
-      return;
-      if(volume.id) {
-        _volumesBuilding.push(volume);
-        fetchAndRemoveIfFinished(volume);
-      }
-    };
-
-    var fetchAndRemoveIfFinished = function(volume){
-      return;
-      setTimeout(function(){
-        volume.fetch().done(function(){
-          var index = _volumesBuilding.indexOf(volume);
-          if(validStates.indexOf(volume.get("status")) >= 0){
-            _volumesBuilding.slice(index, 1);
-          }else{
-            fetchAndRemoveIfFinished(volume);
-          }
-          VolumeStore.emitChange();
-        });
-      }, pollingFrequency);
-    };
-
-    // The pollNow functions poll immediately and then cycle
-    // as opposed to waiting for the delay and THEN polling
-    var pollNowUntilBuildIsFinished = function(volume){
-      return;
-      if(volume.id && _volumesBuilding.indexOf(volume) < 0) {
-        _volumesBuilding.push(volume);
-        fetchNowAndRemoveIfFinished(volume);
-      }
-    };
-
-    var fetchNowAndRemoveIfFinished = function(volume){
-      return;
-      volume.fetch().done(function(){
+  var fetchAndRemoveIfFinished = function (volume) {
+    return;
+    setTimeout(function () {
+      volume.fetch().done(function () {
         var index = _volumesBuilding.indexOf(volume);
-        if(volume.get('state').isInFinalState()){
+        if (validStates.indexOf(volume.get("status")) >= 0) {
           _volumesBuilding.slice(index, 1);
-        }else{
+        } else {
           fetchAndRemoveIfFinished(volume);
         }
         VolumeStore.emitChange();
       });
-    };
+    }, pollingFrequency);
+  };
 
-    //
-    // Volume Store
-    //
+  // The pollNow functions poll immediately and then cycle
+  // as opposed to waiting for the delay and THEN polling
+  var pollNowUntilBuildIsFinished = function (volume) {
+    return;
+    if (volume.id && _volumesBuilding.indexOf(volume) < 0) {
+      _volumesBuilding.push(volume);
+      fetchNowAndRemoveIfFinished(volume);
+    }
+  };
 
-    var VolumeStore = {
-
-      getAll: function (projects) {
-        if(!projects) throw new Error("Must supply projects");
-
-        projects.each(function(project){
-          this.getVolumesInProject(project);
-        }.bind(this));
-
-        return _volumes;
-      },
-
-      getVolumeInProject: function(project, volumeId){
-        var volumes = this.getVolumesInProject(project);
-        var volume = volumes.get(volumeId);
-        if(!volume){
-          NotificationController.error("Volume not in project", "The volume could not be found in the project");
-        }
-        return volume;
-      },
-
-      getVolumesInProject: function (project) {
-
-        var projectVolumeArray = project.get('volumes').map(function(volumeData){
-          // todo: we're converting into a volume object here so we can use
-          // id instead of alias for consistency. Eventually all alias attributes
-          // need to be renamed id and then we can create the object only if
-          // the id isn't in the existing map.
-          var volume = new Volume(volumeData, {parse: true});
-          var existingVolume = _volumes.get(volume);
-
-          if(existingVolume){
-            volume = existingVolume;
-          }else{
-            _volumes.push(volume);
-            pollNowUntilBuildIsFinished(volume);
-          }
-
-          return volume;
-        });
-
-        return new VolumeCollection(projectVolumeArray);
+  var fetchNowAndRemoveIfFinished = function (volume) {
+    return;
+    volume.fetch().done(function () {
+      var index = _volumesBuilding.indexOf(volume);
+      if (volume.get('state').isInFinalState()) {
+        _volumesBuilding.slice(index, 1);
+      } else {
+        fetchAndRemoveIfFinished(volume);
       }
-
-    };
-
-    Dispatcher.register(function (dispatch) {
-      var actionType = dispatch.action.actionType;
-      var payload = dispatch.action.payload;
-      var options = dispatch.action.options || options;
-
-      switch (actionType) {
-
-        case VolumeConstants.ADD_VOLUME:
-          add(payload.volume);
-          break;
-
-        case VolumeConstants.UPDATE_VOLUME:
-          update(payload.volume);
-          break;
-
-        case VolumeConstants.REMOVE_VOLUME:
-          remove(payload.volume);
-          break;
-
-        default:
-          return true;
-      }
-
-      if(!options.silent) {
-        VolumeStore.emitChange();
-      }
-
-      return true;
+      VolumeStore.emitChange();
     });
+  };
 
-    _.extend(VolumeStore, Store);
+  //
+  // Volume Store
+  //
 
-    return VolumeStore;
+  var VolumeStore = {
+
+    getAll: function (projects) {
+      if (!projects) throw new Error("Must supply projects");
+
+      projects.each(function (project) {
+        this.getVolumesInProject(project);
+      }.bind(this));
+
+      return _volumes;
+    },
+
+    getVolumeInProject: function (project, volumeId) {
+      var volumes = this.getVolumesInProject(project);
+      var volume = volumes.get(volumeId);
+      if (!volume) {
+        NotificationController.error("Volume not in project", "The volume could not be found in the project");
+      }
+      return volume;
+    },
+
+    getVolumesInProject: function (project) {
+
+      var projectVolumeArray = project.get('volumes').map(function (volumeData) {
+        // todo: we're converting into a volume object here so we can use
+        // id instead of alias for consistency. Eventually all alias attributes
+        // need to be renamed id and then we can create the object only if
+        // the id isn't in the existing map.
+        var volume = new Volume(volumeData, {parse: true});
+        var existingVolume = _volumes.get(volume);
+
+        if (existingVolume) {
+          volume = existingVolume;
+        } else {
+          _volumes.push(volume);
+          pollNowUntilBuildIsFinished(volume);
+        }
+
+        return volume;
+      });
+
+      return new VolumeCollection(projectVolumeArray);
+    }
+
+  };
+
+  Dispatcher.register(function (dispatch) {
+    var actionType = dispatch.action.actionType;
+    var payload = dispatch.action.payload;
+    var options = dispatch.action.options || options;
+
+    switch (actionType) {
+
+      case VolumeConstants.ADD_VOLUME:
+        add(payload.volume);
+        break;
+
+      case VolumeConstants.UPDATE_VOLUME:
+        update(payload.volume);
+        break;
+
+      case VolumeConstants.REMOVE_VOLUME:
+        remove(payload.volume);
+        break;
+
+      default:
+        return true;
+    }
+
+    if (!options.silent) {
+      VolumeStore.emitChange();
+    }
+
+    return true;
   });
+
+  _.extend(VolumeStore, Store);
+
+  return VolumeStore;
+});
