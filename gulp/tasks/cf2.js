6b16fed14996688f46c41c5c3890d360428f54ac
jchansen <jason_hansen@outlook.com>
2014-08-14 23:15:37 -0700

    configure cf2 task to work with rjs build step

--- gulp/tasks/cf2.js
+++ gulp/tasks/cf2.js
@@ -11,11 +11,12 @@
 
 var paths = require('../paths');
 var notify = require('gulp-notify');
+var gutil = require('gulp-util');
 
 module.exports = function (gulp) {
 
   gulp.task('cf2', function (done) {
-    var dest = 'troposphere/assets/resources';
+    var dest = (gutil.env.type === 'production' ? '.tmp/resources' : 'troposphere/assets/resources');
 
     return gulp.src(paths.cf2)
       .pipe(gulp.dest(dest))
