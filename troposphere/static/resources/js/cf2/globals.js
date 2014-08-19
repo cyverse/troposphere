dc854cc90a7786dce5c8ed40856b33c450c943e0
Jason Hansen <jason_hansen@outlook.com>
2014-08-19 11:13:34 -0700

    modify API_ROOT to use server code is running on unless developing on localhost

--- troposphere/static/resources/js/cf2/globals.js
+++ troposphere/static/resources/js/cf2/globals.js
@@ -1,1 +1,5 @@
-Atmo.API_ROOT = 'https://atmobeta.iplantc.org:443/api/v1';
+if(window.location.hostname === 'localhost'){
+  Atmo.API_ROOT = '/api/v1';
+}else{
+  Atmo.API_ROOT = 'https://atmobeta.iplantc.org:443/api/v1';
+}
