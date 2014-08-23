b783b7bda5cc6ebe1bd83a3aeffeecd58c72ca4b
jchansen <jason_hansen@outlook.com>
2014-08-22 17:26:41 -0700

    allow users to move null resources into an existing project

--- troposphere/static/js/actions/NullProjectActions.js
+++ troposphere/static/js/actions/NullProjectActions.js
@@ -35,11 +35,33 @@ define(
       // Standard CRUD Operations
       // ------------------------
 
+      _migrateResourceIntoProject: function(resource, project){
+        ProjectActions.addResourceToProject(resource, project);
+
+        if(resource instanceof Instance){
+          this.dispatch(NullProjectInstanceConstants.REMOVE_INSTANCE_FROM_NULL_PROJECT, {
+            instance: resource
+          });
+        }else if(resource instanceof Volume){
+          this.dispatch(NullProjectVolumeConstants.REMOVE_VOLUME_FROM_NULL_PROJECT, {
+            volume: resource
+          });
+        }
+      },
+
+      _migrateResourcesIntoProject: function(resources, project){
+        resources.map(function(resource){
+          this._migrateResourceIntoProject(resource, project);
+        }.bind(this));
+
+        var redirectUrl = URL.project(project, {relative: true});
+        Backbone.history.navigate(redirectUrl, {trigger: true});
+      },
+
       migrateResourcesIntoProject: function (nullProject) {
         var that = this;
         var instances = InstanceStore.getInstancesInProject(nullProject);
         var volumes = VolumeStore.getVolumesInProject(nullProject);
-
 
         var resources = new Backbone.Collection();
         instances.each(function(instance){
@@ -53,46 +75,38 @@ define(
           nullProject: nullProject,
           resources: resources
         },{
-          onConfirm: function(projectName){
-
-            var project = new Project({
-              name: projectName,
-              description: projectName,
-              instances: [],
-              volumes:[]
-            });
+          onConfirm: function(params){
 
             var resourcesClone = resources.models.slice(0);
+            var project;
 
-            that.dispatch(ProjectConstants.ADD_PROJECT, {project: project});
-
-            project.save().done(function(){
-              NotificationController.success(null, "Project " + project.get('name') + " created.");
-              that.dispatch(ProjectConstants.UPDATE_PROJECT, {project: project});
-
-              resourcesClone.map(function(resource){
-                ProjectActions.addResourceToProject(resource, project);
-
-                if(resource instanceof Instance){
-                  that.dispatch(NullProjectInstanceConstants.REMOVE_INSTANCE_FROM_NULL_PROJECT, {
-                    instance: resource
-                  });
-                }else if(resource instanceof Volume){
-                  that.dispatch(NullProjectVolumeConstants.REMOVE_VOLUME_FROM_NULL_PROJECT, {
-                    volume: resource
-                  });
-                }
+            if(params.projectName){
+              project = new Project({
+                name: params.projectName,
+                description: params.projectName,
+                instances: [],
+                volumes:[]
               });
 
-              var redirectUrl = URL.project(project, {relative: true});
-              Backbone.history.navigate(redirectUrl, {trigger: true});
+              that.dispatch(ProjectConstants.ADD_PROJECT, {project: project});
 
-            }).fail(function(){
-              var message = "Error creating Project " + project.get('name') + ".";
-              NotificationController.error(null, message);
-              that.dispatch(ProjectConstants.REMOVE_PROJECT, {project: project});
-            });
+              project.save().done(function(){
+                NotificationController.success(null, "Project " + project.get('name') + " created.");
+                that.dispatch(ProjectConstants.UPDATE_PROJECT, {project: project});
+                that._migrateResourcesIntoProject(resourcesClone, project);
+              }).fail(function(){
+                var message = "Error creating Project " + project.get('name') + ".";
+                NotificationController.error(null, message);
+                that.dispatch(ProjectConstants.REMOVE_PROJECT, {project: project});
+              });
 
+            }else if(params.projectId && params.projects){
+              project = params.projects.get(params.projectId);
+              that._migrateResourcesIntoProject(resourcesClone, project);
+
+            }else{
+              throw new Error("expected either projectName OR projectId and projects parameters")
+            }
           }
         });
       }
--- troposphere/static/js/components/modals/NullProjectMigrateResourceModal.react.js
+++ troposphere/static/js/components/modals/NullProjectMigrateResourceModal.react.js
@@ -4,9 +4,11 @@ define(
   [
     'react',
     'backbone',
-    'components/mixins/BootstrapModalMixin.react'
+    'components/mixins/BootstrapModalMixin.react',
+    'components/modals/instance_launch/ProjectSelect.react',
+    'stores/ProjectStore'
   ],
-  function (React, Backbone, BootstrapModalMixin) {
+  function (React, Backbone, BootstrapModalMixin, ProjectSelect, ProjectStore) {
 
     // Example Usage from http://bl.ocks.org/insin/raw/8449696/
     // render: function(){
@@ -38,13 +40,55 @@ define(
 
       propTypes: {
         dateTimeStamp: React.PropTypes.string.isRequired,
-        resources: React.PropTypes.instanceOf(Backbone.Collection).isRequired
+        resources: React.PropTypes.instanceOf(Backbone.Collection).isRequired,
+        projects: React.PropTypes.instanceOf(Backbone.Collection).isRequired
       },
 
+      //
+      // Mounting & State
+      // ----------------
+      //
+
       getInitialState: function () {
-        return {
-          projectName: this.props.dateTimeStamp
+        var initialState = {
+          projectName: "", //this.props.dateTimeStamp,
+          projects: ProjectStore.getAll(),
+          projectId: null
         };
+
+        if(initialState.projects && initialState.projects.length > 0){
+          initialState.projectId = initialState.projects.first().id;
+        }
+
+        return initialState;
+      },
+
+      getState: function(){
+        var state = {
+          projectName: this.state.projectName,
+          projects: ProjectStore.getAll(),
+          projectId: this.state.projectId
+        };
+
+        if(!state.projectId){
+          if(state.projects && state.projects.length > 0){
+            state.projectId = state.projects.first().id;
+          }
+        }
+
+        return state;
+      },
+
+      updateState: function () {
+        if (this.isMounted()) this.setState(this.getState());
+      },
+
+      componentDidMount: function () {
+        ProjectStore.addChangeListener(this.updateState);
+      },
+
+      componentWillUnmount: function () {
+        ProjectStore.removeChangeListener(this.updateState);
       },
 
       //
@@ -58,7 +102,17 @@ define(
 
       confirm: function () {
         this.hide();
-        this.props.onConfirm(this.state.projectName);
+        if(this.state.projectName){
+          this.props.onConfirm({
+            projectName: this.state.projectName
+          });
+        }else{
+          this.props.onConfirm({
+            projectId: this.state.projectId,
+            projects: this.state.projects
+          });
+        }
+
       },
 
       //
@@ -70,79 +124,112 @@ define(
         this.setState({projectName: e.target.value});
       },
 
+      onProjectChange: function (e) {
+        this.setState({projectId: e.target.value});
+      },
+
       //
       // Render
       // ------
       //
 
       render: function () {
-        var buttonArray = [
-          {type: 'danger', text: 'Cancel', handler: this.cancel},
-          {type: 'primary', text: this.props.confirmButtonMessage, handler: this.confirm}
-        ];
+        var content;
 
-        var buttons = buttonArray.map(function (button) {
-          // Enable all buttons be default
-          var isDisabled = false;
+        if(this.state.projects) {
+          var buttonArray = [
+            {type: 'danger', text: 'Cancel', handler: this.cancel},
+            {type: 'primary', text: this.props.confirmButtonMessage, handler: this.confirm}
+          ];
 
-          // Disable the launch button if the user hasn't provided a name, size or identity for the volume
-          var stateIsValid = this.state.projectName;
-          if(button.type === "primary" && !stateIsValid ) isDisabled = true;
+          var buttons = buttonArray.map(function (button) {
+            // Enable all buttons be default
+            var isDisabled = false;
+
+            // Disable the launch button if the user hasn't provided a name, size or identity for the volume
+            var stateIsValid = !!this.state.projectName || !!this.state.projectId;
+            if (button.type === "primary" && !stateIsValid) isDisabled = true;
+
+            return (
+              <button key={button.text} type="button" className={'btn btn-' + button.type} onClick={button.handler} disabled={isDisabled}>
+                {button.text}
+              </button>
+              );
+          }.bind(this));
+
+          var resourceNames = this.props.resources.map(function (resource) {
+            return (
+              <li key={resource.id}>{resource.get('name')}</li>
+              );
+          });
+
+          var body = "Looks like you have resources that aren't in a project. Would you like to migrate them? " +
+            "This will create a new project called '" + this.props.dateTimeStamp +
+            "' and move all of these resources into that project.";
+
+          var projectSelection;
+          if (this.state.projects.length > 0) {
+            projectSelection = (
+              <div className='form-group'>
+                <p>{"Alternately, you may also move the resources into one of your existing projects by selecting one from the dropdown below."}</p>
+                <ProjectSelect projectId={this.state.projectId}
+                projects={this.state.projects}
+                onChange={this.onProjectChange}
+                />
+              </div>
+              );
+          }
+
+          content = (
+            <form role='form'>
+
+              <div className='form-group'>
+                <p>{"Looks like you have some resources that aren't in a project! Would you like to migrate the following resources into a project so that you can interact with them?"}</p>
+                <ul>
+                  {resourceNames}
+                </ul>
+              </div>
+
+              <div className='form-group'>
+                <label htmlFor='project'>Project</label>
+                <p>{"If you accept, a new project will be created with the following name. Feel free to change it to something more meaningful, and you can always rename the project later."}</p>
+                <input type="text"
+                       className="form-control"
+                       value={this.state.projectName}
+                       onChange={this.onProjectNameChange}
+                       placeholder="New project name..."
+                />
+              </div>
+
+              {projectSelection}
+
+            </form>
+            );
+        }else {
+          content = (
+            <div className="loading"></div>
+          );
+        }
 
           return (
-            <button key={button.text} type="button" className={'btn btn-' + button.type} onClick={button.handler} disabled={isDisabled}>
-              {button.text}
-            </button>
-          );
-        }.bind(this));
-
-        var resourceNames = this.props.resources.map(function(resource){
-          return (
-            <li key={resource.id}>{resource.get('name')}</li>
-          );
-        });
-
-        var body = "Looks like you have resources that aren't in a project. Would you like to migrate them? " +
-                   "This will create a new project called '" + this.props.dateTimeStamp +
-                   "' and move all of these resources into that project.";
-
-        var content = (
-          <form role='form'>
-
-            <div className='form-group'>
-              <p>{"Looks like you have some resources that aren't in a project! Would you like to migrate the following resources into a project so that you can interact with them?"}</p>
-              <ul>
-                {resourceNames}
-              </ul>
-            </div>
-
-            <div className='form-group'>
-              <label>Project Name</label>
-              <p>{"If you accept, a new project will be created with the following name. Feel free to change it to something more meaningful, and you can always rename the project later."}</p>
-              <input type='text' className='form-control' value={this.state.projectName} onChange={this.onProjectNameChange}/>
-            </div>
-
-          </form>
-        );
-
-        return (
-          <div className="modal fade">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  {this.renderCloseButton()}
-                  <strong>{this.props.header}</strong>
-                </div>
-                <div className="modal-body">
-                  {content}
-                </div>
-                <div className="modal-footer">
-                  {buttons}
+            <div className="modal fade">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    {this.renderCloseButton()}
+                    <strong>{this.props.header}</strong>
+                  </div>
+                  <div className="modal-body">
+                    {content}
+                  </div>
+                  <div className="modal-footer">
+                    {buttons}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
+          );
+
       }
 
     });
