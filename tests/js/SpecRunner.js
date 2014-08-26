bc73a03bef399cd379451c93fd65f3dc4ebd3781
jchansen <jason_hansen@outlook.com>
2014-08-25 18:44:09 -0700

    first component test passing

--- tests/js/SpecRunner.js
+++ tests/js/SpecRunner.js
@@ -1,14 +1,36 @@
 require.config({
-  baseUrl: './js',
+  baseUrl: '../../troposphere/troposphere/assets/js',
+
   paths: {
-    react: '../../troposphere/static/bower_components/react/react-with-addons',
-    components: '../../troposphere/static/js/components',
-    chai: "../../node_modules/chai/chai",
-    mocha: "../../node_modules/mocha/mocha"
+    react: '../bower_components/react/react-with-addons',
+    jquery: '../bower_components/jquery/dist/jquery',
+    underscore: '../bower_components/underscore/underscore',
+    backbone: '../bower_components/backbone/backbone',
+    moment: '../bower_components/moment/moment',
+    q: '../bower_components/q/q',
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
+
+    specs: '../../../tests/js/specs',
+
+    chai: "../../../node_modules/chai/chai",
+    mocha: "../../../node_modules/mocha/mocha",
+    sinon: "../../../node_modules/sinon/lib/sinon"
+    //jsx: '../../troposphere/static/bower_components/require-jsx/jsx',
+    //JSXTransformer: '../../troposphere/static/bower_components/react/JSXTransformer'
   },
   shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
     mocha: {
       exports: 'mocha'
+    },
+    bootstrap: {
+      deps: ['jquery']
     }
   }
 });
--- tests/js/specs/react.js
+++ tests/js/specs/react.js
@@ -1,9 +1,24 @@
 define(function (require) {
 
   var React = require("react");
+  var $ = require("jquery");
+  var TestUtils = React.addons.TestUtils;
+  var Header = require("components/Header.react");
+  var sinon = require('sinon');
+  var Profile = require('models/Profile');
 
   describe('React Test', function () {
-    it("should pass");
+    it("should pass", function () {
+
+      var profile = new Profile({username: "testName"});
+
+      var header = TestUtils.renderIntoDocument(
+        Header({profile: profile, currentRoute: "projects"})
+      );
+
+      var dropdown = $(header.getDOMNode()).find(".dropdown-toggle > span");
+      dropdown.text().should.equal("testName");
+    });
   });
 
 });
