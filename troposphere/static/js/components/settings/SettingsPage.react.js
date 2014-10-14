bcbcaf40d05967dbc6e94fed9d98f552d2202ad1
jchansen <jason_hansen@outlook.com>
2014-10-14 11:25:01 -0700

    fix bad checkbox styling

--- troposphere/static/js/components/settings/SettingsPage.react.js
+++ troposphere/static/js/components/settings/SettingsPage.react.js
@@ -64,7 +64,7 @@ define(
             <div className="container">
               <div className="notifications">
                 <h3>Notifications</h3>
-                <div className="checkbox">
+                <div>
                   <input type="checkbox" checked={wantsEmails} onChange={this.onChangeEmailPreference}/> Receive an email notification when an instance finishes launching
                 </div>
               </div>
