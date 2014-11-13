53bb31382ca43b8d47a50c9133f382eb09e8f673
jchansen <jason_hansen@outlook.com>
2014-11-12 23:19:35 -0700

    add karma for testing

---
+++ karma.conf.js
@@ -0,0 +1,82 @@
+// Karma configuration
+// Generated on Tue Nov 11 2014 21:52:40 GMT-0700 (MST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'requirejs'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      //{pattern: '.tmp/public/js/**/*.js', included: false},
+      //{pattern: '.tmp/public/bower_components/**/*.js', included: false},
+      {pattern: 'troposphere/assets/bower_components/react/react-with-addons.js', included: false},
+      {pattern: 'troposphere/assets/bower_components/backbone/backbone.js', included: false},
+      {pattern: 'troposphere/assets/bower_components/underscore/underscore.js', included: false},
+      {pattern: 'troposphere/assets/bower_components/jquery/dist/jquery.js', included: false},
+      {pattern: 'troposphere/assets/bower_components/bootstrap/dist/js/bootstrap.min.js', included: false},
+
+      //{pattern: 'node_modules/.bin/*.js', included: false},
+
+      {pattern: 'troposphere/assets/js/**/*.js', included: false},
+
+      {pattern: 'test/**/*.spec.js', included: false},
+      'test/test-main.js'
+    ],
+
+    // list of files to exclude
+    exclude: [
+      //'.tmp/public/js/main.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
---
+++ test/app.spec.js
@@ -0,0 +1,17 @@
+define(['react', 'backbone'], function(React, Backbone) {
+
+  var TestUtils;
+
+  describe('Header', function() {
+
+    beforeEach(function() {
+      TestUtils = React.addons.TestUtils;
+    });
+
+    it("should have 3 links", function () {
+
+    });
+
+  });
+
+});
