75e9df6340ce1590feacbc52ef5a9813327c06a1
jchansen <jason_hansen@outlook.com>
2014-11-14 11:28:18 -0700

    more project tests

--- test/components/projects/list/projectListView.spec.js
+++ test/components/projects/list/projectListView.spec.js
@@ -5,8 +5,9 @@ define(
     'components/projects/list/ProjectListView.react',
     'components/projects/common/ProjectListHeader.react',
     'test/fixtures/projects.fixture',
-    'actions'
-  ], function(React, Backbone, ProjectListView, ProjectListHeader, projectCollectionFixture, actions) {
+    'actions',
+    'stores'
+  ], function(React, Backbone, ProjectListView, ProjectListHeader, projectCollectionFixture, actions, stores) {
 
     var TestUtils,
         projectViewElement;
@@ -18,6 +19,9 @@ define(
 
         var listView = ProjectListView({projects: projectCollectionFixture});
         projectViewElement = TestUtils.renderIntoDocument(listView);
+
+        for(var action in actions) delete actions[action];
+        for(var store in stores) delete stores[store];
       });
 
       it("should have a header", function(){
@@ -26,23 +30,23 @@ define(
       });
 
       describe("create project button", function(){
-        iit("should exist", function(){
+        it("should exist", function(){
           var headerElement = TestUtils.scryRenderedComponentsWithType(projectViewElement, ProjectListHeader);
           expect(headerElement.length).toBe(1);
         });
 
         it("should have read 'Create New Project'", function(){
           var button = TestUtils.findRenderedDOMComponentWithTag(projectViewElement, "button");
-          expect(button.textContent).toBe("Create New Project");
+          expect(button.getDOMNode().textContent).toBe("Create New Project");
         });
 
         it("should open the new project modal when clicked", function(){
+          actions.ProjectActions = {
+            create: sinon.spy()
+          };
           var button = TestUtils.findRenderedDOMComponentWithTag(projectViewElement, "button");
           TestUtils.Simulate.click(button);
-
-
-
-          expect(button.textContent).toBe("Create New Project");
+          expect(actions.ProjectActions.create.calledOnce).toBe(true);
         });
       });
 
