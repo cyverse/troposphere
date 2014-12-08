4ace398665051297b9019469ed722d65859cad92
jchansen <jason_hansen@outlook.com>
2014-12-08 16:42:04 -0700

    add missing file

---
+++ troposphere/static/js/components/projects/detail/details/ProjectDetailsView.react.js
@@ -0,0 +1,92 @@
+/** @jsx React.DOM */
+
+define(
+  [
+    'react',
+    'jquery',
+    'backbone',
+    'components/projects/common/SecondaryProjectNavigation.react',
+    'actions',
+    './InputField.react',
+    './HtmlTextAreaField.react',
+    './ViewDetails.react',
+    './EditDetails.react'
+  ],
+  function (React, $, Backbone, SecondaryProjectNavigation, actions, InputField, TextAreaField, ViewDetails, EditDetails) {
+
+    return React.createClass({
+
+      propTypes: {
+        project: React.PropTypes.instanceOf(Backbone.Model).isRequired
+      },
+
+      getInitialState: function(){
+        return {
+          isEditing: false
+        }
+      },
+
+      onSaveDescription: function(description){
+        actions.ProjectActions.updateProjectAttributes(this.props.project, {description: description})
+      },
+
+      handleCancel: function(){
+        this.setState({isEditing: false})
+      },
+
+      handleSave: function(){
+        this.setState({isEditing: false})
+      },
+
+      handleEdit: function(){
+        this.setState({isEditing: true})
+      },
+
+      // ------
+      // Render
+      // ------
+
+      renderDetailsView: function(project){
+        return (
+          <div className="container">
+            <div className="row edit-link-row">
+              <a className="edit-link" onClick={this.handleEdit}>
+                Edit details
+              </a>
+            </div>
+            <ViewDetails project={project}/>
+          </div>
+        )
+      },
+
+      renderEditDetailsView: function(project){
+        return (
+          <div className="container">
+            <EditDetails project={project}/>
+            <button onClick={this.handleCancel}>Cancel</button>
+            <button onClick={this.handleSave}>Save</button>
+          </div>
+        )
+      },
+
+      render: function () {
+        var project = this.props.project,
+            view;
+
+        if(this.state.isEditing){
+          view = this.renderEditDetailsView(project);
+        }else{
+          view = this.renderDetailsView(project);
+        }
+
+        return (
+          <div className="project-details">
+            <SecondaryProjectNavigation project={this.props.project} currentRoute="details"/>
+            {view}
+          </div>
+        );
+      }
+
+    });
+
+  });
