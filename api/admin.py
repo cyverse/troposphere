2f98f6da475b30789b3c8501c42b81592b1d8138
jchansen <jason_hansen@outlook.com>
2015-02-11 01:37:18 -0700

    add UserToken and UserPreferences models to /admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import MaintenanceRecord
+from .models import MaintenanceRecord, UserToken, UserPreferences
 
 # Register your models here.
 admin.site.register(MaintenanceRecord)
+admin.site.register(UserToken)
+admin.site.register(UserPreferences)
