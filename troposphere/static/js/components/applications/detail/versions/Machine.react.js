2e18918d62e00a2f7579cf3935823f8082e2185d
jchansen <jason_hansen@outlook.com>
2015-02-16 21:00:41 -0700

    bug fix: ATMO-660, hide invalid date machines
    I’m actually leaving this as is, see comment in code as to why

--- troposphere/static/js/components/applications/detail/versions/Machine.react.js
+++ troposphere/static/js/components/applications/detail/versions/Machine.react.js
@@ -18,12 +18,25 @@ define(
       },
 
       render: function () {
-        console.log(this.props.machine.id);
+        var machine = this.props.machine;
+
+        // If the machine doesn't exist, don't render anything.  This happens for some
+        // instances but should probably be looked into on the server side
+        // https://pods.iplantcollaborative.org/jira/browse/ATMO-660
+        // todo: even when I hide this, there are still two options that show up in the
+        // launch modal for versions.  I think this is fundamentally broken and needs to
+        // be re-examined on the server side.
+        //if(!machine.id) {
+        //  return (
+        //    <div></div>
+        //  )
+        //}
+
         // todo: figure out if anything is ever recommended, or if it's just a concept idea
         var isRecommended = false;
-        var dateCreated = this.props.machine.get('start_date').format("M/DD/YYYY");
+        var dateCreated = machine.get('start_date').format("M/DD/YYYY");
 
-        var machineHash = CryptoJS.MD5(this.props.machine.id).toString();
+        var machineHash = CryptoJS.MD5(machine.id).toString();
         var iconSize = 63;
         var type = stores.ProfileStore.get().get('icon_set');
 
@@ -33,11 +46,11 @@ define(
               <Gravatar hash={machineHash} size={iconSize} type={type}/>
               <div className="image-version-details">
                 <div className="version">
-                  {this.props.machine.get('pretty_version')}
+                  {machine.get('pretty_version')}
                   {isRecommended ? <span className="recommended-tag">Recommended</span> : null}
                 </div>
                 <div>{dateCreated}</div>
-                <div>{this.props.machine.get('ownerid')}</div>
+                <div>{machine.get('ownerid')}</div>
               </div>
             </div>
           </li>
