6e69aa4a4a949a8f089842cb6bbb54a76851934a
jchansen <jason_hansen@outlook.com>
2015-02-18 17:15:13 -0700

    bug fix: created tags added to instance
    https://pods.iplantcollaborative.org/jira/browse/ATMO-725

--- troposphere/static/js/actions/TagActions.js
+++ troposphere/static/js/actions/TagActions.js
@@ -4,11 +4,11 @@ define(
     'dispatchers/AppDispatcher',
     'constants/TagConstants',
     'models/Tag',
-    'actions/InstanceActions',
+    'actions',
     'components/modals/ModalHelpers',
     'components/modals/tag/TagCreateModal.react'
   ],
-  function (React, AppDispatcher, TagConstants, Tag, InstanceActions, ModalHelpers, TagCreateModal) {
+  function (React, AppDispatcher, TagConstants, Tag, actions, ModalHelpers, TagCreateModal) {
 
     return {
 
@@ -63,7 +63,7 @@ define(
           tag.save().done(function () {
             this.dispatch(TagConstants.UPDATE_TAG, {tag: tag}, {silent: true});
             this.dispatch(TagConstants.REMOVE_PENDING_TAG_FROM_INSTANCE, {tag: tag, instance: instance});
-            InstanceActions.addTagToInstance(tag, instance);
+            actions.InstanceActions.addTagToInstance(tag, instance);
           }.bind(this)).fail(function () {
             this.dispatch(TagConstants.REMOVE_TAG, {tag: tag}, {silent: true});
             this.dispatch(TagConstants.REMOVE_PENDING_TAG_FROM_INSTANCE, {tag: tag, instance: instance});
