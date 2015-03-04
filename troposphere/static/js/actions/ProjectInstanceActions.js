91c99589103369f6dd56a3ffc9c5544f0f91c5a1
jchansen <jason_hansen@outlook.com>
2015-03-03 22:49:54 -0700

    can migrate instances and volumes into projects again

--- troposphere/static/js/actions/ProjectInstanceActions.js
+++ troposphere/static/js/actions/ProjectInstanceActions.js
@@ -1,82 +1,63 @@
-define(
-  [
-    'react',
-    'dispatchers/AppDispatcher',
-    'constants/ProjectConstants',
-    'constants/ProjectInstanceConstants',
-    'stores/helpers/ProjectInstance',
-    './Utils'
-  ],
-  function (React, AppDispatcher, ProjectConstants, ProjectInstanceConstants, ProjectInstance, Utils) {
+define(function (require) {
 
-    var _isParanoid = false;
+  var AppDispatcher = require('dispatchers/AppDispatcher'),
+      ProjectConstants = require('constants/ProjectConstants'),
+      ProjectInstanceConstants = require('constants/ProjectInstanceConstants'),
+      ProjectInstance = require('models/ProjectInstance'),
+      Utils = require('./Utils');
 
-    return {
+  var _isParanoid = false;
 
-      // ----------------------------
-      // Add/Remove Project Resources
-      // ----------------------------
+  return {
 
-      addInstanceToProject: function(instance, project, options){
-        var that = this;
+    // ----------------------------
+    // Add/Remove Project Resources
+    // ----------------------------
 
-        var projectInstance = new ProjectInstance({
-          instance: instance,
-          project: project
-        });
+    addInstanceToProject: function(instance, project, options){
+      var projectInstance = new ProjectInstance(),
+          data = {
+            project: project.id,
+            instance: instance.id
+          };
 
-        Utils.dispatch(ProjectInstanceConstants.ADD_INSTANCE_TO_PROJECT, {
-          instance: instance,
-          project: project
-        }, options);
+      projectInstance.save(null, { attrs: data }).done(function(){
+        Utils.dispatch(ProjectInstanceConstants.ADD_INSTANCE_TO_PROJECT, projectInstance, options);
+      });
+    },
 
-        projectInstance.save().done(function(){
-          // re-fetch the project to make sure the change was also made on the server
-          if(_isParanoid) {
-            project.fetch().then(function () {
-              Utils.dispatch(ProjectConstants.UPDATE_PROJECT, {project: project});
-            });
-          }
-        }).fail(function(){
-          Utils.dispatch(ProjectInstanceConstants.REMOVE_INSTANCE_FROM_PROJECT, {
-            instance: instance,
-            project: project
+    removeInstanceFromProject: function(instance, project, options){
+      var that = this;
+
+      var projectInstance = new ProjectInstance({
+        instance: instance,
+        project: project
+      });
+
+      Utils.dispatch(ProjectInstanceConstants.REMOVE_INSTANCE_FROM_PROJECT, {
+        instance: instance,
+        project: project
+      }, options);
+
+      projectInstance.destroy().done(function(){
+        // re-fetch the project to make sure the change was also made on the server
+        if(_isParanoid) {
+          project.fetch().then(function () {
+            Utils.dispatch(ProjectConstants.UPDATE_PROJECT, {project: project});
           });
-        });
-      },
+        }
+      }).fail(function(){
+        var warning = "API says instance wasn't removed from project, but is likely " +
+                      "lying. False false bug. This message is here until PAG is over.";
+        console.warn(warning);
 
-      removeInstanceFromProject: function(instance, project, options){
-        var that = this;
+        //Utils.dispatch(ProjectInstanceConstants.ADD_INSTANCE_TO_PROJECT, {
+        //  instance: instance,
+        //  project: project
+        //});
+      });
+    }
 
-        var projectInstance = new ProjectInstance({
-          instance: instance,
-          project: project
-        });
+  };
 
-        Utils.dispatch(ProjectInstanceConstants.REMOVE_INSTANCE_FROM_PROJECT, {
-          instance: instance,
-          project: project
-        }, options);
-
-        projectInstance.destroy().done(function(){
-          // re-fetch the project to make sure the change was also made on the server
-          if(_isParanoid) {
-            project.fetch().then(function () {
-              Utils.dispatch(ProjectConstants.UPDATE_PROJECT, {project: project});
-            });
-          }
-        }).fail(function(){
-          var warning = "API says instance wasn't removed from project, but is likely " +
-                        "lying. False false bug. This message is here until PAG is over.";
-          console.warn(warning);
-
-          //Utils.dispatch(ProjectInstanceConstants.ADD_INSTANCE_TO_PROJECT, {
-          //  instance: instance,
-          //  project: project
-          //});
-        });
-      }
-
-    };
-
-  });
+});
--- troposphere/static/js/actions/ProjectVolumeActions.js
+++ troposphere/static/js/actions/ProjectVolumeActions.js
@@ -1,81 +1,62 @@
-define(
-  [
-    'react',
-    'dispatchers/AppDispatcher',
-    'constants/ProjectConstants',
-    'constants/ProjectVolumeConstants',
-    'stores/helpers/ProjectVolume',
-    './Utils'
-  ],
-  function (React, AppDispatcher, ProjectConstants, ProjectVolumeConstants, ProjectVolume, Utils) {
+define(function (require) {
 
-    var _isParanoid = false;
+  var AppDispatcher = require('dispatchers/AppDispatcher'),
+      ProjectConstants = require('constants/ProjectConstants'),
+      ProjectVolumeConstants = require('constants/ProjectVolumeConstants'),
+      ProjectVolume = require('models/ProjectVolume'),
+      Utils = require('./Utils');
 
-    return {
+  var _isParanoid = false;
 
-      // -------------------------
-      // Add/Remove Project Volume
-      // -------------------------
+  return {
 
-      addVolumeToProject: function(volume, project, options){
-        var that = this;
+    // -------------------------
+    // Add/Remove Project Volume
+    // -------------------------
 
-        var projectVolume = new ProjectVolume({
-          volume: volume,
-          project: project
-        });
+    addVolumeToProject: function(volume, project, options){
+      var projectVolume = new ProjectVolume(),
+          data = {
+            project: project.id,
+            volume: volume.id
+          };
 
-        Utils.dispatch(ProjectVolumeConstants.ADD_VOLUME_TO_PROJECT, {
-          volume: volume,
-          project: project
-        }, options);
+      projectVolume.save(null, {attrs: data}).done(function(){
+        Utils.dispatch(ProjectVolumeConstants.ADD_VOLUME_TO_PROJECT, projectVolume, options);
+      })
+    },
 
-        projectVolume.save().done(function(){
-          // re-fetch the project to make sure the change was also made on the server
-          if(_isParanoid) {
-            project.fetch().then(function () {
-              Utils.dispatch(ProjectConstants.UPDATE_PROJECT, {project: project});
-            });
-          }
-        }).fail(function(){
-          Utils.dispatch(ProjectVolumeConstants.REMOVE_VOLUME_FROM_PROJECT, {
-            volume: volume,
-            project: project
+    removeVolumeFromProject: function(volume, project, options){
+      var that = this;
+
+      var projectVolume = new ProjectVolume({
+        volume: volume,
+        project: project
+      });
+
+      Utils.dispatch(ProjectVolumeConstants.REMOVE_VOLUME_FROM_PROJECT, {
+        volume: volume,
+        project: project
+      }, options);
+
+      projectVolume.destroy().done(function(){
+        // re-fetch the project to make sure the change was also made on the server
+        if(_isParanoid) {
+          project.fetch().then(function () {
+            Utils.dispatch(ProjectConstants.UPDATE_PROJECT, {project: project});
           });
-        });
-      },
+        }
+      }).fail(function(){
+        var warning = "API says volume wasn't removed from project, but is likely " +
+                      "lying. False false bug. This message is here until PAG is over.";
+        console.warn(warning);
+        //Utils.dispatch(ProjectVolumeConstants.ADD_VOLUME_TO_PROJECT, {
+        //  volume: volume,
+        //  project: project
+        //});
+      });
+    }
 
-      removeVolumeFromProject: function(volume, project, options){
-        var that = this;
+  };
 
-        var projectVolume = new ProjectVolume({
-          volume: volume,
-          project: project
-        });
-
-        Utils.dispatch(ProjectVolumeConstants.REMOVE_VOLUME_FROM_PROJECT, {
-          volume: volume,
-          project: project
-        }, options);
-
-        projectVolume.destroy().done(function(){
-          // re-fetch the project to make sure the change was also made on the server
-          if(_isParanoid) {
-            project.fetch().then(function () {
-              Utils.dispatch(ProjectConstants.UPDATE_PROJECT, {project: project});
-            });
-          }
-        }).fail(function(){
-          var warning = "API says volume wasn't removed from project, but is likely " +
-                        "lying. False false bug. This message is here until PAG is over.";
-          console.warn(warning);
-          //Utils.dispatch(ProjectVolumeConstants.ADD_VOLUME_TO_PROJECT, {
-          //  volume: volume,
-          //  project: project
-          //});
-        });
-      }
-
-    };
-
-  });
+});
