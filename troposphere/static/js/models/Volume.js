473417af18cc83057d5b6a9a4dec12814c05f440
jchansen <jason_hansen@outlook.com>
2015-03-08 13:24:32 -0700

    default volume status to "Unknown"

--- troposphere/static/js/models/Volume.js
+++ troposphere/static/js/models/Volume.js
@@ -11,17 +11,15 @@ define(
     return Backbone.Model.extend({
       urlRoot: globals.API_V2_ROOT + "/volumes",
 
-      parse: function (response) {
-
-        var attributes = response;
-
+      parse: function (attributes) {
         attributes.start_date = new Date(attributes.start_date);
         attributes.state = new VolumeState({status_raw: attributes.status});
+        attributes.status = attributes.status || "Unknown";
 
-        if (response.attach_data && response.attach_data.instance_alias) {
+        if (attributes.attach_data && attributes.attach_data.instance_alias) {
           attributes.attach_data = {
-            device: response.attach_data.device,
-            instance_id: response.attach_data.instance_alias
+            device: attributes.attach_data.device,
+            instance_id: attributes.attach_data.instance_alias
           };
         }else{
           attributes.attach_data = {
