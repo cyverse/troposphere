3c7e12c783acbc0e794793cca45d781400f553ae
jchansen <jason_hansen@outlook.com>
2015-03-08 13:31:51 -0700

    switch to commonJS format

--- troposphere/static/js/models/Volume.js
+++ troposphere/static/js/models/Volume.js
@@ -1,38 +1,36 @@
 define(function (require) {
 
-    var Backbone = require('backbone'),
-        _ = require('underscore'),
-        globals = require('globals'),
-        Instance = require('models/Instance'),
-        VolumeState = require('./VolumeState');
+  var Backbone = require('backbone'),
+      globals = require('globals'),
+      VolumeState = require('./VolumeState');
 
-    return Backbone.Model.extend({
-      urlRoot: globals.API_V2_ROOT + "/volumes",
+  return Backbone.Model.extend({
+    urlRoot: globals.API_V2_ROOT + "/volumes",
 
-      parse: function (attributes) {
-        attributes.start_date = new Date(attributes.start_date);
-        attributes.state = new VolumeState({status_raw: attributes.status});
-        attributes.status = attributes.status || "Unknown";
+    parse: function (attributes) {
+      attributes.start_date = new Date(attributes.start_date);
+      attributes.state = new VolumeState({status_raw: attributes.status});
+      attributes.status = attributes.status || "Unknown";
 
-        if (attributes.attach_data && attributes.attach_data.instance_alias) {
-          attributes.attach_data = {
-            device: attributes.attach_data.device,
-            instance_id: attributes.attach_data.instance_alias
-          };
-        }else{
-          attributes.attach_data = {
-            device: null,
-            instance_id: null
-          };
-        }
-
-        return attributes;
-      },
-
-      isAttached: function () {
-        return this.get('status') == 'in-use' || this.get('status') == 'detaching';
+      if (attributes.attach_data && attributes.attach_data.instance_alias) {
+        attributes.attach_data = {
+          device: attributes.attach_data.device,
+          instance_id: attributes.attach_data.instance_alias
+        };
+      }else{
+        attributes.attach_data = {
+          device: null,
+          instance_id: null
+        };
       }
 
-    });
+      return attributes;
+    },
+
+    isAttached: function () {
+      return this.get('status') == 'in-use' || this.get('status') == 'detaching';
+    }
 
   });
+
+});
