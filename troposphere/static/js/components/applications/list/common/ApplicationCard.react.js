804c3952e6af6282cc05d19a47d722f9a90cf4dc
jchansen <jason_hansen@outlook.com>
2015-03-08 23:22:20 -0700

    commonJS formatting

--- troposphere/static/js/components/applications/list/common/ApplicationCard.react.js
+++ troposphere/static/js/components/applications/list/common/ApplicationCard.react.js
@@ -1,83 +1,79 @@
-/** @jsx React.DOM */
+define(function (require) {
 
-define(
-  [
-    'react',
-    'components/common/Gravatar.react',
-    'backbone',
-    'url',
-    '../../common/Bookmark.react',
-    'context',
-    '../../detail/tags/Tags.react',
-    'stores',
-    './ApplicationCardDescription.react',
-    'moment'
-  ],
-  function (React, Gravatar, Backbone, URL, Bookmark, context, Tags, stores, ApplicationCardDescription, moment) {
+  var React = require('react'),
+      Gravatar = require('components/common/Gravatar.react'),
+      Backbone = require('backbone'),
+      URL = require('url'),
+      Bookmark = require('../../common/Bookmark.react'),
+      context = require('context'),
+      Tags = require('../../detail/tags/Tags.react'),
+      stores = require('stores'),
+      ApplicationCardDescription = require('./ApplicationCardDescription.react'),
+      moment = require('moment');
 
-    return React.createClass({
+  return React.createClass({
 
-      propTypes: {
-        application: React.PropTypes.instanceOf(Backbone.Model).isRequired,
-        tags: React.PropTypes.instanceOf(Backbone.Collection).isRequired
-      },
+    propTypes: {
+      application: React.PropTypes.instanceOf(Backbone.Model).isRequired,
+      tags: React.PropTypes.instanceOf(Backbone.Collection).isRequired
+    },
 
-      onAppClick: function (e) {
-        e.preventDefault();
-        var url = URL.application(this.props.application);
-        Backbone.history.navigate(url, {trigger: true});
-      },
+    onAppClick: function (e) {
+      e.preventDefault();
+      var url = URL.application(this.props.application);
+      Backbone.history.navigate(url, {trigger: true});
+    },
 
-      render: function () {
-        var app = this.props.application;
-        var type = stores.ProfileStore.get().get('icon_set');
-        var imageTags = stores.TagStore.getImageTags(app);
-        var applicationCreationDate = moment(app.get('start_date')).format("MMM D, YYYY");
+    render: function () {
+      var app = this.props.application;
+      var type = stores.ProfileStore.get().get('icon_set');
+      var imageTags = stores.TagStore.getImageTags(app);
+      var applicationCreationDate = moment(app.get('start_date')).format("MMM D, YYYY");
 
-        var iconSize = 145;
-        var icon;
-        if (app.get('icon')) {
-          icon = (
-            <img src={app.get('icon')} width={iconSize} height={iconSize}/>
-          );
-        } else {
-          icon = (
-            <Gravatar hash={app.get('uuid_hash')} size={iconSize} type={type}/>
-          );
-        }
-
-        var appUri = URL.application(app);
-
-        // Hide bookmarking on the public page
-        var bookmark;
-        if(context.profile){
-          bookmark = (
-            <Bookmark application={app}/>
-          );
-        }
-
-        return (
-          <div className='app-card'>
-            <div className='icon-container'>
-              <a href={appUri} onClick={this.onAppClick}>
-                {icon}
-              </a>
-            </div>
-            <div className='app-name'>
-              <a href={appUri}>{app.get('name')}</a>
-            </div>
-            <div className="creation-details">
-              <time>{applicationCreationDate}</time> by <strong>{app.get('created_by').username}</strong>
-            </div>
-            {bookmark}
-            <Tags activeTags={imageTags}
-                  tags={this.props.tags}
-            />
-            <ApplicationCardDescription application={app}/>
-          </div>
+      var iconSize = 145;
+      var icon;
+      if (app.get('icon')) {
+        icon = (
+          <img src={app.get('icon')} width={iconSize} height={iconSize}/>
+        );
+      } else {
+        icon = (
+          <Gravatar hash={app.get('uuid_hash')} size={iconSize} type={type}/>
         );
       }
 
-    });
+      var appUri = URL.application(app);
+
+      // Hide bookmarking on the public page
+      var bookmark;
+      if(context.profile){
+        bookmark = (
+          <Bookmark application={app}/>
+        );
+      }
+
+      return (
+        <div className='app-card'>
+          <div className='icon-container'>
+            <a href={appUri} onClick={this.onAppClick}>
+              {icon}
+            </a>
+          </div>
+          <div className='app-name'>
+            <a href={appUri}>{app.get('name')}</a>
+          </div>
+          <div className="creation-details">
+            <time>{applicationCreationDate}</time> by <strong>{app.get('created_by').username}</strong>
+          </div>
+          {bookmark}
+          <Tags activeTags={imageTags}
+                tags={this.props.tags}
+          />
+          <ApplicationCardDescription application={app}/>
+        </div>
+      );
+    }
 
   });
+
+});
--- troposphere/static/js/components/applications/list/common/ApplicationListCard.react.js
+++ troposphere/static/js/components/applications/list/common/ApplicationListCard.react.js
@@ -1,89 +1,85 @@
-/** @jsx React.DOM */
+define(function (require) {
 
-define(
-  [
-    'react',
-    'components/common/Gravatar.react',
-    'backbone',
-    'url',
-    '../../common/Bookmark.react',
-    'context',
-    '../../detail/tags/Tags.react',
-    'stores',
-    'navigator',
-    'showdown',
-    'moment'
-  ],
-  function (React, Gravatar, Backbone, URL, Bookmark, context, Tags, stores, navigator, Showdown, moment) {
+  var React = require('react'),
+      Gravatar = require('components/common/Gravatar.react'),
+      Backbone = require('backbone'),
+      URL = require('url'),
+      Bookmark = require('../../common/Bookmark.react'),
+      context = require('context'),
+      Tags = require('../../detail/tags/Tags.react'),
+      stores = require('stores'),
+      navigator = require('navigator'),
+      Showdown = require('showdown'),
+      moment = require('moment');
 
-    return React.createClass({
+  return React.createClass({
 
-      propTypes: {
-        application: React.PropTypes.instanceOf(Backbone.Model).isRequired,
-        tags: React.PropTypes.instanceOf(Backbone.Collection).isRequired
-      },
+    propTypes: {
+      application: React.PropTypes.instanceOf(Backbone.Model).isRequired,
+      tags: React.PropTypes.instanceOf(Backbone.Collection).isRequired
+    },
 
-      onAppClick: function (e) {
-        e.preventDefault();
-        var url = URL.application(this.props.application);
-        navigator.navigateTo(url);
-      },
+    onAppClick: function (e) {
+      e.preventDefault();
+      var url = URL.application(this.props.application);
+      navigator.navigateTo(url);
+    },
 
-      render: function () {
-        var app = this.props.application,
-            type = stores.ProfileStore.get().get('icon_set'),
-            imageTags = stores.TagStore.getImageTags(app),
-            applicationCreationDate = moment(app.get('start_date')).format("MMM D, YYYY"),
-            converter = new Showdown.converter(),
-            description = app.get('description'),
-            descriptionHtml = converter.makeHtml(description);
+    render: function () {
+      var app = this.props.application,
+          type = stores.ProfileStore.get().get('icon_set'),
+          imageTags = stores.TagStore.getImageTags(app),
+          applicationCreationDate = moment(app.get('start_date')).format("MMM D, YYYY"),
+          converter = new Showdown.converter(),
+          description = app.get('description'),
+          descriptionHtml = converter.makeHtml(description);
 
-        var iconSize = 67;
-        var icon;
-        if (app.get('icon')) {
-          icon = (
-            <img src={app.get('icon')} width={iconSize} height={iconSize}/>
-          );
-        } else {
-          icon = (
-            <Gravatar hash={app.get('uuid_hash')} size={iconSize} type={type}/>
-          );
-        }
-
-        var appUri = URL.application(app);
-
-        // Hide bookmarking on the public page
-        var bookmark;
-        if(context.profile){
-          bookmark = (
-            <Bookmark application={app}/>
-          );
-        }
-
-        return (
-          <div className='app-card'>
-            <div>
-              <span className='icon-container'>
-                <a href={appUri} onClick={this.onAppClick}>
-                  {icon}
-                </a>
-              </span>
-              <span className='app-name'>
-                <h4>
-                  <a href={appUri}>{app.get('name')}</a>
-                </h4>
-                <div><time>{applicationCreationDate}</time> by <strong>{app.get('created_by').username}</strong></div>
-                <Tags activeTags={imageTags}
-                    tags={this.props.tags}
-                />
-              </span>
-            </div>
-            <div dangerouslySetInnerHTML={{__html: descriptionHtml}}/>
-            {bookmark}
-          </div>
+      var iconSize = 67;
+      var icon;
+      if (app.get('icon')) {
+        icon = (
+          <img src={app.get('icon')} width={iconSize} height={iconSize}/>
+        );
+      } else {
+        icon = (
+          <Gravatar hash={app.get('uuid_hash')} size={iconSize} type={type}/>
         );
       }
 
-    });
+      var appUri = URL.application(app);
+
+      // Hide bookmarking on the public page
+      var bookmark;
+      if(context.profile){
+        bookmark = (
+          <Bookmark application={app}/>
+        );
+      }
+
+      return (
+        <div className='app-card'>
+          <div>
+            <span className='icon-container'>
+              <a href={appUri} onClick={this.onAppClick}>
+                {icon}
+              </a>
+            </span>
+            <span className='app-name'>
+              <h4>
+                <a href={appUri}>{app.get('name')}</a>
+              </h4>
+              <div><time>{applicationCreationDate}</time> by <strong>{app.get('created_by').username}</strong></div>
+              <Tags activeTags={imageTags}
+                  tags={this.props.tags}
+              />
+            </span>
+          </div>
+          <div dangerouslySetInnerHTML={{__html: descriptionHtml}}/>
+          {bookmark}
+        </div>
+      );
+    }
 
   });
+
+});
