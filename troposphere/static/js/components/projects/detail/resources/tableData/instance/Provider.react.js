86ef2be3e546f7dd9e5a83d2ea941e4d23b7ea43
jchansen <jason_hansen@outlook.com>
2015-03-08 17:14:35 -0700

    commonJS refactor

---
+++ troposphere/static/js/components/projects/detail/resources/tableData/instance/Provider.react.js
@@ -0,0 +1,26 @@
+define(function (require) {
+
+  var React = require('react'),
+      Backbone = require('backbone'),
+      stores = require('stores');
+
+  return React.createClass({
+
+    propTypes: {
+      instance: React.PropTypes.instanceOf(Backbone.Model).isRequired
+    },
+
+    render: function () {
+      var instance = this.props.instance,
+          provider = stores.ProviderStore.get(instance.get('provider').id);
+
+      if(!provider) return <div className="loading"></div>;
+
+      return (
+        <span>{provider.get('name')}</span>
+      );
+    }
+
+  });
+
+});
