9fcacc73d30b10c969d0ed8b6bbff2d0c60c6dcd
prosif <prosif@gmail.com>
2015-03-13 14:23:23 -0700

    putting out html table of quota requests

--- troposphere/static/js/components/quota/QuotaMaster.react.js
+++ troposphere/static/js/components/quota/QuotaMaster.react.js
@@ -15,10 +15,43 @@ define(function (require) {
 
       if(!quotaRequests) return <div className="loading"></div>;
 
+      var jsonRequests = quotaRequests.toJSON();
+
       return (
         <div>
-            <h1>Quota!</h1>
-            <div>{quotaRequests.toJSON()}</div>
+            <h1>Quota Requests!</h1>
+            <div>
+                <table>
+                    <tr class = >
+                        <th>
+                            <h3>User</h3>
+                        </th>
+                        <th>
+                            <h3>Status</h3>
+                        </th>
+                        <th>
+                            <h3>Quota</h3>
+                        </th>
+                        <th>
+                            <h3>Request</h3>
+                        </th>
+                        <th>
+                            <h3>Description</h3>
+                        </th>
+                    </tr>
+                    {jsonRequests.map(function(item){
+                    return(
+                        <tr>
+                            <td className = "user-name">{item['created_by']}</td>
+                            <td className = "status">{item['status']}</td>
+                            <td className = "quota">{item['quota']}</td>
+                            <td className = "request">{item['request']}</td>
+                            <td className = "description">{item['description']}</td>
+                        </tr>
+                    );
+                    })}
+                </table>
+            </div>
             <RouteHandler/>
         </div>
       );
