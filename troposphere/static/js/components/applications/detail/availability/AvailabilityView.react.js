0350be4d162ebd25f06aeb379cf9230ada993eff
jchansen <jason_hansen@outlook.com>
2015-03-19 00:19:23 -0700

    commonJS formatting

--- troposphere/static/js/components/applications/detail/availability/AvailabilityView.react.js
+++ troposphere/static/js/components/applications/detail/availability/AvailabilityView.react.js
@@ -1,52 +1,48 @@
-/** @jsx React.DOM */
+define(function (require) {
 
-define(
-  [
-    'react',
-    'backbone',
-    'stores'
-  ],
-  function (React, Backbone, stores) {
+  var React = require('react'),
+      Backbone = require('backbone'),
+      stores = require('stores');
 
-    return React.createClass({
+  return React.createClass({
 
-      propTypes: {
-        application: React.PropTypes.instanceOf(Backbone.Model).isRequired
-      },
+    propTypes: {
+      application: React.PropTypes.instanceOf(Backbone.Model).isRequired
+    },
 
-      renderProvider: function(provider){
-        return (
-          <li key={provider.id}>
-            {provider.get('name')}
-          </li>
-        )
-      },
+    renderProvider: function(provider){
+      return (
+        <li key={provider.id}>
+          {provider.get('name')}
+        </li>
+      )
+    },
 
-      render: function () {
-        var image = this.props.application,
-            providers = image.get('machines').filter(function(machine){
-              // filter out providers that don't exist
-              var providerId = machine.get('provider').id,
-                  provider = stores.ProviderStore.get(machine.get('provider').id);
-              if(!provider) console.warn("Image " + image.id + " showing availability on non-existent provider " + providerId);
-              return provider;
-            }).map(function(machine){
-              // convert machine to providers
-              return stores.ProviderStore.get(machine.get('provider').id);
-            });
+    render: function () {
+      var image = this.props.application,
+          providers = image.get('machines').filter(function(machine){
+            // filter out providers that don't exist
+            var providerId = machine.get('provider').id,
+                provider = stores.ProviderStore.get(machine.get('provider').id);
+            if(!provider) console.warn("Image " + image.id + " showing availability on non-existent provider " + providerId);
+            return provider;
+          }).map(function(machine){
+            // convert machine to providers
+            return stores.ProviderStore.get(machine.get('provider').id);
+          });
 
-        return (
-          <div className='image-availability image-info-segment row'>
-            <h4 className="title col-md-2">Available on</h4>
-            <div className="content col-md-10">
-              <ul className="list-unstyled">
-                {providers.map(this.renderProvider)}
-              </ul>
-            </div>
+      return (
+        <div className='image-availability image-info-segment row'>
+          <h4 className="title col-md-2">Available on</h4>
+          <div className="content col-md-10">
+            <ul className="list-unstyled">
+              {providers.map(this.renderProvider)}
+            </ul>
           </div>
-        );
-      }
-
-    });
+        </div>
+      );
+    }
 
   });
+
+});
