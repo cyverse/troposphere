35ec9796183900b2fc5385e1d6342c6794bc3e93
jchansen <jason_hansen@outlook.com>
2015-03-19 00:39:01 -0700

    commonJS formatting

--- troposphere/static/js/components/projects/list/Project.react.js
+++ troposphere/static/js/components/projects/list/Project.react.js
@@ -1,76 +1,71 @@
-/** @jsx React.DOM */
+define(function (require) {
 
-define(
-  [
-    'react',
-    'backbone',
-    'moment',
-    'url',
-    './ProjectResource.react',
-    'showdown',
-    'react-router',
-    'stores'
-  ],
-  function (React, Backbone, moment, URL, ProjectResource, Showdown, Router, stores) {
+  var React = require('react'),
+      Backbone = require('backbone'),
+      stores = require('stores'),
+      Router = require('react-router'),
+      moment = require('moment'),
+      Showdown = require('showdown'),
+      ProjectResource = require('./ProjectResource.react');
 
-    return React.createClass({
+  return React.createClass({
 
-      propTypes: {
-        project: React.PropTypes.instanceOf(Backbone.Model).isRequired
-      },
+    propTypes: {
+      project: React.PropTypes.instanceOf(Backbone.Model).isRequired
+    },
 
-      render: function () {
-        var project = this.props.project,
-            converter = new Showdown.converter(),
-            description = project.get('description'),
-            descriptionHtml = converter.makeHtml(description),
-            projectInstances = stores.ProjectInstanceStore.getInstancesFor(project),
-            projectVolumes = stores.ProjectVolumeStore.getVolumesFor(project),
-            projectCreationDate;
+    render: function () {
+      var project = this.props.project,
+          converter = new Showdown.converter(),
+          description = project.get('description'),
+          descriptionHtml = converter.makeHtml(description),
+          projectInstances = stores.ProjectInstanceStore.getInstancesFor(project),
+          projectVolumes = stores.ProjectVolumeStore.getVolumesFor(project),
+          projectCreationDate;
 
-        if(!project.id || !projectInstances || !projectVolumes) {
-          return (
-            <li>
-              <a>
-                <div>
-                  <h2>{project.get('name')}</h2>
-                  <div className="loading" style={{"margin-top": "65px"}}/>
-                </div>
-              </a>
-            </li>
-          );
-        }
-
-        projectCreationDate = moment(project.get('start_date')).format("MMM D, YYYY");
-
+      if(!project.id || !projectInstances || !projectVolumes) {
         return (
-          <li className="project-card">
-            <Router.Link to="project-resources" params={{projectId: project.id}}>
-              <div style={{"position": "relative"}}>
-                <div className="content">
-                  <h2>{project.get('name')}</h2>
-                  <time>{"Created " + projectCreationDate}</time>
-                  <div className="description" dangerouslySetInnerHTML={{__html: descriptionHtml}}/>
-                </div>
-                <ul className="project-resource-list">
-                  <ProjectResource icon={"tasks"}
-                                   count={projectInstances.length}
-                                   resourceType={"instances"}
-                  />
-                  <ProjectResource icon={"hdd"}
-                                   count={projectVolumes.length}
-                                   resourceType={"volumes"}
-                  />
-                  <ProjectResource icon={"floppy-disk"}
-                                   count={0}
-                                   resourceType={"images"}
-                  />
-                </ul>
+          <li>
+            <a>
+              <div>
+                <h2>{project.get('name')}</h2>
+                <div className="loading" style={{"margin-top": "65px"}}/>
               </div>
-            </Router.Link>
+            </a>
           </li>
         );
       }
-    });
 
+      projectCreationDate = moment(project.get('start_date')).format("MMM D, YYYY");
+
+      return (
+        <li className="project-card">
+          <Router.Link to="project-resources" params={{projectId: project.id}}>
+            <div style={{"position": "relative"}}>
+              <div className="content">
+                <h2>{project.get('name')}</h2>
+                <time>{"Created " + projectCreationDate}</time>
+                <div className="description" dangerouslySetInnerHTML={{__html: descriptionHtml}}/>
+              </div>
+              <ul className="project-resource-list">
+                <ProjectResource icon={"tasks"}
+                                 count={projectInstances.length}
+                                 resourceType={"instances"}
+                />
+                <ProjectResource icon={"hdd"}
+                                 count={projectVolumes.length}
+                                 resourceType={"volumes"}
+                />
+                <ProjectResource icon={"floppy-disk"}
+                                 count={0}
+                                 resourceType={"images"}
+                />
+              </ul>
+            </div>
+          </Router.Link>
+        </li>
+      );
+    }
   });
+
+});
