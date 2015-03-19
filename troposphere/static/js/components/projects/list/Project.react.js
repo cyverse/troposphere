88a01947050ff4c9dc438335c6f7ef031a31f0d8
jchansen <jason_hansen@outlook.com>
2015-03-19 00:36:38 -0700

    add correct resource count to project cards

--- troposphere/static/js/components/projects/list/Project.react.js
+++ troposphere/static/js/components/projects/list/Project.react.js
@@ -8,9 +8,10 @@ define(
     'url',
     './ProjectResource.react',
     'showdown',
-    'react-router'
+    'react-router',
+    'stores'
   ],
-  function (React, Backbone, moment, URL, ProjectResource, Showdown, Router) {
+  function (React, Backbone, moment, URL, ProjectResource, Showdown, Router, stores) {
 
     return React.createClass({
 
@@ -22,53 +23,53 @@ define(
         var project = this.props.project,
             converter = new Showdown.converter(),
             description = project.get('description'),
-            descriptionHtml = converter.makeHtml(description);
+            descriptionHtml = converter.makeHtml(description),
+            projectInstances = stores.ProjectInstanceStore.getInstancesFor(project),
+            projectVolumes = stores.ProjectVolumeStore.getVolumesFor(project),
+            projectCreationDate;
 
-        if(project.id){
-          var projectUrl = URL.projectResources({project: project});
-          var projectCreationDate = moment(project.get('start_date')).format("MMM D, YYYY");
-
-          return (
-            <li className="project-card">
-              <Router.Link to="project-resources" params={{projectId: project.id}}>
-                <div style={{"position": "relative"}}>
-                  <div className="content">
-                    <h2>{project.get('name')}</h2>
-                    <time>{"Created " + projectCreationDate}</time>
-                    <div className="description" dangerouslySetInnerHTML={{__html: descriptionHtml}}/>
-                  </div>
-                  <ul className="project-resource-list">
-                    <ProjectResource icon={"tasks"}
-                                     count={project.get('instances').length}
-                                     resourceType={"instances"}
-                    />
-                    <ProjectResource icon={"hdd"}
-                                     count={project.get('volumes').length}
-                                     resourceType={"volumes"}
-                    />
-                    <ProjectResource icon={"floppy-disk"}
-                                     count={project.get('images').length}
-                                     resourceType={"images"}
-                    />
-                  </ul>
-                </div>
-              </Router.Link>
-            </li>
-          );
-
-        }else{
+        if(!project.id || !projectInstances || !projectVolumes) {
           return (
             <li>
               <a>
                 <div>
                   <h2>{project.get('name')}</h2>
-                  <time>{"Creating..."}</time>
+                  <div className="loading" style={{"margin-top": "65px"}}/>
                 </div>
               </a>
             </li>
           );
         }
 
+        projectCreationDate = moment(project.get('start_date')).format("MMM D, YYYY");
+
+        return (
+          <li className="project-card">
+            <Router.Link to="project-resources" params={{projectId: project.id}}>
+              <div style={{"position": "relative"}}>
+                <div className="content">
+                  <h2>{project.get('name')}</h2>
+                  <time>{"Created " + projectCreationDate}</time>
+                  <div className="description" dangerouslySetInnerHTML={{__html: descriptionHtml}}/>
+                </div>
+                <ul className="project-resource-list">
+                  <ProjectResource icon={"tasks"}
+                                   count={projectInstances.length}
+                                   resourceType={"instances"}
+                  />
+                  <ProjectResource icon={"hdd"}
+                                   count={projectVolumes.length}
+                                   resourceType={"volumes"}
+                  />
+                  <ProjectResource icon={"floppy-disk"}
+                                   count={0}
+                                   resourceType={"images"}
+                  />
+                </ul>
+              </div>
+            </Router.Link>
+          </li>
+        );
       }
     });
 
