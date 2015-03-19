adff34ec47abac2ebb241ef591af3e8b871fbbed
jchansen <jason_hansen@outlook.com>
2015-03-18 23:53:21 -0700

    commonJS formatting

--- troposphere/static/js/components/applications/ApplicationListPage.react.js
+++ troposphere/static/js/components/applications/ApplicationListPage.react.js
@@ -1,53 +1,25 @@
-/** @jsx React.DOM */
+define(function (require) {
 
-define(
-  [
-    'react',
-    './common/SecondaryApplicationNavigation.react',
-    'collections/ApplicationCollection',
-    './list/SearchContainer.react',
-    'stores',
-    './list/ApplicationListView.react'
-  ],
-  function (React, SecondaryApplicationNavigation, ApplicationCollection, ApplicationSearch, stores, ApplicationListView) {
+  var React = require('react'),
+      stores = require('stores'),
+      ApplicationListView = require('./list/ApplicationListView.react');
 
-    function getState() {
-      return {
-        applications: stores.ApplicationStore.getAll(),
-        tags: stores.TagStore.getAll(),
-        isLoadingMoreResults: false
-      };
+  return React.createClass({
+
+    render: function () {
+      var images = stores.ApplicationStore.getAll(),
+          tags = stores.TagStore.getAll();
+
+      if(!images || !tags) return <div className="loading"></div>;
+
+      return (
+        <ApplicationListView
+          applications={images}
+          tags={tags}
+        />
+      );
     }
 
-    return React.createClass({
-
-      getInitialState: function () {
-        return getState();
-      },
-
-      updateState: function () {
-        if (this.isMounted()) this.setState(getState());
-      },
-
-      componentDidMount: function () {
-        stores.ApplicationStore.addChangeListener(this.updateState);
-        stores.TagStore.addChangeListener(this.updateState);
-      },
-
-      componentWillUnmount: function () {
-        stores.ApplicationStore.removeChangeListener(this.updateState);
-        stores.TagStore.removeChangeListener(this.updateState);
-      },
-
-      render: function () {
-        return (
-          <ApplicationListView
-            applications={this.state.applications}
-            tags={this.state.tags}
-          />
-        );
-      }
-
-    });
-
   });
+
+});
