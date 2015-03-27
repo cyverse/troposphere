b7c1b435b9c2063c63efc1ce187985e1bd67fc81
jchansen <jason_hansen@outlook.com>
2015-03-26 17:19:44 -0700

    return expired token error and make DRYer

--- troposphere/auth_backends.py
+++ troposphere/auth_backends.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from caslib import OAuthClient as CAS_OAuthClient
 from api.models import UserToken
 from django.contrib.auth.models import User
-from rest_framework.authentication import BaseAuthentication
+from rest_framework import authentication, exceptions
 
 
 logger = logging.getLogger(__name__)
@@ -14,9 +14,7 @@ cas_oauth_client = CAS_OAuthClient(settings.CAS_SERVER,
                                    auth_prefix=settings.CAS_AUTH_PREFIX)
 
 
-def create_user_token_from_access_token(access_token):
-    profile = cas_oauth_client.get_profile(access_token=access_token)
-
+def create_user_token_from_cas_profile(profile, access_token):
     profile_dict = dict()
     profile_dict['username'] = profile['id']
     for attr in profile['attributes']:
@@ -48,22 +46,8 @@ class OAuthLoginBackend(object):
 
         except UserToken.DoesNotExist:
             profile = cas_oauth_client.get_profile(access_token=access_token)
-
-            profile_dict = dict()
-            profile_dict['username'] = profile['id']
-            for attr in profile['attributes']:
-                key = attr.keys()[0]
-                value = attr[key]
-                profile_dict[key] = value
-
-            user, created = User.objects.get_or_create(username=profile_dict['username'])
-            user.first_name = profile_dict['firstName']
-            user.last_name = profile_dict['lastName']
-            user.email = profile_dict['email']
-            user.is_staff = ('staff' in profile_dict['entitlement'])
-            user.save()
-
-            user_token = UserToken.objects.create(token=access_token, user=user)
+            # todo: handle [profile.get('error') = 'expired_accessToken'] error
+            user_token = create_user_token_from_cas_profile(profile, access_token)
 
         user = user_token.user
         return user
@@ -78,7 +62,7 @@ class OAuthLoginBackend(object):
             return None
 
 
-class OAuthTokenLoginBackend(BaseAuthentication):
+class OAuthTokenLoginBackend(authentication.BaseAuthentication):
     """
     CAS OAuth Authentication Backend
 
@@ -97,7 +81,13 @@ class OAuthTokenLoginBackend(BaseAuthentication):
             user_token = UserToken.objects.get(token=access_token)
 
         except UserToken.DoesNotExist:
-            user_token = create_user_token_from_access_token(access_token)
+            profile = cas_oauth_client.get_profile(access_token=access_token)
+            error = profile.get('error')
+
+            if error:
+                raise exceptions.AuthenticationFailed(error)
+
+            user_token = create_user_token_from_cas_profile(profile, access_token)
 
         user = user_token.user
-        return (user_token.user, user_token)
+        return (user, user_token)
