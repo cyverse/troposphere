f963ba7c6042fffd5094af16d7508ff64859c9ad
Evan Briones <erbriones@gmail.com>
2015-05-13 14:50:43 -0700

    add allocation collection
    	new file:   troposphere/static/js/collections/AllocationCollection.js

---
+++ troposphere/static/js/collections/AllocationCollection.js
@@ -0,0 +1,23 @@
+define(function (require) {
+  "use strict";
+
+  var Backbone = require('backbone'),
+      Allocation = require('models/Allocation'),
+      globals = require('globals');
+
+  return Backbone.Collection.extend({
+    model: Allocation,
+    url: globals.API_V2_ROOT + "/allocations",
+
+    parse: function (response) {
+      this.meta = {
+        count: response.count,
+        next: response.next,
+        previous: response.previous
+     };
+
+      return response.results;
+    }
+  });
+
+});
