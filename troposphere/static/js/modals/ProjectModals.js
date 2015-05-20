f48d7ab4ea65d38f32cabc65036b9142e66ac17d
prosif <prosif@gmail.com>
2015-05-20 10:46:27 -0700

    added moveResources to ProjectModals.js

--- troposphere/static/js/modals/ProjectModals.js
+++ troposphere/static/js/modals/ProjectModals.js
@@ -3,7 +3,9 @@ define(function (require) {
   return {
     create: require('./project/create').create,
     destroy: require('./project/destroy').destroy,
-    explainProjectDeleteConditions: require('./project/explainProjectDeleteConditions').explainProjectDeleteConditions
+    explainProjectDeleteConditions: require('./project/explainProjectDeleteConditions').explainProjectDeleteConditions,
+    moveResources: require('./project/moveResources').moveResources
+
   };
 
 });
