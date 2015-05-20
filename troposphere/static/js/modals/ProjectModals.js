deafca18538b05054a2f23f14a4a82296326ec81
prosif <prosif@gmail.com>
2015-05-20 11:41:38 -0700

    added removeResources to ProjectModals

--- troposphere/static/js/modals/ProjectModals.js
+++ troposphere/static/js/modals/ProjectModals.js
@@ -4,7 +4,8 @@ define(function (require) {
     create: require('./project/create').create,
     destroy: require('./project/destroy').destroy,
     explainProjectDeleteConditions: require('./project/explainProjectDeleteConditions').explainProjectDeleteConditions,
-    moveResources: require('./project/moveResources').moveResources
+    moveResources: require('./project/moveResources').moveResources,
+    removeResources: require('./project/removeResources').removeResources
 
   };
 
