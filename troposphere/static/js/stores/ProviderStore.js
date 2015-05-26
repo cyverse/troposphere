cba200db6f36b1642d90cab4802567139ea94aa9
jchansen <jason_hansen@outlook.com>
2015-05-25 23:22:04 -0700

    refactor quota request store

--- troposphere/static/js/stores/ProviderStore.js
+++ troposphere/static/js/stores/ProviderStore.js
@@ -3,10 +3,12 @@ define(function (require) {
   var BaseStore = require('stores/BaseStore'),
       ProviderCollection = require('collections/ProviderCollection');
 
-  var ProviderStore = new BaseStore(null, {
+  var ProviderStore = BaseStore.extend({
     collection: ProviderCollection
   });
 
-  return ProviderStore;
+  var store = new ProviderStore();
+
+  return store;
 
 });
--- troposphere/static/js/stores/QuotaRequestStore.js
+++ troposphere/static/js/stores/QuotaRequestStore.js
@@ -1,80 +1,27 @@
 define(function (require) {
 
-  var _ = require('underscore'),
-      Dispatcher = require('dispatchers/Dispatcher'),
-      Store = require('stores/Store'),
-      Collection = require('collections/QuotaRequestCollection'),
-      Constants = require('constants/QuotaRequestConstants'),
+  var Dispatcher = require('dispatchers/Dispatcher'),
+      BaseStore = require('stores/BaseStore'),
+      QuotaRequestCollection = require('collections/QuotaRequestCollection'),
+      QuotaRequestConstants = require('constants/QuotaRequestConstants'),
       stores = require('stores');
 
-  var _models = null;
-  var _pendingModels = null;
-  var _isFetching = false;
-
-  //
-  // CRUD Operations
-  //
-
-  var fetchModels = function () {
-    if(!_models && !_isFetching) {
-      _isFetching = true;
-      var models = new Collection();
-      models.fetch().done(function () {
-        _isFetching = false;
-        _models = models;
-        ModelStore.emitChange();
-      });
-    }
-  };
-
-  function add(model){
-    _models.add(model);
-  }
-
-  function remove(model){
-    _models.remove(model);
-  }
-
-  function update(model){
-    var existingModel = _models.get(model);
-    if(!existingModel) throw new Error("Model doesn't exist");
-    _models.add(model, {merge: true});
-  }
-
-  //
-  // Model Store
-  //
-
-  var ModelStore = {
-
-    get: function (modelId) {
-      if (!_models) {
-        fetchModels();
-      } else {
-        return _models.get(modelId);
-      }
-    },
-
-    getAll: function () {
-      if (!_models) {
-        fetchModels();
-      }
-      return _models;
-    },
+  var QuotaRequestStore = BaseStore.extend({
+    collection: QuotaRequestCollection,
 
     getAllPending: function () {
-      if (!_models) {
-        return fetchModels();
-      }
+      if(!this.models) return this.fetchModels();
 
-      var pendingRequests = _models.filter(function(model){
+      var pendingRequests = this.models.filter(function(model){
         return model.get("status").name === "pending";
       });
 
-      return new Collection(pendingRequests);
+      return new QuotaRequestCollection(pendingRequests);
     }
 
-  };
+  });
+
+  var store = new QuotaRequestStore();
 
   Dispatcher.register(function (dispatch) {
     var actionType = dispatch.action.actionType;
@@ -82,15 +29,15 @@ define(function (require) {
     var options = dispatch.action.options || options;
 
     switch (actionType) {
-      case Constants.EMIT_CHANGE:
+      case QuotaRequestConstants.UPDATE:
+        store.update(payload.model);
         break;
 
-      case Constants.UPDATE:
-        update(payload.model);
+      case QuotaRequestConstants.REMOVE:
+        store.remove(payload.model);
         break;
 
-      case Constants.REMOVE:
-        remove(payload.model);
+      case QuotaRequestConstants.EMIT_CHANGE:
         break;
 
       default:
@@ -98,12 +45,12 @@ define(function (require) {
     }
 
     if(!options.silent) {
-      ModelStore.emitChange();
+      store.emitChange();
     }
 
     return true;
   });
 
-  _.extend(ModelStore, Store);
-  return ModelStore;
+  return store;
+
 });
