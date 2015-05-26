47803e15b7382c34006114d9b43003618c1d3142
jchansen <jason_hansen@outlook.com>
2015-05-25 23:24:30 -0700

    refactor quota status store

--- troposphere/static/js/stores/QuotaStatusStore.js
+++ troposphere/static/js/stores/QuotaStatusStore.js
@@ -1,60 +1,21 @@
 define(function (require) {
 
-  var _ = require('underscore'),
-      Dispatcher = require('dispatchers/Dispatcher'),
-      Store = require('stores/Store'),
-      Collection = require('collections/QuotaStatusCollection'),
-      stores = require('stores');
+  var BaseStore = require('stores/BaseStore'),
+      QuotaStatusCollection = require('collections/QuotaStatusCollection');
 
-  var _models = null;
-  var _isFetching = false;
+  var QuotaStatusStore = BaseStore.extend({
 
-  //
-  // CRUD Operations
-  //
-
-  var fetchModels = function () {
-    if(!_models && !_isFetching) {
-      _isFetching = true;
-      var models = new Collection();
-      models.fetch().done(function () {
-        _isFetching = false;
-        _models = models;
-        ModelStore.emitChange();
-      });
-    }
-  };
-
-  //
-  // Model Store
-  //
-
-  var ModelStore = {
-
-    get: function (modelId) {
-      if(!_models) {
-        fetchModels();
-      } else {
-        return _models.get(modelId);
-      }
-    },
-
-    getAll: function () {
-      if(!_models) {
-        fetchModels()
-      }
-      return _models;
-    },
+    collection: QuotaStatusCollection,
 
     getStatusWithName: function(text) {
-      if(!_models){
-        return fetchModels();
-      }
-      return _models.findWhere({name: text});
+      if(!this.models) return this.fetchModels();
+      return this.models.findWhere({name: text});
     }
 
-  };
+  });
 
-  _.extend(ModelStore, Store);
-  return ModelStore;
+  var store = new QuotaStatusStore();
+
+  return store;
+
 });
