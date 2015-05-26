c147cc249fa7040cf2a29328d1ca721d9db0f548
jchansen <jason_hansen@outlook.com>
2015-05-26 00:01:26 -0700

    refactor instance store

--- troposphere/static/js/stores/InstanceStore.js
+++ troposphere/static/js/stores/InstanceStore.js
@@ -1,167 +1,134 @@
 define(function (require) {
 
-    var _ = require('underscore'),
-        Dispatcher = require('dispatchers/Dispatcher'),
-        Store = require('stores/Store'),
-        Collection = require('collections/InstanceCollection'),
-        Constants = require('constants/InstanceConstants');
+  var Dispatcher = require('dispatchers/Dispatcher'),
+      BaseStore = require('stores/BaseStore'),
+      InstanceCollection = require('collections/InstanceCollection'),
+      InstanceConstants = require('constants/InstanceConstants');
 
-    var _models = null;
-    var _isFetching = false;
+  var pollingFrequency = 10*1000;
+  var _instancesBuilding = [];
 
-    var pollingFrequency = 10*1000;
-    var _instancesBuilding = [];
+  var InstanceStore = BaseStore.extend({
+    collection: InstanceCollection,
 
-    //
-    // CRUD Operations
-    //
-
-    var fetchModels = function () {
-      if(!_models && !_isFetching) {
-        _isFetching = true;
-        var models = new Collection();
+    // todo: differences between this and base class implementation
+    // page_size query param
+    // pollNowUntilBuildIsFinished
+    fetchModels: function () {
+      if (!this.models && !this.isFetching) {
+        this.isFetching = true;
+        var models = new this.collection();
         models.fetch({
           url: models.url + "?page_size=100"
-        }).done(function () {
-          _isFetching = false;
-          _models = models;
-          _models.each(pollNowUntilBuildIsFinished);
-          ModelStore.emitChange();
-        });
+        }).done(function(){
+          this.isFetching = false;
+          this.models = models;
+          this.models.each(this.pollNowUntilBuildIsFinished.bind(this));
+          this.emitChange();
+        }.bind(this));
       }
-    };
-
-    function add(model) {
-      _models.add(model);
-    }
-
-    function update(model) {
-      var existingModel = _models.get(model);
-      if (!existingModel) throw new Error("Instance doesn't exist.");
-      _models.add(model, {merge: true});
-    }
-
-    function remove(model) {
-      _models.remove(model);
-    }
+    },
 
     //
-    // Polling Functions
+    // Custom functions
     //
 
-    var pollNowUntilBuildIsFinished = function(instance){
+    getInstancesOnProvider: function (provider) {
+      if(!this.models) return this.fetchModels();
+
+      var instances = this.models.filter(function(instance){
+        return instance.get('provider').id === provider.id;
+      });
+
+      return new InstanceCollection(instances);
+    },
+
+    getInstancesNotInAProject: function (provider) {
+      if(!this.models) return this.fetchModels();
+
+      var instances = this.models.filter(function(instance){
+        return instance.get('projects').length === 0
+      });
+
+      return new InstanceCollection(instances);
+    },
+
+    //
+    // Polling functions
+    //
+
+    pollNowUntilBuildIsFinished: function(instance){
       if(_instancesBuilding.indexOf(instance) < 0) {
         _instancesBuilding.push(instance);
-        fetchNowAndRemoveIfFinished(instance);
+        this.fetchNowAndRemoveIfFinished(instance);
       }
-    };
+    },
 
-    var fetchAndRemoveIfFinished = function(instance){
+    fetchAndRemoveIfFinished: function(instance){
       setTimeout(function(){
         instance.fetchFromCloud(function(){
-          update(instance);
+          this.update(instance);
           var index = _instancesBuilding.indexOf(instance);
           if(instance.get('state').isInFinalState()){
             _instancesBuilding.splice(index, 1);
           }else{
-            fetchAndRemoveIfFinished(instance);
+            this.fetchAndRemoveIfFinished(instance);
           }
-          ModelStore.emitChange();
-        });
-      }, pollingFrequency);
-    };
+          this.emitChange();
+        }.bind(this));
+      }.bind(this), pollingFrequency);
+    },
 
-    var fetchNowAndRemoveIfFinished = function(instance){
+    fetchNowAndRemoveIfFinished: function(instance){
       instance.fetchFromCloud(function(){
-        update(instance);
+        this.update(instance);
         var index = _instancesBuilding.indexOf(instance);
         if(instance.get('state').isInFinalState()){
           _instancesBuilding.splice(index, 1);
         }else{
-          fetchAndRemoveIfFinished(instance);
+          this.fetchAndRemoveIfFinished(instance);
         }
-        ModelStore.emitChange();
-      });
-    };
+        this.emitChange();
+      }.bind(this));
+    }
 
-    //
-    // Instance Store
-    //
-
-    var ModelStore = {
-
-      getAll: function () {
-        if(!_models) {
-          fetchModels()
-        }
-        return _models;
-      },
-
-      get: function (modelId) {
-        if(!_models) {
-          fetchModels();
-        } else {
-          return _models.get(modelId);
-        }
-      },
-
-      getInstancesOnProvider: function (provider) {
-        if(!_models) return fetchModels();
-
-        var instances = _models.filter(function(instance){
-          return instance.get('provider').id === provider.id;
-        });
-
-        return new Collection(instances);
-      },
-
-      getInstancesNotInAProject: function (provider) {
-        if(!_models) return fetchModels();
-
-        var instances = _models.filter(function(instance){
-          return instance.get('projects').length === 0
-        });
-
-        return new Collection(instances);
-      }
-
-    };
-
-    Dispatcher.register(function (dispatch) {
-      var actionType = dispatch.action.actionType;
-      var payload = dispatch.action.payload;
-      var options = dispatch.action.options || options;
-
-      switch (actionType) {
-
-        case Constants.ADD_INSTANCE:
-          add(payload.instance);
-          break;
-
-        case Constants.UPDATE_INSTANCE:
-          update(payload.instance);
-          break;
-
-        case Constants.REMOVE_INSTANCE:
-          remove(payload.instance);
-          break;
-
-        case Constants.POLL_INSTANCE:
-          pollNowUntilBuildIsFinished(payload.instance);
-          break;
-
-        default:
-          return true;
-      }
-
-      if(!options.silent) {
-        ModelStore.emitChange();
-      }
-
-      return true;
-    });
-
-    _.extend(ModelStore, Store);
-
-    return ModelStore;
   });
+
+  var store = new InstanceStore();
+
+  Dispatcher.register(function (dispatch) {
+    var actionType = dispatch.action.actionType;
+    var payload = dispatch.action.payload;
+    var options = dispatch.action.options || options;
+
+    switch (actionType) {
+
+      case InstanceConstants.ADD_INSTANCE:
+        store.add(payload.instance);
+        break;
+
+      case InstanceConstants.UPDATE_INSTANCE:
+        store.update(payload.instance);
+        break;
+
+      case InstanceConstants.REMOVE_INSTANCE:
+        store.remove(payload.instance);
+        break;
+
+      case InstanceConstants.POLL_INSTANCE:
+        store.pollNowUntilBuildIsFinished(payload.instance);
+        break;
+
+      default:
+        return true;
+    }
+
+    if(!options.silent) {
+      store.emitChange();
+    }
+
+    return true;
+  });
+
+  return store;
+});
