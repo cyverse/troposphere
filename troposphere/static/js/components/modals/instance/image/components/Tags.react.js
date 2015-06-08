cbe4bd8a4bbf40eecd26c5f975a2e7baf9e03028
jchansen <jason_hansen@outlook.com>
2015-06-08 09:06:11 -0700

    change tags on query

--- troposphere/static/js/components/modals/instance/image/components/Tags.react.js
+++ troposphere/static/js/components/modals/instance/image/components/Tags.react.js
@@ -13,15 +13,29 @@ define(function (require) {
       imageTags: React.PropTypes.instanceOf(Backbone.Collection).isRequired
     },
 
+    getInitialState: function(){
+      return {
+        query: ""
+      }
+    },
+
     onQueryChange: function(query){
       this.setState({query: query});
     },
 
     render: function () {
       var imageTags = this.props.imageTags,
-          tags = stores.TagStore.getAll();
+          tags = stores.TagStore.getAll(),
+          query = this.state.query;
 
       if(!tags) return <div className="loading"/>;
+
+      if(query){
+        tags = tags.filter(function(tag){
+          return tag.get('name').toLowerCase().indexOf(query) >= 0;
+        });
+        tags = new Backbone.Collection(tags);
+      }
 
       return (
         <div className="form-group">
