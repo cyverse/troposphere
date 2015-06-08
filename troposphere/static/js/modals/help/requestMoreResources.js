20c5b60d2297e5a55b501382d26106c1947689cd
jchansen <jason_hansen@outlook.com>
2015-06-08 15:41:06 -0700

    add missing semi-colon

--- troposphere/static/js/modals/help/requestMoreResources.js
+++ troposphere/static/js/modals/help/requestMoreResources.js
@@ -3,15 +3,14 @@ define(function (require) {
 
   var actions = require('actions'),
       ModalHelpers = require('components/modals/ModalHelpers'),
-      RequestMoreResourcesModal = require('components/modals/RequestMoreResourcesModal.react')
+      RequestMoreResourcesModal = require('components/modals/RequestMoreResourcesModal.react');
+
   return {
 
     requestMoreResources: function(){
-
       var modal = RequestMoreResourcesModal();
 
       ModalHelpers.renderModal(modal, function(identity, quota, reason){
-
         actions.HelpActions.requestMoreResources({
           identity: identity,
           quota: quota,
