41d61cb85e27eb1730bdeb9663816d20bee46536
prosif <prosif@gmail.com>
2015-06-25 14:18:26 -0700

    centralized admin settings file for easier modification

---
+++ troposphere/views/admin_settings.py
@@ -0,0 +1,7 @@
+def get_settings():
+    return {
+        "header_text": "Atmosphere: iPlant Cloud Service",
+        "header_logo": "/assets/resources/images/mini_logo.png",
+        "css_file": "/assets/css/app/app.css",
+        "footer_text": "iPlant Collaborative BRUH"
+    }
