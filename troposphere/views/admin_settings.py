2cdd114ff1741166800ffff7ec3681658bf3261b
prosif <prosif@gmail.com>
2015-06-25 14:34:18 -0700

    made footer text better

--- troposphere/views/admin_settings.py
+++ troposphere/views/admin_settings.py
@@ -3,5 +3,5 @@ def get_settings():
         "header_text": "Atmosphere: iPlant Cloud Service",
         "header_logo": "/assets/resources/images/mini_logo.png",
         "css_file": "/assets/css/app/app.css",
-        "footer_text": "iPlant Collaborative BRUH"
+        "footer_text": "iPlant Collaborative"
     }
