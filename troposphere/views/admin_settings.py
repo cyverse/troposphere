ce181e88be72ed4c5b8564bc314e0c27d9f8cfee
prosif <prosif@gmail.com>
2015-06-30 13:14:23 -0700

    removed unnecessary "admin_settings.py"

--- troposphere/views/admin_settings.py
+++
@@ -1,7 +0,0 @@
-def get_settings():
-    return {
-        "header_text": "Atmosphere: iPlant Cloud Service",
-        "header_logo": "/assets/resources/images/mini_logo.png",
-        "css_file": "/assets/css/app/app.css",
-        "footer_text": "iPlant Collaborative"
-    }
