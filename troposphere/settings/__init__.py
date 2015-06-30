55594a2334853f25dc5e940a439352b4407bf36d
prosif <prosif@gmail.com>
2015-06-30 13:39:05 -0700

    added default base settings to __init__.py

--- troposphere/settings/__init__.py
+++ troposphere/settings/__init__.py
@@ -3,3 +3,9 @@ try:
     from troposphere.settings.local import *
 except ImportError:
     raise Exception("No local settings module found. Refer to README.md")
+
+HEADER_TEXT = 'Atmosphere'
+LOGO = '/assets/images/mini_logo.png'
+FAVICON = '/assets/images/favicon.ico'
+CSS_FILE = '/assets/css/app/app.css'
+FOOTER_TEXT = "iPlant Collaborative"
