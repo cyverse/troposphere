0198914f63d03f188870e4f6bc656f112d9646db
prosif <prosif@gmail.com>
2015-06-30 13:19:25 -0700

    get HEADER_TEXT and FOOTER_TEXT in globals

--- troposphere/static/js/globals.js
+++ troposphere/static/js/globals.js
@@ -3,7 +3,9 @@ define(function(require){
   return {
     API_ROOT: window.API_ROOT || '/api/v1',
     API_V2_ROOT: window.API_V2_ROOT || '/api/v2',
-    API_V2_MOCK_ROOT: window.API_V2_MOCK_ROOT
+    API_V2_MOCK_ROOT: window.API_V2_MOCK_ROOT,
+    HEADER_TEXT: window.HEADER_TEXT,
+    FOOTER_TEXT: window.FOOTER_TEXT
   }
 
 });
