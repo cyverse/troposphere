78c4acdc62121f99a13eca99606b8c27fd8e9442
prosif <prosif@gmail.com>
2015-07-02 11:15:18 -0700

    show current quota and current allocation in admin pane

--- troposphere/static/js/components/admin/ResourceAdmin.react.js
+++ troposphere/static/js/components/admin/ResourceAdmin.react.js
@@ -43,11 +43,33 @@ define(function (require) {
     },
 
     render: function () {
+
       var quotaRequest = stores.QuotaRequestStore.get(this.getParams().resourceRequestId);
       var quotas = stores.QuotaStore.getAll();
+      var allocations = stores.AllocationStore.getAll();
       var statuses = stores.QuotaStatusStore.getAll();
 
-      if(!quotaRequest || !quotas || !statuses) return <div className="loading"></div>;
+      if(!quotaRequest || !quotas || !allocations || !statuses) return <div className="loading"></div>;
+
+      var currentQuota = stores.QuotaStore.get(quotaRequest.get('current_quota'));
+      var currentAllocation = stores.AllocationStore.get(quotaRequest.get('current_allocation'));
+
+      if(!currentQuota || !currentAllocation) return <div className="loading"></div>;
+
+      var currentCPU = "  CPU: " + currentQuota.get('cpu'),
+          currentMemory = "  Memory: " + currentQuota.get('memory'),
+          currentStorage = "  Storage: " + currentQuota.get('storage'),
+          currentStorageCount = "  Storage Count: " + currentQuota.get('storage_count'),
+          currentSuspendedCount = "  Suspended: " + currentQuota.get('suspended_count'),
+          currentThreshold = "  Threshold: " + currentAllocation.get('threshold'),
+          currentDelta = "  Delta: " + currentAllocation.get('delta');
+
+
+      var currentQuotaString = currentCPU + currentMemory + currentStorage + currentStorageCount + currentSuspendedCount,
+          currentAllocationString = currentThreshold + currentDelta;
+
+      console.log("current quota", currentQuotaString);
+      console.log("current allocation", currentAllocationString);
 
       return(
         <div className="quota-detail">
@@ -56,6 +78,8 @@ define(function (require) {
           <div><strong>Admin message:</strong> {quotaRequest.get('admin_message')}</div>
           <div><strong>Request:</strong> {quotaRequest.get('request')}</div>
           <div><strong>Description:</strong> {quotaRequest.get('description')}</div>
+          <div><strong>Current quota:</strong>{currentQuotaString}</div>
+          <div><strong>Current allocation:</strong>{currentAllocationString}</div>
           <div>
             <label>New quota:&nbsp;</label>
             <select value={this.state.quota} onChange={this.handleQuotaChange} ref="selectedQuota">{quotas.map(function(quota){
@@ -66,6 +90,18 @@ define(function (require) {
                   Storage: {quota.get('storage')}&nbsp;
                   Storage Count: {quota.get('storage_count')}&nbsp;
                   Suspended: {quota.get('suspended_count')}&nbsp;
+                </option>
+              );
+            })}
+            </select>
+          </div>
+          <div>
+            <label>New allocation:&nbsp;</label>
+            <select value={this.state.quota} onChange={this.handleAllocationChange} ref="selectedAllocation">{allocations.map(function(allocation){
+              return(
+                <option value={allocation.id} key={allocation.id}>
+                  Threshold: {allocation.get('threshold')}&nbsp;
+                  Delta: {allocation.get('delta')}&nbsp;
                 </option>
               );
             })}
