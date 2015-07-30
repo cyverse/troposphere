d25ec435f488690385392d31829c7819b57ef813
prosif <prosif@gmail.com>
2015-07-30 09:26:33 -0700

    add defaults in globals.js

--- troposphere/static/js/globals.js
+++ troposphere/static/js/globals.js
@@ -4,9 +4,9 @@ define(function (require) {
     API_ROOT: window.API_ROOT || '/api/v1',
     API_V2_ROOT: window.API_V2_ROOT || '/api/v2',
     API_V2_MOCK_ROOT: window.API_V2_MOCK_ROOT,
-    THEME_HEADER_TEXT: window.THEME_HEADER_TEXT,
-    THEME_FOOTER_TEXT: window.THEME_FOOTER_TEXT,
-    UI_VERSION: window.UI_VERSION
+    THEME_HEADER_TEXT: window.THEME_HEADER_TEXT || 'Atmosphere',
+    THEME_FOOTER_TEXT: window.THEME_FOOTER_TEXT || 'iPlant Collaborative',
+    UI_VERSION: window.UI_VERSION || 'Hawaiian Hawk'
   }
 
 });
