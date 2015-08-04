f8d986c7a11146a91ab85704e27652b0f5d566dd
prosif <prosif@gmail.com>
2015-08-04 14:16:22 -0700

    basic menu for tabbing through badges

---
+++ troposphere/static/js/components/badges/BadgeMaster.react.js
@@ -0,0 +1,35 @@
+define(function (require) {
+  "use strict";
+
+  var React = require('react'),
+      Router = require('react-router'),
+      Badge = require('./Badge.react'),
+      actions = require('actions'),
+      EarnedBadge = require('./EarnedBadge.react'),
+      stores = require('stores'),
+      RouteHandler = Router.RouteHandler;
+
+  return React.createClass({
+
+    mixins: [Router.State],
+
+    render: function(){
+      return(
+        <div className="container badges buttons">
+          <Router.Link to="my-badges">
+            <div className="btn btn-default">My Badges</div>
+          </Router.Link>
+          <Router.Link to="all-badges">
+            <div className="btn btn-default">All Badges</div>
+          </Router.Link>
+          <Router.Link to="unearned-badges">
+            <div className="btn btn-default">Unearned Badges</div>
+          </Router.Link>
+          <RouteHandler />
+        </div>
+      )
+    }
+
+  });
+
+});
