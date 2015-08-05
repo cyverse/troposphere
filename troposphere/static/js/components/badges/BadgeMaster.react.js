c3b310911ac9111a754c64d7a1a6f2a08290bd5f
prosif <prosif@gmail.com>
2015-08-05 13:38:21 -0700

    center badge buttons

--- troposphere/static/js/components/badges/BadgeMaster.react.js
+++ troposphere/static/js/components/badges/BadgeMaster.react.js
@@ -15,16 +15,18 @@ define(function (require) {
 
     render: function(){
       return(
-        <div className="container badges buttons">
-          <Router.Link to="my-badges">
-            <div className="btn btn-default">My Badges</div>
-          </Router.Link>
-          <Router.Link to="all-badges">
-            <div className="btn btn-default">All Badges</div>
-          </Router.Link>
-          <Router.Link to="unearned-badges">
-            <div className="btn btn-default">Unearned Badges</div>
-          </Router.Link>
+        <div className="container badges">
+          <span className="buttons">
+            <Router.Link to="my-badges">
+              <div className="btn btn-default">My Badges</div>
+            </Router.Link>
+            <Router.Link to="all-badges">
+              <div className="btn btn-default">All Badges</div>
+            </Router.Link>
+            <Router.Link to="unearned-badges">
+              <div className="btn btn-default">Unearned Badges</div>
+            </Router.Link>
+          </span>
           <RouteHandler />
         </div>
       )
