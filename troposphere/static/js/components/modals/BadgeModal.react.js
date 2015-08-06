609a5d9f6b2e433b1646fb6f8164006c357575d4
prosif <prosif@gmail.com>
2015-08-06 16:20:58 -0700

    changed layout for badge modal

--- troposphere/static/js/components/modals/BadgeModal.react.js
+++ troposphere/static/js/components/modals/BadgeModal.react.js
@@ -53,14 +53,18 @@ define(
         var content = (
           <div>
             <img className="badge-modal-image" src={this.props.badge.get('imageUrl')} />
-            <span className="badge-modal description"><strong>Criteria: </strong>{criteria}</span>
+            <span className="badge-modal-text">
+              <strong>Criteria: </strong>{criteria}
+              <p><strong>What does this mean{'?'}</strong></p>
+              <p>Text here about how to earn this badge in detail</p>
+            </span>
           </div>
         );
 
         return (
           <div className="modal fade">
             <div className="modal-dialog">
-              <div className="modal-content">
+              <div className="modal-content badge-modal-content">
                 <div className="modal-header">
                   {this.renderCloseButton()}
                   <strong>{this.props.badge.get('name')}</strong>
