670a7eabfe44787301d5b360677a381e6e528f56
Steve Gregory <contact@steve-gregory.com>
2015-08-12 07:01:04 -0700

    Mole found!
    	modified:   troposphere/static/js/components/projects/detail/resources/ButtonBar.react.js

--- troposphere/static/js/components/projects/detail/resources/ButtonBar.react.js
+++ troposphere/static/js/components/projects/detail/resources/ButtonBar.react.js
@@ -1,6 +1,6 @@
 define(function (require) {
 
-  var React = require('react/addons'),
+  var React = require('react'),
     Backbone = require('backbone'),
     context = require('context'),
     Button = require('./Button.react'),
