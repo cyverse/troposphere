78aab56ef40b60f6cacc9c882087ce467bd9b636
Connor Osborn <cdosborn@uw.edu>
2015-08-13 15:27:46 -0700

    Temporary fix for tropo API_ROOT vars

--- troposphere/settings/__init__.py
+++ troposphere/settings/__init__.py
@@ -4,9 +4,5 @@ try:
 except ImportError:
     raise Exception("No local settings module found. Refer to README.md")
 
-THEME_HEADER_TEXT = 'Atmosphere'
-THEME_LOGO = '/assets/images/mini_logo.png'
-THEME_FAVICON = '/assets/images/favicon.ico'
-THEME_CSS_FILE = '/assets/style.css'
-THEME_FOOTER_TEXT = "iPlant Collaborative"
-UI_VERSION = 'Hawaiian Hawk'
+API_ROOT    = SERVER_URL + "/api/v1"
+API_V2_ROOT = SERVER_URL + "/api/v2"
