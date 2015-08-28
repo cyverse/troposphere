86326088a3f0fcf2f3b4992502973912bebe117c
prosif <prosif@gmail.com>
2015-08-28 14:26:34 -0700

    don't show modal for earned badge, show button to add to backpack

--- troposphere/static/js/components/badges/EarnedBadge.react.js
+++ troposphere/static/js/components/badges/EarnedBadge.react.js
@@ -6,30 +6,41 @@ define(function (require) {
       stores = require('stores');
 
   return React.createClass({
-    renderBadgeDetail: function(e){
-      e.preventDefault();
-      modals.BadgeModals.showMyBadge(this.props.badge);
+    handleClick: function(e){
+      this.setState({selected: !this.state.selected});
     },
 
     getInitialState: function(){
-      var badgeText = this.props.badge.get('strapline');
-
       return({
-        text: badgeText,
-        badgeSlug: this.props.badge.get('slug')
-      });
+            selected: false
+        });
+    },
+
+    addToBackpack: function(){
+        console.log("You want to add " + this.props.badge + " to your backpack!");
+        OpenBadges.issue(this.props.badge.get('assertionUrl'));
     },
 
     render: function () {
       var badge = this.props.badge;
+      var content = (
+              <p>{badge.get('strapline')}</p>
+      );
+      if(this.state.selected){
+        content = (
+            <div className="text">
+            <p>{badge.get('strapline')}</p>
+            <p>Awarded on: {badge.get('issuedOn')}</p>
+            <div className = "btn btn-default" onClick={this.addToBackpack}>Add to backpack!</div>
+            </div>
+        );
+      }
 
       return(
-        <li onClick={this.renderBadgeDetail} className='badge-li'>
+        <li onClick={this.handleClick} className='badge-li'>
           <img className='image' src={badge.get('imageUrl')} />
           <h4 className='badge-name'>{badge.get('name')}</h4>
-          <p className='text'>
-          {this.state.text}
-          </p>
+          {content}
         </li>
       );
     }
