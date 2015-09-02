fa654228109160b788c8608215be40c7d10d5559
prosif <prosif@gmail.com>
2015-09-02 16:04:21 -0700

    show number of user images in navigation header

--- troposphere/static/js/components/images/common/SecondaryImageNavigation.react.js
+++ troposphere/static/js/components/images/common/SecondaryImageNavigation.react.js
@@ -1,6 +1,7 @@
 define(function (require) {
 
   var React = require('react/addons'),
+    stores = require('stores'),
     Router = require('react-router'),
     Glyphicon = require('components/common/Glyphicon.react'),
     context = require('context');
@@ -21,6 +22,17 @@ define(function (require) {
     },
 
     render: function () {
+      var profile = stores.ProfileStore.get(),
+          images = stores.ImageStore.fetchWhere({
+            created_by__username: profile.get('username')
+          });
+
+      if(!images){
+        return <div className="loading"></div>
+      }
+
+      var myImagesText = "My Images (" + images.length + ")";
+
       return (
         <div>
           <div className="secondary-nav">
@@ -28,7 +40,7 @@ define(function (require) {
               <ul className="secondary-nav-links">
                 {this.renderRoute("Search", "search", "search", false)}
                 {this.renderRoute("Favorites", "favorites", "bookmark", true)}
-                {this.renderRoute("My Images", "authored", "user", true)}
+                {this.renderRoute(myImagesText, "authored", "user", true)}
                 {this.renderRoute("Tags", "tags", "tags", false)}
               </ul>
             </div>
