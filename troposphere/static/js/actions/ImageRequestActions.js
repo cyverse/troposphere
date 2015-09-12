5be7cd0ee06ca696dfde1a91bcf5e4a915285ade
prosif <prosif@gmail.com>
2015-09-11 17:09:41 -0700

    add basic fluxification for ImageRequest

---
+++ troposphere/static/js/actions/ImageRequestActions.js
@@ -0,0 +1,31 @@
+define(function (require) {
+
+  var Utils = require('./Utils'),
+    Router = require('../Router'),
+    Constants = require('constants/ImageRequestConstants');
+
+  return {
+    update: function (params) {
+      var request = params.request,
+        response = params.response,
+        //quota = params.quota,
+        //allocation = params.allocation,
+        //status = params.status;
+
+      var newAttributes = {
+        //admin_message: response,
+        //quota: quota,
+        //allocation: allocation,
+        //status: status
+      };
+
+      request.set(newAttributes);
+      Router.getInstance().transitionTo("admin");
+      request.save(newAttributes, {patch: true}).done(function () {
+        Utils.dispatch(Constants.UPDATE, {model: request});
+        Utils.dispatch(Constants.REMOVE, {model: request});
+      });
+    }
+  };
+
+});
