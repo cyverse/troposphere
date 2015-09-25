ef65d9e34c8535d753ae899fb3e3026669e3397d
prosif <prosif@gmail.com>
2015-09-25 13:23:56 -0700

    add my image requests page, base for image edit modal

--- troposphere/static/js/components/images/MyImageRequestsPage.react.js
+++ troposphere/static/js/components/images/MyImageRequestsPage.react.js
@@ -6,21 +6,25 @@ define(function(require) {
 
   return React.createClass({
 
+    onEditImage: function(requestId){
+      modals.ImageModals.edit(stores.ImageRequestStore.get(requestId));
+    },
+
     render: function() {
-      var username = stores.ProfileStore.get('username'),
+      var username = stores.ProfileStore.get().id,
           imagingDocsUrl = "https://pods.iplantcollaborative.org/wiki/display/atmman/Requesting+an+Image+of+an+Instance";
       
       if(username == null){
         return <div className = "loading"></div>
       }
 
-      var requests = stores.ImageRequestStore.fetchWhere({username: username});
+      var requests = stores.ImageRequestStore.fetchWhere({new_machine_owner__username: username});
       
       if(requests == null){
         return <div className = "loading"></div>;
       }
 
-      if(!requests[0]){
+      if(!requests.models[0]){
         return (
           <div className="container">
             <p style={{marginBottom: "16px"}}>
@@ -33,12 +37,29 @@ define(function(require) {
       }
 
       var displayRequests = requests.map(function(request){
-        return <tr>
-                 <td>{moment(request.get('start_date')).format("MMM D, YYYY")}</td>
-                 <td>{request.get('instance').name}</td>
-                 <td>{request.get('status')}</td>
+        var trClass;
+        switch(request.get('status').name){
+          case "approved":
+            trClass = "success";
+            break;
+          case "failed":
+            trClass = "warning"
+            break;
+          default:
+            trClass = "active"
+            break;
+        }
+
+        var newMachineId = !!request.get('new_machine') ? request.get('new_machine').id : "N/A";
+
+        return <tr className={trClass}>
+                    <td>{moment(request.get('start_date')).format("MMM D, YYYY")}</td>
+                    <td>#{request.get('instance').id} - {request.get('instance').name}</td>
+                    <td>{request.get('status').name}</td>
+                    <td>{request.get('old_status')}</td>
+                    <td>{newMachineId}</td>
                 </tr>
-      });
+      }.bind(this));
       
       return (
         <div className="container">
@@ -46,9 +67,9 @@ define(function(require) {
             {"Looking for more information about the imaging process? Check out the "}
             <a href={imagingDocsUrl} target="_blank">documention on imaging</a>.
           </p>
-          <table className="quota-table table table-hover col-md-6">
+          <table className = "table table-condensed image-requests">
             <tbody>
-              <tr className="quota-row">
+              <tr>
                 <th>
                   <h3>Date requested</h3>
                 </th>
@@ -57,6 +78,12 @@ define(function(require) {
                 </th>
                 <th>
                   <h3>Status</h3>
+                </th>
+                <th>
+                  <h3>Machine State</h3>
+                </th>
+                <th>
+                  <h3>New Machine ID</h3>
                 </th>
               </tr>
               {displayRequests}
--- troposphere/static/js/components/modals/ImageEditModal.react.js
+++ troposphere/static/js/components/modals/ImageEditModal.react.js
@@ -11,21 +11,39 @@ define(
     return React.createClass({
       mixins: [BootstrapModalMixin],
 
-      //
-      // Mounting & State
-      // ----------------
-      //
+      getInitialState: function(){
+        return{
+          edits: {
+            minMem: this.props.image.get('new_version_memory_min'),
+            minCPU: this.props.image.get('new_version_cpu_min')
+          }
+        };
+      },
 
-      //
-      // Internal Modal Callbacks
-      // ------------------------
-      //
+      handleMemChange: function(e){
+        this.setState({
+          edits:{
+            minMem: e.target.value,
+            minCPU: this.state.edits.minCPU
+          }
+        });
+      },
 
+      handleStorageChange: function(e){
+        this.setState({
+          edits:{
+            minMem: this.state.edits.minMem,
+            minCPU: e.target.value
+          }
+        });
+      },
+      
       cancel: function(){
         this.hide();
       },
 
       confirm: function () {
+        ImageActions.editRequirements(this.props.image, this.state.edits);
         this.hide();
       },
 
@@ -42,10 +60,28 @@ define(
               <div className="modal-content badge-modal-content">
                 <div className="modal-header">
                   {this.renderCloseButton()}
-                  <strong>Edit image- {image.get('name')}</strong>
+                  <strong>Edit image metadata - {image.get('new_application_name')}</strong>
                 </div>
                 <div className="modal-body">
-                  WHAT
+                  <div className="form-group">
+                    <label htmlFor="name" className="control-label">
+                      Minimum memory (GB):
+                    </label>
+                  <div className="help-block">
+                    Let other users know how much memory (RAM) is required to properly run an instance based on this image.
+                  </div>
+                  <input type="text" onChange={this.handleMemChange} value={this.state.edits.minMem} />
+                  </div>
+                <hr />
+                  <div className="form-group">
+                    <label htmlFor="name" className="control-label">
+                      Minimum storage (GB):
+                    </label>
+                    <div className="help-block">
+                      Let other users know how much storage is required to properly run an instance based on this image.
+                    </div>
+                    <input type="text" onChange={this.handleStorageChange} value={this.state.edits.minCPU} />
+                  </div>
                 </div>
                 <div className="modal-footer">
                   <button type="button" className="btn btn-danger" onClick={this.cancel}>
