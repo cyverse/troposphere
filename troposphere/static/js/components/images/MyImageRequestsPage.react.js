75b1693f4b7ba6a48d14c3b491fcfb0cc9e4fd2d
prosif <prosif@gmail.com>
2015-09-28 16:20:34 -0700

    more descriptive image requests page

--- troposphere/static/js/components/images/MyImageRequestsPage.react.js
+++ troposphere/static/js/components/images/MyImageRequestsPage.react.js
@@ -2,25 +2,50 @@ define(function(require) {
 
   var React = require('react/addons'),
     moment = require('moment'),
+    RefreshComponent = require('components/projects/resources/instance/details/sections/metrics/RefreshComponent.react'),
     stores = require('stores');
 
   return React.createClass({
 
+    getInitialState: function(){
+      // start fetching the relevant models before the component is rendered
+      stores.ImageRequestStore.fetchFirstPageWhere({new_machine_owner__username: stores.ProfileStore.get().id});
+      return {};
+    },
+
+    onEditImage: function(requestId){
+      modals.ImageModals.edit(stores.ImageRequestStore.get(requestId));
+    },
+
+    refreshHistory: function(){
+      stores.ImageRequestStore.fetchFirstPageWhere({new_machine_owner__username: stores.ProfileStore.get().id});
+      stores.ImageRequestStore.lastUpdated = Date.now();
+      this.forceUpdate();
+    },
+
+    renderRefreshButton: function(){
+      return (
+        <span className="refresh-button">
+            <RefreshComponent onRefcreshClick = {this.refreshHistory} timestamp = {stores.ImageRequestStore.lastUpdated} delay = {1000 * 30} />
+        </span>
+      );
+    },
+
     render: function() {
-      var username = stores.ProfileStore.get('username'),
+      var username = stores.ProfileStore.get().id,
           imagingDocsUrl = "https://pods.iplantcollaborative.org/wiki/display/atmman/Requesting+an+Image+of+an+Instance";
       
       if(username == null){
         return <div className = "loading"></div>
       }
 
-      var requests = stores.ImageRequestStore.fetchWhere({username: username});
+      var requests = stores.ImageRequestStore.getAll();
       
       if(requests == null){
         return <div className = "loading"></div>;
       }
 
-      if(!requests[0]){
+      if(!requests.models[0]){
         return (
           <div className="container">
             <p style={{marginBottom: "16px"}}>
@@ -33,12 +58,31 @@ define(function(require) {
       }
 
       var displayRequests = requests.map(function(request){
-        return <tr>
-                 <td>{moment(request.get('start_date')).format("MMM D, YYYY")}</td>
-                 <td>{request.get('instance').name}</td>
-                 <td>{request.get('status')}</td>
+        
+        // set the color of the row based on the status of the request
+        var trClass;
+        switch(request.get('status').name){
+          case "approved":
+            trClass = "success";
+            break;
+          case "failed":
+            trClass = "warning"
+            break;
+          default:
+            trClass = "active"
+            break;
+        }
+
+        var newMachineId = !!request.get('new_machine') ? request.get('new_machine').id : "N/A";
+
+        return <tr className={trClass}>
+                    <td>{moment(request.get('start_date')).format("MMM D, YYYY")}</td>
+                    <td>#{request.get('instance').id} - {request.get('instance').name}</td>
+                    <td>{request.get('status').name}</td>
+                    <td>{request.get('old_status')}</td>
+                    <td>{newMachineId}</td>
                 </tr>
-      });
+      }.bind(this));
       
       return (
         <div className="container">
@@ -46,9 +90,12 @@ define(function(require) {
             {"Looking for more information about the imaging process? Check out the "}
             <a href={imagingDocsUrl} target="_blank">documention on imaging</a>.
           </p>
-          <table className="quota-table table table-hover col-md-6">
+
+          {this.renderRefreshButton()}
+
+          <table className = "table table-condensed image-requests">
             <tbody>
-              <tr className="quota-row">
+              <tr>
                 <th>
                   <h3>Date requested</h3>
                 </th>
@@ -57,6 +104,12 @@ define(function(require) {
                 </th>
                 <th>
                   <h3>Status</h3>
+                </th>
+                <th>
+                  <h3>Machine State</h3>
+                </th>
+                <th>
+                  <h3>New Machine ID</h3>
                 </th>
               </tr>
               {displayRequests}
