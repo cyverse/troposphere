5f96e060dca41476fc31237b23088ff953dd5234
prosif <prosif@gmail.com>
2015-09-30 10:39:50 -0700

    send proper fields in PATCH

--- troposphere/static/js/actions/ImageRequestActions.js
+++ troposphere/static/js/actions/ImageRequestActions.js
@@ -2,20 +2,26 @@ define(function (require) {
 
   var Utils = require('./Utils'),
     Router = require('../Router'),
+    stores = require('stores'),
     Constants = require('constants/ImageRequestConstants');
 
   return {
     update: function (params) {
       var request = params.request,
-        action = params.action,
+        status = params.status,
         response = params.response;
 
       var newAttributes = {
-        status: action
+        admin_message: response,
+        // we need to send these in to get around our MachineRequestSerializer API validation
+        instance: request.get('instance').id,
+        new_machine_owner: request.get('new_machine_owner').id,
+        new_machine_provider: request.get('new_machine_provider').id,
+        status: status
       };
 
       request.set(newAttributes);
-      Router.getInstance().transitionTo("admin");
+      Router.getInstance().transitionTo("image-request-manager");
       request.save(newAttributes, {patch: true}).done(function () {
         Utils.dispatch(Constants.UPDATE, {model: request});
         Utils.dispatch(Constants.REMOVE, {model: request});
