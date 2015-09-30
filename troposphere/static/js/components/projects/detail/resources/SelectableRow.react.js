1c9acd38386111f877dbf990fba7f552254aec82
Andrew Lenards <andrew.lenards@gmail.com>
2015-09-30 13:21:31 -0700

    Corrected event attribute typo preventing multi-select on resources.

--- troposphere/static/js/components/projects/detail/resources/SelectableRow.react.js
+++ troposphere/static/js/components/projects/detail/resources/SelectableRow.react.js
@@ -38,7 +38,7 @@ define(function (require) {
 
       return (
         <tr className={"sm-row " + rowClassName} style={{cursor:"pointer"}} onClick={this.previewResource}>
-          <td className="sm-cell hidden-sm hidden-xs" Click={this.toggleCheckbox}>
+          <td className="sm-cell hidden-sm hidden-xs" onClick={this.toggleCheckbox}>
             <Checkbox isChecked={this.props.isSelected}/>
           </td>
           {this.props.children}
