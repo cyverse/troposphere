81421fa8cc449df580bd56da9fd6be7f84e4e7fa
Andrew Lenards <andrew.lenards@gmail.com>
2015-10-05 11:14:32 -0700

    Refactored to ES6 modules plus indent/format change.

--- troposphere/static/js/components/notifications/VolumeAttachNotifications.react.js
+++ troposphere/static/js/components/notifications/VolumeAttachNotifications.react.js
@@ -1,64 +1,58 @@
+import React from 'react';
 
-define(
-  [
-    'react'
-  ],
-  function (React) {
+let mountVolumeDocumentationUrl = "https://pods.iplantcollaborative.org/wiki/x/OKxm#AttachinganEBSVolumetoanInstance-Step6%3AMountthefilesystemonthepartition.";
+let createFileSystemDocumentationUrl = "https://pods.iplantcollaborative.org/wiki/x/OKxm#AttachinganEBSVolumetoanInstance-Step5%3ACreatethefilesystem%28onetimeonly%29.";
 
-    var mountVolumeDocumentationUrl = "https://pods.iplantcollaborative.org/wiki/x/OKxm#AttachinganEBSVolumetoanInstance-Step6%3AMountthefilesystemonthepartition.";
-    var createFileSystemDocumentationUrl = "https://pods.iplantcollaborative.org/wiki/x/OKxm#AttachinganEBSVolumetoanInstance-Step5%3ACreatethefilesystem%28onetimeonly%29.";
+export default {
 
-    return {
-      success: function () {
-
+    success: function () {
         var message = (
-          <div>
-            {'You must '}
-            <a href={mountVolumeDocumentationUrl} target="_blank">
-              {'mount the volume'}
-            </a>
-            {' before you can use it.'}
-            <br />
-            {'If the volume is new, you will need to '}
-            <a href={createFileSystemDocumentationUrl} target="_blank">
-              {'create the file system'}
-            </a>
-            {' first.'}
-          </div>
-        );
-
-        return React.renderToString(message);
-      },
-
-      error: function () {
-        var message = (
-          <div>
-            {"If this problem persists, contact support at "}
-            <a href="mailto:support@iplantcollaborative.org">
-              {"support@iplantcollaborative.org"}
-            </a>
-          </div>
+            <div>
+                {'You must '}
+                <a href={mountVolumeDocumentationUrl} target="_blank">
+                    {'mount the volume'}
+                </a>
+                {' before you can use it.'}
+                <br />
+                {'If the volume is new, you will need to '}
+                <a href={createFileSystemDocumentationUrl} target="_blank">
+                    {'create the file system'}
+                </a>
+                {' first.'}
+            </div>
         );
 
         return React.renderToString(message);
     },
 
-      attachError: function (volume, instance) {
+    error: function () {
+        var message = (
+        <div>
+            {"If this problem persists, contact support at "}
+            <a href="mailto:support@iplantcollaborative.org">
+                {"support@iplantcollaborative.org"}
+            </a>
+        </div>
+        );
+
+        return React.renderToString(message);
+    },
+
+    attachError: function (volume, instance) {
         var volumeName = volume.get('name');
         var instanceName = instance.get('name');
 
         var message = (
-          <div>
-            {"Volume '" + volumeName + "' could not be auto-attached to the instance '" + instanceName + "'. "}
-            {"Please see "}
+            <div>
+                {"Volume '" + volumeName + "' could not be auto-attached to the instance '" + instanceName + "'. "}
+                {"Please see "}
             <a href={mountVolumeDocumentationUrl} target="_blank" style={{"textDecoration":"underline"}}>
-              {'Mounting a Volume'}
+                {'Mounting a Volume'}
             </a>
-            {"in the help documentation to learn how to mount the volume manually"}
-          </div>
+                {"in the help documentation to learn how to mount the volume manually"}
+            </div>
         );
 
         return React.renderToString(message);
     }
-
-  });
+};
