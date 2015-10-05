857ea342c1e3f04e1f4611b776040cb33b9e27e6
Andrew Lenards <andrew.lenards@gmail.com>
2015-10-05 11:15:13 -0700

    Refactored to ES6 modules plus indent/format change.

--- troposphere/static/js/components/projects/detail/resources/ProjectResourcesWrapper.react.js
+++ troposphere/static/js/components/projects/detail/resources/ProjectResourcesWrapper.react.js
@@ -1,35 +1,25 @@
+import React from 'react';
+import Backbone from 'backbone';
+import SubMenu from './Submenu.react';
 
-define(
-  [
-    'react',
-    'backbone',
-    './SubMenu.react'
-  ],
-  function (React, Backbone, SubMenu) {
+export default React.createClass({
 
     propTypes: {
       project: React.PropTypes.instanceOf(Backbone.Model).isRequired,
       children: React.PropTypes.element.isRequired
     },
 
-      propTypes: {
-        project: React.PropTypes.instanceOf(Backbone.Model).isRequired,
-        children: React.PropTypes.component.isRequired
-      },
+    render: function () {
+      return (
+        <div className="container">
+              <div className="td-sub-menu">
+                <SubMenu project={this.props.project}/>
+              </div>
+              <div className="td-project-content">
+                {this.props.children}
+              </div>
+        </div>
+      );
+    }
 
-      render: function () {
-        return (
-          <div className="container">
-                <div className="td-sub-menu">
-                  <SubMenu project={this.props.project}/>
-                </div>
-                <div className="td-project-content">
-                  {this.props.children}
-                </div>
-          </div>
-        );
-      }
-
-    });
-
-  });
+});
