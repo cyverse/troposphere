02ba7a65dad75b6a489c72ae1b46c0eb0d717175
Andrew Lenards <andrew.lenards@gmail.com>
2015-11-02 14:46:06 -0700

    Included format changes.

--- troposphere/views/app.py
+++ troposphere/views/app.py
@@ -149,9 +149,9 @@ def application_backdoor(request):
     # This should only apply when in maintenance//login is disabled
     if not disabled_login or maintenance_records.count() == 0:
         return application(request)
-   
+
     if request.user.is_authenticated() and request.user.username not in STAFF_LIST_USERNAMES:
-        logger.warn('[Backdoor] %s is NOT in staff_list_usernames' % request.user.username) 
+        logger.warn('[Backdoor] %s is NOT in staff_list_usernames' % request.user.username)
         return redirect('maintenance')
     disabled_login = False
     maintenance_records = MaintenanceRecord.objects.none()
@@ -166,7 +166,7 @@ def application(request):
     maintenance_records, disabled_login = get_maintenance(request)
 
     if disabled_login and request.user.is_staff is not True and request.user.username not in STAFF_LIST_USERNAMES:
-        logger.warn('[App] %s logged in but is NOT in staff_list_usernames' % request.user.username) 
+        logger.warn('[App] %s logged in but is NOT in staff_list_usernames' % request.user.username)
         return redirect('maintenance')
     if request.user.is_authenticated():
         return _handle_authenticated_application_request(request, maintenance_records)
