f69c37ffa72504b054a4f4eb8465f53cd92f8f77
Tharon Carlson <design@tharoncarlson.com>
2015-11-09 13:49:53 -0700

    Fix typo in modal (CantMoveAttached) message.

--- troposphere/static/js/components/modals/project/CantMoveAttachedModal.react.js
+++ troposphere/static/js/components/modals/project/CantMoveAttachedModal.react.js
@@ -16,7 +16,7 @@ define(function (require) {
         var content = (
             <div>
                 <h4>You are trying to move attached resources</h4>
-                <p>An instance or volume can not be moved while attached. To move these resources, please dettach them by first selecting the attached volume and then selecting the dettach option top right.</p>
+                <p>An instance or volume cannot be moved while attached. To move these resources, please detach them by first selecting the attached volume and then selecting the dettach option on the top right.</p>
             </div>
         );
 
