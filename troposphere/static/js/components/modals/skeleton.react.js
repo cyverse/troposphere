9cf272c85fea22b9b07b7417328cbade0ab56eb6
Connor Osborn <cdosborn@uw.edu>
2015-11-18 13:23:00 -0700

    Add a skeleton file for a bare bones (hehe) modal

---
+++ troposphere/static/js/components/modals/skeleton.react.js
@@ -0,0 +1,35 @@
+var React = require('react');
+var BootstrapModalMixin = require('components/mixins/BootstrapModalMixin.react');
+
+module.exports = React.createClass({
+
+      mixins: [BootstrapModalMixin],
+
+      render: function () {
+          var self = this;
+        return (
+          <div className="modal fade">
+            <div className="modal-dialog">
+              <div className="modal-content badge-modal-content">
+                <div className="modal-header">
+                  {this.renderCloseButton()}
+                  <strong>hello</strong>
+                </div>
+                <div className="modal-body">
+                  hello body
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-danger" onClick={this.hide}>
+                    Cancel
+                  </button>
+                  <button type="button" className="btn btn-primary" onClick={this.props.onConfirm}>
+                    Confirm
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+
+});
