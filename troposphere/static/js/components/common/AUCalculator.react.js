6d30d21f58098b6c6ab87949ec540232b3861172
Andrew Lenards <andrew.lenards@gmail.com>
2015-11-20 08:07:01 -0700

    Altered formatting and removed trailing whitespace.
    (cherry picked from commit 916d65ad406f9f183c6ee76f084bf56f88fc39c9)

--- troposphere/static/js/components/common/AUCalculator.react.js
+++ troposphere/static/js/components/common/AUCalculator.react.js
@@ -16,7 +16,7 @@ define(
       },
 
       componentDidMount: function () {
-        /* Calculate AU calculator options list one time as soon as the component initially renders 
+        /* Calculate AU calculator options list one time as soon as the component initially renders
         to avoid having to constantly calculate the same list every time a render occurs. */
 
         // Get instances user has and add create option elements for each of them.
@@ -33,7 +33,7 @@ define(
         var defaultOptions = sizes.map(function(size){
           return (
             <option key={size} value={size}>a {size} CPU instance</option>
-          ); 
+          );
         });
 
         // Combine the instance size list and defualt size list into one to display
@@ -47,7 +47,7 @@ define(
       handleCPUChange: function(e){
         this.setState({selectedCPU: Number(e.target.value)});
       },
-      
+
       render: function(){
         var instances = stores.InstanceStore.getAll();
 
@@ -62,7 +62,7 @@ define(
         }
         var selectedCPU = this.state.selectedCPU ? this.state.selectedCPU: options[0];
 
-        return(
+        return (
           <div>
             <div className='form-group'>
               <a role="button" data-toggle="collapse" href="#au-calculator" aria-expanded="true">
@@ -85,7 +85,7 @@ define(
                     Duration
                   </th>
                   <th>
-                    AU needed 
+                    AU needed
                   </th>
                   <tr className={remainingAU >= this.state.selectedCPU * 24 * 1 ? "success" : "warning"}><td>1 day</td><td>{(this.state.selectedCPU * 24 * 1)}</td></tr>
                   <tr className={remainingAU >= this.state.selectedCPU * 24 * 3 ? "success" : "warning"}><td>3 days</td><td>{(this.state.selectedCPU * 24 * 3)}</td></tr>
