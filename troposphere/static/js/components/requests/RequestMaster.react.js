ff366ae3d200495f3e6ec4b13eff9e7528d73b88
Joseph Garcia <prosif@gmail.com>
2015-11-23 10:05:35 -0700

    new requests directory for components related to user requests

---
+++ troposphere/static/js/components/requests/RequestMaster.react.js
@@ -0,0 +1,29 @@
+define(function (require) {
+
+  var React = require('react/addons'),
+    stores = require('stores'),
+    SecondaryRequestNavigation = require('./SecondaryRequestNavigation.react'),
+    Router = require('react-router'),
+    RouteHandler = Router.RouteHandler;
+
+  return React.createClass({
+    displayName: "MyRequestsPage",
+
+    mixins: [Router.State],
+
+    render: function() {
+      return (
+        <div>
+            <SecondaryRequestNavigation/>
+            <div className = "container admin">
+            <span className="adminHeader">
+            <RouteHandler />
+            </span>
+            </div>
+        </div>
+      );
+    }
+
+  });
+
+});
