7d02318ec1fdcaa096e7b483ae069535f72feb61
Tharon Carlson <design@tharoncarlson.com>
2015-11-24 10:17:52 -0700

    comments and formatting

--- troposphere/static/js/components/providers/ProviderDetail.react.js
+++ troposphere/static/js/components/providers/ProviderDetail.react.js
@@ -1,16 +1,13 @@
-define(function (require) {
-  "use strict";
+import React from 'react/addons';
+import Router from 'react-router';
+import stores from 'stores';
+import Name from './Name.react';
+import Stats from './Stats.react';
+import Description from './Description.react';
+import Instances from './Instances.react';
+import Resources from './Resources.react';
 
-  var React = require('react/addons'),
-    Router = require('react-router'),
-    stores = require('stores'),
-    Name = require('./Name.react'),
-    Stats = require('./Stats.react'),
-    Description = require('./Description.react'),
-    Instances = require('./Instances.react'),
-    Resources = require('./Resources.react');
-
-  return React.createClass({
+export default React.createClass({
     displayName: "ProviderListView",
 
     mixins: [Router.State],
@@ -20,31 +17,21 @@ define(function (require) {
       // we are fetching the provider here (and not in getInitialState) because the component
       // doesn't get re-mounted when the url changes, so those functions won't run twice
       var provider_id = Number(this.getParams().id);
-     // if (!provider_id) {
-     //     var provider_list = stores.ProviderStore.getAll();
-     //     if (provider_list !== null && provider_list.length > 0) {
-     //         provider_id = provider_list.first().id;
-     //     } else {
-     //         provider_id = -1;
-     //     }
-     // }
       var provider = stores.ProviderStore.get(provider_id);
 
-      if (!provider) return <div className="loading"></div>;
+      if (!provider) return <div className = "loading" ></div>;
 
       return (
-        <div className="provider-details">
-          <Name provider={provider}/>
+        <div className = "provider-details" >
+          <Name provider = {provider} />
           <hr/>
-          <Stats provider={provider}/>
-          <Description provider={provider}/>
-          <Instances provider={provider}/>
-          <Resources provider={provider}/>
+          <Stats provider = {provider} />
+          <Description provider = {provider} />
+          <Instances provider = {provider} />
+          <Resources provider = {provider} />
         </div>
       );
 
     }
 
   });
-
-});
