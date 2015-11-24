fa582c334900c86a2d5bef0a77d8ca884312915c
Tharon Carlson <design@tharoncarlson.com>
2015-11-24 10:29:58 -0700

    formatting

--- troposphere/static/js/components/providers/ProviderDetail.react.js
+++ troposphere/static/js/components/providers/ProviderDetail.react.js
@@ -14,24 +14,24 @@ export default React.createClass({
 
 
     render: function () {
-      // we are fetching the provider here (and not in getInitialState) because the component
-      // doesn't get re-mounted when the url changes, so those functions won't run twice
-      var provider_id = Number(this.getParams().id);
-      var provider = stores.ProviderStore.get(provider_id);
+        // we are fetching the provider here (and not in getInitialState) because the component
+        // doesn't get re-mounted when the url changes, so those functions won't run twice
+        var provider_id = Number(this.getParams().id);
+        var provider = stores.ProviderStore.get(provider_id);
 
-      if (!provider) return <div className = "loading" ></div>;
+        if (!provider) return <div className = "loading" ></div>;
 
-      return (
-        <div className = "provider-details" >
-          <Name provider = {provider} />
-          <hr/>
-          <Stats provider = {provider} />
-          <Description provider = {provider} />
-          <Instances provider = {provider} />
-          <Resources provider = {provider} />
-        </div>
-      );
+        return (
+            <div className = "provider-details" >
+                <Name provider = {provider} />
+                <hr/>
+                <Stats provider = {provider} />
+                <Description provider = {provider} />
+                <Instances provider = {provider} />
+                <Resources provider = {provider} />
+            </div>
+        );
 
     }
 
-  });
+});
