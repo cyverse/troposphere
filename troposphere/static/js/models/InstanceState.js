18d2ac88673dd50f015b425fe5d949466868a36c
Connor Osborn <cdosborn@uw.edu>
2015-12-20 11:57:47 -0700

    Refactor initialize, make es6
    The only changes are to initialize which I think is cleaner. Not a huge
    difference.

--- troposphere/static/js/models/InstanceState.js
+++ troposphere/static/js/models/InstanceState.js
@@ -1,122 +1,120 @@
-define(
-  [
-    'underscore',
-    'backbone'
-  ],
-  function (_, Backbone) {
+import _ from "underscore";
+import Backbone from "backbone";
 
-    var get_percent_complete = function (state, activiy) {
+// InstanceState manages the status/activity of an instance. An instance would
+// have an status/activity of active/rebooting for example.
 
-      // Number represents percent task *completed* when in this state
-      var states = {
-        'build': {
-          'block_device_mapping': 10,
-          'scheduling': 20,
-          'networking': 30,
-          'spawning': 40
-        },
-        'active': {
-          'powering-off': 50,
-          'image_uploading': 50,
-          'deleting': 50,
-          'suspending': 50,
-          'initializing': 50,
-          'networking': 60,
-          'deploying': 70
-        },
-        'hard_reboot': {
-          'rebooting-hard': 50
-        },
-        'reboot': {
-          'rebooting': 50
-        },
-        'shutoff': {
-          'powering-on': 50
-        },
-        'suspended': {
-          'resuming': 50
-        }
-      };
+var InstanceState = Backbone.Model.extend({
 
-      return states[state][activiy];
-    };
+  isInFinalState: function () {
+    var validStates = [
+      "active",
+      "error",
+      "active - deploy_error",
+      "suspended",
+      "shutoff"
+    ];
 
-    var get_final_state = function (activity) {
-      // Check for the final state to prevent reverting if a queued task hasn't begun yet
-      if (activity === 'powering-off') {
-        return 'shutoff';
-      }
-      else if (activity === 'deleting') {
-        return 'deleted';
-      }
-      else if (activity === 'suspending') {
-        return 'suspended';
-      }
-      else if (activity === 'deploy_error') {
-        return 'deploy_error';
-      }
-      else {
-        // Applies for: build, shutoff, and suspended
-        return 'active';
-      }
-    };
+    if (this.get('status') === "build") return false;
 
-    return Backbone.Model.extend({
+    var isInFinalState = validStates.indexOf(this.get('status_raw')) >= 0;
+    return isInFinalState;
+  },
 
-      isInFinalState: function () {
-        var validStates = [
-          "active",
-          "error",
-          "active - deploy_error",
-          "suspended",
-          "shutoff"
-        ];
+  isDeployError: function () {
+    return false;
+    var status = this.get('status');
+    var activity = this.get('activity');
 
-        if (this.get('status') === "build") return false;
+    if (status === "active" && activity === "deploy_error") {
+      return true;
+    } else {
+      return false;
+    }
+  },
 
-        var isInFinalState = validStates.indexOf(this.get('status_raw')) >= 0;
-        return isInFinalState;
-      },
+  getPercentComplete: function () {
+    var status = this.get('status');
+    var activity = this.get('activity');
+    var percentComplete = 100;
+    if (status && activity) {
+      percentComplete = get_percent_complete(status, activity);
+    }
+    return percentComplete;
+  },
 
-      isDeployError: function () {
-        return false;
-        var status = this.get('status');
-        var activity = this.get('activity');
+  initialize: function (attributes, options) {
 
-        if (status === "active" && activity === "deploy_error") {
-          return true;
-        } else {
-          return false;
-        }
-      },
+    var tokens = attributes.status_raw.split('-').map(s => s.trim());
+    var state = tokens[0];
+    var activity = tokens[1];
 
-      getPercentComplete: function () {
-        var status = this.get('status');
-        var activity = this.get('activity');
-        var percentComplete = 100;
-        if (status && activity) {
-          percentComplete = get_percent_complete(status, activity);
-        }
-        return percentComplete;
-      },
+    if (tokens.length == 3) {
+      // Deal with Openstack Grizzly's hyphenated states "powering-on" and "powering-off"
+      activity = tokens[1] + '-' + tokens[2];
+    }
 
-      initialize: function (attributes, options) {
+    this.set('status', state);
+    this.set('activity', activity);
+  }
 
-        var statusTokens = attributes.status_raw.split('-');
-        var state = statusTokens[0].trim();
-        var activity = null;
+});
 
-        if (statusTokens.length === 2) {
-          activity = statusTokens[1].trim();
-        } else if (statusTokens.length === 3) {
-          // Deal with Openstack Grizzly's hyphenated states "powering-on" and "powering-off"
-          activity = statusTokens[1].trim() + '-' + statusTokens[2].trim();
-        }
+var get_percent_complete = function (state, activiy) {
 
-        this.set('status', state);
-        this.set('activity', activity);
-      }
+  // Number represents percent task *completed* when in this state
+  var states = {
+    'build': {
+      'block_device_mapping': 10,
+      'scheduling': 20,
+      'networking': 30,
+      'spawning': 40
+    },
+    'active': {
+      'powering-off': 50,
+      'image_uploading': 50,
+      'deleting': 50,
+      'suspending': 50,
+      'initializing': 50,
+      'networking': 60,
+      'deploying': 70
+    },
+    'hard_reboot': {
+      'rebooting-hard': 50
+    },
+    'reboot': {
+      'rebooting': 50
+    },
+    'shutoff': {
+      'powering-on': 50
+    },
+    'suspended': {
+      'resuming': 50
+    }
+  };
 
-    });
+  // TODO: Handle the case where this dict lookup fails
+  return states[state][activiy];
+};
 
-  });
+var get_final_state = function (activity) {
+  // Check for the final state to prevent reverting if a queued task hasn't begun yet
+  if (activity === 'powering-off') {
+    return 'shutoff';
+  }
+  else if (activity === 'deleting') {
+    return 'deleted';
+  }
+  else if (activity === 'suspending') {
+    return 'suspended';
+  }
+  else if (activity === 'deploy_error') {
+    return 'deploy_error';
+  }
+  else {
+    // Applies for: build, shutoff, and suspended
+    return 'active';
+  }
+};
+
+export default InstanceState;
