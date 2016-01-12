01ea065b17c94147c9b8451c1632e53fb06c6036
Andrew Lenards <andrew.lenards@gmail.com>
2016-01-12 09:45:15 -0700

    Added missing `default` keyword for export.

--- troposphere/static/js/models/Quota.js
+++ troposphere/static/js/models/Quota.js
@@ -1,6 +1,6 @@
 import Backbone from 'backbone';
 import globals from 'globals';
 
-export Backbone.Model.extend({
+export default Backbone.Model.extend({
     url: globals.API_V2_ROOT + "/quotas"
 });
