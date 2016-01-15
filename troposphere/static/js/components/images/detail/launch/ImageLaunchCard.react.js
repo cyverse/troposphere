21251255f4d89ad82f194c9072bb7885210fae00
Andrew Lenards <andrew.lenards@gmail.com>
2016-01-15 15:14:57 -0700

    Changed _logged in_ concept to if `profile.get('selected_identity')`.
    Determining if a user is logged in has shifted over time from merely doing a
    check on `context.profile` to `context.profile.get('username')` to, now, if
    a valid `'selected_identity'` is available in the `profile` object.
    
    This notion may **not** hold true across various "clouds", so enriching the
    `context` to assign in making these checks (and, really, encapsulating the
    details away from the component) is *needed* in the future.

--- troposphere/static/js/components/images/detail/launch/ImageLaunchCard.react.js
+++ troposphere/static/js/components/images/detail/launch/ImageLaunchCard.react.js
@@ -23,6 +23,7 @@ define(
         var image = this.props.image;
         var versions = stores.ImageStore.getVersions(image.id);
         var type = stores.ProfileStore.get().get('icon_set');
+        var userLoggedIn = (context.profile && context.profile.get('selected_identity'));
 
         var iconSize = 145;
         // always use the Gravatar icons
@@ -32,7 +33,7 @@ define(
 
         // Hide bookmarking on the public page
         var bookmark;
-        if (context.profile) {
+        if (userLoggedIn) {
           bookmark = (
             <Bookmark image={image}/>
           );
@@ -40,9 +41,11 @@ define(
         //When versions is 'not loaded' OR 'has length > 0', you can launch.
         var canLaunch = (versions !== null && versions.length !== 0) ? true : false;
         var button;
-        if (context.profile) {
+        if (userLoggedIn) {
           button = (
-            <button className='btn btn-primary launch-button' onClick={this.props.onLaunch} disabled={!canLaunch}>
+            <button className='btn btn-primary launch-button'
+                onClick={this.props.onLaunch}
+                disabled={!canLaunch}>
               Launch
             </button>
           );
