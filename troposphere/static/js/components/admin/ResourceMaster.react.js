d0618b12097df36bba387fd5914e3c3640595fe6
Joseph Garcia <prosif@gmail.com>
2016-01-20 09:24:11 -0700

    link requests from ResourceMaster

--- troposphere/static/js/components/admin/ResourceMaster.react.js
+++ troposphere/static/js/components/admin/ResourceMaster.react.js
@@ -3,6 +3,7 @@ define(function (require) {
 
   var React = require('react/addons'),
     Router = require('react-router'),
+    OtherRouter = require('../../Router'),
     stores = require('stores'),
     ResourceRequest = require('./ResourceRequest.react'),
     RouteHandler = Router.RouteHandler;
@@ -11,23 +12,48 @@ define(function (require) {
 
     mixins: [Router.State],
 
+    getInitialState: function(){
+      return{
+        statusTypes: null,
+        requests: null
+      }
+    },
+
+    componentDidMount: function(){
+      var statusCallback = function(response){
+        this.setState({statusTypes: response.models});
+      }.bind(this);
+      var requestsCallBack = function(response){
+        this.setState({requests: response.models});
+      }.bind(this);
+      stores.StatusStore.getAllWithCallBack(statusCallback);
+      stores.ResourceRequestStore.getAllWithCallBack(requestsCallBack);
+    }, 
+
+    onResourceClick: function(request){
+      OtherRouter.getInstance().transitionTo("resource-request-detail", {request: request, id: request.id});
+    },
+
     render: function () {
-      var requests = stores.ResourceRequestStore.fetchWhere({
-          'status__name': 'pending'
-        }),
-        statuses = stores.StatusStore.getAll();
+      // var requests = stores.ResourceRequestStore.fetchWhere({
+      //     'status__name': 'pending'
+      //   });//,
+        //statuses = stores.StatusStore.getAll();
+      if (!this.state.requests || !this.state.statusTypes) return <div className="loading"></div>;
 
-      if (!requests || !statuses) return <div className="loading"></div>;
+      var resourceRequests = this.state.requests.map(function (request) {
+        var doThing = function(){
+          this.onResourceClick(request);
+        }.bind(this);
 
-      requests = stores.ResourceRequestStore.getAll();
-
-      var resourceRequestRows = requests.map(function (request) {
         return (
-          <ResourceRequest key={request.id} request={request}/>
-        )
-      });
+          <li onClick={doThing}>
+            {request.get('request')}
+          </li>
+        );
+      }.bind(this));
 
-      if (!resourceRequestRows[0]) {
+      if (!resourceRequests[0]) {
         return  (
           <div>
             <h3>No resource requests</h3>
@@ -37,16 +63,11 @@ define(function (require) {
 
       return (
         <div className="resource-master">
-          <h1>Resource Requests</h1>
-            <ul className="requests">
-              <li>
-                <h3>User</h3>
-                <h3>Request</h3>
-                <h3>Description</h3>
-              </li>
-              {resourceRequestRows}
-            </ul>
-            <RouteHandler />
+          <h3>Resource Requests</h3>
+          <ul className="requests-list pull-left">
+            {resourceRequests}
+          </ul>
+          <RouteHandler /> 
         </div>
       );
     }
