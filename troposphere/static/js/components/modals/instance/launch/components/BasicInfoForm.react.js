6cfd5c54b491545be894cd144f207801b93f8fc9
Connor Osborn <cdosborn@uw.edu>
2016-01-21 10:27:06 -0700

    WIP

--- troposphere/static/js/components/modals/instance/launch/components/BasicInfoForm.react.js
+++ troposphere/static/js/components/modals/instance/launch/components/BasicInfoForm.react.js
@@ -5,22 +5,28 @@ import SelectMenu from 'components/common/ui/SelectMenu.react';
 
 export default React.createClass({
 
+    // TODO: Its not clear what the interface for this method is, what props
+    // do i need to pass? One way to resolve this is to fill out prop types,
+    // or to make aliases to every prop at the top of the func, like 
+    //  var im = this.props.imageVersion
    render: function () {
         if (!this.props.imageVersion || !this.props.imageVersionList) {
             return (<div className="loading"/>);
         }
 
-        let name = function(item) { return item.get('name') };
-        let imageName = this.props.image.get('name');
         let imageVersionId = this.props.imageVersion.get('id');
+        let project = this.props.project;
+        let instanceName = this.props.instanceName || this.props.imageName;
+        console.log("BasicInfoForm i img?", this.props.instanceName, this.props.imageName);
+
         return (
             <form>
                 <div className="form-group">
                     <label for="instanceName">
                         Instance Name
                     </label>
-                    <input type="Name" className="form-control" id="instanceName" value={imageName}
-                        onChange={this.props.onNameChange}/>
+                    <input type="Name" className="form-control" id="instanceName" value={instanceName}
+                        onChange={(e) => this.props.onNameChange(e.target.value)}/>
                 </div>
                 <div className="form-group">
                     <label for="imageVersion">
@@ -29,7 +35,7 @@ export default React.createClass({
                     <SelectMenu
                         defaultId={this.props.imageVersion.get('id')}
                         list={this.props.imageVersionList}
-                        optionName={name}
+                        optionName={item => item.get('name')}
                         onSelectChange={this.props.onVersionChange}/>
                 </div>
                 <div className="form-group">
@@ -39,7 +45,7 @@ export default React.createClass({
                     <SelectMenu
                         defaultId={this.props.project.id}
                         list={this.props.projectList}
-                        optionName={name}
+                        optionName={item => item.get('name')}
                         onSelectChange={this.props.onProjectChange}/>
                 </div>
             </form>
