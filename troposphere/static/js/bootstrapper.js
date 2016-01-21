48f1fa864eaa6be03a9de6bb4abc3dbedaa5de00
Andrew Lenards <andrew.lenards@gmail.com>
2016-01-21 14:11:53 -0700

    Correct malformed merge resolution in bootstrapper.

--- troposphere/static/js/bootstrapper.js
+++ troposphere/static/js/bootstrapper.js
@@ -168,59 +168,13 @@ export default {
           }
         });
 
-        // We're wrapping Backbone.sync so that we can observe every AJAX request.
-        // If any request returns a 503 (service unavailable) then we're going to
-        // throw up the maintenance splash page. Otherwise, just pass the response
-        // up to chain.
-        var originalSync = Backbone.sync;
-        Backbone.sync = function(attrs, textStatus, xhr) {
-            // NOTE: a conceptually simpler solution would be to do this:
-            //
-            //    var xhr = originalSync.apply(this, arguments).catch(function(response){
-            //      if(response.status === 503) {
-            //        $('.splash-image').remove();
-            //        var MaintenanceComponent = React.createFactory(MaintenanceScreen);
-            //        React.render(MaintenanceComponent(), document.getElementById('application'));
-            //      }
-            //    });
-            //
-            //    return xhr;
-            //
-            // However, since we're using jQuery deferred objects for the promise chain, and they
-            // don't have a way to cancel promise propagation, we can end up in a scenario where we
-            // display a toast with an error on the maintenance splash screen (because other error
-            // handlers can still get called).  To get around that, we need to create our own promise
-            // and, based on the result of the AJAX request, either manually resolve or reject it.
+        return dfd.promise();
+      };
 
-
-            var dfd = $.Deferred();
-
-            originalSync.apply(this, arguments).then(function() {
-                dfd.resolve.apply(this, arguments);
-            }).fail(function(response) {
-                if (response.status === 503) {
-                    // need to make sure we remove the splash-image element is included in the HTML
-                    // template by default but re-apply the splash screen-class to body so that the
-                    // splash page displays correctly
-                    $('.splash-image').remove();
-                    $('body').addClass('splash-screen');
-
-                    // replace the current view with the
-                    var MaintenanceComponent = React.createFactory(MaintenanceScreen);
-                    React.render(MaintenanceComponent(), document.getElementById('application'));
-                } else {
-                    dfd.reject.apply(this, arguments);
-                }
-            });
-
-            return dfd.promise();
-        };
-
-        // render the splash page which will load the rest of the application
-        $(document).ready(function() {
-            var SplashScreenComponent = React.createFactory(SplashScreen);
-            React.render(SplashScreenComponent(), document.getElementById('application'));
-        });
-      }
+      // render the splash page which will load the rest of the application
+      $(document).ready(function () {
+        var SplashScreenComponent = React.createFactory(SplashScreen);
+        React.render(SplashScreenComponent(), document.getElementById('application'));
+      });
     }
 }
