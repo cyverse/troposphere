17928821ca2863dce49d44b09509784a8bc64f38
Andrew Lenards <andrew.lenards@gmail.com>
2016-02-03 15:24:54 -0700

    Remove tab indentation characters; Pro Tip™ - never mix tabs & spaces

--- troposphere/static/js/components/modals/instance/image/components/Tags.react.js
+++ troposphere/static/js/components/modals/instance/image/components/Tags.react.js
@@ -4,7 +4,7 @@ define(function (require) {
     Backbone = require('backbone'),
     stores = require('stores'),
     actions = require('actions'),
-		TagMultiSelect = require('components/common/tags/TagMultiSelect.react');
+        TagMultiSelect = require('components/common/tags/TagMultiSelect.react');
 
   return React.createClass({
     displayName: "Tags",
@@ -19,64 +19,64 @@ define(function (require) {
     getInitialState: function () {
       return {
         query: "",
-				newTagName: "",
-				newTagDescription: "",
-				showTagCreateForm: false
+                newTagName: "",
+                newTagDescription: "",
+                showTagCreateForm: false
       }
     },
 
-		createTagAndAddToImage: function(){
-			var newTag = actions.TagActions.create({name: this.state.newTagName, description: this.state.newTagDescription});
-			this.props.onTagAdded(newTag);
-			this.setState({newTagName: "", newTagDescription: "", showTagCreateForm: false});
-		},
+        createTagAndAddToImage: function(){
+            var newTag = actions.TagActions.create({name: this.state.newTagName, description: this.state.newTagDescription});
+            this.props.onTagAdded(newTag);
+            this.setState({newTagName: "", newTagDescription: "", showTagCreateForm: false});
+        },
 
     onQueryChange: function (query) {
       this.setState({query: query.toLowerCase()});
     },
 
-		onNewTagNameChange: function(name){
-			this.setState({newTagName: name.target.value});
-		},
+        onNewTagNameChange: function(name){
+            this.setState({newTagName: name.target.value});
+        },
 
-		onNewTagDescriptionChange: function(description){
-			this.setState({newTagDescription: description.target.value});
-		},
+        onNewTagDescriptionChange: function(description){
+            this.setState({newTagDescription: description.target.value});
+        },
 
-		isSubmittable: function(){
-			return this.state.newTagName && this.state.newTagDescription;
-		},
+        isSubmittable: function(){
+            return this.state.newTagName && this.state.newTagDescription;
+        },
 
-		onCreateNewTag: function(tagName) {
-			this.setState({
-				newTagName: tagName,
-				showTagCreateForm: true
-			});
-		},
+        onCreateNewTag: function(tagName) {
+            this.setState({
+                newTagName: tagName,
+                showTagCreateForm: true
+            });
+        },
 
     render: function () {
       var imageTags = this.props.imageTags,
         tags = stores.TagStore.getAll(),
         query = this.state.query,
-				tagCreateForm = null;
+                tagCreateForm = null;
 
-			if(this.state.showTagCreateForm){
-				tagCreateForm = (
-					<div className="form-group">
-						<label for="tag-create" className="control-label">Create new tag</label>
-						<form>
-							Name:<br />
-							<input className="form-control" type="text" onChange={this.onNewTagNameChange} value={this.state.newTagName} /><br />
-							Description:<br />
-							<textarea className="form-control" type="text" onChange={this.onNewTagDescriptionChange} value={this.state.newTagDescription} /><br />
-						</form>
-						<button disabled={!this.isSubmittable()} onClick={this.createTagAndAddToImage}
-							className="btn btn-primary btn-sm pull-right">
-							Create and add
-						</button>
-					</div>
-				);
-			}
+            if(this.state.showTagCreateForm){
+                tagCreateForm = (
+                    <div className="form-group">
+                        <label for="tag-create" className="control-label">Create new tag</label>
+                        <form>
+                            Name:<br />
+                            <input className="form-control" type="text" onChange={this.onNewTagNameChange} value={this.state.newTagName} /><br />
+                            Description:<br />
+                            <textarea className="form-control" type="text" onChange={this.onNewTagDescriptionChange} value={this.state.newTagDescription} /><br />
+                        </form>
+                        <button disabled={!this.isSubmittable()} onClick={this.createTagAndAddToImage}
+                            className="btn btn-primary btn-sm pull-right">
+                            Create and add
+                        </button>
+                    </div>
+                );
+            }
 
       if (!tags) return <div className="loading"/>;
 
@@ -105,14 +105,14 @@ define(function (require) {
               models={tags}
               activeModels={imageTags}
               onCreateNewTag={this.onCreateNewTag}
-							onModelAdded={this.props.onTagAdded}
+                            onModelAdded={this.props.onTagAdded}
               onModelRemoved={this.props.onTagRemoved}
               onModelCreated={this.props.onTagCreated}
               onQueryChange={this.onQueryChange}
               width={"100%"}
               placeholderText="Search by tag name..."
               />
-						{tagCreateForm}
+                        {tagCreateForm}
           </div>
         </div>
       );
