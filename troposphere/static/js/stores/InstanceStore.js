97389d1a4bb31427511b1e27f6151186ede99c81
Joseph Garcia <prosif@gmail.com>
2016-02-03 09:51:40 -0700

    removed at when adding to InstanceStore

--- troposphere/static/js/stores/InstanceStore.js
+++ troposphere/static/js/stores/InstanceStore.js
@@ -83,7 +83,7 @@ define(function (require) {
     switch (actionType) {
 
       case InstanceConstants.ADD_INSTANCE:
-        store.add({data: payload.instance, at: 0});
+        store.add(data: payload.instance);
         break;
 
       case InstanceConstants.UPDATE_INSTANCE:
