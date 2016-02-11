aa04d3a8229037642a6984ba19813c336c0744f9
Andrew Lenards <andrew.lenards@gmail.com>
2016-02-11 12:44:08 -0700

    Remove comment
    (cherry picked from commit ef303880d1310b853ee30a81fc30522af53dcac1)

--- troposphere/static/js/modals/PublicModals.js
+++ troposphere/static/js/modals/PublicModals.js
@@ -2,11 +2,6 @@
 import ModalHelpers from 'components/modals/ModalHelpers';
 import PublicSupportModal from 'components/modals/PublicSupportModal.react';
 
-/*
- I implement this without the severe nesting that other modals ´exhibit.
- Consider this a protest against the current approach.
- -- lenards
- */
 
 export default {
     showPublicSupportModal: function() {
