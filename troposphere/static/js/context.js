9061475dcf4f2e0c0d5a00a6eb655d21396aa51c
Andrew Lenards <andrew.lenards@gmail.com>
2016-02-18 10:28:36 -0700

    Use selected_identity for logged in user predicate

--- troposphere/static/js/context.js
+++ troposphere/static/js/context.js
@@ -1,7 +1,7 @@
 define([], function () {
   return {
     hasLoggedInUser: function() {
-        return !!(this.profile && this.profile.get('username'));
+        return !!(this.profile && this.profile.get('selected_identity'));
     },
     profile: null
   }
