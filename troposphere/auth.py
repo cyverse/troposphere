6df1e7a7f0987efc8e34c521e8c4de9a75f9dfde
Andrew Lenards <andrew.lenards@gmail.com>
2016-02-24 13:25:15 -0700

    Use exists() check from QuerySet; give logger-info context

--- troposphere/auth.py
+++ troposphere/auth.py
@@ -11,14 +11,17 @@ def has_valid_token(user):
     Returns boolean indicating if there are non-expired authentication
     tokens associated with the user.
     """
-    logger.info(hasattr(user, "auth_tokens"))
-    non_expired_tokens = user.auth_tokens.filter(only_current_tokens())
-    return len(non_expired_tokens) > 0
+    logger.info("user has auth_tokens attributes? %s" %
+        (hasattr(user, "auth_tokens")))
+
+    return user.auth_tokens.filter(only_current_tokens()).exists()
 
 
 def get_current_tokens(user):
     """
     Returns the non-expired authentication tokens.
     """
-    logger.info(hasattr(user, "auth_tokens"))
+    logger.info("user has auth_tokens attributes? %s" %
+        (hasattr(user, "auth_tokens")))
+
     return user.auth_tokens.filter(only_current_tokens())
