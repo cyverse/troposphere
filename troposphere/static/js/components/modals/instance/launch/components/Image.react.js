ab9bbba71e12ceb5b4d4b3feae9431d88e11de81
Tharon Carlson <design@tharoncarlson.com>
2016-03-07 11:36:16 -0700

    Refactor using import modules

--- troposphere/static/js/components/modals/instance/launch/components/Image.react.js
+++ troposphere/static/js/components/modals/instance/launch/components/Image.react.js
@@ -1,94 +1,88 @@
+import React from 'react';
+import Backbone from 'backbone';
+import stores from 'stores';
+import moment from 'moment';
+import Tags from 'components/common/tags/ViewTags.react';
+import Gravatar from 'components/common/Gravatar.react';
 
-define(
-  [
-    'react',
-    'backbone',
-    'stores',
-    'moment',
-    'components/common/tags/ViewTags.react',
-    'components/common/Gravatar.react'
-  ],
-  function (React, Backbone, stores, moment, Tags, Gravatar) {
+export default React.createClass({
+    displayName: "Image",
 
-    return React.createClass({
-      displayName: "Image",
+    getInitialState: function(){
+        let image = this.props.image;
+        let versionList = null;
+        let active = false;
+        if (image) {
+            versionList = image.get('versions');
 
-      getInitialState: function(){
-          let image = this.props.image;
-          let versionList = null;
-          let active = false;
-            if (image) {
-                versionList = image.get('versions');
-
-                if (versionList.length > 0) {
-                    active = true;
-                }
+            if (versionList.length > 0) {
+                active = true;
             }
-            return {
-                active
-            }
-      },
-
-      propTypes: {
-        image: React.PropTypes.instanceOf(Backbone.Model).isRequired,
-        onClick: React.PropTypes.func
-      },
-
-      handleClick: function () {
-        if (this.state.active) {
-            this.props.onSelectImage(this.props.image);
         }
-      },
+        return {
+            active
+        }
+    },
 
-      renderTags: function () {
-        var tags = stores.TagStore.getAll();
-        var activeTags = stores.TagStore.getImageTags(this.props.image);
+    propTypes: {
+    image: React.PropTypes.instanceOf(Backbone.Model).isRequired,
+    onClick: React.PropTypes.func
+    },
 
-        return (
-          <Tags tags={tags} activeTags={activeTags} renderLinks={false}/>
-        );
-      },
+    handleClick: function () {
+    if (this.state.active) {
+        this.props.onSelectImage(this.props.image);
+    }
+    },
 
-      render: function () {
-        var image = this.props.image,
-          type = stores.ProfileStore.get().get('icon_set'),
-          imageCreationDate = moment(image.get('start_date')).format("MMM D, YYYY hh:mm a"),
-          iconSize = 67,
-          icon;
-        let fullDescription = image.get('description');
-        let renderDescription = fullDescription.length < 150 ? fullDescription : (fullDescription.substring(0,150) + " ...");
+    renderTags: function () {
+    var tags = stores.TagStore.getAll();
+    var activeTags = stores.TagStore.getImageTags(this.props.image);
 
-        // always use the Gravatar icons
-        icon = (
-          <Gravatar hash={image.get('uuid_hash')} size={iconSize} type={type}/>
-        );
+    return (
+        <Tags tags={tags} activeTags={activeTags} renderLinks={false}/>
+    );
+    },
 
-        let inactiveClass = !this.state.active ? "disabled" : "";
+    render: function () {
+    var image = this.props.image,
+        type = stores.ProfileStore.get().get('icon_set'),
+        imageCreationDate = moment(image.get('start_date')).format("MMM D, YYYY hh:mm a"),
+        iconSize = 67,
+        icon;
+    let fullDescription = image.get('description');
+    let renderDescription = fullDescription.length < 150 ? fullDescription : (fullDescription.substring(0,150) + " ...");
 
-        return (
-            <li className={`media card ${inactiveClass}`} onClick={this.handleClick}>
-                    <div className="media__img">
-                        {icon}
-                    </div>
-                    <div className="media__content">
-                        <div className="row">
-                            <div className="col-md-3 t-wordBreaker">
-                                <h2 className="t-body-1" style={{margin: "0 0 5px"}}>{image.get('name')}</h2>
-                                <hr style={{margin: "0 0 5px" }}/>
-                                <time>{imageCreationDate}</time> by <strong>{image.get('created_by').username}</strong>
-                            </div>
-                            <p className="media__description col-md-5">
-                                {renderDescription}
-                            </p>
-                            <div className="col-md-4">
-                                {this.renderTags()}
-                            </div>
+    // always use the Gravatar icons
+    icon = (
+        <Gravatar hash={image.get('uuid_hash')} size={iconSize} type={type}/>
+    );
+
+    let inactiveClass = !this.state.active ? "disabled" : "";
+
+    return (
+        <li className={`media card ${inactiveClass}`} onClick={this.handleClick}>
+                <div className="media__img">
+                    {icon}
+                </div>
+                <div className="media__content">
+                    <div className="row">
+                        <div className="col-md-3 t-wordBreaker">
+                            <h2 className="t-body-1" style={{margin: "0 0 5px"}}>{image.get('name')}</h2>
+                            <hr style={{margin: "0 0 5px" }}/>
+                            <time>{imageCreationDate}</time> by <strong>{image.get('created_by').username}</strong>
+                        </div>
+                        <p className="media__description col-md-5">
+                            {renderDescription}
+                        </p>
+                        <div className="col-md-4">
+                            {this.renderTags()}
                         </div>
                     </div>
-            </li>
-        )
-      }
+                </div>
+        </li>
+    )
+    }
 
-    });
+});
 
-  });
