c5261c074649a46125e3a35d6e635d544db6395a
Joseph Garcia <prosif@gmail.com>
2016-03-09 17:50:51 -0700

    remove unused modals

--- troposphere/static/js/modals/BadgeModals.js
+++ troposphere/static/js/modals/BadgeModals.js
@@ -1,9 +1,7 @@
 define(function (require) {
 
   return {
-    showBadge: require('./badge/ShowBadge').ShowBadge,
     showHelp: require('./badge/ShowHelp').ShowHelp,
-    showMyBadge: require('./badge/ShowMyBadge').ShowMyBadge
   };
 
 });
