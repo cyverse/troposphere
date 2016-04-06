df115439dbd79fc5f9d0667d062ef2c9c8b6b89c
Joseph Garcia <prosif@gmail.com>
2016-04-06 15:18:07 -0700

    add past instance history details

---
+++ troposphere/static/js/components/projects/resources/instance/details/sections/PastInstanceDetailsSection.react.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Backbone from 'backbone';
+import Status from './details/Status.react';
+import Id from './details/Id.react';
+import Alias from './details/Alias.react';
+import Size from './details/Size.react';
+import LaunchDate from './details/LaunchDate.react';
+import CreatedFrom from './details/CreatedFrom.react';
+import Identity from './details/Identity.react';
+
+var PastInstanceDetailsSection = React.createClass({
+    displayName: "PastInstanceDetailsSection",
+
+    propTypes: {
+        instance: React.PropTypes.instanceOf(Backbone.Model).isRequired
+    },
+
+    render: function () {
+        var instance = this.props.instance;
+
+        return (
+          <div className="resource-details-section section">
+            <h4 className="t-title">Instance Details</h4>
+            <ul>
+              <Status instance={instance}/>
+              <Size instance={instance}/>
+              <LaunchDate instance={instance}/>
+              <CreatedFrom instance={instance}/>
+              <Identity instance={instance}/>
+              <Id instance={instance}/>
+              <Alias instance={instance}/>
+            </ul>
+          </div>
+        );
+    }
+
+});
+
+export default PastInstanceDetailsSection;
