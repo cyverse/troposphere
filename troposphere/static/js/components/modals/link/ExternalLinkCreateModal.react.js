ac365a25d1e80a7a865e4523ce35c18c35e0165c
Tharon Carlson <design@tharoncarlson.com>
2016-04-11 15:30:36 -0700

    refactor and include empty field messages

--- troposphere/static/js/components/modals/link/ExternalLinkCreateModal.react.js
+++ troposphere/static/js/components/modals/link/ExternalLinkCreateModal.react.js
@@ -25,28 +25,7 @@ define(
         initialExternalLinkName: React.PropTypes.string
       },
 
-      isSubmittable: function () {
-        var hasName = !!this.state.name;
-        var hasDescription = !!this.state.description;
-        var hasLink = !!this.state.link;
-        var title_error = !!this.state.title_error;
-        var validLink = (new RegExp('https?://')).test(this.state.link) && this.state.link.startsWith('http');
-        return hasName && hasDescription && hasLink && !title_error && validLink;
-      },
-
       componentDidMount: function (){
-        if (this.state.name) {
-          var lower = this.state.name.toLowerCase();
-          external_links = stores.ExternalLinkStore.getAll().filter(function (external_link) {
-            return external_link.get('title').toLowerCase() === lower;
-          });
-          if(external_links.length > 0){
-            this.setState({title_error: true, existsText: "ExternalLink " + this.state.name + " already exists"});
-          }
-          else{
-            this.setState({title_error: false, existsText: ""});
-          }
-        }
       },
 
       //
@@ -57,8 +36,9 @@ define(
       getInitialState: function () {
         return {
           name: this.props.initialExternalLinkName,
-          description: null,
-          link: null
+          description: "",
+          link: "",
+          shouldValidate: false
         }
       },
 
@@ -76,10 +56,15 @@ define(
       },
 
       confirm: function () {
+        if (!this.isSubmitable) {
+          this.setState({
+              shouldValidate: true
+          }); return
+        }
+
         this.hide();
         this.props.onConfirm($.trim(this.state.name), this.state.description, this.state.link);
       },
-
 
       //
       // Custom Modal Callbacks
@@ -87,36 +72,121 @@ define(
       //
 
       onNameChange: function (e) {
-        var newName = e.target.value;
+        let newName = e.target.value;
         this.setState({name: newName});
-        if (newName) {
-          var lower = $.trim(newName.toLowerCase());
-          external_links = stores.ExternalLinkStore.getAll().filter(function (external_link) {
-            return external_link.get('title').toLowerCase() === lower;
-          });
-          if(external_links.length > 0){
-            this.setState({title_error: true, titleErrorText: "ExternalLink with name \"" + newName + "\" already exists"});
-          } else {
-            this.setState({title_error: false, titleErrorText: ""});
-          }
-        }
       },
 
       onLinkChange: function (e) {
-        var newLink = e.target.value;
-        var validLink = (new RegExp('https?://')).test(newLink);
-        if (!validLink) {
-            this.setState({link: newLink, link_error: true, linkErrorText: "ExternalLink is missing http(s)://"});
-        } else if(!newLink.startsWith('http')) {
-            this.setState({link: newLink, link_error: true, linkErrorText: "ExternalLink must start with http(s)://"});
-        } else {
-            this.setState({link: newLink, link_error: false, linkErrorText: ""});
-        }
+        let newLink = e.target.value;
+        this.setState({link: newLink});
       },
 
       onDescriptionChange: function (e) {
-        var newDescription = e.target.value;
+        let newDescription = e.target.value;
         this.setState({description: newDescription});
+      },
+
+      //
+      // Modal Validation
+      // ------------------
+      //
+
+      validateTitle: function() {
+        let title = this.state.name;
+        if (!title.length) {
+          return {
+            valid: false,
+            cause: "empty"
+          }
+        }
+
+        if (title) {
+          let lower = $.trim(title.toLowerCase());
+          let externalLinks = stores.ExternalLinkStore.getAll()
+            .filter(function (externalLink) {
+              return externalLink.get('title').toLowerCase() === lower;
+          });
+
+          if (externalLinks.length > 0) {
+            return {
+              valid: false,
+              cause: "duplicate"
+            }
+          }
+        }
+
+        return {
+            valid: true,
+            cause: ""
+        }
+      },
+
+      validateLink: function() {
+        let link = this.state.link;
+        let validLink = (link.search("https?://") !== -1);
+        if (!link.length) {
+            return {
+                valid: false,
+                cause: "empty"
+            }
+        }
+        if (!validLink) {
+          return {
+            valid: false,
+            cause: "notURL"
+          }
+        }
+
+        return {
+          valid: true,
+          cause: ""
+        }
+      },
+
+      isSubmittable: function() {
+        let title = this.validateTitle().valid;
+        let description = !!this.state.description;
+        let link = this.validateLink().valid;
+        return title && description && link;
+      },
+
+      errorMessages: function() {
+        // Title Messages
+        let title = this.validateTitle;
+        let titleMessage;
+        if (!title().valid) {
+          if (title().cause === "empty") {
+            titleMessage = "Link must have a title";
+          }
+          if (title().cause === "duplicate") {
+            titleMessage = "ExternalLink with name \"" + this.state.name + "\" already exists";
+          }
+        }
+
+        // Description messages
+        let description = this.state.description;
+        let descriptionMessage;
+        if (!description) {
+          descriptionMessage = "Link must have a description";
+        }
+
+        // Link Messages
+        let link = this.validateLink;
+        let linkMessage;
+        if (!link().valid) {
+          if (link().cause === "empty") {
+            linkMessage = "Link must have a URL";
+          }
+          if (link().cause === "notURL") {
+            linkMessage = "Link URL must start with http(s)://";
+          }
+        }
+
+        return {
+          titleMessage,
+          descriptionMessage,
+          linkMessage
+        }
       },
 
       //
@@ -125,39 +195,63 @@ define(
       //
 
       renderBody: function () {
-        var formattedTitleError = <p className="no-results text-danger">{this.state.titleErrorText}</p>
-        var formattedLinkError = <p className="no-results text-danger">{this.state.linkErrorText}</p>
+        let formattedTitleError;
+        let formattedDescriptionError;
+        let formattedLinkError;
+        if (!this.isSubmittable() && this.state.shouldValidate) {
+          formattedTitleError = (
+            <p className="no-results text-danger">
+              {this.errorMessages().titleMessage}
+            </p>
+          );
+
+          formattedDescriptionError = (
+            <p className="no-results text-danger">
+              {this.errorMessages().descriptionMessage}
+            </p>
+          );
+
+          formattedLinkError = (
+            <p className="no-results text-danger">
+              {this.errorMessages().linkMessage}
+            </p>
+          );
+        }
+
         return (
           <div role='form'>
 
             <div className='form-group'>
               <label htmlFor='linkName'>Link Title</label>
-              <input type="text"
-                     className="form-control"
-                     value={this.state.name}
-                     onChange={this.onNameChange}
-                />
+              <input 
+                type="text"
+                className="form-control"
+                value={this.state.name}
+                onChange={this.onNameChange}
+              />
               {formattedTitleError}
             </div>
 
             <div className='form-group'>
               <label htmlFor='linkSize'>Link Description</label>
-              <textarea id='project-description'
-                        type='text'
-                        className='form-control'
-                        rows="7"
-                        value={this.state.description}
-                        onChange={this.onDescriptionChange}
-                />
+              <textarea 
+                 id='project-description'
+                 type='text'
+                 className='form-control'
+                 rows="7"
+                 value={this.state.description}
+                 onChange={this.onDescriptionChange}
+              />
+              {formattedDescriptionError}
             </div>
 
             <div className='form-group'>
               <label htmlFor='linkName'>Link URL</label>
               <input type="text"
-                     className="form-control"
-                     value={this.state.link}
-                     onChange={this.onLinkChange}
-                />
+                className="form-control"
+                value={this.state.link}
+                onChange={this.onLinkChange}
+              />
               {formattedLinkError}
             </div>
 
@@ -166,12 +260,18 @@ define(
       },
 
       render: function () {
-        var footerErrorText;
+        let footerErrorText;
 
-        if(this.state.title_error || this.state.link_error){
+       if (!this.isSubmittable() && this.state.shouldValidate){
           footerErrorText = <p className="text-danger">ExternalLink can not be created. Please fix the error(s) above.</p>;
         }
 
+        let isDisabled = false;
+        if (this.state.shouldValidate) {
+            if (!this.isSubmittable()) {
+                isDisabled = true;
+            }
+        }
         return (
           <div className="modal fade">
             <div className="modal-dialog">
@@ -189,7 +289,7 @@ define(
                     Cancel
                   </button>
                   <button type="button" className="btn btn-primary" onClick={this.confirm}
-                          disabled={!this.isSubmittable()}>
+                          disabled={isDisabled}>
                     Create Link
                   </button>
                 </div>
@@ -198,7 +298,6 @@ define(
           </div>
         );
       }
-
     });
 
   });
