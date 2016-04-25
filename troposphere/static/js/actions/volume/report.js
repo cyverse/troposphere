c555b959a94c5199a89eb6ce80736cf2466d52e2
Connor Osborn <cdosborn@uw.edu>
2016-04-25 19:02:45 +0000

    Let atmo build emails, just send data

--- troposphere/static/js/actions/volume/report.js
+++ troposphere/static/js/actions/volume/report.js
@@ -13,35 +13,18 @@ define(function (require) {
       if (!params.reportInfo) throw new Error("Missing reportInfo");
       if (!params.volume) throw new Error("Missing volume");
 
-      var profile = stores.ProfileStore.get(),
-        username = profile.get('username'),
-        reportUrl = globals.API_ROOT + "/email/support",
-        problemText = "",
-        reportInfo = params.reportInfo,
-        reportData = {},
-        volume = params.volume;
+      let volume = params.volume, 
+          reportInfo = params.reportInfo;
 
-      if (reportInfo.problems) {
-        _.each(reportInfo.problems, function (problem) {
-          problemText = problemText + "  -" + problem + "\n";
-        })
-      }
-
-      reportData = {
-        username: username,
-        message: "Volume ID: " + volume.id + "\n" +
-                 "Provider ID: " + volume.get('identity').provider + "\n" +
-                 "\n" +
-                 "Problems" + "\n" +
-                 problemText + "\n" +
-                 "Details \n" +
-                 reportInfo.details + "\n",
-        subject: "Atmosphere Volume Report from " + username,
-        "user-interface": 'troposphere'
+      let reportData = {
+        message:  reportInfo.details,
+        volume: volume.id,
+        problems: reportInfo.problems ? reportInfo.problems : [],
+        ...Utils.browserContext()
       };
 
       $.ajax({
-        url: reportUrl,
+        url: globals.API_V2_ROOT + "/email_volume_report",
         type: 'POST',
         data: JSON.stringify(reportData),
         dataType: 'json',
