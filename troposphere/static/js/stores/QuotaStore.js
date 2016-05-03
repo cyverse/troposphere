ad5f99df1c098345f0800186bdb318fafc522b34
Joseph Garcia <prosif@gmail.com>
2016-05-03 14:53:19 -0700

    register dispatcher to quota store

--- troposphere/static/js/stores/QuotaStore.js
+++ troposphere/static/js/stores/QuotaStore.js
@@ -1,17 +1,36 @@
-define(function (require) {
+import BaseStore from 'stores/BaseStore';
+import QuotaCollection from 'collections/QuotaCollection';
+import Dispatcher from 'dispatchers/Dispatcher';
+import QuotaConstants from 'constants/QuotaConstants';
 
-  var BaseStore = require('stores/BaseStore'),
-    QuotaCollection = require('collections/QuotaCollection');
-
-  var QuotaStore = BaseStore.extend({
+var QuotaStore = BaseStore.extend({
     collection: QuotaCollection,
 
     queryParams: {
       page_size: 100
     }
-  });
-
-  var store = new QuotaStore();
-
-  return store;
 });
+
+var store = new QuotaStore();
+
+Dispatcher.register(function (dispatch) {
+    var actionType = dispatch.action.actionType;
+    var payload = dispatch.action.payload;
+    var options = dispatch.action.options || options;
+
+    switch (actionType) {
+        case QuotaConstants.CREATE_QUOTA:
+            store.add(payload.quota);
+            break;
+        default:
+            return true;
+    }
+
+    if (!options.silent) {
+        store.emitChange();
+    }
+
+    return true;
+});
+
+export default store;
