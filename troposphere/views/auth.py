b2ac9b85f8a0c79c3b8ea6ca06e06ae7e26641c6
Andrew Lenards <andrew.lenards@gmail.com>
2016-05-06 13:32:53 -0700

    [revertible-triage-commit] Trace info for sessionid cookie

--- troposphere/views/auth.py
+++ troposphere/views/auth.py
@@ -119,7 +119,24 @@ def _oauth_login(request):
     redirect_url = request.GET.get('redirect_to')
     if redirect_url:
         request.session['redirect_to'] = redirect_url
-    return redirect(cas_oauth_client.authorize_url())
+
+    response = redirect(cas_oauth_client.authorize_url())
+
+    logger.info(request.META['REMOTE_ADDR'])
+    if (request.META['REMOTE_ADDR'] == '128.196.38.108' or
+        request.META['REMOTE_ADDR'] == '127.0.0.1'):
+        logger.info("REQUEST ******************** \n")
+        logger.info(request.session)
+        logger.info(request.COOKIES)
+        logger.info(request.META['REMOTE_ADDR'])
+        logger.info(request.user.username)
+
+        logger.info("RESPONSE ******************** \n")
+        logger.info(response.cookies)
+        logger.info('access_token' in response)
+
+
+    return response
 
 
 # CAS OAuth callback ( After the Authorize is OK)
@@ -147,4 +164,19 @@ def cas_oauth_service(request):
         redirect_url = request.session.pop('redirect_to')
         return redirect(redirect_url)
 
-    return redirect('application')
+    response = redirect('application')
+
+    logger.info(request.META['REMOTE_ADDR'])
+    if (request.META['REMOTE_ADDR'] == '128.196.38.108' or
+        request.META['REMOTE_ADDR'] == '127.0.0.1'):
+        logger.info("REQUEST ******************** \n")
+        logger.info(request.session)
+        logger.info(request.COOKIES)
+        logger.info(request.META['REMOTE_ADDR'])
+        logger.info(request.user.username)
+
+        logger.info("RESPONSE ******************** \n")
+        logger.info(response.cookies)
+        logger.info('access_token' in response)
+
+    return response
