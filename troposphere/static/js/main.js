7de64acfb5badaa8c0dce1db5bbb6af6cdd272dd
Andrew Lenards <andrew.lenards@gmail.com>
2016-05-12 16:12:58 -0700

    Initial JavaScript logging
    Currently, we have an organization (CyVerse) & project (Troposphere) in
    the Free plan with Sentry:
    
    https://app.getsentry.com/cyverse/troposphere
    
    This only allows 1 member - me, lenards; we will be evaluate this &
    consider "hosted vs on-site" options.

--- troposphere/static/js/main.js
+++ troposphere/static/js/main.js
@@ -1,4 +1,9 @@
 import bootstrapper from "bootstrapper";
 import "css/app/app.scss";
+import Raven from 'raven-js';
+
+let sentryDSN = 'https://27643f06676048be96ad6df686c17da3@app.getsentry.com/73366';
+
+Raven.config(sentryDSN, {release: '6a34f86c188811e698eb0242ac130001'}).install();
 
 bootstrapper.run();
