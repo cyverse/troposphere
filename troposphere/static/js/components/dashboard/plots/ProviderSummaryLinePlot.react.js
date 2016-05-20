ad09a2f48b196a3947ba24d681d31450acc19d41
Andrew Lenards <andrew.lenards@gmail.com>
2016-05-20 14:58:38 -0700

    Provide explanatory comment for findMaxDataPt

--- troposphere/static/js/components/dashboard/plots/ProviderSummaryLinePlot.react.js
+++ troposphere/static/js/components/dashboard/plots/ProviderSummaryLinePlot.react.js
@@ -7,7 +7,15 @@ define(function (require) {
         ResourceUseTooltip = require("./tooltips/ResourceUseTooltip.react");
 
 /**
-
+ * Finds a maximum within the data points for all providers
+ *
+ * If the maximum does not exceed the _soft_ limit, `ceiling`, return
+ * the greatest value present.
+ *
+ * @param {object} seriesData - array of provider's series data [1]
+ * @param {int} ceiling - y Axis soft limit
+ *
+ * [1] http://api.highcharts.com/highcharts#plotOptions.series
  */
 function findMaxDataPt(seriesData, ceiling) {
     // series data has an array of data points *per* provider
