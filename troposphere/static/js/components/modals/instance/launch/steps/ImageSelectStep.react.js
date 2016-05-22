f1981cc295348919805e9ae19098f5712cfc2041
Connor Osborn <cdosborn@uw.edu>
2016-05-22 06:00:19 +0000

    Implement input with delay and refactor
    - Add static variables
    - Replace string comparison with int comparison (VIEW vars)
    - Refactor
    
    This class will be a good starting point for the network dependency refactor,
    because I made the different network parts more explicit (added get vs.
    fetch). This should make it more clear what a solution will need to provide.

--- troposphere/static/js/components/modals/instance/launch/steps/ImageSelectStep.react.js
+++ troposphere/static/js/components/modals/instance/launch/steps/ImageSelectStep.react.js
@@ -6,240 +6,317 @@ import stores from 'stores';
 import TabLinks from 'components/common/ui/TabLinks.react';
 import ImageCollection from 'collections/ImageCollection';
 import ImageList from '../components/ImageList.react';
+import ComponentHandleInputWithDelay from 'components/mixins/ComponentHandleInputWithDelay';
 import InstanceLaunchFooter from '../components/InstanceLaunchFooter.react';
 
 export default React.createClass({
     displayName: "InstanceLaunchWizardModal-ImageSelectStep",
-    views: ['Show Featured', 'Show Favorites', 'Show All'],
+    mixins: [ComponentHandleInputWithDelay],
 
-    getInitialState: function () {
+    RESULTS_PER_PAGE: 20,
+    FEATURED_VIEW: 0,
+    FAVORITE_VIEW: 1,
+    ALL_VIEW:      2,
+
+    getInitialState: function() {
         return {
-            listView: this.views[0],
+            images: null,
             query: null,
-            resultsPerPage: 20,
+            view: this.FEATURED_VIEW,
             page: 1
         }
     },
 
-    componentDidMount: function () {
-        stores.ImageBookmarkStore.addChangeListener(this.triggerRenderOnStore);
-        stores.ImageStore.addChangeListener(this.triggerRenderOnStore);
-        stores.TagStore.addChangeListener(this.triggerRenderOnStore);
+    componentDidMount: function() {
+        stores.ImageBookmarkStore.addChangeListener(this.updateState);
+        stores.ImageStore.addChangeListener(this.updateState);
+        stores.TagStore.addChangeListener(this.updateState);
         this.focusSearchInput();
+
+        // Here we have the option to prime the data, in the future
+        // subscribing to a data source should do this automatically
+        //
+        // this.fetchAll();
+        // this.fetchFeatured();
+        // this.fetchFavorites();
     },
 
-    componentWillUnmount: function () {
-        stores.ImageBookmarkStore.removeChangeListener(this.triggerRenderOnStore);
-        stores.ImageStore.removeChangeListener(this.triggerRenderOnStore);
-        stores.TagStore.removeChangeListener(this.triggerRenderOnStore);
+    componentWillUnmount: function() {
+        stores.ImageBookmarkStore.removeChangeListener(this.updateState);
+        stores.ImageStore.removeChangeListener(this.updateState);
+        stores.TagStore.removeChangeListener(this.updateState);
     },
 
-    triggerRenderOnStore: function() {
-        this.setState({})
+    updateState: function() {
+        let images;
+        switch (this.state.view) {
+            case this.ALL_VIEW:
+                images = this.fetchAll();
+                break;
+            case this.FEATURED_VIEW:
+                images = this.fetchFeatured();
+                break;
+            case this.FAVORITE_VIEW:
+                images = this.fetchFavorites();
+                break;
+        }
+        this.setState({ images });
     },
 
-    handleChange: function(e) {
-        this.setState({query: e.target.value})
+    onSearchChange: function(e) {
+        let input = e.target.value.trim();
+
+        // Skip the timeout, if the query is empty
+        if (!input) {
+            this.setState({query: input}, this.updateState);
+        } else {
+
+            // The callback will be called at least after 500 ms, if the
+            // function is called again, its internal timer will be reset
+            this.setState({query: input}, () => {
+                this.callIfNotInterruptedAfter(500 /*ms*/, this.updateState);
+            });
+        }
+
     },
 
     handleLoadMoreImages: function() {
         this.setState({page: this.state.page + 1})
     },
 
-    onChangeView: function(listView) {
-        this.setState({
-            listView
-        });
+    onChangeView: function(view) {
+        this.setState({ view }, this.updateState);
     },
 
-    showAll: function() {
+    fetchAll: function() {
         let query = this.state.query;
-        let images = stores.ImageStore.getAll();
+        let images;
         if (query) {
             images = stores.ImageStore.fetchWhere({
                 search: query
             });
+        } else {
+            images = stores.ImageStore.getAll();
         }
 
-        return images
+        return images;
     },
-
-    showFeatured: function() {
+    getAll: function() {
         let query = this.state.query;
-        let images = stores.ImageStore.fetchWhere({
-            tags__name: 'Featured'
-        });
+        let images;
+        if (query) {
+            images = stores.ImageStore.getWhere({
+                search: query
+            });
+        } else {
+            images = stores.ImageStore.getAll();
+        }
+
+        return images;
+    },
+    fetchFeatured: function() {
+        let query = this.state.query;
+        let images;
         if (query) {
             images = stores.ImageStore.fetchWhere({
                 tags__name: 'Featured',
                 search: query
             });
+        } else {
+            images = stores.ImageStore.fetchWhere({
+                tags__name: 'Featured'
+            });
         }
 
-        return images
+        return images;
+    },
+    getFeatured: function() {
+        let query = this.state.query;
+        let images;
+        if (query) {
+            images = stores.ImageStore.getWhere({
+                tags__name: 'Featured',
+                search: query
+            });
+        } else {
+            images = stores.ImageStore.getWhere({
+                tags__name: 'Featured'
+            });
+        }
+
+        return images;
     },
 
-    showFavorites: function() {
+    fetchFavorites: function() {
         let query = this.state.query;
-        let images = stores.ImageStore.fetchWhere({
-            bookmarked: true
-        });
+        let images;
         if (query) {
             images = stores.ImageStore.fetchWhere({
                 bookmarked: true,
                 search: query
             });
+        } else {
+            images = stores.ImageStore.fetchWhere({
+                bookmarked: true
+            });
         }
 
-        return images
+        return images;
     },
 
-    imageList: function() {
-        let listView = this.state.listView;
-        switch(listView) {
-            case 'Show All': return this.showAll()
-            case 'Show Featured': return this.showFeatured()
-            case 'Show Favorites': return this.showFavorites()
-        }
-    },
-
-    renderFilterDescription: function (images) {
-        let message;
+    getFavorites: function() {
         let query = this.state.query;
-        let queryText = "";
-        if (query) { queryText = `for "${query}"` }
-        if (images) {
-            if (images.length >= 20) {
-                message = `Showing first ${images.length} images ${queryText}`;
-            }
-            else {
-                message = `Showing ${images.length} image(s) ${queryText}`;
-            }
-        }
-
-        return (
-            <div className="filter-description">{message}</div>
-        );
-    },
-
-    renderNoResultsFor: function () {
-        let query = this.state.query;
-        let message = "";
-        let listView = this.state.listView;
-        if (listView === "Show Favorites") {
-            message = 'You currently have no favorited images';
-        }
+        let images;
         if (query) {
-            message = 'No images found matching "' + query + '"';
+            images = stores.ImageStore.getWhere({
+                bookmarked: true,
+                search: query
+            });
+        } else {
+            images = stores.ImageStore.getWhere({
+                bookmarked: true
+            });
         }
 
-        return (
-            <div className="filter-description">{message}</div>
-        );
+        return images;
     },
 
-    renderMoreImagesButton: function (images, totalNumberOfImages) {
-        if (images.length < totalNumberOfImages) {
+    renderFilterDescription: function(images, total) {
+        let query = this.state.query;
+        let view = this.state.view;
+        total = total || 0;
+
+        // Waiting on a network request, or user is typing
+        if (!images || this.awaitingTimeout()) {
             return (
-                <li>
-                    <button style={{
-                        "margin": "15px auto",
-                        "display": "block"
-                    }} className="btn btn-default" onClick={this.handleLoadMoreImages}>
-                        Show more images...
-                    </button>
-                </li>
+                // Note: this div collapses w/o nbsp
+                <div className="filter-description">&nbsp;</div>
             );
         }
+
+        let message;
+        if (images.length === 0) {
+            if (view == this.FAVORITE_VIEW) {
+                message = 'No favorited images found';
+            } else {
+                message = "No images found"; 
+            }
+        } else {
+            if (total > images.length) {
+                message = `Showing first ${images.length} images`;
+            } else {
+                message = `Showing ${images.length} image(s)`;
+            }
+        }
+
+        if (query) {
+            message += ` for "${query}"`;
+        }
+
+        return (
+            <div className="filter-description">{message}</div>
+        );
     },
 
-    focusSearchInput: function () {
+    renderMoreImagesButton: function() {
+        return (
+            <li>
+                <button style={{
+                    "margin": "15px auto",
+                    "display": "block"
+                }} className="btn btn-default" onClick={this.handleLoadMoreImages}>
+                    Show more images...
+                </button>
+            </li>
+        );
+    },
+
+    focusSearchInput: function() {
         this.refs.searchField.getDOMNode().focus();
     },
-    renderSearchInput: function () {
+
+    renderSearchInput: function() {
         return (
             <input
                 ref="searchField"
                 type="text"
                 placeholder="Search across image name, tag or description"
                 className="form-control search-input"
-                onChange={this.handleChange}
+                onChange={this.onSearchChange}
             />
         );
     },
-    renderImages: function (images, allImages) {
-        let totalNumberOfImages = allImages.length;
+
+    // This render method is a good model for the future. It appears that no
+    // network requests are being made, only gets (getAll, getFeat..).
+    // However, BaseStore.getAll actually makes a network request on a cold
+    // cache. So on first call these do actually make network requests. When
+    // that bug is fixed, componentDidMount will have to prime the cache, this
+    // will be done transparently. i.e. Subscribing to a data source will auto
+    // populate that data source. 
+    renderImageTabView: function() {
+        let tags = stores.TagStore.getAll();
         let query = this.state.query;
-        let searchInput = this.renderSearchInput();
-        if (!images) { return ( <div className="loading"/> ) }
-        
-        return (
-        <div>
-            {searchInput}
-            {this.renderFilterDescription(images)}
-            <ImageList
-                images={images}
-                onSelectImage={this.props.onSelectImage}
-            >
-            {this.renderMoreImagesButton(images, totalNumberOfImages)}
-            </ImageList>
-        </div>
-        );
-    },
-    renderZeroImages: function () {
-        return (
-        <div>
-            {this.renderSearchInput()}
-            {this.renderNoResultsFor()}
-        </div>
-        );
-    },
-    renderLoadingImages: function () {
-        return (
-        <div>
-            {this.renderSearchInput()}
-            {this.renderFilterDescription()}
-            <div className="loading"/>
-        </div>
-        );
-    },
+        let view = this.state.view;
 
-    renderBody: function () {
-        let allImages = this.imageList(),
-            // This might get paginated below
-            images = this.imageList(),
-            tags = stores.TagStore.getAll(),
-            query = this.state.query,
-            numberOfResults,
-            totalNumberOfImages;
-
-        // Render Images
-        if (images && tags) {
-            numberOfResults = this.state.page * this.state.resultsPerPage;
-
-            images = images.first(numberOfResults);
-            images = new ImageCollection(images);
-
-            if (images.length > 0) {
-                return this.renderImages(images, allImages);
-            } else {
-                return this.renderZeroImages();
-            }
+        let images;
+        switch (view) {
+            case this.ALL_VIEW:
+                images = this.getAll();
+                break;
+            case this.FEATURED_VIEW:
+                images = this.getFeatured();
+                break;
+            case this.FAVORITE_VIEW:
+                images = this.getFavorites();
+                break;
         }
-        return this.renderLoadingImages();
+
+        // Render loading
+        if (!(images && tags)) {
+            return ( 
+                <div>
+                    {this.renderSearchInput()}
+                    {this.renderFilterDescription()}
+                    <div className="loading"/> 
+                </div>
+            );
+        }
+
+        let numberOfResults = this.state.page * this.RESULTS_PER_PAGE;
+        let shownImages = new ImageCollection(images.first(numberOfResults));
+                
+        return (
+            <div>
+                {this.renderSearchInput()}
+                {this.renderFilterDescription(shownImages, images.length)}
+                <ImageList
+                    images={shownImages}
+                    onSelectImage={this.props.onSelectImage}
+                >
+                    { 
+                        images.length > shownImages.length 
+                        ? this.renderMoreImagesButton() 
+                        : null 
+                    }
+                </ImageList>
+            </div>
+        );
+
     },
-    render: function () {
+
+    render: function() {
         return (
             <div>
                 <div className="modal-section">
                     <h3 className="t-title">First choose an image for your instance</h3>
                     <hr/>
-                    <TabLinks 
-                        linkList={this.views}
-                        currentView={this.state.listView}
-                        onChangeView={this.onChangeView}
+                    <TabLinks
+                        links={['Show Featured', 'Show Favorites', 'Show All']}
+                        defaultLink={this.FEATURED_VIEW}
+                        onTabClick={this.onChangeView}
                     />
-                    {this.renderBody()}
+                    {this.renderImageTabView()}
                 </div>
                 <InstanceLaunchFooter
                     showValidationErr={true}
