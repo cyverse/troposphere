b1d37d82e0ee2081c42d7c4c7353cf03829d0115
Andrew Lenards <andrew.lenards@gmail.com>
2016-05-26 16:58:41 -0700

    Add hash to assets; restructure PATHS
    Bundled assets will have a "hash" component added to the filename to
    facilitate cache invalidation when pushing out changes.
    
    The context & output paths within webpack have been restructured to be
    part of a JS object in-line with how @bebraw illustrated in the gitbook
    "survivejs".
    
    See ATMO-1356.

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,10 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CompressionPlugin = require("compression-webpack-plugin");
 var BundleTracker = require('webpack-bundle-tracker');
 
-var OUTPUT_PATH = path.join(__dirname, "/troposphere/assets");
-var CONTEXT_PATH = path.join(__dirname, "/troposphere/static/js");
+var PATHS = {
+    output: path.join(__dirname, "/troposphere/assets/"),
+    context: path.join(__dirname, "/troposphere/static/js")
+}
 
 var plugins = [
     new ExtractTextPlugin("[name].css", { allChunks: true }),
@@ -37,22 +39,27 @@ module.exports = {
     analytics: "./analytics",
     public: "./public_site/main"
   },
-  context: CONTEXT_PATH,
+  context: PATHS.context,
   output: {
-    path: OUTPUT_PATH,
+    path: PATHS.output,
     publicPath: "/assets/",
-    filename: "[name].js"
+    filename: "[name]-[hash].js"
   },
   module: {
     loaders: [
       { test: /bootstrap-sass/, loader: "imports?jQuery=jquery" },
-      { test: /modernizr-latest\.js/, loader: "imports?this=>window,html5=>window.html5!exports?window.Modernizr" },
-      { test: /\.json$/, loader: 'json-loader', include: path.join(__dirname, 'node_modules/moment-timezone')},
+      { test: /modernizr-latest\.js/,
+        loader: "imports?this=>window,html5=>window.html5!exports?window.Modernizr" },
+      { test: /\.json$/,
+        loader: 'json-loader',
+        include: path.join(__dirname, 'node_modules/moment-timezone')},
       { test: /\.js$/,
         loader: "babel",
         query: { cacheDirectory: '/tmp/' },
         exclude: /(node_modules|troposphere\/static\/js\/lib)/ },
-      { test: /\.(scss|sass)$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader?browsers=last 2 versions!sass-loader") },
+      { test: /\.(scss|sass)$/,
+        loader: ExtractTextPlugin.extract("style-loader",
+            "css-loader!autoprefixer-loader?browsers=last 2 versions!sass-loader") },
       { test: /\.woff$/ , loader: "url?limit=10000&mimetype=application/font-woff" },
       { test: /\.woff2$/, loader: "url?limit=10000&mimetype=application/font-woff2" },
       { test: /\.ttf$/  , loader: "file?mimetype=application/vnd.ms-fontobject" },
@@ -61,7 +68,6 @@ module.exports = {
       { test: /\.(jpe?g|png|gif|ico)$/, loader: "file?name=images/[name].[ext]" }
     ]
   },
-
   plugins: plugins,
   resolve: {
     alias: {
@@ -71,7 +77,7 @@ module.exports = {
       highcharts: "highcharts-commonjs"
     },
     root: [
-      CONTEXT_PATH,
+      PATHS.context,
     ],
     extensions: ["", ".js", ".scss", ".sass"]
   }
