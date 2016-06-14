3d0af3a74d5f5f2eb7c3dd9e479e6186455a49fe
Andrew Lenards <andrew.lenards@gmail.com>
2016-06-14 14:16:03 -0700

    Make Sentry release for public-site consistent

--- troposphere/static/js/public_site/main.js
+++ troposphere/static/js/public_site/main.js
@@ -3,6 +3,6 @@ import Raven from 'raven-js';
 
 let sentryDSN = 'https://27643f06676048be96ad6df686c17da3@app.getsentry.com/73366';
 
-Raven.config(sentryDSN, {release: '6a34f86c188811e698eb0242ac130001'}).install();
+Raven.config(sentryDSN, {release: 'dfdfd4c7765c8aa5e2e4e34ac3baea92431ca511'}).install();
 
 bootstrapper.run();
