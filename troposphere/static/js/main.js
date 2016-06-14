f77675f9fc439391556a5bd648eb434053891a47
Andrew Lenards <andrew.lenards@gmail.com>
2016-06-14 14:07:18 -0700

    Define Sentry release for P-P

--- troposphere/static/js/main.js
+++ troposphere/static/js/main.js
@@ -4,6 +4,6 @@ import Raven from 'raven-js';
 
 let sentryDSN = 'https://27643f06676048be96ad6df686c17da3@app.getsentry.com/73366';
 
-Raven.config(sentryDSN, {release: 'dfdfd4c7765c8aa5e2e4e34ac3baea92431ca511'}).install();
+Raven.config(sentryDSN, {release: '22854c9be9aad248d509dd0a5b9af38fef0cd856'}).install();
 
 bootstrapper.run();
