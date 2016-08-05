294ff64b4c183f087a835ebac0c836a5301744ee
Connor Osborn <cdosborn@email.arizona.edu>
2016-08-05 20:15:07 +0000

    Cleanup BasicLaunchStep

--- troposphere/static/js/components/modals/instance/launch/steps/BasicLaunchStep.react.js
+++ troposphere/static/js/components/modals/instance/launch/steps/BasicLaunchStep.react.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import Backbone from 'backbone';
-import _ from 'underscore';
-import stores from 'stores';
+import React from "react";
+import Backbone from "backbone";
+import _ from "underscore";
 
-import BasicInfoForm from '../components/BasicInfoForm.react';
-import ResourcesForm from '../components/ResourcesForm.react';
-import ResourcesFormAS from '../components/ResourcesFormAS.react';
-import InstanceLaunchFooter from '../components/InstanceLaunchFooter.react';
+import globals from "globals";
+import stores from "stores";
+import BasicInfoForm from "../components/BasicInfoForm.react";
+import ResourcesForm from "../components/ResourcesForm.react";
+import ResourcesFormAS from "../components/ResourcesFormAS.react";
+import InstanceLaunchFooter from "../components/InstanceLaunchFooter.react";
 
 export default React.createClass({
-    render: function () {
-        let defaults = { advancedIsDisabled: false };
-        let ResourceForm = true ? 
-            <ResourcesFormAS {...this.props}/> :
-                <ResourcesForm {...this.props}/>;
+    render: function() {
+        let defaults = {
+            advancedIsDisabled: false
+        };
+
+        let resourceForm;
+        if (globals.USE_ALLOCATION_SOURCES) {
+            resourceForm = <ResourcesFormAS {...this.props} />;
+        } else {
+            resourceForm = <ResourcesForm {...this.props} />;
+        }
 
         return (
-            <div>
-                <div className="modal-section row">
-                    <div className="col-md-6">
-                        <h3 className="t-title">Basic Info</h3>
-                        <hr/>
-                        <BasicInfoForm {...this.props}/>
-                    </div>
-                    <div className="col-md-6">
-                        <h3 className="t-title">Resources</h3>
-                        <hr/>
-                        { ResourceForm }
-                    </div>
+        <div>
+            <div className="modal-section row">
+                <div className="col-md-6">
+                    <h3 className="t-title">Basic Info</h3>
+                    <hr/>
+                    <BasicInfoForm {...this.props}/>
                 </div>
-                <InstanceLaunchFooter {..._.extend(defaults, this.props)} />
+                <div className="col-md-6">
+                    <h3 className="t-title">Resources</h3>
+                    <hr/>
+                    { resourceForm }
+                </div>
             </div>
+            <InstanceLaunchFooter {..._.extend(defaults, this.props)} />
+        </div>
         );
     }
 });
