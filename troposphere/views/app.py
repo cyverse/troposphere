de5e26a0eae741ed686632c92d4558f4fbe01cf4
Connor Osborn <cdosborn@email.arizona.edu>
2016-08-23 00:55:42 +0000

    Fix incorrect theme path
    The app is trying to load the theme from /themes/$THEME_NAME, but it should be
    loading from /assets/theme

--- troposphere/views/app.py
+++ troposphere/views/app.py
@@ -114,10 +114,7 @@ def _populate_template_params(request, maintenance_records, notice_t, disabled_l
     template_params['USE_MOCK_DATA'] = getattr(settings, "USE_MOCK_DATA", False)
     template_params['USE_ALLOCATION_SOURCES'] = getattr(settings,
             "USE_ALLOCATION_SOURCES", False)
-
-    #TODO: Replace this line when theme support is re-enabled.
-    #template_params["THEME_URL"] = "assets/"
-    template_params['THEME_URL'] = "/themes/%s" % settings.THEME_NAME
+    template_params['THEME_URL'] = "/assets/theme"
     template_params['ORG_NAME'] = settings.ORG_NAME
     template_params['DYNAMIC_ASSET_LOADING'] = settings.DYNAMIC_ASSET_LOADING
 
@@ -321,7 +318,7 @@ def forbidden(request):
     metadata = get_site_metadata()
     template_params = {}
 
-    template_params['THEME_URL'] = "/themes/%s" % settings.THEME_NAME
+    template_params['THEME_URL'] = "/assets/theme"
     template_params['ORG_NAME'] = settings.ORG_NAME
     template_params['SITE_TITLE'] = settings.SITE_TITLE
     template_params['SITE_FOOTER'] = settings.SITE_FOOTER
