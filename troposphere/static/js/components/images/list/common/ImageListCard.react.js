b02017cf9149e16f9c0c9e5d287240d8c2cdd9c6
Tharon Carlson <design@tharoncarlson.com>
2016-09-27 22:52:55 +0000

    make card clickable

--- troposphere/static/js/components/images/list/common/ImageListCard.react.js
+++ troposphere/static/js/components/images/list/common/ImageListCard.react.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Router from "react-router";
+import RouterInstance from "Router";
 import Gravatar from "components/common/Gravatar.react";
 import Backbone from "backbone";
 import Bookmark from "components/images/common/Bookmark.react";
@@ -14,11 +15,20 @@ import stores from "stores";
 export default React.createClass({
     displayName: "ImageListCard",
 
+    mixins: [Router.State],
+
     propTypes: {
         image: React.PropTypes.instanceOf(Backbone.Model).isRequired
     },
 
-    render: function() {
+    onCardClick() {
+        RouterInstance.getInstance()
+            .transitionTo("image-details",{ 
+                imageId: this.props.image.id 
+            });
+    },
+
+    render() {
         var image = this.props.image,
             hasLoggedInUser = context.hasLoggedInUser(),
             type = stores.ProfileStore.get().get("icon_set"),
@@ -28,11 +38,20 @@ export default React.createClass({
                 .format("MMM Do YY hh:mm "),
             converter = new Showdown.Converter(),
             description = image.get("description");
+            name = image.get('name');
 
+        if (imageTags.length > 10) {
+            imageTags = imageTags.slice(0,10);
+        }
+        if (name.length > 30) {
+            name = name.substring(0,30) + " ..."
+        }
         if (!description) {
             description = "No Description Provided."
+        } else if ( description.length > 90 ) {
+            description = description.substring(0,90) + " ..."
         }
-        var descriptionHtml = converter.makeHtml(description),
+        var descriptionHtml = converter.makeHtml( description ),
             iconSize = 40,
             icon;
 
@@ -68,44 +87,53 @@ export default React.createClass({
         }
 
         return (
-        <div className="media card">
-            {endDated}
-            <div className="media__img">
-                <Router.Link to="image-details" params={{ imageId: image.id }}> 
-                    {icon} 
-                </Router.Link>
-            </div>
             <div 
-                className="media__content"
-                style={{ display: "flex" }} 
+                onClick={ this.onCardClick } 
+                className="media card"
+                style={{
+                    cursor: "pointer"
+                }}
             >
+                {endDated}
+                <div className="media__img">
+                    {icon} 
+                </div>
                 <div 
-                    className="media__title"
+                    className="media__content"
                     style={{ 
-                        minWidth: "250px",
-                        marginRight: "40px"
-                    }}    
+                        display: "flex", 
+                        flexFlow: "wrap",
+                    }} 
                 >
-                    <h2 className="t-body-2" >
-                        <Router.Link to="image-details" params={{ imageId: image.id }}> 
-                            {image.get("name")} 
-                        </Router.Link>
-                    </h2> 
-                    <div style={{ fontSize: "12px" }}> 
-                        <time> 
-                            {imageCreationDate} 
-                        </time> 
-                        by 
-                        <strong> {image.get("created_by").username}</strong> 
-                    </div> 
+                    <div 
+                        className="media__title"
+                        style={{ 
+                            minWidth: "220px",
+                            marginRight: "40px"
+                        }}    
+                    >
+                        <h2 className="t-body-2" >
+                            { name }  
+                        </h2> 
+                        <div style={{ fontSize: "12px" }}> 
+                            <time> 
+                                {imageCreationDate} 
+                            </time> 
+                            by 
+                            <strong> {image.get("created_by").username}</strong> 
+                        </div> 
+                    </div>
+                    <div className="description"
+                        style={{
+                            minWidth: "250px"
+                        }}
+                    >
+                        <span dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
+                        <Tags activeTags={imageTags}/>
+                    </div>
                 </div>
-                <div className="description">
-                    <span dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
-                    <Tags activeTags={imageTags}/>
-                </div>
+                {bookmark}
             </div>
-            {bookmark}
-        </div>
         );
     }
 });
