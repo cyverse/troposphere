3349cb9c43c037d97fae4d29054a02947238f6c6
Andrew J Lenards <andrew.lenards@gmail.com>
2016-10-27 23:05:43 +0000

    Update Sentry/Raven release version

--- troposphere/static/js/public_site/main.js
+++ troposphere/static/js/public_site/main.js
@@ -4,7 +4,7 @@ import Raven from "raven-js";
 let sentryDSN = "https://27643f06676048be96ad6df686c17da3@app.getsentry.com/73366";
 
 Raven.config(sentryDSN, {
-    release: "205096b5fde3a47303ad8d1fef9ff8052cbbd7d4"
+    release: "0985ad55a5dc118e6da249494e59c173c3770bb3"
 }).install();
 
 bootstrapper.run();
