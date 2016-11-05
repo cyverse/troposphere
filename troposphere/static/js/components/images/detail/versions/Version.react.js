48b8da2e961b782f6feb91ec54aa63b954fdf0da
Tharon Carlson <design@tharoncarlson.com>
2016-11-05 16:21:12 +0000

    Refactor version to use expanding card

--- troposphere/static/js/components/images/detail/versions/Version.react.js
+++ troposphere/static/js/components/images/detail/versions/Version.react.js
@@ -13,6 +13,12 @@ import showdown from "showdown";
 export default React.createClass({
     displayName: "Version",
 
+    getInitialState() {
+        return {
+            isOpen: false
+        }
+    },
+
     propTypes: {
         image: React.PropTypes.instanceOf(Backbone.Model).isRequired,
         version: React.PropTypes.instanceOf(Backbone.Model).isRequired,
@@ -21,29 +27,43 @@ export default React.createClass({
         showAvailability: React.PropTypes.bool
     },
 
-    getDefaultProps: function() {
+    getDefaultProps() {
         return {
             showAvailability: true,
             editable: true
         }
     },
 
-    onEditClicked: function() {
+    onCardClick() {
+        console.log('clicked')
+        let isOpen = this.state.isOpen
+            ? false : true;
+
+        this.setState({
+            isOpen
+        });
+    },
+
+    onEditClicked() {
         return this.props.onEditClicked(this.props.version);
     },
 
-    renderAvailability: function() {
-        var version = this.props.version;
+    renderAvailability() {
+        let isOpen = this.state.isOpen;
+        let version = this.props.version;
         if (!this.props.showAvailability) {
             return;
         }
 
         return (
-        <AvailabilityView version={version} />
+        <AvailabilityView
+            isSummary={ !isOpen }
+            version={ version }
+        />
         );
     },
 
-    renderEditLink: function() {
+    renderEditLink() {
         //NOTE: Undefined/null/etc. defaults to "TRUE" case.
         if (this.props.editable == false) {
             return;
@@ -72,7 +92,7 @@ export default React.createClass({
         }
     },
 
-    renderDateString: function(version) {
+    renderDateString(version) {
         let date_str;
         let dateCreated = moment(version.get("start_date"))
                 .tz(globals.TZ_REGION)
@@ -91,26 +111,51 @@ export default React.createClass({
 
     },
 
-    render: function() {
-        // todo: figure out if anything is ever recommended, or if it's just a concept idea
-        let { version, image } = this.props;
-        let style = this.style();
+    renderDetail() {
+        let { version } = this.props;
+        let styles = this.styles();
 
-        let versionHash = CryptoJS.MD5(version.id.toString()).toString();
-        let type = stores.ProfileStore.get().get("icon_set");
-        let owner = image.get("created_by").username;
         let changeLog = version.get("change_log");
         let converter = new showdown.Converter();
         let changeLogHTML = converter.makeHtml(changeLog);
 
+        return (
+            <div
+                style={ styles.content }
+            >
+                <div style={ styles.description }
+                    dangerouslySetInnerHTML={{
+                        __html: changeLogHTML
+                    }}
+                />
+                <div style={ styles.availability }
+                >
+                    { this.renderAvailability() }
+                </div>
+            </div>
+        )
+    },
+
+    render() {
+        // todo: figure out if anything is ever recommended, or if it's just a concept idea
+        let { version, image } = this.props;
+        let styles = this.styles();
+
+        let versionHash = CryptoJS.MD5(version.id.toString()).toString();
+        let type = stores.ProfileStore.get().get("icon_set");
+        let owner = image.get("created_by").username;
+        let date = this.renderDateString(version);
+
         let subheading =  (
             <span>
-                { `${this.renderDateString(version)} by ${owner}` }
+                { `${date} by ${owner}` }
                 { this.renderEditLink() }
             </span>
         );
         return (
             <MediaCard
+                onCardClick={ this.onCardClick }
+                isOpen={ this.state.isOpen }
                 title={ version.get("name") }
                 subheading={ subheading }
                 avatar={
@@ -120,41 +165,51 @@ export default React.createClass({
                         type={type}
                     />
                 }
-                description={
-                    <div
-                        style={ style.content }
-                    >
-                        <div style={ style.description }
-                            dangerouslySetInnerHTML={{ __html: changeLogHTML }}
-                        />
-                        <div style={ style.availability }
-                        >
-                            { this.renderAvailability() }
-                        </div>
-                    </div>
-                }
+                summary={ this.renderDetail() }
+                detail={ this.renderDetail() }
             />
         );
     },
 
-    style() {
-        return {
-            content: {
-                display: "flex",
+    styles() {
+        let isOpen = this.state.isOpen;
+        let styles = {}
+
+            let contentDisplay = isOpen
+                ? "block" : "flex";
+            styles.content = {
+                display: contentDisplay,
                 flexWrap: "wrap",
                 justifyContent: "space-between",
-            },
+            };
 
-            description: {
-                marginRight: "40px"
-            },
+            let descriptionWidth = isOpen
+                ? {
+                    width: "100%",
+                    maxWidth: "700px"
+                } : {};
 
-            availability: {
+            styles.description = {
+                marginRight: "40px",
+                flex: "1",
+                ...descriptionWidth
+            };
+
+
+            let availabilityWidth = isOpen
+                ? {
+                    width: "100%",
+                }
+                : {
+                    width: "30%",
+                    minWidth: "200px",
+                };
+
+            styles.availability = {
                 fontSize: "12px",
-                width: "30%",
-                minWidth: "220px",
-                marginRight: "30px",
-            },
-        };
+                ...availabilityWidth,
+            };
+
+        return styles;
     }
 });
