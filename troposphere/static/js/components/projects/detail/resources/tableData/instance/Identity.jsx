3ceb7a3c8fa75a37cd2331b723dcbd16c5ea1d95
Steve Gregory <contact@steve-gregory.com>
2016-11-23 08:49:30 -0700

    Render Identity instead of Provider textual information

--- troposphere/static/js/components/projects/detail/resources/tableData/instance/Identity.jsx
+++ troposphere/static/js/components/projects/detail/resources/tableData/instance/Identity.jsx
@@ -3,20 +3,24 @@ import Backbone from "backbone";
 import stores from "stores";
 
 export default React.createClass({
-    displayName: "Provider",
+    displayName: "Identity",
 
     propTypes: {
         instance: React.PropTypes.instanceOf(Backbone.Model).isRequired
     },
 
+    render_text: function(provider, identity) {
+        return identity.get('key')+" on " + provider.get("name");
+    },
     render: function() {
         var instance = this.props.instance,
-            provider = stores.ProviderStore.get(instance.get("provider").id);
+            provider = stores.ProviderStore.get(instance.get("provider").id),
+            identity = stores.IdentityStore.get(instance.get("identity").id);
 
-        if (!provider) return <div className="loading-tiny-inline"></div>;
+        if (!provider || !identity) return <div className="loading-tiny-inline"></div>;
 
         return (
-        <span>{provider.get("name")}</span>
+        <span>{this.render_text(provider, identity)}</span>
         );
     }
 });
