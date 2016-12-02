1d8b4dffab0939137de3781f1fb5faa4aeef9c70
Steve Gregory <contact@steve-gregory.com>
2016-12-02 10:20:01 -0700

    Move ShareIcon to common/ui, use ShareIcon x2 places

--- troposphere/static/js/components/projects/detail/resources/tableData/instance/Name.jsx
+++ troposphere/static/js/components/projects/detail/resources/tableData/instance/Name.jsx
@@ -3,59 +3,8 @@ import Backbone from "backbone";
 import Router from "react-router";
 import stores from "stores";
 import context from "context";
-import Tooltip from "react-tooltip";
+import ShareIcon from "components/common/ui/ShareIcon";
 
-const ShareIcon = React.createClass({
-    propTypes: {
-        owner: React.PropTypes.string.isRequired
-    },
-
-    getDefaultProps() {
-        return {
-            isLeader: false,
-        };
-    },
-    getInitialState() {
-        let share_message = "You have view-only access to this shared resource from " + this.props.owner;
-        let leader_message = "You have full control to this shared resource from " + this.props.owner;
-        //Note: I want to differentiate this icon based on leader-ship/viewer-ship.. Can we tap into theme here?
-        let color = (this.props.isLeader) ? "green" : "red";
-        let tip = (this.props.isLeader) ? leader_message : share_message;
-        return {
-            opacity: "0.4",
-            color,
-            tip,
-        };
-    },
-    onMouseOver() {
-        this.setState({
-            opacity: "1"
-        });
-    },
-    onMouseOut() {
-        this.setState(this.getInitialState());
-    },
-    render() {
-        let opacity = this.state.tip ? this.state.opacity : "0";
-        let { color } = this.state;
-        let marginRight = "3px";
-        let style = { opacity,
-                      color,
-                      marginRight};
-        let rand = Math.random() + "";
-        return (
-        <span><span onMouseOver={this.onMouseOver}
-                  onMouseOut={this.onMouseOut}
-                  style={ style }
-                  data-tip={this.state.tip}
-                  data-for={rand}
-                  className="glyphicon glyphicon-user"
-                  aria-hidden="true"></span>
-        <Tooltip id={rand} place="top" effect="solid" />
-        </span>
-        );
-    }
-});
 
 export default React.createClass({
     displayName: "Name",
