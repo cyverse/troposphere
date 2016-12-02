5770ffde329552577a5adab5cd689ded91b58711
Andrew J Lenards <andrew.lenards@gmail.com>
2016-12-02 23:25:01 +0000

    Include reference to possible glyphicons

--- troposphere/static/js/components/common/Glyphicon.react.js
+++ troposphere/static/js/components/common/Glyphicon.react.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+/**
+ * NOTE: a cheatsheet of available glyphicons can be found at:
+ *   - http://glyphicons.bootstrapcheatsheets.com/
+ *
+ *   this becomes reference-able within the app via the
+ *   `bootstrap-sass` module
+ */
 
 export default React.createClass({
     displayName: "Glyphicon",
