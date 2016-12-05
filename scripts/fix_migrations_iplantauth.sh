2fc533da02a9d78615d69c3db7ea57bdaa5bc87c
Steve Gregory <contact@steve-gregory.com>
2016-12-05 10:56:09 -0700

    Moving from 'iplantauth' to 'django_cyverse_auth'

--- scripts/fix_migrations_iplantauth.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-ATMO_DB='troposphere'
-#Fix the migrations in atmosphere db
-psql_command="update django_migrations set app='django_cyverse_auth' where app='iplantauth';"
-su - postgres -c "psql -d $ATMO_DB -c \"$psql_command\""
