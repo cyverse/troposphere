d7baf65805bb96f0e3f2c12924bffd6810667d99
Andrew J Lenards <andrew.lenards@gmail.com>
2016-12-24 01:04:51 +0000

    Replace Route.name usage with proper path

--- troposphere/static/js/components/providers/Name.jsx
+++ troposphere/static/js/components/providers/Name.jsx
@@ -15,7 +15,7 @@ export default React.createClass({
         return (
         <div style={{ paddingTop: "50px" }} className="row">
             <h1 className="t-display-1">{provider.get("name")}</h1>
-            <Link className="btn btn-default" to="all-providers">
+            <Link className="btn btn-default" to="/providers">
                 <span className="glyphicon glyphicon-arrow-left"></span>
                 {" Back to All Providers"}
             </Link>
