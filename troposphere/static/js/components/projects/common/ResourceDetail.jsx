a58c198764b7612b037803b07fe56812ace63287
Andrew J Lenards <andrew.lenards@gmail.com>
2017-01-06 23:45:29 +0000

    Allow copy of contents for resource
    This adds a "copy-to-clipboard" feature for the following metadata:
    - Instances
      - IP Address
      - alias (uuid)
    - Volumes
      - Identifier (alias/uuid)
    
    Given the styling on these elements, the "contents" will make it to the
    clipboard without spurious white-space (this is not true for <Code />
    used by <AvailabilityView /> in image versions - soon to be fixed).

--- troposphere/static/js/components/projects/common/ResourceDetail.jsx
+++ troposphere/static/js/components/projects/common/ResourceDetail.jsx
@@ -5,18 +5,33 @@ export default React.createClass({
 
     propTypes: {
         label: React.PropTypes.string.isRequired,
-        children: React.PropTypes.node.isRequired
+        children: React.PropTypes.node.isRequired,
+        onClick: React.PropTypes.func
     },
 
     render: function() {
+        let detailValue;
+
+        if (this.props.onClick) {
+            detailValue = (
+            <div className="detail-value" onClick={this.props.onClick}>
+                {this.props.children}
+            </div>
+            );
+        } else {
+            detailValue = (
+            <div className="detail-value">
+                {this.props.children}
+            </div>
+            );
+        }
+
         return (
         <li className="clearfix">
             <div className="t-body-2 detail-label">
                 {this.props.label + " "}
             </div>
-            <div className="detail-value">
-                {this.props.children}
-            </div>
+            {detailValue}
         </li>
         );
     }
