900794e6059a29983eb5fa533cdda9c0cd1f556e
Andrew J Lenards <andrew.lenards@gmail.com>
2017-01-25 18:58:19 +0000

    Allow *bulk* resume actions for instances
     - Restores "resume" on multiple instances
    
    This allows for a user-directed action of "resume" performed on all
    selected instances within the Project-Details view.
    
    This relates to overall work to all "batch" (e.g. bulk) actions like
    `suspend` / `resume` and `stop` / `start` complement(s).
    
    See ATMO-981 [0]
    
    [0] https://pods.iplantcollaborative.org/jira/browse/ATMO-981

--- troposphere/static/js/modals/instance/resume.js
+++ troposphere/static/js/modals/instance/resume.js
@@ -1,16 +1,22 @@
 import ModalHelpers from "components/modals/ModalHelpers";
-
 import InstanceResumeModal from "components/modals/instance/InstanceResumeModal";
+
 import actions from "actions";
 
 
 export default {
-
-    resume: function(instance) {
-        ModalHelpers.renderModal(InstanceResumeModal, null, function() {
-            actions.InstanceActions.resume({
-                instance: instance
-            })
+    /**
+     * Resume any valid/actionable resources
+     *
+     * The action method handles resources verifying type (via instanceof)
+     * to ensure that multi0ple resources can be handled.
+     *
+     * @param resources - collection of resources to act on
+     */
+    resume: function(resources) {
+        let props = { resources };
+        ModalHelpers.renderModal(InstanceResumeModal, props, function() {
+            actions.InstanceActions.resume({ resources })
         });
     }
 
