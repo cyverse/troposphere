7e92d20dd5e78bf6eb8e816cb8cce4677f12c7dc
Andrew J Lenards <andrew.lenards@gmail.com>
2017-01-25 18:21:41 +0000

    Fix typo in feature flag name

--- troposphere/static/js/utilities/featureFlags.js
+++ troposphere/static/js/utilities/featureFlags.js
@@ -1,5 +1,5 @@
 
 export default {
     WEB_DESKTOP: !!window.WEB_DESKTOP_INCLUDE_LINK || false,
-    BULK_RESOURCE_ACTIONS: !!window.BULK_RESOURCES_ACTION || false
+    BULK_RESOURCE_ACTIONS: !!window.BULK_RESOURCE_ACTIONS || false
 }
