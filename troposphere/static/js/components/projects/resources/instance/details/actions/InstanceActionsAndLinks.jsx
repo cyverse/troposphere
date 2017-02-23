e7b1b377728a38c159e4a5a92ec1d3c47115f6be
Andrew J Lenards <andrew.lenards@gmail.com>
2017-02-23 17:33:38 +0000

    Add missing markup variable for render

--- troposphere/static/js/components/projects/resources/instance/details/actions/InstanceActionsAndLinks.jsx
+++ troposphere/static/js/components/projects/resources/instance/details/actions/InstanceActionsAndLinks.jsx
@@ -236,6 +236,12 @@ export default React.createClass({
         if (link.isDangerLink)
             className += " danger";
 
+            let linkLabelMarkup = (
+                <span>
+                    <Glyphicon name={link.icon}/>{` ${link.label}`}
+                </span>
+            );
+
             // todo: This isn't implemented well at all.  We should be disabling these
             // buttons if there isn't a valid href for the link, or (perhaps) not even
             // showing the buttons at all...but I think it's better communication to
