0f83cf28a2fbc31798b2b0e791b7e27cdb09d06e
Connor Osborn <cdosborn@email.arizona.edu>
2017-02-24 18:33:20 +0000

    Fix data loading issue for my resource requests

--- troposphere/static/js/components/requests/ResourceHistoryMaster.jsx
+++ troposphere/static/js/components/requests/ResourceHistoryMaster.jsx
@@ -4,9 +4,24 @@ import ResourceActions from "actions/ResourceActions";
 
 import stores from "stores";
 
-
 export default React.createClass({
     displayName: "MyResourceRequestsPage",
+
+    componentDidMount: function() {
+        stores.StatusStore.addChangeListener(this.updateState);
+        stores.ProfileStore.addChangeListener(this.updateState);
+        stores.ResourceRequestStore.addChangeListener(this.updateState);
+    },
+
+    componentWillUnmount: function() {
+        stores.StatusStore.removeChangeListener(this.updateState);
+        stores.ProfileStore.removeChangeListener(this.updateState);
+        stores.ResourceRequestStore.removeChangeListener(this.updateState);
+    },
+
+    updateState: function() {
+        this.forceUpdate();
+    },
 
     closeRequest: function(request) {
         var closedStatus = stores.StatusStore.findWhere({
