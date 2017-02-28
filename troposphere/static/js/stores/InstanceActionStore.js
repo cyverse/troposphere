f098e69f09e77784fd4fbbcf685e4dcd6b8c5d91
Andrew J Lenards <andrew.lenards@gmail.com>
2017-02-28 21:26:01 +0000

    Restructure store to track models by instance.alias
    This uses a similar `_modelsFor` and `_fetchingFor` pattern found in
    several other stores. The difference here is that we're not using the
    BaseStore to avoid anything to deal with potential Liskov Substitution
    principle violations [0]. Chief among them would be the _local_ cache.
    The design intent here is that we don't want any of the convenience
    associated with cached models; always fetch actions - just track what
    you've fetched by :uuid (aka Instance alias).
    
    [0] https://en.wikipedia.org/wiki/Liskov_substitution_principle

--- troposphere/static/js/stores/InstanceActionStore.js
+++ troposphere/static/js/stores/InstanceActionStore.js
@@ -6,25 +6,33 @@ import InstanceConstants from "constants/InstanceConstants";
 import InstanceActionCollection from "collections/InstanceActionCollection";
 
 
-const InstanceActionStore = function(attributes, options) {
-    this.models = null;
+let _modelsFor = {};
+let _isFetchingFor = {};
 
-    // isFetching: True or false depending on whether this.models is being
-    // fetch from the server. Used to prevent multiple server calls for the same data.
-    this.isFetching = false;
-}
 
-/**
- * The (Generic) Store provides Events and `emitChange`
- */
-_.extend(InstanceActionStore, Store, {
+const InstanceActionStore = {
     collection: InstanceActionCollection,
 
+    contains(instance) {
+        let alias = instance.get("uuid");
+        return _.has(_modelsFor, alias);
+    },
+
+    isFetching(alias) {
+        return _.has(_isFetchingFor, alias) && _isFetchingFor[alias];
+    },
+
+    clear(instance) {
+        let alias = instance.get("uuid");
+        _modelsFor[alias] = null;
+    },
+
     getActionsFor(instance) {
-        if (!this.models) {
-            return this.fetchFor(instance.get("uuid"));
+        let alias = instance.get("uuid");
+        if (!this.contains(instance)) {
+            return this.fetchFor(alias);
         } else {
-            return this.models;
+            return _modelsFor[alias];
         }
     },
 
@@ -34,8 +42,8 @@ _.extend(InstanceActionStore, Store, {
 
     fetchFor(alias) {
         // alter to a "fetch every time" pattern
-        if (!this.isFetching) {
-            this.isFetching = true;
+        if (!this.isFetching(alias)) {
+            _isFetchingFor[alias] = true;
             // pass instance alias as `options` to the collection
             // the :alias attribute is used in fetching _context_
             // *specific* instance actions - the status of an
@@ -43,14 +51,18 @@ _.extend(InstanceActionStore, Store, {
             var models = new this.collection(null, { alias });
 
             models.fetch().done(() => {
-                this.isFetching = false;
-                this.models = models;
+                _isFetchingFor[alias] = false;
+                _modelsFor[alias] = models;
                 this.emitChange();
             });
         }
     }
-});
+};
 
+/**
+ * Extend the (Generic) "store/Store" to listeners, `emitChange`, & Events
+ */
+_.extend(InstanceActionStore, Store);
 
 
 Dispatcher.register(function(dispatch) {
