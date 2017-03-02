313ef7b154cbd0dce485f706837b59d549f1111a
Tharon Carlson <design@tharoncarlson.com>
2017-03-02 17:44:39 +0000

    Add jetstream theme

---
+++ troposphere/themes/jetstream_theme/theme.js
@@ -0,0 +1,13 @@
+var primary1Color =  "#E5004A"
+var accent1Color =  "#00AAD5";
+var THEME = {
+    palette: {
+        primary1Color: primary1Color
+        accent1Color: accent1Color
+    },
+    mainHeader: {
+        background: "white",
+        border: primary1Color,
+        text: "#grey",
+    }
+};
