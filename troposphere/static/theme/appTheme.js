dd6b52ff5951da92b58d89c685413683388c8858
Connor Osborn <cdosborn@email.arizona.edu>
2017-04-03 18:54:44 +0000

    Fix bad import to use .js rather than .json

--- troposphere/static/theme/appTheme.js
+++ troposphere/static/theme/appTheme.js
@@ -1,4 +1,4 @@
-import cyverseTheme from 'cyverse-ui/styles/cyverseTheme.json';
+import cyverseTheme from 'cyverse-ui/styles/cyverseTheme.js';
 import theme from './theme.json';
 import _ from 'lodash';
 
