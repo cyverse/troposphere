6e61dbfffab83253d65f61c47a23da2e28dce218
Connor Osborn <cdosborn@email.arizona.edu>
2017-04-14 20:51:58 +0000

    Remove USE_ALLOCATION_SOURCES variable

--- troposphere/views/app.py
+++ troposphere/views/app.py
@@ -151,8 +151,6 @@ def _populate_template_params(request, maintenance_records, notice_t, disabled_l
     template_params['UI_VERSION'] = settings.UI_VERSION
     template_params['BADGE_HOST'] = getattr(settings, "BADGE_HOST", None)
     template_params['USE_MOCK_DATA'] = getattr(settings, "USE_MOCK_DATA", False)
-    template_params['USE_ALLOCATION_SOURCES'] = getattr(settings,
-            "USE_ALLOCATION_SOURCES", False)
     template_params['EXTERNAL_ALLOCATION'] = getattr(settings,
             "EXTERNAL_ALLOCATION", False)
     template_params['ALLOCATION_UNIT_NAME'] = getattr(settings, "ALLOCATION_UNIT_NAME", None)
