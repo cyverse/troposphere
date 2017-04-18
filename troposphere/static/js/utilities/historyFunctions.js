60c41c083d1b67d79bdceb20dd46254b3655d3c3
Andrew J Lenards <andrew.lenards@gmail.com>
2017-04-18 15:47:16 +0000

    Define preconfigured instance of browserHistory
    From the helper function module, `historyFunctions`, we create a
    singleton for the application's browserHistory - ensuring that the
    `basename` is set using `withAppBasename`. This means that all history
    operations will have the URL root, hard-coded to "application" for now,
    prepended to the URI.
    
    If the unconfigured `browserHistory` from "react-router" is used, then
    you could not use _relative_ URIs, but they *must* included the basename
    or they will end up as "malformed" locations that the web server will
    *not* know how to handle.

--- troposphere/static/js/utilities/historyFunctions.js
+++ troposphere/static/js/utilities/historyFunctions.js
@@ -1,17 +1,36 @@
+import { browserHistory } from "react-router";
+
 import useBasename from 'history/lib/useBasename';
 
 /**
  * Helper functions for simplifying the use of the `history` module
  */
 
+
 const withBasename = (history, base) => {
     return useBasename(() => history)({basename: `/${base}`});
 }
 
 const withAppBasename = (history) => {
-    // might want to pull this from the "render context" provided
-    // in `globals` to allow for configuration transparency
+    // might want to pull the second argument, "application" from the
+    // "render context" provided in `globals` to allow for
+    // configuration transparency, and the ability to "re-root" the app
+    // to another _base_ URL using `basename`
     return withBasename(history, "application");
 }
 
-export { withBasename, withAppBasename };
+function browserHistoryWithBase() {
+    return withAppBasename(browserHistory);
+}
+
+/**
+ * Expose the history as an object/singleton to mirror the react-router
+ * `browserHistory` object.
+ *
+ * We are merely providing a 'preconfigured' version of `browserHistory`
+ * with the application basename applied so it will be prepended to
+ * routes as we navigate.
+ */
+const appBrowserHistory = browserHistoryWithBase();
+
+export { withBasename, withAppBasename, appBrowserHistory };
