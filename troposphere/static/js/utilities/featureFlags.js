646a3073dfbd19613e13734399a651d952d2394d
Calvin Mclean <calvinmclean@email.arizona.edu>
2017-04-19 09:31:50 -0700

    Fix merge conflict with master

--- troposphere/static/js/utilities/featureFlags.js
+++ troposphere/static/js/utilities/featureFlags.js
@@ -2,4 +2,8 @@
 export default {
     WEB_DESKTOP: !!window.WEB_DESKTOP_INCLUDE_LINK || false,
     GUACAMOLE: !!window.GUACAMOLE_ENABLED || false
+
+    hasIntercomActive() {
+      return window.intercom_app_id && window.Intercom;
+    }
 }
