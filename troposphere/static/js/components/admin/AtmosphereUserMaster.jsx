be499907b5eea487fa96b689242705390a8618d2
Andrew J Lenards <andrew.lenards@gmail.com>
2017-04-23 21:59:35 +0000

    Apply title class to section heading
    "Atmosphere Users" is the heading for this "section", or table. It
    appears as "t-body-2" likely be mistake.
    
    If we apply a "t-title", then we do not need the padding-top on the
    table. Also, this top padding distances the "section heading" from
    appropriately identifying, labeling, the table contents.

--- troposphere/static/js/components/admin/AtmosphereUserMaster.jsx
+++ troposphere/static/js/components/admin/AtmosphereUserMaster.jsx
@@ -80,7 +80,7 @@ export default React.createClass({
         }
 
         return (
-        <table className="table table-hover" style={{ marginTop: "20px" }}>
+        <table className="table table-hover">
             <tbody>
                 <tr className="admin-row">
                     <th style={{ border: "none" }}>
@@ -140,7 +140,7 @@ export default React.createClass({
                     value={this.state.query}
                     ref="textField" />
             </div>
-            <h3 className="t-body-2">Atmosphere Users</h3>
+            <h3 className="t-title">Atmosphere Users</h3>
             {this.renderTable()}
         </div>
         );
