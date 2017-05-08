5764b938edede4c2986619e46ba76c122b4769f4
Calvin Mclean <calvinmclean@email.arizona.edu>
2017-05-08 12:47:24 -0700

    Change gucamole functions to one function that handles vnc and ssh

--- troposphere/static/js/components/projects/resources/instance/details/actions/InstanceActionsAndLinks.jsx
+++ troposphere/static/js/components/projects/resources/instance/details/actions/InstanceActionsAndLinks.jsx
@@ -211,7 +211,7 @@ export default React.createClass({
         form[0].submit();
     },
 
-    onGuacDesktop: function(ipAddr, instance) {
+    onGuacConn: function(ipAddr, instance, proto) {
         // TODO:
         //      move this into a utilties file
         var CSRFToken = findCookie("tropo_csrftoken");
@@ -230,38 +230,7 @@ export default React.createClass({
         form.append($("<input>")
             .attr("type", "hidden")
             .attr("name", "protocol")
-            .attr("value", "vnc"));
-
-        form.append($("<input>")
-            .attr("type", "hidden")
-            .attr("name", "csrfmiddlewaretoken")
-            .attr("style", "display: none;")
-            .attr("value", CSRFToken));
-
-        $("body").append(form);
-        form[0].submit();
-    },
-
-    onGuacShell: function(ipAddr, instance) {
-        // TODO:
-        //      move this into a utilties file
-        var CSRFToken = findCookie("tropo_csrftoken");
-
-        // build a form to POST to web_desktop
-        var form = $("<form>")
-            .attr("method", "POST")
-            .attr("action", "/guacamole")
-            .attr("target", "_blank");
-
-        form.append($("<input>")
-            .attr("type", "hidden")
-            .attr("name", "ipAddress")
-            .attr("value", ipAddr));
-
-        form.append($("<input>")
-            .attr("type", "hidden")
-            .attr("name", "protocol")
-            .attr("value", "ssh"));
+            .attr("value", proto));
 
         form.append($("<input>")
             .attr("type", "hidden")
@@ -307,10 +276,11 @@ export default React.createClass({
           links.push({
               label: "Open New Web Shell (beta)",
               icon: "text-background",
-              onClick: this.onGuacShell.bind(
+              onClick: this.onGuacConn.bind(
                   this,
                   ipAddress,
-                  this.props.instance),
+                  this.props.instance,
+                  "ssh"),
               openInNewWindow: true,
               isDisabled: disableWebLinks
           });
@@ -319,10 +289,11 @@ export default React.createClass({
             links.push({
               label: "Open New Web Desktop (beta)",
               icon: "sound-dolby",
-              onClick: this.onGuacDesktop.bind(
+              onClick: this.onGuacConn.bind(
                 this,
                 ipAddress,
-                this.props.instance),
+                this.props.instance,
+                "vnc"),
               openInNewWindow: true,
               isDisabled: disableWebLinks
             });
