5e001a2ba1fe01aef9724af60184495d1fd7d6de
Tharon Carlson <design@tharoncarlson.com>
2017-05-21 20:23:25 +0000

    Make NotFoundPage more flexible

--- troposphere/static/js/components/NotFoundPage.jsx
+++ troposphere/static/js/components/NotFoundPage.jsx
@@ -1,30 +1,41 @@
 import React from "react";
+import { hasLoggedInUser } from 'utilities/profilePredicate';
+import { capitalize } from 'utilities/str';
+
 import Glyphicon from "components/common/Glyphicon";
+import stores from "stores";
 
 
 export default React.createClass({
     displayName: "NotFoundPage",
 
     render: function() {
-        let window_location = window.location.pathname;
+        const { resource = "page" } = this.props;
+        const window_location = window.location.pathname;
+        const profile = stores.ProfileStore.get();
+        const isLoggedIn = hasLoggedInUser(profile);
 
         return (
         <div style={{ paddingTop: "50px" }} className="container">
-            <h1 className="t-display-1">Page Unavailable</h1>
+            <h1 className="t-display-1">
+                {`${capitalize(resource)} Unavailable`}
+            </h1>
             <div>
-                <p style={{ "fontSize": "133%" }}>
+                <p style={{ fontSize: "20px" }}>
                     <Glyphicon name="info-sign" />
-                    {`
-                    The requested page cannot be viewed because
+                    {` The requested ${resource} cannot be viewed because
                     it either does not exist or you do not currently
-                    have permission to view it.
-                `}
+                    have permission to view it.`}
                 </p>
-                <p style={{ "fontSize": "133%" }}>
-                    {`This page may be visible if you `}
-                    <a href={"/login?redirect_to=" + window_location}>log in</a>
-                    {`.`}
-                </p>
+                {
+                    isLoggedIn ?  null : (
+                        <p style={{ fontSize: "20px" }}>
+                            {`This ${resource} may be visible if you `}
+                            <a href={"/login?redirect_to=" + window_location}>log in</a>
+                            {`.`}
+                        </p>
+                    )
+                }
             </div>
         </div>
         );
