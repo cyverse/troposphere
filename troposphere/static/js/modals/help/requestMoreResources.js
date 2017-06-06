7041b04c8d0ac247a8b43fd32b3f9c99ec44a911
Lucas H. Xu <xuh@bu.edu>
2017-06-06 14:07:12 -0400

    fix CI error on the y branch

--- troposphere/static/js/modals/help/requestMoreResources.js
+++ troposphere/static/js/modals/help/requestMoreResources.js
@@ -1,9 +1,7 @@
 import actions from "actions";
-import globals from "globals";
 import ModalHelpers from "components/modals/ModalHelpers";
 
 import RequestMoreResourcesModal from "components/modals/RequestMoreResourcesModal";
-import RequestMoreQuotaOrAllocationModal from "components/modals/RequestMoreQuotaOrAllocationModal";
 
 // HOTFIX: show the NON-Jetstream modal ;__;
 let RequestModal = RequestMoreResourcesModal;
