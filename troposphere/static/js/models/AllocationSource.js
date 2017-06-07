e6cebf60f20cdf0b25562a452147f6d024655119
Connor Osborn <cdosborn@email.arizona.edu>
2017-06-07 17:00:50 +0000

    Update model definition so that model.id is uuid

--- troposphere/static/js/models/AllocationSource.js
+++ troposphere/static/js/models/AllocationSource.js
@@ -2,5 +2,6 @@ import Backbone from "backbone";
 import globals from "globals";
 
 export default Backbone.Model.extend({
+    idAttribute: "uuid",
     urlRoot: globals.API_V2_ROOT + "/allocation_sources"
 });
