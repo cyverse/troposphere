4d7e9fad42c8f846f6bf828aa0bec2a41180e55b
Andrew J Lenards <andrew.lenards@gmail.com>
2017-06-15 00:17:45 +0000

    Remove custom tooltip for react-tooltip
    This responds to feedback during review related to using the
    `react-tooltip` library over a ReactDOM-style approach. We have
    favored this in prior component, but `react-tooltip` appears to
    handle nearly all of our usage scenarios.
    
    Thanks to Connor for the feedback and to point out how this component
    was used in components/admin/ResourceRequest.jsx

--- troposphere/static/js/components/admin/Emulate.jsx
+++ troposphere/static/js/components/admin/Emulate.jsx
@@ -1,9 +1,9 @@
 import $ from "jquery";
 import React from "react";
 import ReactDOM from "react-dom";
+import Tooltip from "react-tooltip";
 
 import Glyphicon from 'components/common/Glyphicon';
-import Tooltip from "components/common/ui/Tooltip";
 
 
 export default React.createClass({
@@ -13,27 +13,18 @@ export default React.createClass({
         username: React.PropTypes.string.isRequired
     },
 
-    componentDidMount() {
-        this.generateTooltip();
-    },
-
-    componentDidUpdate() {
-        this.generateTooltip();
-    },
-
-    generateTooltip() {
-        let $el = $(ReactDOM.findDOMNode(this));
-        $el.tooltip({
-            title: "Emulate"
-        });
-    },
-
     render() {
         let { username } = this.props;
 
         return (
         <a href={`emulate/${username}`}>
-            <Glyphicon name="sunglasses" />
+            <i className={"glyphicon glyphicon-sunglasses"}
+               data-for={username}
+               data-tip="Emulate"
+               aria-hidden="true" />
+            <Tooltip id={username}
+                     place="top"
+                     effect="solid" />
         </a>
         );
     }
