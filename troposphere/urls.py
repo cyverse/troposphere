9302ef1df88ad9bc9205d35d7773c94d03ad870a
Connor Osborn <cdosborn@email.arizona.edu>
2017-06-29 23:51:20 +0000

    Unify the definitions for web_desktop and guacamole

--- troposphere/urls.py
+++ troposphere/urls.py
@@ -36,9 +36,6 @@ ui_urlpatterns = [
     url(r'^web_desktop$', views.web_desktop),
 ]
 
-if settings.GUACAMOLE_ENABLED:
-    ui_urlpatterns.append(url(r'^guacamole$', views.guacamole))
-
 # NOTE: Have to remove the leading slash on 'BASE_URL'
 base_url = settings.BASE_URL.lstrip("/")
 
--- troposphere/views/web_desktop.py
+++ troposphere/views/web_desktop.py
@@ -12,80 +12,45 @@ from troposphere.views.exceptions import failure_response
 logger = logging.getLogger(__name__)
 
 
-def guacamole(request):
-    """
-    Signs a redirect to transparent proxy for web desktop view.
-    """
-
-    if request.user.is_authenticated():
-        # logger.debug("user %s is authenticated, well done." % request.user)
-        if 'instanceId' in request.POST:
-            instance_id = request.POST['instanceId']
-            protocol = request.POST['protocol']
-            auth_token = request.session.get('access_token')
-            access_token_route = settings.API_V2_ROOT+"/web_tokens/%s?client=guacamole&protocol=%s" % (instance_id, protocol)
-            headers = {
-                'Authorization': "Token %s" % auth_token,
-                'Accept': 'application/json',
-            }
-            # FIXME: Remove verify=False in the future
-            response = requests.get(access_token_route, headers=headers, verify=False)
-            # Raise exceptions for HTTP errors
-            response.raise_for_status()
-            data = response.json()
-            url = data.get('token_url')
-            response = HttpResponseRedirect(url)
-            response.set_cookie('original_referer', request.META['HTTP_REFERER'],
-                    domain=settings.WEB_DESKTOP['redirect']['COOKIE_DOMAIN'])
-
-            logger.info("redirect response: %s" % (response))
-            return response
-        else:
-            logger.info("Failed request - POST body: %s" % request.POST)
-            return failure_response(
-                status.HTTP_400_BAD_REQUEST,
-                "POST does not contain the required data")
-
-    else:
-        logger.info("not authenticated: \nrequest:\n %s" % request)
-        raise PermissionDenied
-
-
 def web_desktop(request):
     """
     Signs a redirect to transparent proxy for web desktop view.
     """
+    if not request.user.is_authenticated():
+        raise PermissionDenied
 
-    if request.user.is_authenticated():
-        # logger.debug("user %s is authenticated, well done." % request.user)
-        if 'instanceId' in request.POST:
-            instance_id = request.POST['instanceId']
-            auth_token = request.session.get('access_token')
-            access_token_route = settings.API_V2_ROOT+"/web_tokens/%s" % instance_id
-            headers = {
-                'Authorization': "Token %s" % auth_token,
-                'Accept': 'application/json',
-            }
-            #FIXME: Remove verify=False in the future
-            response = requests.get(access_token_route, headers=headers, verify=False)
-            # Raise exceptions for HTTP errors
-            response.raise_for_status()
-            data = response.json()
-            url = data.get('token_url')
-
-            response = HttpResponseRedirect(url)
-            response.set_cookie('original_referer', request.META['HTTP_REFERER'],
-                    domain=settings.WEB_DESKTOP['redirect']['COOKIE_DOMAIN'])
-
-            logger.info("redirect response: %s" % (response))
-
-            return response
-        else:
-            logger.info("Failed request - POST body: %s" % request.POST)
-            return failure_response(
+    # Check that all query params are present
+    requiredFields = ['client', 'instance_id', 'protocol']
+    if not all(field in request.POST for field in requiredFields):
+        return failure_response(
                 status.HTTP_400_BAD_REQUEST,
                 "POST does not contain the required data")
 
-    else:
-        logger.info("not authenticated: \nrequest:\n %s" % request)
-        raise PermissionDenied
+    auth_token = request.session.get('access_token')
+
+    access_token_route = \
+        "{api_root}/web_tokens/{instance_id}?client={client}&protocol={protocol}" \
+        .format(
+            api_root=settings.API_V2_ROOT,
+            client=request.POST['client'],
+            protocol=request.POST['protocol'],
+            instance_id=request.POST['instance_id']
+        )
+
+    headers = {
+        'Authorization': "Token %s" % auth_token,
+        'Accept': 'application/json',
+    }
+
+    # FIXME: Remove verify=False in the future
+    response = requests.get(access_token_route, headers=headers, verify=False)
+
+    # Raise exceptions for HTTP errors
+    response.raise_for_status()
+
+    data = response.json()
+    url = data.get('token_url')
+    response = HttpResponseRedirect(url)
+    response.set_cookie('original_referer', request.META['HTTP_REFERER'])
+
+    return response
