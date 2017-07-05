f872181630ef237d0ee919f768f9339f6f698d2b
Connor Osborn <cdosborn@email.arizona.edu>
2017-07-05 22:51:08 +0000

    Fix warnings that suggest using onChange over onInput

--- troposphere/static/js/components/common/boot_script/EditScript.jsx
+++ troposphere/static/js/components/common/boot_script/EditScript.jsx
@@ -160,7 +160,7 @@ export default React.createClass({
                 <input className="form-control"
                     placeholder="http://yourscript.org"
                     value={this.state.text}
-                    onInput={this.onChangeText}
+                    onChange={this.onChangeText}
                     onBlur={this.onBlurText} />
                 <span className="help-block">{errorMessage}</span>
             </div>
@@ -182,7 +182,7 @@ export default React.createClass({
                     placeholder="#!/bin/bash"
                     rows="6"
                     value={this.state.text}
-                    onInput={this.onChangeText}
+                    onChange={this.onChangeText}
                     onBlur={this.onBlurText} />
                 <span className="help-block">{errorMessage}</span>
             </div>
@@ -218,7 +218,7 @@ export default React.createClass({
                         <input className="form-control"
                             placeholder="My Script"
                             value={this.state.title}
-                            onInput={this.onChangeTitle}
+                            onChange={this.onChangeTitle}
                             onBlur={this.onBlurTitle} />
                         <span className="help-block">{errorMessage}</span>
                     </div>
