341965c9c58547552e310dfdc7fb5ec5ecd1f87e
Connor Osborn <cdosborn@email.arizona.edu>
2017-07-19 23:34:01 +0000

    Show editable allocations only if we control allocations

--- troposphere/static/js/components/admin/ResourceRequest/AllocationSourcesView.jsx
+++ troposphere/static/js/components/admin/ResourceRequest/AllocationSourcesView.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import globals from "globals";
 import ProgressBar from "components/common/ui/ProgressBar";
 
 export default React.createClass({
@@ -9,11 +10,16 @@ export default React.createClass({
         onAllocationChange: React.PropTypes.func.isRequired
     },
 
-    renderEditableAllocation(allocationSource) {
+    renderAllocation(allocationSource) {
         let { name } = allocationSource;
         let props = {
             allocationSource,
             onAllocationChange: this.props.onAllocationChange
+        }
+        if (globals.EXTERNAL_ALLOCATION) {
+            return (
+            <AllocationView key={name} { ...{ allocationSource } } />
+            );
         }
         return (
         <AllocationEditableView key={name} { ...props } />
@@ -25,7 +31,7 @@ export default React.createClass({
 
         let body = "There are no allocations.";
         if (allocationSources.length) {
-            body = allocationSources.map(this.renderEditableAllocation);
+            body = allocationSources.map(this.renderAllocation);
         }
 
         return (
@@ -36,18 +42,53 @@ export default React.createClass({
     }
 });
 
-var AllocationEditableView = React.createClass({
+class AllocationView extends React.Component {
+    getUsage() {
+        let {
+            allocationSource: { compute_used, compute_allowed }
+        } = this.props;
 
-    propTypes: {
-        allocationSource: React.PropTypes.object.isRequired,
-        onAllocationChange: React.PropTypes.func.isRequired
-    },
+        let percent = 0;
+        let total = 0;
+        // Prevent division by 0
+        if (compute_allowed !== 0) {
 
-    getInitialState() {
-        return {
-            isEditing: false
+            // Note: Multiplying/rounding to get 2 decimal places
+            percent = Math.round((compute_used / compute_allowed * 100) * 100) / 100;
+
+            total = compute_allowed;
         }
-    },
+
+        return { percent, total };
+    }
+
+    render() {
+        let { allocationSource: { name } } = this.props;
+        let { percent, total } = this.getUsage();
+
+        let allocationLabel = (
+        <span>
+             { `${percent}% used of ${total} Allocation from ${name}` }
+        </span>
+        );
+
+        return (
+        <ProgressBar startColor="#56AA21" startValue={percent} label={allocationLabel} />
+        );
+    }
+}
+
+AllocationView.propTypes = {
+    allocationSource: React.PropTypes.object.isRequired
+}
+
+class AllocationEditableView extends AllocationView {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false
+        };
+    }
 
     renderEditIcon() {
         let { isEditing } = this.state;
@@ -56,64 +97,50 @@ var AllocationEditableView = React.createClass({
             onClick={() => this.setState({ isEditing: !isEditing })}
             className="glyphicon glyphicon-pencil" />
         );
-    },
+    }
 
-    onInputChange(e) {
+    onInputChange({ target: { value }}) {
         let { id } = this.props.allocationSource;
-        let compute_allowed = +e.target.value;
+        let compute_allowed = +value;
         this.props.onAllocationChange({ id, compute_allowed });
-    },
+    }
 
-    renderAllocationInput(num) {
+    renderEditableField(num) {
         let style = {
             margin:"0 5px"
         }
         return (
         <input style={style}
-               onChange={this.onInputChange}
+               onChange={this.onInputChange.bind(this)}
                type="number"
                value={num} />
         );
-    },
+    }
 
     render() {
-        let {
-            allocationSource: {
-                compute_used: consumed,
-                compute_allowed,
-                name
-            }
-        } = this.props;
+        let { allocationSource: { name } } = this.props;
         let { isEditing } = this.state;
-
-
-        let percent = 0;
-        let total = 0;
-        // Prevent division by 0
-        if (compute_allowed !== 0) {
-
-            // Note: Multiplying/rounding to get 2 decimal places
-            percent = Math.round((consumed / compute_allowed * 100) * 100) / 100;
-
-            total = compute_allowed;
-        }
-
-        // Render total as an input field
-        if (isEditing) {
-            total = this.renderAllocationInput(compute_allowed);
-        }
+        let { percent, total } = this.getUsage();
+        let editableTotal =
+            isEditing
+            ? this.renderEditableField(total)
+            : total;
 
         let allocationLabel = (
         <span>
              { `${percent}% used of ` }
-             { total }
+             { editableTotal }
              { ` Allocation from ${name}` }
              { this.renderEditIcon() }
         </span>
-        )
+        );
 
         return (
         <ProgressBar startColor="#56AA21" startValue={percent} label={allocationLabel} />
         );
     }
-});
+}
+
+AllocationEditableView.propTypes = {
+    onAllocationChange: React.PropTypes.func.isRequired
+}
