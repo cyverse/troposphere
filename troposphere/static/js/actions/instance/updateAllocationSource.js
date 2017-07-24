5a7d22f2032f4e2e51bf9eb2b7d21b083aa209a6
Connor Osborn <cdosborn@email.arizona.edu>
2017-07-24 17:43:07 +0000

    Handle failure on allocation source change

--- troposphere/static/js/actions/instance/updateAllocationSource.js
+++ troposphere/static/js/actions/instance/updateAllocationSource.js
@@ -1,5 +1,8 @@
+import Raven from "raven-js";
+
 import Utils from "../Utils";
 import InstanceConstants from "constants/InstanceConstants";
+import NotificationController from "controllers/NotificationController";
 
 export default function updateAllocationSource(params) {
     if (!params.instance)
@@ -8,9 +11,31 @@ export default function updateAllocationSource(params) {
         throw new Error("Missing allocation source");
 
     let { instance, allocationSource } = params;
-
-    instance.save({"allocation_source": allocationSource}, { patch: true });
-    Utils.dispatch(InstanceConstants.UPDATE_INSTANCE, {
-        instance: instance
+    let p = Promise.resolve(
+        instance.save({"allocation_source": allocationSource}, { patch: true })
+    ).then(() => {
+        Utils.dispatch(InstanceConstants.UPDATE_INSTANCE, { instance });
     });
+
+    let allocationName = allocationSource.get("name");
+    let instanceName = instance.get("name");
+    p.catch(response => {
+        let err =
+            `Assigning allocation source: "${allocationName}" to "${instanceName}" failed`;
+
+        NotificationController.error(
+            "Error assigning allocation source",
+            err
+        );
+        if (Raven.isSetup()) {
+            Raven.captureMessage(err);
+        }
+
+        // Components that are using an optimistic version of instance, need
+        // to get sent the latest version of instance without the optimistic
+        // change to allocation_source
+        Utils.dispatch(InstanceConstants.UPDATE_INSTANCE, { instance });
+    })
+
+    return p;
 }
