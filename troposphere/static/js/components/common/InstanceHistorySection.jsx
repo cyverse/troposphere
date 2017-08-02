3250dab3af97635b3c6ec959863aa88142baab50
Steve Gregory <contact@steve-gregory.com>
2017-08-02 11:02:51 -0700

    Solution: Show fault message (and traceback, if staff)
    - Remove redundancy -- Use a common 'InstanceDetail' component for both 'historical' and 'active-and-in-a-project' instances.
    
    - Create an InstanceStatusHistory table
    - Add polish to the table:
    - Include a refresh button
    - Show additional traceback if staff user is viewing (Even when emulated!)
    - Include 'hasEmulatedSession' in context based on template variables.

--- troposphere/static/js/components/common/InstanceHistorySection.jsx
+++ troposphere/static/js/components/common/InstanceHistorySection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Backbone from "backbone";
 import stores from "stores";
+import context from "context";
 import moment from "moment";
 
 
@@ -15,16 +16,26 @@ var InstanceHistorySection = React.createClass({
         return {
             instanceHistory: stores.InstanceHistoryStore.fetchWhere({
                 "instance": this.props.instance.id
-            })
+            }),
+            refreshing: false
         }
     },
 
     componentDidMount: function() {
         stores.InstanceHistoryStore.addChangeListener(this.onNewData);
+        stores.InstanceStore.addChangeListener(this.onNewData);
+        stores.InstanceHistoryStore.addChangeListener(this.requestListener);
     },
 
     componentWillUnmount: function() {
         stores.InstanceHistoryStore.removeChangeListener(this.onNewData);
+        stores.InstanceStore.removeChangeListener(this.onNewData);
+        stores.InstanceHistoryStore.removeChangeListener(this.requestListener);
+    },
+    requestListener() {
+        this.setState({
+            refreshing: false
+        });
     },
 
     onNewData: function() {
@@ -35,19 +46,73 @@ var InstanceHistorySection = React.createClass({
         });
     },
 
+    style() {
+        return {
+            refreshIcon: {
+                float: "right",
+                color: "lightgrey"
+            },
+        }
+    },
+
+    renderRefreshButton() {
+        let { refreshing } = this.state;
+        let { refreshIcon } = this.style();
+        let controlsClass = "glyphicon glyphicon-refresh";
+
+        if (refreshing) {
+            controlsClass += " refreshing"
+            refreshIcon.color = "inherit";
+        }
+
+        return (
+        <span className={controlsClass} style={refreshIcon} onClick={this.onRefresh} />
+        );
+    },
+
+    renderHistoryRow: function(historyItem) {
+        let profile = stores.ProfileStore.get();
+        let is_staff_user = (profile) ? profile.get("is_staff") : false;
+        let extra = historyItem.get('extra'),
+            formattedStartDate = moment(historyItem.get("start_date")).format("MMMM Do YYYY, h:mm a"),
+            formattedEndDate = "Present";
+        if (historyItem.get("end_date") && historyItem.get("end_date").isValid()) {
+            formattedEndDate = moment(historyItem.get("end_date")).format("MMMM Do YYYY, h:mm a");
+        }
+        let formattedExtra = "";
+        let formattedExtraLines = [];
+        let show_traceback = (is_staff_user || context.hasEmulatedSession() );
+        if(extra && 'display_error' in extra) {
+            formattedExtra = extra['display_error'];
+            if('traceback' in extra && show_traceback) {
+                formattedExtra = formattedExtra + "\\n" + extra['traceback']
+                formattedExtraLines = formattedExtra.split('\\n');
+            }
+        }
+        return (<tr key={historyItem.cid}>
+                    <td>{historyItem.get("status")}</td>
+                    <td>{formattedStartDate}</td>
+                    <td>{formattedEndDate}</td>
+                    <td>{formattedExtraLines.map(
+                        (strng, idx) => (<p key={idx}>{strng}</p>)) }
+                    </td>
+                </tr>);
+    },
+    onRefresh() {
+        stores.InstanceHistoryStore.clearCache();
+        let instanceHistory = stores.InstanceHistoryStore.fetchWhere({
+            "instance": this.props.instance.id
+        })
+        this.setState({
+            refreshing: true,
+            instanceHistory
+        });
+
+    },
     render: function() {
         var instance = this.props.instance;
-        var content,
-            items,
-            deletedInfo;
+        var content;
 
-        if (instance.get("end_date")) {
-            deletedInfo = (
-                <lh>
-                    <strong>{"Deleted on " + moment(instance.get("end_date")).format("MMMM Do YYYY, h:mm a")}</strong>
-                </lh>
-            );
-        }
         if (!this.state.instanceHistory) {
             if (stores.InstanceHistoryStore.isFetching) {
                 content = (
@@ -61,30 +126,25 @@ var InstanceHistorySection = React.createClass({
                 );
             }
         } else {
-            items = this.state.instanceHistory.map(function(historyItem) {
-                let formattedAUTotal = historyItem.get("total_hours"),
-                    formattedStartDate = moment(historyItem.get("start_date")).format("MMMM Do YYYY, h:mm a"),
-                    formattedEndDate = "Present";
-                if (historyItem.get("end_date") && historyItem.get("end_date").isValid()) {
-                    formattedEndDate = moment(historyItem.get("end_date")).format("MMMM Do YYYY, h:mm a");
-                }
-                return (<div key={historyItem.cid}>
-                            {historyItem.get("status")}:
-                            {formattedStartDate} -
-                            {formattedEndDate} -
-                            {formattedAUTotal}
-                        </div>);
-            });
             content = (
-                <ul>
-                    {deletedInfo}
-                    {items}
-                </ul>
+                <table className="clearfix table" style={{ tableLayout: "fixed" }}>
+                    <thead>
+                        <tr>
+                            <th style={{ width: "100px"}}>Status</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Message</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.instanceHistory.map(this.renderHistoryRow) }
+                    </tbody>
+                </table>
             );
         }
         return (
         <div className="resource-details-section section">
-            <h4 className="t-title">Instance Status History</h4>
+            <h4 className="t-headline">Instance Status History {this.renderRefreshButton()}</h4>
             {content}
         </div>
         );
