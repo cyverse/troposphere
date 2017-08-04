e37c5016a4dfc727b260f72fcd76580cf121f287
Andrew J Lenards <andrew.lenards@gmail.com>
2017-08-04 23:26:32 +0000

    Add wrapping function for Sentry "capture" calls
    This is added so that the components are not "bound", or coupled, to the
    Raven/Sentry's client. It would act as a facade around the client,
    though not all methods are exposed - just "captureMessage" [0]. It does
    also allow for additional data to be associated with the captured
    occurrence [1].
    
    This could be done like so:
    
    Raven.captureMessage('Increase transparency', {
      extra: { for: 'fun', also: 'dominate' }
    });
    
    The second argument will be merged with the context available to Raven
    when the message is captured. [2]
    
    [0] https://docs.sentry.io/clients/javascript/usage/#capturing-messages
    [1] https://docs.sentry.io/clients/javascript/usage/#passing-additional-data
    [2] https://github.com/getsentry/raven-js/blob/864fa390d3f99e872e3b89c779ee10757675d25d/src/raven.js#L398-L405

---
+++ troposphere/static/js/utilities/capture.js
@@ -0,0 +1,44 @@
+import Raven from "raven-js";
+
+/**
+ * Send message out to logging/context capture service
+ *
+ * @param {string} message short description of occurrence
+ * @param {object} data any extra context to associate
+ *
+ * Additional data can be passed as the second parameter to
+ * `captureMessage` and will be merged with any global
+ * context set on the raven client.
+ *
+ * There are some 'keyword' properties, like `level`. The
+ * example below is provided in the Sentry docs:
+ * ```
+ *   Raven.captureMessage('Something happened', {
+ *     level: 'info' // one of 'info', 'warning', or 'error'
+ *   });
+ * ```
+ *
+ * Though, you can pass keys in and they will be merged with
+ * the existing context.
+ *
+ * (Note: believe the global context will overwrite data
+ * passed in. You can verify this in the raven-js source,
+ * look for `objectMerge()`)
+ *
+ * ```
+ *   Raven.captureMessage('Something happened', {
+ *     instance: '<uuid>',
+ *     troposphere_context: 'page-view'
+ *   });
+ * ```
+ */
+const captureMessage = (message, data) => {
+    // only call if we have a configuration client available
+    if (Raven && Raven.isSetup()) {
+        Raven.captureMessage(message, {
+            extra: data
+        });
+    }
+}
+
+export { captureMessage };
