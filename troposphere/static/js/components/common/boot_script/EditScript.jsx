509587f0ca4e5f992abed2cdfa1301710011ac1d
Steve Gregory <contact@steve-gregory.com>
2017-09-25 09:17:21 -0700

    Consolidate to common code, 'EditScript'
    Currently, warnings exist for EditScript:
    - The element to display: "Run script on every deployment" doesn't exist
    in the list of available elements
    - SelectMenu: The element to display: "Execute scripts asynchronously.
    Store stdout/stderr to log files." doesn't exist in the list of
    available elements
    - SelectMenu: The element to display: "Import by Text" doesn't exist in
    the list of available elements

--- troposphere/static/js/components/common/boot_script/EditScript.jsx
+++ troposphere/static/js/components/common/boot_script/EditScript.jsx
@@ -1,16 +1,17 @@
 import Backbone from "backbone";
 import React from "react";
 import RaisedButton from "material-ui/RaisedButton";
+import SelectMenu from "components/common/ui/SelectMenu";
 import actions from "actions";
 
 export default React.createClass({
 
     propTypes: {
-        script: React.PropTypes.instanceOf(Backbone.Model).isRequired,
+        script: React.PropTypes.instanceOf(Backbone.Model),
         style: React.PropTypes.object,
-        wait_for_deploy: React.PropTypes.bool.isRequired,
-        close: React.PropTypes.func.isRequired,
-        onSave: React.PropTypes.func.isRequired
+        footerClassName: React.PropTypes.string,
+        onClose: React.PropTypes.func.isRequired,
+        onScriptChanged: React.PropTypes.func.isRequired
     },
 
     getInitialState() {
@@ -45,14 +46,22 @@ export default React.createClass({
 
     getDefaultProps: function() {
         return {
-            style: { position: "absolute", bottom: "75px", right: "15px" }
+            style: {},
+            footerStyle: { position: "absolute", bottom: "75px", right: "15px" },
+            footerClassName: "",
+            script: null
         }
     },
 
-    onChangeStrategy: function(e) {
-        let strategy = e.target.value;
+    onChangeDeploymentType: function(deploymentType) {
         this.setState({
-            strategy
+            deploymentType
+        })
+    },
+
+    onChangeStrategyType: function(strategyType) {
+        this.setState({
+            strategyType
         })
     },
 
@@ -64,10 +73,9 @@ export default React.createClass({
         })
     },
 
-    onChangeType: function(e) {
-        let type = e.target.value;
+    onChangeInputType: function(inputType) {
         this.setState({
-            type
+            inputType
         })
     },
 
@@ -99,26 +107,6 @@ export default React.createClass({
         });
     },
 
-    onSaveScript: function() {
-        if (!this.state.validate) {
-            this.setState({
-                validate: true
-            });
-        }
-        if (this.isSubmittable) {
-            let script = actions.ScriptActions.update(this.props.script, {
-                type: this.state.type,
-                strategy: this.state.strategy,
-                title: this.state.title.trim(),
-                text: this.state.text.trim()
-            });
-            this.props.onSave(script);
-            this.props.close();
-        }
-    },
-
-    // A utility function testing for whitespace or empty string at the beginning or end of string.
-    // There is probably a better place to put this.
     isValidString: function(str) {
         if (str !== "") {
             return true
@@ -194,7 +182,7 @@ export default React.createClass({
                     placeholder="#!/bin/bash"
                     rows="6"
                     value={this.state.text}
-                    onChange={this.onChangeText}
+                    onInput={this.onChangeText}
                     onBlur={this.onBlurText} />
                 <span className="help-block">{errorMessage}</span>
             </div>
@@ -202,120 +190,152 @@ export default React.createClass({
         }
     },
 
+    onCreateScript: function() {
+        if (!this.state.validate) {
+            this.setState({
+                validate: true
+            });
+        }
+        if (this.isSubmittable) {
+            actions.ScriptActions.create({
+                title: this.state.title.trim(),
+                text: this.state.text.trim(),
+                type: this.state.type,
+                strategy: this.state.strategy
+            });
+            this.hide();
+        }
+    },
+
+    onSaveScript: function() {
+        if (!this.state.validate) {
+            this.setState({
+                validate: true
+            });
+        }
+        if (this.isSubmittable) {
+            let script = actions.ScriptActions.update(this.props.script, {
+                title: this.state.title.trim(),
+                text: this.state.text.trim(),
+                type: this.state.type,
+                strategy: this.state.strategy
+            });
+            return script;
+        }
+    },
+
+    onSubmit: function() {
+        let script;
+        if( this.props.script) {
+            script = this.onSaveScript();
+        } else {
+            script = this.onCreateScript();
+        }
+        this.props.onScriptChanged(script);
+    },
+
     render: function() {
-        let title = this.state.title;
         let classNames = "form-group";
         let errorMessage = null;
-        let disable = false;
+        let notSubmittable = false;
+        let headerText = (this.props.script) ? "Edit Script" : "Create Script";
+        let inputTypes = [
+                {"name": "URL"},
+                {"name": "Raw Text"}],
+            inputChoices = {
+                "URL": "Import by URL",  // Advantages: update script external from Atmosphere
+                "Raw Text": "Import by Text"
+            },
+            strategyTypes = [
+                {"name": "once"},
+                {"name": "always"}],
+            strategyChoices = {
+                "once": "Run Script on first boot",
+                "always": "Run script on every deployment"
+            },
+            deploymentTypes = [
+                {"name": "sync"},
+                {"name": "async"}
+            ],
+            deploymentChoices = {
+                "sync": "Wait for script to complete, ensure exit code 0, email me if there is a failure.",
+                "async": "Execute scripts asynchronously. Store stdout/stderr to log files."};
+        let {deploymentType, strategyType, inputType, title} = this.state;
+
+        // TODO: Looking to tell the user where the scripts output/stderr will be stored? Add this content.. somewhere.
+        // {"Log stdout in '/var/log/atmo/instance-scripts/"+this.state.title+".YYYY-MM-DD_HH:MM:SS.stdout'"}
+        // {"Log stderr in '/var/log/atmo/instance-scripts/"+this.state.title+".YYYY-MM-DD_HH:MM:SS.stderr'"}
+
+        if(!deploymentType) {
+            deploymentType = deploymentTypes[0];
+        }
+        if(!strategyType) {
+            strategyType = strategyTypes[0];
+        }
+        if(!inputType) {
+            inputType = inputTypes[0];
+        }
+
 
         if (this.state.validate) {
             if (!this.isValidString(title)) {
                 classNames = "form-group has-error";
                 errorMessage = `This field is required`;
             }
-            disable = !this.isSubmittable();
+            notSubmittable = !this.isSubmittable();
         }
 
         return (
-
-        <div style={{ position: "reletive" }}>
-            <h3 className="t-subheading">Edit Script</h3>
+        <div style={this.props.style}>
+            <h3 className="t-subheading">{headerText}</h3>
             <hr/>
             <div className="row">
-                <div className="col-md-6">
-                    <div className={classNames}>
-                        <label>
-                            Script Title
-                        </label>
-                        <input className="form-control"
-                            placeholder="My Script"
-                            value={this.state.title}
-                            onChange={this.onChangeTitle}
-                            onBlur={this.onBlurTitle} />
-                        <span className="help-block">{errorMessage}</span>
-                    </div>
-                    <h4 className="t-body-2">Input Type</h4>
-                    <div className="radio-inline">
-                        <label className="radio">
-                            <input type="radio"
-                                name="optionsRadios"
-                                value="URL"
-                                defaultChecked={this.state.type === "URL"}
-                                onClick={this.onChangeType} /> URL
-                        </label>
-                    </div>
-                    <div className="radio-inline">
-                        <label className="radio">
-                            <input type="radio"
-                                name="optionsRadios"
-                                value="Raw Text"
-                                defaultChecked={this.state.type === "Raw Text"}
-                                onClick={this.onChangeType} /> Raw Text
-                        </label>
-                    </div>
-                    <h4 className="t-body-2">Boot Script Type</h4>
-                    <div className="radio-inline">
-                        <label className="radio">
-                            <input type="radio"
-                                name="optionsRadios-2"
-                                value="once"
-                                defaultChecked={this.state.strategy === "once"}
-                                onClick={this.onChangeStrategy} /> {"Run script on first boot"}
-                        </label>
-                    </div>
-                    <div className="radio-inline">
-                        <label className="radio">
-                            <input type="radio"
-                                name="optionsRadios-2"
-                                value="always"
-                                defaultChecked={this.state.strategy === "always"}
-                                onClick={this.onChangeStrategy} /> {"Run script on each deployment"}
-                        </label>
-                    </div>
-                    <h4 className="t-body-2">{"Wait for Deployment"}</h4>
-                    <div className="radio-inline">
-                        {"Wait for script to complete, ensure exit code 0, email me if there is a failure."}
-                        <label className="radio">
-                            <input type="radio"
-                                name="optionsRadios-2"
-                                value="true"
-                                defaultChecked={this.state.wait_for_deploy === true}
-                                onClick={this.onChangeWaitForDeploy} /> {"Wait for script to complete."}
-                        </label>
-                    </div>
-                    <div className="radio-inline">
-                        {"Execute scripts asynchronously. "}
-                        {"Log stdout in '/var/log/atmo/instance-scripts/"+this.state.title+"YYYY-MM-DD_HH:MM:SS.stdout'"}
-                        {"Log stderr in '/var/log/atmo/instance-scripts/"+this.state.title+"YYYY-MM-DD_HH:MM:SS.stderr'"}
-                        <label className="radio">
-                            <input type="radio"
-                                name="optionsRadios-2"
-                                value="false"
-                                defaultChecked={this.state.wait_for_deploy === false}
-                                onClick={this.onChangeWaitForDeploy} /> {"Execute scripts asynchronously."}
-                        </label>
-                    </div>
+                <div className={classNames}>
+                    <label>{"Script Title"}</label>
+                    <input className="form-control"
+                        placeholder="My Script"
+                        value={this.state.title}
+                        onInput={this.onChangeTitle}
+                        onBlur={this.onBlurTitle} />
+                    <span className="help-block">{errorMessage}</span>
                 </div>
+                <h4 className="t-body-2">{"Boot Script Type"}</h4>
+                <SelectMenu current={strategyType}
+                    optionName={strategyType => strategyChoices[strategyType.name] }
+                    list={strategyTypes}
+                    onSelect={this.onChangeStrategyType} />
+
+                <h4 className="t-body-2">{"Deployment Type"}</h4>
+                <SelectMenu current={deploymentType}
+                    optionName={deploymentType => deploymentChoices[deploymentType.name] }
+                    list={deploymentTypes}
+                    onSelect={this.onChangeDeploymentType} />
+
+                <h4 className="t-body-2">{"Input Type"}</h4>
+                <SelectMenu current={inputType}
+                    optionName={inputType => inputChoices[inputType.name] }
+                    list={inputTypes}
+                    onSelect={this.onChangeInputType} />
                 <div className="col-md-6">
                     {this.renderInputType()}
                 </div>
             </div>
-            <div style={this.props.style}>
+            <div className={this.props.footerClassName} style={this.props.footerStyle}>
                 <RaisedButton
                     primary
                     className="pull-right"
-                    disabled={disable}
-                    onTouchTap={this.onSaveScript}
+                    disabled={notSubmittable}
+                    onTouchTap={this.onSubmit}
                     label="Save"
                 />
                 <RaisedButton
                     className="pull-right"
                     style={{ marginRight: "10px" }}
-                    onTouchTap={this.props.close}
+                    onTouchTap={this.props.onClose}
                     label="Cancel"
                 />
             </div>
         </div>
-        )
+        );
     }
 });
--- troposphere/static/js/components/modals/script/ScriptCreateModal.jsx
+++ troposphere/static/js/components/modals/script/ScriptCreateModal.jsx
@@ -2,6 +2,7 @@ import Backbone from "backbone";
 import React from "react";
 import BootstrapModalMixin from "components/mixins/BootstrapModalMixin";
 import SelectMenu from "components/common/ui/SelectMenu";
+import EditScript from "components/common/boot_script/EditScript";
 import actions from "actions";
 
 export default React.createClass({
@@ -9,289 +10,30 @@ export default React.createClass({
 
     mixins: [BootstrapModalMixin],
 
-    getInitialState: function() {
-        let script = this.props.script;
-        if(script) {
-            return ({
-                type: script.get('type'),
-                strategy: script.get('strategy'),
-                title: script.get('title'),
-                text: script.get('text'),
-                wait_for_deploy: script.get('wait_for_deploy'),
-                validate: false
-            });
-        }
-        return ({
-            type: "URL",
-            strategy: "always",
-            title: "",
-            text: "",
-            wait_for_deploy: true,
-            validate: false
-        })
-    },
-
     propTypes: {
         script: React.PropTypes.instanceOf(Backbone.Model),
     },
 
-    onChangeStrategy: function(strategy) {
-        this.setState({
-            strategy
-        })
+    onScriptChanged: function() {
+        this.hide();
     },
-
-    onChangeWaitForDeploy: function(e) {
-        let boolStr = e.target.value,
-            wait_for_deploy = boolStr == "true";
-        this.setState({
-            wait_for_deploy
-        })
-    },
-
-    onChangeType: function(type) {
-        this.setState({
-            type
-        })
-    },
-
-    onChangeTitle: function(e) {
-        let title = e.target.value;
-        this.setState({
-            title
-        })
-    },
-
-    onBlurTitle: function() {
-        let title = this.state.title.trim();
-        this.setState({
-            title
-        });
-    },
-
-    onChangeText: function(e) {
-        let text = e.target.value;
-        this.setState({
-            text
-        })
-    },
-
-    onBlurText: function() {
-        let text = this.state.text.trim();
-        this.setState({
-            text
-        });
-    },
-
-    isValidString: function(str) {
-        if (str.trim() !== "") {
-            return true
-        }
-        return false
-    },
-
-    isValidUrl: function(str) {
-        if (!(str.search("https?://") < 0)) {
-            if (str.indexOf(" ") >= 0) {
-                return false
-            }
-            return true
-        }
-        return false
-    },
-
-    isSubmittable: function() {
-        let title = this.state.title;
-        let text = this.state.text;
-
-        if (this.isValidString(title) && this.isValidString(text)) {
-            if (this.state.type === "URL") {
-                if (!this.isValidUrl(text)) {
-                    return false
-                }
-            }
-            return true;
-        }
-        return false
-    },
-
-    renderInputType: function() {
-        let text = this.state.text;
-        let classNames = "form-group";
-        let errorMessage = null;
-
-        if (this.state.type === "URL") {
-            if (this.state.validate) {
-                if (!this.isValidUrl(text) || !this.isValidString(text)) {
-                    classNames = "form-group has-error";
-                    errorMessage = `URL must start with "https://" or "http://" and have no spaces`;
-                }
-            }
-
-            return (
-            <div className={classNames}>
-                <label>
-                    Script URL
-                </label>
-                <input className="form-control"
-                    placeholder="http://yourscript.org"
-                    value={this.state.text}
-                    onInput={this.onChangeText}
-                    onBlur={this.onBlurText} />
-                <span className="help-block">{errorMessage}</span>
-            </div>
-            )
-        } else {
-            if (this.state.validate) {
-                if (!this.isValidString(text)) {
-                    classNames = "form-group has-error";
-                    errorMessage = `This field is required`;
-                }
-            }
-
-            return (
-            <div className={classNames}>
-                <label>
-                    Raw Text
-                </label>
-                <textarea className="form-control"
-                    placeholder="#!/bin/bash"
-                    rows="6"
-                    value={this.state.text}
-                    onInput={this.onChangeText}
-                    onBlur={this.onBlurText} />
-                <span className="help-block">{errorMessage}</span>
-            </div>
-            )
-        }
-    },
-
-    renderBody: function() {
-        let title = this.state.title;
-        let classNames = "form-group";
-        let errorMessage = null;
-
-        if (this.state.validate) {
-            if (!this.isValidString(title)) {
-                classNames = "form-group has-error";
-                errorMessage = `This field is required`;
-            }
-        }
-        let typeChoices = ["URL", "Raw Text"],
-            strategyTypes = [
-                "once",
-                "always"],
-            strategyChoices = {
-                "once": "Run Script on first boot",
-                "always": "Run script on every deployment"};
-
-
-        return (
-            <div>
-                <div>
-                    <div className={classNames}>
-                        <label>
-                            Script Title
-                        </label>
-                        <input className="form-control"
-                            placeholder="My Script"
-                            value={this.state.title}
-                            onInput={this.onChangeTitle}
-                            onBlur={this.onBlurTitle} />
-                        <span className="help-block">{errorMessage}</span>
-                    </div>
-                    <h4 className="t-body-2">Boot Script Type</h4>
-                    <SelectMenu current={this.state.strategy}
-                        optionName={strategy => strategyChoices[strategy] }
-                        list={strategyTypes}
-                        onSelect={this.onChangeStrategy} />
-
-                    <h4 className="t-body-2">{"Wait for Deployment"}</h4>
-                    <div className="radio-inline">
-                        {"Wait for script to complete, ensure exit code 0, email me if there is a failure."}
-                        <label className="radio">
-                            <input type="radio"
-                                name="optionsRadios-2"
-                                value="true"
-                                defaultChecked={this.state.wait_for_deploy === true}
-                                onClick={this.onChangeWaitForDeploy} /> {"Wait for script to complete."}
-                        </label>
-                    </div>
-                    <div className="radio-inline">
-                        {"Execute scripts asynchronously. "}
-                        {"Log stdout in '/var/log/atmo/instance-scripts/"+this.state.title+"YYYY-MM-DD_HH:MM:SS.stdout'"}
-                        {"Log stderr in '/var/log/atmo/instance-scripts/"+this.state.title+"YYYY-MM-DD_HH:MM:SS.stderr'"}
-                        <label className="radio">
-                            <input type="radio"
-                                name="optionsRadios-2"
-                                value="false"
-                                defaultChecked={this.state.wait_for_deploy === false}
-                                onClick={this.onChangeWaitForDeploy} /> {"Execute scripts asynchronously."}
-                        </label>
-                    </div>
-
-                    <h4 className="t-body-2">Input Type</h4>
-                    <SelectMenu current={this.state.type}
-                        optionName={type => type }
-                        list={typeChoices}
-                        onSelect={this.onChangeType} />
-
-                    <div className="col-md-6">
-                        {this.renderInputType()}
-                    </div>
-                </div>
-            </div>
-        )
-    },
-
-    onCreateScript: function() {
-        if (!this.state.validate) {
-            this.setState({
-                validate: true
-            });
-        }
-        if (this.isSubmittable) {
-            actions.ScriptActions.create({
-                title: this.state.title.trim(),
-                text: this.state.text.trim(),
-                type: this.state.type,
-                strategy: this.state.strategy
-            });
-            this.hide();
-        }
-    },
-
-    onSaveScript: function() {
-        if (!this.state.validate) {
-            this.setState({
-                validate: true
-            });
-        }
-        if (this.isSubmittable) {
-            actions.ScriptActions.update(this.props.script, {
-                title: this.state.title.trim(),
-                text: this.state.text.trim(),
-                type: this.state.type,
-                strategy: this.state.strategy
-            });
-            // do action after save is completed?
-            this.hide();
-        }
-    },
-
-    onSubmit: function() {
-        if( this.props.script) {
-            this.onSaveScript();
-        } else {
-            this.onCreateScript();
-        }
+    onClose: function() {
         this.hide();
     },
 
-    render: function() {
-        // Only show the warning if the field has content
-        let notSubmittable = !this.isSubmittable();
+    renderBody: function() {
 
+        return (
+            <EditScript
+                script={this.props.script}
+                footerClassName={"modal-footer"}
+                onClose={this.onClose}
+                onScriptChanged={this.onScriptChanged}
+            />
+        );
+    },
+
+    render: function() {
         return (
         <div className="modal fade">
             <div className="modal-dialog">
@@ -302,18 +44,6 @@ export default React.createClass({
                     </div>
                     <div className="modal-body">
                         {this.renderBody()}
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-danger" onClick={this.hide}>
-                            Cancel
-                        </button>
-                        <button type="button"
-                            aria-invalid={notSubmittable}
-                            className="btn btn-primary"
-                            onClick={this.onSubmit}
-                            disabled={notSubmittable}>
-                            Confirm
-                        </button>
                     </div>
                 </div>
             </div>
