75cd911616e74484156292806fd077febdf10567
Andrew J Lenards <andrew.lenards@gmail.com>
2017-10-04 22:15:31 +0000

    Provide Image Metrics getters

--- troposphere/static/js/models/ImageMetrics.js
+++ troposphere/static/js/models/ImageMetrics.js
@@ -12,5 +12,44 @@ export default Backbone.Model.extend({
     toJSON: function(options) {
         var attributes = _.clone(this.attributes);
         return attributes;
+    },
+
+    hasMetrics: function() {
+        return this.attributes.metrics && Object.keys(this.attributes.metrics).length > 0;
+    },
+
+    getProjectsTotal: function() {
+        let { projects } = this.attributes.metrics;
+        return projects || 0;
+    },
+
+    getBookmarksTotal: function() {
+        let { bookmarks } = this.attributes.metrics;
+        return bookmarks || 0;
+    },
+
+    getApplicationForks: function() {
+        let { forks } = this.attributes.metrics;
+        return forks || 0;
+    },
+
+    getInstancesTotal: function() {
+        let { instances } = this.attributes.metrics;
+        return instances ? instances.total : 0;
+    },
+
+    getInstancesSuccess: function() {
+        let { instances } = this.attributes.metrics;
+        return instances ? instances.success : 0;
+    },
+
+    getInstancesPercent: function() {
+        let { instances } = this.attributes.metrics;
+        if (instances) {
+            let percent = instances.percent;
+            return (percent) ? percent.toFixed(3) : 0;
+        } else {
+            return 0;
+        }
     }
 });
