fb7d2615d0c909b29e521c74d143ad8d6c3f5ac7
Connor Osborn <cdosborn@email.arizona.edu>
2017-11-28 15:12:36 +0000

    Remove unused method

--- troposphere/static/js/stores/ResourceRequestStore.js
+++ troposphere/static/js/stores/ResourceRequestStore.js
@@ -6,57 +6,6 @@ import ResourceRequestConstants from "constants/ResourceRequestConstants";
 
 let ResourceRequestStore = BaseStore.extend({
     collection: ResourceRequestCollection,
-
-    // FIXME:
-    // ------
-    // this is part of an attempt to make the RequestMoreResouresModal
-    // not crash for a specific user.
-    //
-    // the real cause of the issue is not 100% understood - it may be
-    // mismatched validation logic (from what has been seen within the
-    // commit history explored).
-    // -----------------------------------------------------------------
-    // Looks through the local cache and returns any models matched the
-    // provided parameters
-    //
-    // @params: Object, like {name: 'example'} or {'provider.id': 1}
-    //          provided params can be at most 1 extra level deep
-    //          ('provider.id' or 'provider')
-    findResourceRequestsWhere: function(params) {
-        if (!this.models) return this.fetchModels();
-
-        var keys = Object.keys(params);
-
-        var models = this.models.cfilter(function(model) {
-            var matchesCriteria = true;
-
-            keys.forEach(function(key) {
-                if (!matchesCriteria) return;
-
-                var tokens = key.split(".");
-                if (tokens.length === 1) {
-                    if (model.get(key) !== params[key])
-                        matchesCriteria = false;
-                } else {
-                    var lookup = model.get(tokens[0])
-                    // this is really a De Morgan's Law opposite to
-                    // !(lookup && lookup[tokens[1]] == params[key])
-                    //
-                    // since the condition is really guarding a logical
-                    // negation - it seems that this form would be more
-                    // in line w/ the intent (but the real weight of
-                    // that is not defended nor is a proof attempted).
-                    if (!lookup || lookup[tokens[1]] !== params[key])
-                        matchesCriteria = false;
-                }
-            });
-
-            return matchesCriteria;
-        });
-
-        return models;
-    }
-
 });
 
 let store = new ResourceRequestStore();
