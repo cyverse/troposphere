6615c1b6f2900418cea3cf113fa96f9591d3e0a6
Andrew J Lenards <andrew.lenards@gmail.com>
2018-02-27 03:20:18 +0000

    Render conditional batch delete modals

--- troposphere/static/js/modals/instance/destroy.js
+++ troposphere/static/js/modals/instance/destroy.js
@@ -6,33 +6,58 @@ import VolumeStore from "stores/VolumeStore";
 
 import ModalHelpers from "components/modals/ModalHelpers";
 
+import BatchInstanceDeleteModal from "components/modals/instance/BatchInstanceDeleteModal";
 import InstanceDeleteModal from "components/modals/instance/InstanceDeleteModal";
 import ExplainInstanceDeleteConditionsModal from "components/modals/instance/ExplainInstanceDeleteConditionsModal";
 
 import actions from "actions";
+import features from "utilities/featureFlags";
+
+
+function selectModal(props) {
+    let { attachedVolumes } = props;
+
+    if (features.BATCH_RESOURCE_ACTIONS) {
+        // offer a "best offer" approach to deleting
+        // the instances selected that don't have
+        // attached volumes; here, "best offer" means
+        // to delete what we can & apologize/explain
+        // why we don't be deleting the others
+        return BatchInstanceDeleteModal;
+    } else {
+        // when batch resource actions are disabled,
+        // if we have volumes attached to the instance
+        // targeted for deletion, we offer a "polite"
+        // explanation for why the UI doesn't let that
+        // happen; otherwise, delete delete delete!
+        return (attachedVolumes && attachedVolumes.length > 0)
+            ? ExplainInstanceDeleteConditionsModal
+            : InstanceDeleteModal;
+    }
+}
 
 
 export default {
     destroy: function(payload, options) {
-        if (!payload.instance)
-            throw new Error("Missing instance");
+        if (!payload.instances)
+            throw new Error("Missing instance(s)");
 
-        var project = payload.project,
-            instance = payload.instance,
-            attachedVolumes = VolumeStore.getVolumesAttachedToInstance(instance),
-            ModalComponent,
+        let project = payload.project,
+            instances = payload.instances,
+            attachedResults = VolumeStore.getVolumesAttachedToInstances(instances),
+            { volumes: attachedVolumes,
+              matchedIds } = attachedResults,
             props = {
-                instance,
-                attachedVolumes
+                instances: instances.creject((i) => matchedIds.includes(i.get("uuid"))),
+                rejected: instances.cfilter((i) => matchedIds.includes(i.get("uuid"))),
+                attachedVolumes,
+                matchedIds
             };
 
-        ModalComponent = attachedVolumes.length > 0
-            ? ExplainInstanceDeleteConditionsModal
-            : InstanceDeleteModal;
+        ModalHelpers.renderModal(selectModal(props), props, function() {
+            // destroy all instances without attached volumes ...
+            actions.InstanceActions.destroy(props, options);
 
-        ModalHelpers.renderModal(ModalComponent, props, function() {
-            attachedVolumes.forEach((volume) => VolumeStore.pollUntilDetached(volume));
-            actions.InstanceActions.destroy(payload, options);
             if (project) {
                 appBrowserHistory.push(`/projects/${project.id}/resources`);
             }
