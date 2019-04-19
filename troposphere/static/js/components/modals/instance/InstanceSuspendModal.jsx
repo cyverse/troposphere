dd071588dd90bd5e3758979befc1558586a28906
tharon-c <tharonica@gmail.com>
2019-04-19 15:22:23 -0700

    JETSTREAM-215: Add volume attached alert to stop/suspend/shelve

--- troposphere/static/js/components/modals/instance/InstanceSuspendModal.jsx
+++ troposphere/static/js/components/modals/instance/InstanceSuspendModal.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import RaisedButton from "material-ui/RaisedButton";
 import BootstrapModalMixin from "components/mixins/BootstrapModalMixin";
 import Glyphicon from "components/common/Glyphicon";
+import AttachedVolumeWarning from "./components/AttachedVolumeWarning";
+import stores from "stores";
 
 export default React.createClass({
     displayName: "InstanceSuspendModal",
@@ -22,38 +24,58 @@ export default React.createClass({
         this.props.onConfirm();
     },
 
+    attachedVolumes: function() {
+        const volumes = stores.VolumeStore.getVolumesAttachedToInstance(
+            this.props.instance
+        );
+        const isAttached = !!volumes.length;
+        return {
+            volumes,
+            isAttached
+        };
+    },
+
     //
     // Render
     // ------
     //
 
     renderBody: function() {
+        const {isAttached, volumes} = this.attachedVolumes();
         return (
             <div>
-                <p className="alert alert-warning">
-                    <Glyphicon name="warning-sign" /> <strong>WARNING</strong>
-                    {
-                        " Suspending an instance will freeze its state, and the IP address may change when you resume the instance."
-                    }
-                </p>
-                <p>
-                    {
-                        "Suspending an instance frees up resources for other users and allows you to safely preserve the state of your instance without imaging. "
-                    }
-                    {
-                        "Your time allocation no longer counts against you in the suspended mode."
-                    }
-                </p>
-                <p>
-                    {
-                        'Your resource usage charts will only reflect the freed resources once the instance\'s state is "suspended."'
-                    }
-                </p>
+                {isAttached ? (
+                    <AttachedVolumeWarning volumes={volumes} />
+                ) : (
+                    <div>
+                        <p className="alert alert-warning">
+                            <Glyphicon name="warning-sign" />{" "}
+                            <strong>WARNING</strong>
+                            {
+                                " Suspending an instance will freeze its state, and the IP address may change when you resume the instance."
+                            }
+                        </p>
+                        <p>
+                            {
+                                "Suspending an instance frees up resources for other users and allows you to safely preserve the state of your instance without imaging. "
+                            }
+                            {
+                                "Your time allocation no longer counts against you in the suspended mode."
+                            }
+                        </p>
+                        <p>
+                            {
+                                'Your resource usage charts will only reflect the freed resources once the instance\'s state is "suspended."'
+                            }
+                        </p>
+                    </div>
+                )}
             </div>
         );
     },
 
     render: function() {
+        const {isAttached} = this.attachedVolumes();
         return (
             <div className="modal fade">
                 <div className="modal-dialog">
@@ -67,13 +89,15 @@ export default React.createClass({
                             <RaisedButton
                                 style={{marginRight: "10px"}}
                                 onTouchTap={this.cancel}
-                                label="Cancel"
+                                label={isAttached ? "OK" : "Cancel"}
                             />
-                            <RaisedButton
-                                primary
-                                onTouchTap={this.confirm}
-                                label="Yes, suspend this instance"
-                            />
+                            {isAttached || (
+                                <RaisedButton
+                                    primary
+                                    onTouchTap={this.confirm}
+                                    label="Yes, suspend this instance"
+                                />
+                            )}
                         </div>
                     </div>
                 </div>
